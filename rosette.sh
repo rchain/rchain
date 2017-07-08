322a3c080cdfb4e1aa202a774992747b7ed78c1b
Kent Shikama <kent@kentshikama.com>
2017-07-07 21:05:37 -0700

    Clean up rosette.sh and sbt.sh

--- rosette.sh
+++ rosette.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
-#cd // Rosette
+
+RWORK=/Users/kent/Documents/
+ROSETTE_DIR=/Users/kent/Documents/Rosette-VM/
+
+cp $RWORK/RholangWeb/output3.txt $ROSETTE_DIR/input.txt
+cd $ROSETTE_DIR
 #make
-#cat // Rosette output
-echo "Hello"
+#sed -n '/loading:/!p' $ROSETTE_DIR/output.txt > $ROSETTE_DIR/output2.txt
+#cat $ROSETTE_DIR/output.txt
+
+echo "hello"
--- sbt.sh
+++ sbt.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
-cd /Users/kent/Documents/Rholang/
-sbt 'run-main coop.rchain.rho2rose.Rholang2RosetteCompiler /Users/kent/Documents/RholangREPL/test.rho' > ~/Documents/RholangREPL/output.txt
-# Navneet's script
-cp ~/Documents/RholangREPL/output.txt ~/Documents/RholangREPL/original_output.txt
-sed -n '/\[0m\[\[32msuccess\[0m\]/!p' ~/Documents/RholangREPL/output.txt > ~/Documents/RholangREPL/output2.txt
-sed -n '/\[0m\[\[0minfo\[0m\]/!p' ~/Documents/RholangREPL/output2.txt > ~/Documents/RholangREPL/output3.txt
-cat ~/Documents/RholangREPL/output3.txt
+
+RWORK=/Users/kent/Documents/
+RHOLANG_DIR=$RWORK/Rholang
+RHOLANG_WEB_DIR=$RWORK/RholangREPL
+
+cd $RHOLANG_DIR
+sbt "run-main coop.rchain.rho2rose.Rholang2RosetteCompiler ${RHOLANG_WEB_DIR}/test.rho" > $RHOLANG_WEB_DIR/output.txt
+cp $RHOLANG_WEB_DIR/output.txt $RHOLANG_WEB_DIR/original_output.txt
+sed -n '/\[0m\[[32msuccess[0m\]/!p' $RHOLANG_WEB_DIR/output.txt > $RHOLANG_WEB_DIR/output2.txt
+sed -n '/\[0m\[[0minfo[0m\]/!p' $RHOLANG_WEB_DIR/output2.txt > $RHOLANG_WEB_DIR/output3.txt
+cat $RHOLANG_WEB_DIR/output3.txt
 
 
