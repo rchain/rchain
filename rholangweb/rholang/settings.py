30aa834b1d2daea9d91ac2abaa51b1ca387a7f04
Kent Shikama <kent@kentshikama.com>
2017-07-07 20:49:49 -0700

    Update STATIC_ROOT

--- rholangweb/rholang/settings.py
+++ rholangweb/rholang/settings.py
@@ -105,7 +105,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
