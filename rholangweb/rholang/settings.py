d6a092d0bdaf32fa4ca55d359f6374bd340ca938
Kent Shikama <kent@kentshikama.com>
2017-07-07 17:08:45 -0700

    Fix static file path

--- rholangweb/rholang/settings.py
+++ rholangweb/rholang/settings.py
@@ -105,6 +105,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
