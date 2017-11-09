89fd19d499f926772522bbaed2e7284eaad1e2d7
Kent Shikama <kent@kentshikama.com>
2017-11-08 21:19:56 -0800

    Add test bash scripts and integrate with travis

---
+++ rholang/test.sh
@@ -0,0 +1,10 @@
+sbt bnfc:generate
+sbt assembly
+# Run compiler over all test rholang files
+for i in tests/*; do
+	./rho2rbl "$i"
+done
+# Rewrite all tests to print to a text file results.txt instead of stdout
+sed -i 's/print/ostream-print (ostream-new "results.txt")/g' tests/*.rbl
+# Move all generated tests under the rosette/tests folder to run
+mv tests/*.rbl ../rosette/rbl/rosette/tests/
