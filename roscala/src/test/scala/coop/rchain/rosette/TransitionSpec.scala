a903c23eee9ee5f97a98cd695d2fab0f31044344
Timm Schäuble <timm.schaeuble@gmail.com>
2017-11-23 18:03:48 -0500

    Add execution of bytecode derived from if expression

--- roscala/src/test/scala/coop/rchain/rosette/TransitionSpec.scala
+++ roscala/src/test/scala/coop/rchain/rosette/TransitionSpec.scala
@@ -5,6 +5,74 @@ import org.scalatest._
 import coop.rchain.rosette.utils.opcodes._
 
 class TransitionSpec extends FlatSpec with Matchers {
+  "Executing bytecode from expression \"(if #t 1 2)\"" should "result in state.ctxt.rslt == Fixnum(1)" in {
+
+    /**
+      * litvec:
+      *  0:   {IfExpr}
+      * codevec:
+      *  0:   lit #t,rslt
+      *  1:   jf 4
+      *  2:   lit 1,rslt
+      *  3:   rtn/nxt
+      *  4:   lit 2,rslt
+      *  5:   rtn/nxt
+      */
+    val start =
+      testState
+        .set(_ >> 'pc >> 'relative)(0)
+        .set(_ >> 'ctxt >> 'tag)(LocationGT(Location.LTCtxtRegister(0)))
+        .set(_ >> 'ctxt >> 'pc)(PC(0))
+        .set(_ >> 'ctxt >> 'ctxt)(testState.ctxt)
+        .set(_ >> 'ctxt >> 'ctxt >> 'outstanding)(1)
+        .set(_ >> 'ctxt >> 'ctxt >> 'pc)(PC(6)) // Setting pc to 6 so that the VM halts after strand is installed
+        .set(_ >> 'sleeperPool)(Seq())
+
+    val codevec = Seq(OpImmediateLitToReg(v = 8, r = 0),
+                      OpJmpFalse(4),
+                      OpImmediateLitToReg(v = 1, r = 0),
+                      OpRtn(n = true),
+                      OpImmediateLitToReg(v = 2, r = 0),
+                      OpRtn(n = true))
+
+    val end = VirtualMachine.executeSeq(codevec, start)
+    end.ctxt.rslt shouldBe Fixnum(1)
+  }
+
+  "Executing bytecode from expression \"(if #f 1 2)\"" should "result in state.ctxt.rslt == Fixnum(2)" in {
+
+    /**
+      * litvec:
+      *  0:   {IfExpr}
+      * codevec:
+      *  0:   lit #f,rslt
+      *  1:   jf 4
+      *  2:   lit 1,rslt
+      *  3:   rtn/nxt
+      *  4:   lit 2,rslt
+      *  5:   rtn/nxt
+      */
+    val start =
+      testState
+        .set(_ >> 'pc >> 'relative)(0)
+        .set(_ >> 'ctxt >> 'tag)(LocationGT(Location.LTCtxtRegister(0)))
+        .set(_ >> 'ctxt >> 'pc)(PC(0))
+        .set(_ >> 'ctxt >> 'ctxt)(testState.ctxt)
+        .set(_ >> 'ctxt >> 'ctxt >> 'outstanding)(1)
+        .set(_ >> 'ctxt >> 'ctxt >> 'pc)(PC(6)) // Setting pc to 6 so that the VM halts after strand is installed
+        .set(_ >> 'sleeperPool)(Seq())
+
+    val codevec = Seq(OpImmediateLitToReg(v = 9, r = 0),
+                      OpJmpFalse(4),
+                      OpImmediateLitToReg(v = 1, r = 0),
+                      OpRtn(n = true),
+                      OpImmediateLitToReg(v = 2, r = 0),
+                      OpRtn(n = true))
+
+    val end = VirtualMachine.executeSeq(codevec, start)
+    end.ctxt.rslt shouldBe Fixnum(2)
+  }
+
   "Executing bytecode from expression \"(+ 1 2)\"" should "result in Fixnum(3)" in {
 
     /**
@@ -27,9 +95,9 @@ class TransitionSpec extends FlatSpec with Matchers {
         .set(_ >> 'ctxt >> 'ctxt)(testState.ctxt)
 
     val codevec = Seq(OpAlloc(2),
-                      OpImmediateLitToArg(1, 0),
-                      OpImmediateLitToArg(2, 1),
-                      OpXferGlobalToReg(1, 668),
+                      OpImmediateLitToArg(v = 1, a = 0),
+                      OpImmediateLitToArg(v = 2, a = 1),
+                      OpXferGlobalToReg(r = 1, g = 668),
                       OpXmit(u = false, n = true, 2))
 
     val end = VirtualMachine.executeSeq(codevec, start)
