rchain/rchain

commit eebbdd8425e53fe558323e7071d7a39bb660b73b

Build each subproject with travis (#60)
* Adjustments required to compile under Ubuntu 16.04

* The draft implementation of the build script

* Revert "fix pointer cast build errors"

This reverts commit ebdd9f98b88f0dec9ca446fef3294753c29f9c4f.

* Add README to rosette/bin

This prevents make from failing because rosette/bin doesn't exist.

* Add test bash scripts and integrate with travis

* Add README with travis build mark

* Remove generated arithmetic_test.rbl

* Add tests/ folder for generated RBL tests

* Build each subproject, not just rholang, rosette

In the Nov 3 "CI configuring notes" design, only directories changed
in the last commit are built. But if a PR has a number of commits, the
last of which is only a change to the top-level README, any
subdirectories wouldn't be re-built. Some searching suggests this is
an unsolved problem:

  - https://stackoverflow.com/questions/39596177/monorepo-travis-and-matrix-builds-how-do-i-build-a-specific-path-only-when-it
  - https://github.com/travis-ci/travis-ci/issues/6301

So let's just build everything.

Also:
  - use `test.sh` in preference to `build.sbt` if present
    to support
    - the C++ VM
    - the BNFC step etc. in rholang
  - use `-Dsbt.log.noformat=true` to avoid clutter in logs

* oops: typo in travis script

* uniform sbt version: 0.13.16

* storage: scalaVersion 2.12.13 to match comm etc.

* rholang: scalaVersion 2.12.3 to match comm etc.

  - use scalaz-core, spire kind-projector versions
    that support scala 2.12.x

* .travis.yml: cache ~/.ivy2, ~/.sbt

* .travis.yml: use matrix for subproject builds

  - push install deps into subprojects (ack @DoSOfRedRiver)
    - g++ to rosette
    - BNFC to rholang

* oops: +x for install script

* install.sh: oops: SUBPROJECT, not SUBDIR

* .travis.yml: sudo required for rosette

* .travis.yml: language: scala to override ruby default

rholang/project/BNFC.scala
@@ -29,9 +29,10 @@ object BNFC {
     val targPath: String  = makeOutputPath(grammarFile, outputDir, namespace)
     val bnfcCmd: String   = s"bnfc --java -o ${outputDir.getAbsolutePath} -p $namespace $grammarFile"
     val jlexCmd: String   = s"java -cp $classpath JLex.Main $targPath/Yylex"
-    val cupCmd: String    = s"java -cp $classpath java_cup.Main -nopositions -expect 100 $targPath/${stripSuffix(grammarFile.getName)}.cup"
+    val renameDefaultCmd: String = s"mv $targPath/_cup.cup $targPath/${stripSuffix(grammarFile.getName)}.cup"
+    val cupCmd: String    = s"java -cp $classpath java_cup.Main -nopositions -expect 100 $targPath/${stripSuffix(grammarFile.getName)}.cup" // TODO: Figure out naming behind _cup.cup
     val mvCmd: String     = s"mv sym.java parser.java $targPath"
-    Process(bnfcCmd) #&& Process(jlexCmd) #&& Process(cupCmd) #&& Process(mvCmd) !
+    Process(bnfcCmd) #&& Process(jlexCmd) #&& Process(renameDefaultCmd) #&& Process(cupCmd) #&& Process(mvCmd) !
   }
 
   def bnfcGenerateLaTeX(grammarFile: File, outputDir: File): Unit = {

.gitignore
@@ -38,3 +38,8 @@ boot/
 # BNFC
 ###################
 rholang/src/main/java/coop/**
+
+# Rosette
+###################
+!rosette/bin/README
+rosette/src/*.o

.travis.yml
@@ -1,0 +1,18 @@
+language: scala
+scala:
+  - 2.12.3
+sbt_args: -no-colors
+dist: trusty
+env:
+  - SUBPROJECT=comm
+  - SUBPROJECT=rholang
+  - SUBPROJECT=roscala
+  - SUBPROJECT=rosette
+  - SUBPROJECT=storage
+script: ./scripts/build-subproject.sh
+sudo: required
+install: ./scripts/install.sh
+cache:
+  directories:
+    - "$HOME/.ivy2/cache"
+    - "$HOME/.sbt"

rholang/build.sbt
@@ -21,7 +21,7 @@ lazy val consoleOptions = commonOptions diff Seq("-Ywarn-unused-import")
 lazy val commonSettings = Seq(
   name := "rholang",
   organization := "coop.rchain",
-  scalaVersion := "2.11.8",
+  scalaVersion := "2.12.3",
   crossScalaVersions := Seq("2.10.6", scalaVersion.value),
   scalacOptions := commonOptions,
   scalacOptions in (Compile, console) := consoleOptions,
@@ -32,10 +32,10 @@ lazy val root = (project in file("."))
   .settings(bnfcSettings: _*)
 
 // Scalaz
-libraryDependencies += "org.scalaz" %% "scalaz-core" % "7.2.4"
+libraryDependencies += "org.scalaz" %% "scalaz-core" % "7.3.0-M17"
 
 // Kind projector
 resolvers += Resolver.sonatypeRepo("releases")
-addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.8.0")
+addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.9.4")
 
 mainClass in (Compile, packageBin) := Some("coop.rchain.rho2rose.Rholang2RosetteCompiler")

rholang/install.sh
@@ -1,0 +1,8 @@
+# BNFC install for travis
+# Ubuntu 14.04 has an outdated BNFC package, so we build BNFC from source
+#
+# see also: ../.travis.yml, ../scripts/install.sh
+sudo apt install haskell-platform
+git clone https://github.com/BNFC/bnfc.git
+cd bnfc/source
+sudo cabal install --global

rholang/test.sh
@@ -1,0 +1,10 @@
+sbt -Dsbt.log.noformat=true bnfc:generate
+sbt -Dsbt.log.noformat=true assembly
+# Run compiler over all test rholang files
+for i in tests/*; do
+	./rho2rbl "$i"
+done
+# Rewrite all tests to print to a text file results.txt instead of stdout
+sed -i 's/print/ostream-print (ostream-new "results.txt")/g' tests/*.rbl
+# Move all generated tests under the rosette/tests folder to run
+mv tests/*.rbl ../rosette/rbl/rosette/tests/

rosette/Makefile
@@ -24,7 +24,6 @@
 
 all:
 	$(MAKE) -C src
-	$(MAKE) -C rbl
 
 
 clean:

commit d369b5d438abff9e6dfb59b6ef43c4c27d23df62

Rholang: Unbreak BNFC sbt rules.
The sbt BNFC rules were accidentally broken by:
eebbdd8425e53fe558323e7071d7a39bb660b73b
Revert the specific portion of the change that broke the Rholang compiler,
leaving the TODO in place.

rholang/project/BNFC.scala
@@ -29,10 +29,9 @@ object BNFC {
     val targPath: String  = makeOutputPath(grammarFile, outputDir, namespace)
     val bnfcCmd: String   = s"bnfc --java -o ${outputDir.getAbsolutePath} -p $namespace $grammarFile"
     val jlexCmd: String   = s"java -cp $classpath JLex.Main $targPath/Yylex"
-    val renameDefaultCmd: String = s"mv $targPath/_cup.cup $targPath/${stripSuffix(grammarFile.getName)}.cup"
-    val cupCmd: String    = s"java -cp $classpath java_cup.Main -nopositions -expect 100 $targPath/${stripSuffix(grammarFile.getName)}.cup" // TODO: Figure out naming behind _cup.cup
+    val cupCmd: String    = s"java -cp $classpath java_cup.Main -nopositions -expect 100 $targPath/${stripSuffix(grammarFile.getName)}.cup"
     val mvCmd: String     = s"mv sym.java parser.java $targPath"
-    Process(bnfcCmd) #&& Process(jlexCmd) #&& Process(renameDefaultCmd) #&& Process(cupCmd) #&& Process(mvCmd) !
+    Process(bnfcCmd) #&& Process(jlexCmd) #&& Process(cupCmd) #&& Process(mvCmd) !
   }
 
   def bnfcGenerateLaTeX(grammarFile: File, outputDir: File): Unit = {