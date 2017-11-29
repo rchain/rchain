fb3917dd777ffa8110a63d6f2c244baf2fae4726
Timm Schäuble <timm.schaeuble@gmail.com>
2017-11-29 18:00:06 -0500

    Add correct prim indices

--- roscala/src/main/scala/coop/rchain/rosette/prim/package.scala
+++ roscala/src/main/scala/coop/rchain/rosette/prim/package.scala
@@ -2,11 +2,10 @@ package coop.rchain.rosette
 
 package object prim {
   // For now keys in Prims correspond to Prim::primnum in Rosette
-  // TODO:
-  val Prims = Map[Int, Prim](226 -> Number.fxPlus,
-                             1 -> Number.fxDiv,
-                             2 -> Number.fxMinus,
-                             3 -> Number.fxTimes,
-                             4 -> Number.fxMod)
+  val Prims = Map[Int, Prim](222 -> Number.fxMod,
+                             223 -> Number.fxDiv,
+                             224 -> Number.fxTimes,
+                             225 -> Number.fxMinus,
+                             226 -> Number.fxPlus)
 
 }
