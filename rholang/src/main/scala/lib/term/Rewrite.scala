f98e3acd8fece0f3167040150710892c3af6a01b
Kyle Butt <kyle@pyrofex.net>
2017-12-15 09:44:03 -0800

    Rholang: Remove Either's in favor of sealed traits.
    Use sealed traits that look like either, but the names have more meaning than
    just left, right.

--- rholang/src/main/scala/lib/term/Rewrite.scala
+++ rholang/src/main/scala/lib/term/Rewrite.scala
@@ -11,20 +11,20 @@ package coop.rchain.lib.term
 import coop.rchain.lib.zipper._
 import coop.rchain.lib.navigation.{ Right => _, Left => _, _ }
 
-trait TermNavigation[L,V,T] extends ZipperNavigation[Either[T,V]]
-trait TermMutation[L,V,T] extends ZipperMutation[Either[T,V]]
+trait TermNavigation[L,V,T] extends ZipperNavigation[TagOrVar[T,V]]
+trait TermMutation[L,V,T] extends ZipperMutation[TagOrVar[T,V]]
 
 trait TermZipperComposition[L,V,T] {
   // Composing a context places the inner context in the hole of the outer
   // context, leaving a larger context with a single hole.
   def compose(
-    ctxtInner : Context[Either[T,V]],
-    ctxtOuter : Context[Either[T,V]]
-  ) : Context[Either[T,V]] = {
+    ctxtInner : Context[TagOrVar[T,V]],
+    ctxtOuter : Context[TagOrVar[T,V]]
+  ) : Context[TagOrVar[T,V]] = {
     ctxtInner match {
       case Top() => ctxtOuter
-      case LabeledTreeContext(lbl: L @unchecked, left: List[TermCtxt[L, V, T] with Factual] @unchecked, ctxt: LabeledTreeContext[L, Either[T, V]] @unchecked, right: List[TermCtxt[L, V, T] with Factual] @unchecked) => {
-	LabeledTreeContext[L,Either[T,V]](
+      case LabeledTreeContext(lbl: L @unchecked, left: List[TermCtxt[L, V, T] with Factual] @unchecked, ctxt: LabeledTreeContext[L, TagOrVar[T, V]] @unchecked, right: List[TermCtxt[L, V, T] with Factual] @unchecked) => {
+	LabeledTreeContext[L,TagOrVar[T,V]](
 	  lbl, left, compose( ctxt, ctxtOuter ), right
 	)
       }
