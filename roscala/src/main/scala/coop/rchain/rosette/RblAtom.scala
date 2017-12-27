99ebe47178592664f5018734571fda5edf1ccaf0
AbnerZheng <abnerzheng@gmail.com>
2017-12-27 21:34:39 +0800

    finish tickets ROS-197: Port fx primitives

--- roscala/src/main/scala/coop/rchain/rosette/RblAtom.scala
+++ roscala/src/main/scala/coop/rchain/rosette/RblAtom.scala
@@ -1,11 +1,15 @@
 package coop.rchain.rosette
 
 import coop.rchain.rosette.Meta.StdMeta
+import coop.rchain.rosette.Ob.{OTbool, OTfixnum}
 
 trait RblAtom extends Ob
 
-case class Fixnum(value: Int, override val slot: Seq[Ob] = Seq(StdMeta()))
+case class Fixnum(value: Int,
+                  override val obTag: Ob.ObTag = OTfixnum,
+                  override val slot: Seq[Ob] = Seq(StdMeta()))
     extends RblAtom {
+
   override def toString = s"Fixnum($value)"
 
   def +(that: Fixnum) = Fixnum(this.value + that.value)
@@ -15,6 +19,38 @@ case class Fixnum(value: Int, override val slot: Seq[Ob] = Seq(StdMeta()))
   def *(that: Fixnum) = Fixnum(this.value * that.value)
 
   def /(that: Fixnum) = Fixnum(this.value / that.value)
+
+  def %(that: Fixnum) = Fixnum(this.value % that.value)
+
+  def <(that: Fixnum) = RblBool(this.value < that.value)
+
+  def >(that: Fixnum) = RblBool(this.value > that.value)
+
+  def <=(that: Fixnum) = RblBool(this.value <= that.value)
+
+  def >=(that: Fixnum) = RblBool(this.value >= that.value)
+
+  def ==(that: Fixnum) = RblBool(this.value == that.value)
+
+  def !=(that: Fixnum) = RblBool(this.value != that.value)
+  def |(that: Fixnum) = Fixnum(this.value | that.value)
+
+  def &(that: Fixnum) = Fixnum(this.value & that.value)
+
+  def <<(that: Fixnum) = Fixnum(this.value << that.value)
+
+  def >>(that: Fixnum) = Fixnum(this.value >> that.value)
+
+  def >>>(that: Fixnum) = Fixnum(this.value >>> that.value)
+
+  def <<<(that: Fixnum) = Fixnum(this.value << that.value)
+
+  def ^(that: Fixnum) = Fixnum(this.value ^ that.value)
+}
+
+case class RblBool(value: Boolean, override val obTag: Ob.ObTag = OTbool)
+    extends RblAtom {
+  override def toString = s"RblBool($value)"
 }
 
 case class RblFloat(value: Double, override val slot: Seq[Ob] = Seq(StdMeta()))
