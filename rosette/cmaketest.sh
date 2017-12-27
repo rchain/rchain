87c5978f04d0bd2397548ce57afb2f51ff2dfd95
nash e. foster <nashef@gmail.com>
2017-12-27 13:22:30 -0700

    Update cmaketest.sh and configuration.rbl to be mo betta.

--- rosette/cmaketest.sh
+++ rosette/cmaketest.sh
@@ -1,22 +1,20 @@
-# Modify configuration.rbl to run tests
-for i in rbl/rosette/tests/*; do
-	filename=$(basename "$i")
-	filename_without_extension="${filename%.*}"
-	echo "(load 'tests/$filename_without_extension 'silent)" >> rbl/rosette/configuration.rbl
-done
-#echo "(exit)" >> rbl/rosette/configuration.rbl
+#!/bin/bash
+
+
 
 # Create file for printing results
 touch rbl/rosette/results.txt
 
 # Build Rosette
+./clean.sh
 ./build.sh
 
 # Increase stack limit so that Rosette can load all files
 ulimit -s unlimited
 
 # Execute Rosette instance
-./build.out/src/rosette -boot rbl/rosette/boot.rbl -run ../rholang/examples/hello_world_again.rbl
+./build.out/src/rosette -boot rbl/rosette/boot.rbl \
+    -run rbl/rosette/hello_world.rbl
 
 # Check if any tests have failed
 cat rbl/rosette/results.txt | grep "Fail"
