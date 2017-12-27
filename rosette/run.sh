c19bbca700afa0c56f313d7ccbf0c065c9ec4808
nash e. foster <nashef@gmail.com>
2017-12-27 13:00:54 -0700

    Add test from Keith.

--- rosette/run.sh
+++ rosette/run.sh
@@ -4,4 +4,4 @@ ulimit -s unlimited
 # Execute Rosette instance
 export ESS_SYSDIR=rbl/rosette
 ./build.out/src/rosette -boot rbl/rosette/boot.rbl \
-    -run ../rholang/examples/hello_world_again.rho
+    -run rbl/rosette/hello_world.rbl
