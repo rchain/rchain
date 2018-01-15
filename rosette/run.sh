06064a39337dcaa2bcca1cd21f7681d5bf1310c8
Keith McQueen <keith@pyrofex.net>
2018-01-15 15:22:44 -0700

    Changed run.sh to use $@ instead of $1 $2 $3...

--- rosette/run.sh
+++ rosette/run.sh
@@ -3,4 +3,4 @@ ulimit -s unlimited
 
 # Execute Rosette instance
 export ESS_SYSDIR=rbl/rosette
-./build.out/src/rosette --quiet --interactive-repl --boot-dir=rbl/rosette --boot=boot.rbl $1 $2 $3 $4
+./build.out/src/rosette --quiet --interactive-repl --boot-dir=rbl/rosette --boot=boot.rbl $@
