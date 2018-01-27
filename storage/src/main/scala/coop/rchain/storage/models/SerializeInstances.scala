d8639811bb5a907555ce7c067393a8cc77b46be1
Henry Till <henrytill@gmail.com>
2018-01-27 13:18:20 -0500

    storage: add example {,System}Contract protos and tests

--- storage/src/main/scala/coop/rchain/storage/models/SerializeInstances.scala
+++ storage/src/main/scala/coop/rchain/storage/models/SerializeInstances.scala
@@ -15,4 +15,26 @@ trait SerializeInstances {
         .catchNonFatal(Block.parseFrom(bytes))
         .leftMap(SerializeError.apply)
   }
+
+  implicit object contractInstance extends Serialize[Contract] {
+
+    def encode(a: Contract): Array[Byte] =
+      a.toByteArray
+
+    def decode(bytes: Array[Byte]): Either[SerializeError, Contract] =
+      Either
+        .catchNonFatal(Contract.parseFrom(bytes))
+        .leftMap(SerializeError.apply)
+  }
+
+  implicit object systemContractInstance extends Serialize[SystemContract] {
+
+    def encode(a: SystemContract): Array[Byte] =
+      a.toByteArray
+
+    def decode(bytes: Array[Byte]): Either[SerializeError, SystemContract] =
+      Either
+        .catchNonFatal(SystemContract.parseFrom(bytes))
+        .leftMap(SerializeError.apply)
+  }
 }
