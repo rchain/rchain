a0899fbf4932670e5e404998b3ef8b48771ee1c0
ys-pyrofex <yaraslau@pyrofex.net>
2018-01-30 20:18:48 +0300

    Updates following Henry's PR

--- storage/src/main/scala/coop/rchain/storage/regex/PathRegex.scala
+++ storage/src/main/scala/coop/rchain/storage/regex/PathRegex.scala
@@ -1,4 +1,4 @@
-package main.scala.coop.rchain.storage.regex
+package coop.rchain.storage.regex
 
 import java.nio.charset.StandardCharsets
 import scala.annotation.tailrec
@@ -15,27 +15,63 @@ private[regex] trait RegexOptions extends ParseOptions {
   val endsWith: Either[String, List[String]]
 }
 
+case class PathRegexOptions(
+    caseSensitive: Boolean = false,
+    strict: Boolean = false,
+    end: Boolean = true,
+    delimiter: Char = PathRegexOptions.defaultDelimiter,
+    delimiters: Set[Char] = PathRegexOptions.defaultDelimiters,
+    endsWith: Either[String, List[String]] = Right(Nil)
+) extends RegexOptions
+
 object PathRegexOptions {
+  private[regex] val defaultDelimiter  = '/'
+  private[regex] val defaultDelimiters = "./".toSet
+
   val default       = PathRegexOptions()
   val caseSensitive = PathRegexOptions(caseSensitive = true)
   val strict        = PathRegexOptions(strict = true)
   val nonEnd        = PathRegexOptions(end = false)
 }
 
-case class PathRegexOptions(
-    caseSensitive: Boolean = false,
-    strict: Boolean = false,
-    end: Boolean = true,
-    delimiter: Char = '/',
-    delimiters: Set[Char] = "./".toSet,
-    endsWith: Either[String, List[String]] = Right(Nil)
-) extends RegexOptions {}
+private[regex] case class PathToken(name: Option[String],
+                                    key: Int,
+                                    prefix: Option[Char],
+                                    delimiter: Option[Char],
+                                    optional: Boolean,
+                                    repeat: Boolean,
+                                    partial: Boolean,
+                                    pattern: Option[String],
+                                    rawPathPart: Option[String]) {
+  def isRawPathPart: Boolean = rawPathPart.isDefined
+
+  def isToken: Boolean = rawPathPart.isEmpty
+}
+
+private[regex] object PathToken {
+  def apply(name: Option[String],
+            key: Int,
+            prefix: Option[Char],
+            delimiter: Option[Char],
+            optional: Boolean,
+            repeat: Boolean,
+            partial: Boolean,
+            pattern: String) =
+    new PathToken(name, key, prefix, delimiter, optional, repeat, partial, Some(pattern), None)
+  def apply(substring: String) =
+    new PathToken(None, -1, None, None, false, false, false, None, Some(substring))
+}
+
+case class PathRegex(tokens: List[PathToken], options: RegexOptions) {
+  def withOptions(newOptions: RegexOptions): PathRegex = new PathRegex(tokens, newOptions)
+
+  def toPath(args: List[(String, Any)] = Nil): Either[Throwable, String] =
+    throw new NotImplementedError("TODO")
+
+  def toPath(arg: (String, Any)): Either[Throwable, String] = toPath(arg :: Nil)
+}
 
 object PathRegex {
-  private[this] val defaultDelimiter = '/'
-
-  private[this] val defaultDelimiters = "./".toSet
-
   private[this] val rxEscapeString = """([.+*?=^!:${}()\[\]|/\\])""".r
 
   /**
@@ -43,8 +79,6 @@ object PathRegex {
     */
   def escapeString(str: String): String =
     rxEscapeString.replaceAllIn(str, """\\$1""")
-
-  private[this] val utf8charSet = StandardCharsets.UTF_8
 
   private[this] val uriAllowedChars =
     (('a' to 'z') ++ ('A' to 'Z') ++ ('0' to '9') ++ "-_.!~*'()".toList).toSet
@@ -66,7 +100,7 @@ object PathRegex {
             c.toString
           } else {
             c.toString
-              .getBytes(utf8charSet)
+              .getBytes(StandardCharsets.UTF_8)
               .map(b => "%02X".format(b))
               .mkString("")
           }
@@ -85,22 +119,18 @@ object PathRegex {
   private[this] val rxPath =
     """(\\.)|(?:\:(\w+)(?:\(((?:\\.|[^\\()])+)\))?|\(((?:\\.|[^\\()])+)\))([+*?])?""".r
 
+  private[this] case class ParseState(subStr: String,
+                                      rawPathPart: String,
+                                      tokens: List[PathToken],
+                                      pathEscaped: Boolean)
+
   /**
     * Parse a string for the raw tokens.
     */
   def parse(str: String, options: ParseOptions): List[PathToken] = {
-    val defaultDelimiter = options.delimiter
-    val delimiters       = options.delimiters
-
-    case class ParseState(subStr: String,
-                          rawPathPart: String,
-                          tokens: List[PathToken],
-                          pathEscaped: Boolean)
-
     @tailrec
-    def toTokens(parseState: ParseState): ParseState = {
-      val mcFound = rxPath.findFirstMatchIn(parseState.subStr)
-      mcFound match {
+    def toTokens(parseState: ParseState): ParseState =
+      rxPath.findFirstMatchIn(parseState.subStr) match {
         case Some(mc) =>
           val rawPathPart = if (mc.start > 0) {
             parseState.rawPathPart + parseState.subStr.substring(0, mc.start)
@@ -108,65 +138,61 @@ object PathRegex {
             parseState.rawPathPart
           }
 
-          val grpEscaped = Option(mc.group(1))
-          if (grpEscaped.isDefined) {
-            //we found escape sequence, add it to our collectedPath
-            toTokens(
-              ParseState(rawPathPart + grpEscaped.map(_(1)),
-                         parseState.subStr.substring(mc.end),
-                         parseState.tokens,
-                         true))
-          } else {
-            val grpName     = Option(mc.group(2))
-            val grpCapture  = Option(mc.group(3))
-            val grpGroup    = Option(mc.group(4))
-            val grpModifier = Option(mc.group(5))
-
-            val (prev, actualPath) =
-              if (!parseState.pathEscaped && rawPathPart.nonEmpty) {
-                val k            = rawPathPart.length - 1
-                val lastPathChar = rawPathPart(k)
-                if (delimiters.contains(lastPathChar)) {
-                  (Some(lastPathChar), rawPathPart.substring(0, k))
+          Option(mc.group(1)) match {
+            case Some(escapeStr) =>
+              //we found escape sequence, add it to our collectedPath
+              toTokens(
+                ParseState(parseState.subStr.substring(mc.end),
+                           rawPathPart + escapeStr.charAt(1),
+                           parseState.tokens,
+                           true))
+            case None =>
+              val (prev, actualPath) =
+                if (!parseState.pathEscaped && rawPathPart.nonEmpty) {
+                  val k            = rawPathPart.length - 1
+                  val lastPathChar = rawPathPart(k)
+                  if (options.delimiters.contains(lastPathChar)) {
+                    (Some(lastPathChar), rawPathPart.substring(0, k))
+                  } else {
+                    (None, rawPathPart)
+                  }
                 } else {
                   (None, rawPathPart)
                 }
+
+              val tokens = if (actualPath.nonEmpty) {
+                PathToken(actualPath) :: parseState.tokens
               } else {
-                (None, rawPathPart)
+                parseState.tokens
               }
 
-            val tokens = if (actualPath.nonEmpty) {
-              PathToken(actualPath) :: parseState.tokens
-            } else {
-              parseState.tokens
-            }
+              val next =
+                if (mc.end < parseState.subStr.length)
+                  Some(parseState.subStr(mc.end))
+                else None
 
-            val next =
-              if (mc.end < parseState.subStr.length)
-                Some(parseState.subStr(mc.end))
-              else None
+              val grpCapture  = Option(mc.group(3))
+              val grpGroup    = Option(mc.group(4))
+              val grpModifier = Option(mc.group(5))
 
-            val partial   = prev.isDefined && next.isDefined && (next != prev)
-            val repeat    = grpModifier.contains("+") || grpModifier.contains("*")
-            val optional  = grpModifier.contains("?") || grpModifier.contains("*")
-            val delimiter = prev.getOrElse(defaultDelimiter)
-            val key       = tokens.count(_.isToken)
-            val pattern   = if (grpCapture.nonEmpty) grpCapture else grpGroup
+              val delimiter    = prev.getOrElse(options.delimiter)
+              val patternGroup = if (grpCapture.nonEmpty) grpCapture else grpGroup
 
-            val userToken = PathToken(
-              grpName,
-              key,
-              prev,
-              Some(delimiter),
-              optional,
-              repeat,
-              partial,
-              pattern
-                .map(escapeGroup)
-                .getOrElse("[^%s]+?".format(escapeString(delimiter.toString))))
+              val userToken = PathToken(
+                name = Option(mc.group(2)),
+                key = tokens.count(_.isToken),
+                prev,
+                Some(delimiter),
+                optional = grpModifier.contains("?") || grpModifier.contains("*"),
+                repeat = grpModifier.contains("+") || grpModifier.contains("*"),
+                partial = prev.isDefined && next.isDefined && (next != prev),
+                pattern = patternGroup
+                  .map(escapeGroup)
+                  .getOrElse("[^%s]+?".format(escapeString(delimiter.toString)))
+              )
 
-            toTokens(
-              ParseState(parseState.subStr.substring(mc.end), "", userToken :: tokens, false))
+              toTokens(
+                ParseState(parseState.subStr.substring(mc.end), "", userToken :: tokens, false))
           }
         case None =>
           val finalCollectedPath = parseState.rawPathPart + parseState.subStr
@@ -176,64 +202,21 @@ object PathRegex {
             //some chars were left in path, but they are not a token
             ParseState("", "", PathToken(finalCollectedPath) :: parseState.tokens, false)
       }
-    }
+
     toTokens(ParseState(str, "", Nil, false)).tokens.reverse
   }
 
   /**
     * Compile a string to a template function for the path.
     */
-  def compile(str: String): Either[Throwable, PathRegex] =
-    compile(str, PathRegexOptions.default)
+  def compile(str: String): PathRegex = compile(str, PathRegexOptions.default)
 
   /**
     * Compile a string to a template function for the path.
     */
-  def compile(str: String, options: PathRegexOptions): Either[Throwable, PathRegex] =
-    tokensToFunction(parse(str, options))
+  def compile(str: String, options: PathRegexOptions): PathRegex =
+    PathRegex(parse(str, options), options)
 
-  /**
-    * Expose a method for transforming tokens into the path function.
-    */
-  def tokensToFunction(tokens: List[PathToken]): Either[Throwable, PathRegex] =
-    throw new NotImplementedError("TODO")
-
-  def apply(str: String): Either[Throwable, PathRegex] = compile(str)
-  def apply(str: String, options: PathRegexOptions): Either[Throwable, PathRegex] =
-    compile(str, options)
-}
-
-private object PathToken {
-  def apply(name: Option[String],
-            key: Int,
-            prefix: Option[Char],
-            delimiter: Option[Char],
-            optional: Boolean,
-            repeat: Boolean,
-            partial: Boolean,
-            pattern: String) =
-    new PathToken(name, key, prefix, delimiter, optional, repeat, partial, Some(pattern), None)
-  def apply(substring: String) =
-    new PathToken(None, -1, None, None, false, false, false, None, Some(substring))
-}
-
-private[regex] case class PathToken(name: Option[String],
-                                    key: Int,
-                                    prefix: Option[Char],
-                                    delimiter: Option[Char],
-                                    optional: Boolean,
-                                    repeat: Boolean,
-                                    partial: Boolean,
-                                    pattern: Option[String],
-                                    rawPathPart: Option[String]) {
-  def isRawPathPart: Boolean = rawPathPart.isDefined
-
-  def isToken: Boolean = rawPathPart.isEmpty
-}
-
-class PathRegex {
-  def toPath(args: List[(String, Any)] = Nil): Either[Throwable, String] =
-    throw new NotImplementedError("TODO")
-
-  def toPath(arg: (String, Any)): Either[Throwable, String] = toPath(arg :: Nil)
+  def apply(str: String): PathRegex                            = compile(str)
+  def apply(str: String, options: PathRegexOptions): PathRegex = compile(str, options)
 }
--- storage/src/test/scala/coop/rchain/storage/regex/PathRegexUnitTests.scala
+++ storage/src/test/scala/coop/rchain/storage/regex/PathRegexUnitTests.scala
@@ -1,4 +1,4 @@
-package main.scala.coop.rchain.storage.regex
+package coop.rchain.storage.regex
 
 import org.scalatest._
 
@@ -6,82 +6,230 @@ class PathRegexUnitTests extends FlatSpec with Matchers {
 
   def doCheck(path: String,
               tokens: Option[List[PathToken]],
-              //matchCases : Seq[(String, Seq[String])],
+              matchCases: Seq[(String, Seq[String])],
               //compileCases : Seq[String],
               options: PathRegexOptions = PathRegexOptions.default): Unit = {
-    val parsed = PathRegex.parse(path, options)
-    tokens.foreach(t => assert(parsed == t))
+    val parsed = PathRegex.compile(path, options)
+    tokens.foreach(t => assert(parsed.tokens == t))
   }
 
   def check(path: String,
             tokens: List[Any],
+            matchCases: Seq[(String, Seq[String])] = Nil,
             options: PathRegexOptions = PathRegexOptions.default): Unit =
     doCheck(path, Some(tokens.map {
       case t: PathToken => t
       case s: String    => PathToken(s)
-    }))
+    }), matchCases, options)
 
   "Simple paths" should "parse" in {
-    check("/", "/" :: Nil)
-    check("/test", "/test" :: Nil)
-    check("/test/", "/test/" :: Nil)
+    check("/",
+          "/" :: Nil,
+          ("/", "/" :: Nil)
+            :: ("/route", Nil) :: Nil)
+
+    check("/test",
+          "/test" :: Nil,
+          ("/test", "/test" :: Nil)
+            :: ("/route", Nil)
+            :: ("/test/route", Nil)
+            :: ("/test/", "/test/" :: Nil)
+            :: Nil)
+
+    check("/test/",
+          "/test/" :: Nil,
+          ("/test", Nil)
+            :: ("/test/", "/test/" :: Nil)
+            :: ("/test//", "/test//" :: Nil)
+            :: Nil)
+  }
+
+  "Simple path" should "support escaped groups" in {
+    check("/te\\~st",
+          "/te~st" :: Nil,
+          ("/te~st", "/te~st" :: Nil)
+            :: ("/TE~ST", Nil)
+            :: Nil,
+          PathRegexOptions.caseSensitive)
   }
 
   "Case-sensitive" should "work" in {
-    check("/test", "/test" :: Nil, PathRegexOptions.caseSensitive)
-    check("/TEST", "/TEST" :: Nil, PathRegexOptions.caseSensitive)
+    check("/test",
+          "/test" :: Nil,
+          ("/test", "/test" :: Nil)
+            :: ("/TEST", Nil)
+            :: Nil,
+          PathRegexOptions.caseSensitive)
+
+    check("/TEST",
+          "/TEST" :: Nil,
+          ("/test", Nil)
+            :: ("/TEST", "/TEST" :: Nil)
+            :: Nil,
+          PathRegexOptions.caseSensitive)
   }
 
   "Strict mode" should "work" in {
-    check("/test", "/test" :: Nil, PathRegexOptions.strict)
-    check("/test/", "/test/" :: Nil, PathRegexOptions.strict)
-    check(
-      "/:test/",
-      PathToken(Some("test"), 0, Some('/'), Some('/'), false, false, false, """[^\/]+?""") :: PathToken(
-        "/") :: Nil,
-      PathRegexOptions.strict)
+    check("/test",
+          "/test" :: Nil,
+          ("/test", "/test" :: Nil)
+            :: ("/test/", Nil)
+            :: ("/TEST", "/TEST" :: Nil)
+            :: Nil,
+          PathRegexOptions.strict)
+    check("/test/",
+          "/test/" :: Nil,
+          ("/test", Nil)
+            :: ("/test/", "/test/" :: Nil)
+            :: ("/test//", Nil)
+            :: Nil,
+          PathRegexOptions.strict)
   }
 
   "Non-ending mode" should "work" in {
-    check("/test", "/test" :: Nil, PathRegexOptions.nonEnd)
-    check("/test/", "/test/" :: Nil, PathRegexOptions.nonEnd)
+    check("/test",
+          "/test" :: Nil,
+          ("/test/route", "/test" :: Nil)
+            :: ("/test", "/test" :: Nil)
+            :: ("/test/", "/test/" :: Nil)
+            :: ("/route", Nil)
+            :: Nil,
+          PathRegexOptions.nonEnd)
+    check(
+      "/test/",
+      "/test/" :: Nil,
+      ("/test/route", "/test/" :: Nil)
+        :: ("/test//route", "/test/" :: Nil)
+        :: ("/test", Nil)
+        :: ("/test//", "/test//" :: Nil)
+        :: Nil,
+      PathRegexOptions.nonEnd
+    )
     check(
       "/:test",
       PathToken(Some("test"), 0, Some('/'), Some('/'), false, false, false, """[^\/]+?""") :: Nil,
-      PathRegexOptions.nonEnd)
+      ("/route", "/route" :: "route" :: Nil) :: Nil,
+      PathRegexOptions.nonEnd
+    )
     check(
       "/:test/",
       PathToken(Some("test"), 0, Some('/'), Some('/'), false, false, false, """[^\/]+?""") :: PathToken(
         "/") :: Nil,
-      PathRegexOptions.nonEnd)
+      ("/route", Nil)
+        :: ("/route/", "/route/" :: "route" :: Nil)
+        :: Nil,
+      PathRegexOptions.nonEnd
+    )
   }
 
-  "Combine modes" should "work" in {
-    val options = PathRegexOptions(end = false, strict = true)
-    check("/test", "/test" :: Nil, options)
-    check("/test/", "/test/" :: Nil, options)
-    check("/test.json", "/test.json" :: Nil, options)
+  val noEndStrictOptions = PathRegexOptions(end = false, strict = true)
+
+  "Combine modes" should "accept simple paths" in {
+    check("/test",
+          "/test" :: Nil,
+          ("/test", "/test" :: Nil)
+            :: ("/test/", "/test" :: Nil)
+            :: ("/test/route", "/test" :: Nil)
+            :: Nil,
+          noEndStrictOptions)
+    check(
+      "/test/",
+      "/test/" :: Nil,
+      ("/test", Nil)
+        :: ("/test/", "/test/" :: Nil)
+        :: ("/test//", "/test/" :: Nil)
+        :: ("/test/route", "/test/" :: Nil)
+        :: Nil,
+      noEndStrictOptions
+    )
+  }
+
+  "Combine modes" should "accept file path" in {
+    check(
+      "/test.json",
+      "/test.json" :: Nil,
+      ("/test.json", "/test.json" :: Nil)
+        :: ("/test.json.hbs", Nil)
+        :: ("/test.json/route", "/test.json" :: Nil)
+        :: Nil,
+      noEndStrictOptions
+    )
+  }
+
+  "Combine modes" should "work with named argument" in {
     check(
       "/:test",
       PathToken(Some("test"), 0, Some('/'), Some('/'), false, false, false, """[^\/]+?""") :: Nil,
-      options)
+      ("/route", "/route" :: "route" :: Nil)
+        :: ("/route/", "/route" :: "route" :: Nil)
+        :: Nil,
+      noEndStrictOptions
+    )
     check(
       "/:test/",
       PathToken(Some("test"), 0, Some('/'), Some('/'), false, false, false, """[^\/]+?""") :: PathToken(
         "/") :: Nil,
-      options)
+      ("/route", Nil)
+        :: ("/route/", "/route/" :: "route" :: Nil)
+        :: Nil,
+      noEndStrictOptions
+    )
   }
 
   "Optional named parameter" should "work" in {
-    check("/:test?",
-          PathToken(Some("test"),
-                    0,
-                    Some('/'),
-                    Some('/'),
-                    optional = true,
-                    repeat = false,
-                    partial = false,
-                    """[^\/]+?""") :: Nil,
-          PathRegexOptions.nonEnd)
+    check(
+      "/:test?",
+      PathToken(Some("test"),
+                0,
+                Some('/'),
+                Some('/'),
+                optional = true,
+                repeat = false,
+                partial = false,
+                """[^\/]+?""") :: Nil,
+      ("/route", "/route" :: "route" :: Nil)
+        :: ("/route/nested", Nil)
+        :: ("/", "/" :: null :: Nil)
+        :: ("//", Nil)
+        :: Nil,
+      PathRegexOptions.default
+    )
+  }
+
+  "Optional named parameter" should "work with strict mode" in {
+    check(
+      "/:test?",
+      PathToken(Some("test"),
+                0,
+                Some('/'),
+                Some('/'),
+                optional = true,
+                repeat = false,
+                partial = false,
+                """[^\/]+?""") :: Nil,
+      ("/route", "/route" :: "route" :: Nil)
+        :: ("/", Nil)
+        :: ("//", Nil)
+        :: Nil,
+      PathRegexOptions.strict
+    )
+
+    check(
+      "/:test?/",
+      PathToken(Some("test"),
+                0,
+                Some('/'),
+                Some('/'),
+                optional = true,
+                repeat = false,
+                partial = false,
+                """[^\/]+?""") :: "/" :: Nil,
+      ("/route", Nil)
+        :: ("/route/", "/route/" :: "route" :: Nil)
+        :: ("/", "/" :: null :: Nil)
+        :: ("//", Nil)
+        :: Nil,
+      PathRegexOptions.strict
+    )
   }
 }
