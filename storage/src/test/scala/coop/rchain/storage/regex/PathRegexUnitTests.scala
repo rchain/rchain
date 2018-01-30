34a47ede7a7ee9b66594c80cb8c4910f4bf5906a
ys-pyrofex <yaraslau@pyrofex.net>
2018-01-30 21:56:22 +0300

    Refactored unit-tests for better readability

--- storage/src/test/scala/coop/rchain/storage/regex/PathRegexUnitTests.scala
+++ storage/src/test/scala/coop/rchain/storage/regex/PathRegexUnitTests.scala
@@ -1,235 +1,180 @@
 package coop.rchain.storage.regex
-
 import org.scalatest._
+import scala.language.implicitConversions
 
 class PathRegexUnitTests extends FlatSpec with Matchers {
 
-  def doCheck(path: String,
-              tokens: Option[List[PathToken]],
-              matchCases: Seq[(String, Seq[String])],
-              //compileCases : Seq[String],
-              options: PathRegexOptions = PathRegexOptions.default): Unit = {
-    val parsed = PathRegex.compile(path, options)
-    tokens.foreach(t => assert(parsed.tokens == t))
+  implicit class ExPathRegex(value: PathRegex) {
+    def accept(matchCases: (String, Seq[String])*): ExPathRegex =
+      this
+
+    def build(buildCases: (Seq[String], String)*): ExPathRegex =
+      this
+
+    def struct(testTokens: List[PathToken]): ExPathRegex = {
+      assert(testTokens == value.tokens)
+      this
+    }
   }
 
-  def check(path: String,
-            tokens: List[Any],
-            matchCases: Seq[(String, Seq[String])] = Nil,
-            options: PathRegexOptions = PathRegexOptions.default): Unit =
-    doCheck(path, Some(tokens.map {
-      case t: PathToken => t
-      case s: String    => PathToken(s)
-    }), matchCases, options)
+  implicit def stringToPathToken(str: String): PathToken = PathToken(str)
+
+  def pt(str: String): PathToken = PathToken(str)
+
+  def parse(path: String, options: PathRegexOptions = PathRegexOptions.default): ExPathRegex =
+    PathRegex.compile(path, options)
 
   "Simple paths" should "parse" in {
-    check("/",
-          "/" :: Nil,
-          ("/", "/" :: Nil)
-            :: ("/route", Nil) :: Nil)
+    parse("/")
+      .struct("/" :: Nil)
+      .accept(("/", "/" :: Nil), ("/route", Nil))
 
-    check("/test",
-          "/test" :: Nil,
-          ("/test", "/test" :: Nil)
-            :: ("/route", Nil)
-            :: ("/test/route", Nil)
-            :: ("/test/", "/test/" :: Nil)
-            :: Nil)
+    parse("/test")
+      .struct("/test" :: Nil)
+      .accept(("/test", "/test" :: Nil),
+              ("/route", Nil),
+              ("/test/route", Nil),
+              ("/test/", "/test/" :: Nil))
 
-    check("/test/",
-          "/test/" :: Nil,
-          ("/test", Nil)
-            :: ("/test/", "/test/" :: Nil)
-            :: ("/test//", "/test//" :: Nil)
-            :: Nil)
+    parse("/test/")
+      .struct("/test/" :: Nil)
+      .accept(("/test", Nil), ("/test/", "/test/" :: Nil), ("/test//", "/test//" :: Nil))
   }
 
   "Simple path" should "support escaped groups" in {
-    check("/te\\~st",
-          "/te~st" :: Nil,
-          ("/te~st", "/te~st" :: Nil)
-            :: ("/TE~ST", Nil)
-            :: Nil,
-          PathRegexOptions.caseSensitive)
+    parse("/te\\~st", PathRegexOptions.caseSensitive)
+      .struct("/te~st" :: Nil)
+      .accept(("/te~st", "/te~st" :: Nil), ("/TE~ST", Nil))
   }
 
   "Case-sensitive" should "work" in {
-    check("/test",
-          "/test" :: Nil,
-          ("/test", "/test" :: Nil)
-            :: ("/TEST", Nil)
-            :: Nil,
-          PathRegexOptions.caseSensitive)
+    parse("/test", PathRegexOptions.caseSensitive)
+      .struct("/test" :: Nil)
+      .accept(("/test", "/test" :: Nil), ("/TEST", Nil))
 
-    check("/TEST",
-          "/TEST" :: Nil,
-          ("/test", Nil)
-            :: ("/TEST", "/TEST" :: Nil)
-            :: Nil,
-          PathRegexOptions.caseSensitive)
+    parse("/TEST", PathRegexOptions.caseSensitive)
+      .struct("/TEST" :: Nil)
+      .accept(("/test", Nil), ("/TEST", "/TEST" :: Nil))
   }
 
   "Strict mode" should "work" in {
-    check("/test",
-          "/test" :: Nil,
-          ("/test", "/test" :: Nil)
-            :: ("/test/", Nil)
-            :: ("/TEST", "/TEST" :: Nil)
-            :: Nil,
-          PathRegexOptions.strict)
-    check("/test/",
-          "/test/" :: Nil,
-          ("/test", Nil)
-            :: ("/test/", "/test/" :: Nil)
-            :: ("/test//", Nil)
-            :: Nil,
-          PathRegexOptions.strict)
+    parse("/test", PathRegexOptions.strict)
+      .struct("/test" :: Nil)
+      .accept(("/test", "/test" :: Nil), ("/test/", Nil), ("/TEST", "/TEST" :: Nil))
+
+    parse("/test/", PathRegexOptions.strict)
+      .struct("/test/" :: Nil)
+      .accept(("/test", Nil), ("/test/", "/test/" :: Nil), ("/test//", Nil))
   }
 
   "Non-ending mode" should "work" in {
-    check("/test",
-          "/test" :: Nil,
-          ("/test/route", "/test" :: Nil)
-            :: ("/test", "/test" :: Nil)
-            :: ("/test/", "/test/" :: Nil)
-            :: ("/route", Nil)
-            :: Nil,
-          PathRegexOptions.nonEnd)
-    check(
-      "/test/",
-      "/test/" :: Nil,
-      ("/test/route", "/test/" :: Nil)
-        :: ("/test//route", "/test/" :: Nil)
-        :: ("/test", Nil)
-        :: ("/test//", "/test//" :: Nil)
-        :: Nil,
-      PathRegexOptions.nonEnd
-    )
-    check(
-      "/:test",
-      PathToken(Some("test"), 0, Some('/'), Some('/'), false, false, false, """[^\/]+?""") :: Nil,
-      ("/route", "/route" :: "route" :: Nil) :: Nil,
-      PathRegexOptions.nonEnd
-    )
-    check(
-      "/:test/",
-      PathToken(Some("test"), 0, Some('/'), Some('/'), false, false, false, """[^\/]+?""") :: PathToken(
-        "/") :: Nil,
-      ("/route", Nil)
-        :: ("/route/", "/route/" :: "route" :: Nil)
-        :: Nil,
-      PathRegexOptions.nonEnd
-    )
+    parse("/test", PathRegexOptions.nonEnd)
+      .struct("/test" :: Nil)
+      .accept(("/test/route", "/test" :: Nil),
+              ("/test", "/test" :: Nil),
+              ("/test/", "/test/" :: Nil),
+              ("/route", Nil))
+
+    parse("/test/", PathRegexOptions.nonEnd)
+      .struct("/test/" :: Nil)
+      .accept(("/test/route", "/test/" :: Nil),
+              ("/test//route", "/test/" :: Nil),
+              ("/test", Nil),
+              ("/test//", "/test//" :: Nil))
+
+    parse("/:test", PathRegexOptions.nonEnd)
+      .struct(
+        PathToken(Some("test"), 0, Some('/'), Some('/'), false, false, false, """[^\/]+?""") :: Nil)
+      .accept(("/route", "/route" :: "route" :: Nil))
+
+    parse("/:test/", PathRegexOptions.nonEnd)
+      .struct(
+        PathToken(Some("test"), 0, Some('/'), Some('/'), false, false, false, """[^\/]+?""") :: pt(
+          "/") :: Nil)
+      .accept(("/route", Nil), ("/route/", "/route/" :: "route" :: Nil))
   }
 
   val noEndStrictOptions = PathRegexOptions(end = false, strict = true)
 
   "Combine modes" should "accept simple paths" in {
-    check("/test",
-          "/test" :: Nil,
-          ("/test", "/test" :: Nil)
-            :: ("/test/", "/test" :: Nil)
-            :: ("/test/route", "/test" :: Nil)
-            :: Nil,
-          noEndStrictOptions)
-    check(
-      "/test/",
-      "/test/" :: Nil,
-      ("/test", Nil)
-        :: ("/test/", "/test/" :: Nil)
-        :: ("/test//", "/test/" :: Nil)
-        :: ("/test/route", "/test/" :: Nil)
-        :: Nil,
-      noEndStrictOptions
-    )
+    parse("/test", noEndStrictOptions)
+      .struct("/test" :: Nil)
+      .accept(("/test", "/test" :: Nil),
+              ("/test/", "/test" :: Nil),
+              ("/test/route", "/test" :: Nil))
+
+    parse("/test/", noEndStrictOptions)
+      .struct("/test/" :: Nil)
+      .accept(("/test", Nil),
+              ("/test/", "/test/" :: Nil),
+              ("/test//", "/test/" :: Nil),
+              ("/test/route", "/test/" :: Nil))
   }
 
   "Combine modes" should "accept file path" in {
-    check(
-      "/test.json",
-      "/test.json" :: Nil,
-      ("/test.json", "/test.json" :: Nil)
-        :: ("/test.json.hbs", Nil)
-        :: ("/test.json/route", "/test.json" :: Nil)
-        :: Nil,
-      noEndStrictOptions
-    )
+    parse("/test.json", noEndStrictOptions)
+      .struct("/test.json" :: Nil)
+      .accept(("/test.json", "/test.json" :: Nil),
+              ("/test.json.hbs", Nil),
+              ("/test.json/route", "/test.json" :: Nil))
   }
 
   "Combine modes" should "work with named argument" in {
-    check(
-      "/:test",
-      PathToken(Some("test"), 0, Some('/'), Some('/'), false, false, false, """[^\/]+?""") :: Nil,
-      ("/route", "/route" :: "route" :: Nil)
-        :: ("/route/", "/route" :: "route" :: Nil)
-        :: Nil,
-      noEndStrictOptions
-    )
-    check(
-      "/:test/",
-      PathToken(Some("test"), 0, Some('/'), Some('/'), false, false, false, """[^\/]+?""") :: PathToken(
-        "/") :: Nil,
-      ("/route", Nil)
-        :: ("/route/", "/route/" :: "route" :: Nil)
-        :: Nil,
-      noEndStrictOptions
-    )
+    parse("/:test", noEndStrictOptions)
+      .struct(
+        PathToken(Some("test"), 0, Some('/'), Some('/'), false, false, false, """[^\/]+?""") :: Nil)
+      .accept(("/route", "/route" :: "route" :: Nil), ("/route/", "/route" :: "route" :: Nil))
+
+    parse("/:test/", noEndStrictOptions)
+      .struct(
+        PathToken(Some("test"), 0, Some('/'), Some('/'), false, false, false, """[^\/]+?""") :: pt(
+          "/") :: Nil)
+      .accept(("/route", Nil), ("/route/", "/route/" :: "route" :: Nil))
   }
 
   "Optional named parameter" should "work" in {
-    check(
-      "/:test?",
-      PathToken(Some("test"),
-                0,
-                Some('/'),
-                Some('/'),
-                optional = true,
-                repeat = false,
-                partial = false,
-                """[^\/]+?""") :: Nil,
-      ("/route", "/route" :: "route" :: Nil)
-        :: ("/route/nested", Nil)
-        :: ("/", "/" :: null :: Nil)
-        :: ("//", Nil)
-        :: Nil,
-      PathRegexOptions.default
-    )
+    parse("/:test?", PathRegexOptions.default)
+      .struct(
+        PathToken(Some("test"),
+                  0,
+                  Some('/'),
+                  Some('/'),
+                  optional = true,
+                  repeat = false,
+                  partial = false,
+                  """[^\/]+?""") :: Nil)
+      .accept(("/route", "/route" :: "route" :: Nil),
+              ("/route/nested", Nil),
+              ("/", "/" :: null :: Nil),
+              ("//", Nil))
   }
 
   "Optional named parameter" should "work with strict mode" in {
-    check(
-      "/:test?",
-      PathToken(Some("test"),
-                0,
-                Some('/'),
-                Some('/'),
-                optional = true,
-                repeat = false,
-                partial = false,
-                """[^\/]+?""") :: Nil,
-      ("/route", "/route" :: "route" :: Nil)
-        :: ("/", Nil)
-        :: ("//", Nil)
-        :: Nil,
-      PathRegexOptions.strict
-    )
+    parse("/:test?", PathRegexOptions.strict)
+      .struct(
+        PathToken(Some("test"),
+                  0,
+                  Some('/'),
+                  Some('/'),
+                  optional = true,
+                  repeat = false,
+                  partial = false,
+                  """[^\/]+?""") :: Nil)
+      .accept(("/route", "/route" :: "route" :: Nil), ("/", Nil), ("//", Nil))
 
-    check(
-      "/:test?/",
-      PathToken(Some("test"),
-                0,
-                Some('/'),
-                Some('/'),
-                optional = true,
-                repeat = false,
-                partial = false,
-                """[^\/]+?""") :: "/" :: Nil,
-      ("/route", Nil)
-        :: ("/route/", "/route/" :: "route" :: Nil)
-        :: ("/", "/" :: null :: Nil)
-        :: ("//", Nil)
-        :: Nil,
-      PathRegexOptions.strict
-    )
+    parse("/:test?/", PathRegexOptions.strict)
+      .struct(
+        PathToken(Some("test"),
+                  0,
+                  Some('/'),
+                  Some('/'),
+                  optional = true,
+                  repeat = false,
+                  partial = false,
+                  """[^\/]+?""") :: pt("/") :: Nil)
+      .accept(("/route", Nil),
+              ("/route/", "/route/" :: "route" :: Nil),
+              ("/", "/" :: null :: Nil),
+              ("//", Nil))
   }
 }
