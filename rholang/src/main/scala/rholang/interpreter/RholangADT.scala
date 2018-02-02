2f6ff22ba72840257f7eba3d28cf6531fcf21a9b
Kyle Butt <kyle@pyrofex.net>
2018-02-02 16:50:24 -0700

    Rholang: Normalizer: Add Name normalization
    Mostly, name normalization delegates to process normalization except for vars
    and wildcards. Add tests for var, wildcard, and a sampling of delegation tests.

--- rholang/src/main/scala/rholang/interpreter/RholangADT.scala
+++ rholang/src/main/scala/rholang/interpreter/RholangADT.scala
@@ -43,12 +43,12 @@ case class FreeVar(level: Int) extends Var
 
 // Upon send, all free variables in data are substituted with their values.
 // also if a process is sent, it is auto-quoted.
-case class Send(chan: Channel, data: Channel)
+case class Send(chan: Channel, data: List[Par])
 
 // [Par] is an n-arity Pattern.
 // It's an error for free Variable to occur more than once in a pattern.
 // Don't currently support conditional receive
-case class Receive(binds: List[(List[Par], Channel)])
+case class Receive(binds: List[(List[Channel], Channel)])
 
 case class Eval(channel: Channel)
 
@@ -82,6 +82,9 @@ case class EGt(p1: Par, p2: Par) extends Expr
 case class EGte(p1: Par, p2: Par) extends Expr
 case class EEq(p1: Par, p2: Par) extends Expr
 case class ENeq(p1: Par, p2: Par) extends Expr
+case class ENot(p: Par) extends Expr
+case class EAnd(p1: Par, p2: Par) extends Expr
+case class EOr(p1: Par, p2: Par) extends Expr
 
 case class GBool(b: Boolean) extends Ground
 case class GInt(i: Integer) extends Ground
--- rholang/src/test/scala/rholang/interpretr/NormalizeTest.scala
+++ rholang/src/test/scala/rholang/interpretr/NormalizeTest.scala
@@ -51,7 +51,7 @@ class NilVisitSpec extends FlatSpec with Matchers {
   }
 }
 
-class VarVisitSpec extends FlatSpec with Matchers {
+class ProcVarVisitSpec extends FlatSpec with Matchers {
   val visitor = new ProcNormalizeVisitor() {}
   val inputs = ProcVisitInputs(
       Par(),
@@ -70,7 +70,7 @@ class VarVisitSpec extends FlatSpec with Matchers {
   "PVar" should "Compile as FreeVar if it's not in env" in {
     val result = pvar.accept(visitor, inputs)
     result.par should be (inputs.par.copy(expr = List(EVar(FreeVar(0)))))
-    result.knownFree should be
+    result.knownFree shouldEqual
         (inputs.knownFree.newBindings(
             List(("x", ProcSort)))._1)
   }
@@ -79,7 +79,7 @@ class VarVisitSpec extends FlatSpec with Matchers {
       inputs.env.newBindings(List(("x", NameSort)))._1)
     
     an [Error] should be thrownBy {
-      val result = pvar.accept(visitor, boundinputs)
+      pvar.accept(visitor, boundinputs)
     }
   }
   "PVar" should "Not compile if it's used free somewhere else" in {
@@ -87,7 +87,145 @@ class VarVisitSpec extends FlatSpec with Matchers {
       inputs.knownFree.newBindings(List(("x", ProcSort)))._1)
     
     an [Error] should be thrownBy {
-      val result = pvar.accept(visitor, boundinputs)
+      pvar.accept(visitor, boundinputs)
     }
   }
 }
+
+class ParVisitSpec extends FlatSpec with Matchers {
+  val visitor = new ProcNormalizeVisitor() {}
+  val inputs = ProcVisitInputs(
+      Par(),
+      DebruijnLevelMap[VarSort](),
+      DebruijnLevelMap[VarSort]())
+
+  "PPar" should "Compile both branches into a par object" in {
+    val parGround = new PPar(
+        new PGround(
+            new GroundInt(7)),
+        new PGround(
+            new GroundInt(8)))
+    val result = parGround.accept(visitor, inputs)
+    result.par should be (
+        inputs.par.copy(expr =
+            List(GInt(8), GInt(7))))
+    result.knownFree should be (inputs.knownFree)
+  }
+  "PPar" should "Compile both branches with the same environment" in {
+    val parDoubleBound = new PPar(
+        new PVar("x"),
+        new PVar("x"))
+    val boundinputs = inputs.copy(env =
+      inputs.env.newBindings(List(("x", ProcSort)))._1)
+
+    val result = parDoubleBound.accept(visitor, boundinputs)
+    result.par should be (
+        inputs.par.copy(expr =
+            List(EVar(BoundVar(0)), EVar(BoundVar(0)))))
+    result.knownFree should be (inputs.knownFree)
+  }
+  "PPar" should "Not compile if both branches use the same free variable" in {
+    val parDoubleFree = new PPar(
+        new PVar("x"),
+        new PVar("x"))
+    an [Error] should be thrownBy {
+      parDoubleFree.accept(visitor, inputs)
+    }
+  }
+}
+
+class NameWildcardVisitSpec extends FlatSpec with Matchers {
+  val procVisitor = new ProcNormalizeVisitor() {}
+  val nameVisitor = new NameNormalizeVisitor() {
+    val procVisitor = NameWildcardVisitSpec.this.procVisitor
+  }
+  val inputs = NameVisitInputs(
+      DebruijnLevelMap[VarSort](),
+      DebruijnLevelMap[VarSort]())
+
+  "NameWildcard" should "Set wildcard flag in knownFree" in {
+    val nw = new NameWildcard()
+    val result = nw.accept(nameVisitor, inputs)
+    result.chan should be (ChanVar(WildCard()))
+    result.knownFree.wildcardUsed should be (true)
+    result.knownFree shouldEqual (inputs.knownFree.setWildcardUsed())
+  }
+}
+
+class NameVarVisitSpec extends FlatSpec with Matchers {
+  val procVisitor = new ProcNormalizeVisitor() {}
+  val nameVisitor = new NameNormalizeVisitor() {
+    val procVisitor = NameVarVisitSpec.this.procVisitor
+  }
+  val inputs = NameVisitInputs(
+      DebruijnLevelMap[VarSort](),
+      DebruijnLevelMap[VarSort]())
+  val nvar = new NameVar("x")
+
+  "NameVar" should "Compile as BoundVar if it's in env" in {
+    val boundinputs = inputs.copy(env =
+      inputs.env.newBindings(List(("x", NameSort)))._1)
+  
+    val result = nvar.accept(nameVisitor, boundinputs)
+    result.chan should be (ChanVar(BoundVar(0)))
+    result.knownFree should be (inputs.knownFree)
+  }
+  "NameVar" should "Compile as FreeVar if it's not in env" in {
+    val result = nvar.accept(nameVisitor, inputs)
+    result.chan should be (ChanVar(FreeVar(0)))
+    result.knownFree shouldEqual
+        (inputs.knownFree.newBindings(
+            List(("x", NameSort)))._1)
+  }
+  "NameVar" should "Not compile if it's in env of the wrong sort" in {
+    val boundinputs = inputs.copy(env =
+      inputs.env.newBindings(List(("x", ProcSort)))._1)
+    
+    an [Error] should be thrownBy {
+      nvar.accept(nameVisitor, boundinputs)
+    }
+  }
+  "NameVar" should "Not compile if it's used free somewhere else" in {
+    val boundinputs = inputs.copy(knownFree =
+      inputs.knownFree.newBindings(List(("x", NameSort)))._1)
+    
+    an [Error] should be thrownBy {
+      nvar.accept(nameVisitor, boundinputs)
+    }
+  }
+}
+
+class NameQuoteVisitSpec extends FlatSpec with Matchers {
+  val procVisitor = new ProcNormalizeVisitor() {}
+  val nameVisitor = new NameNormalizeVisitor() {
+    val procVisitor = NameQuoteVisitSpec.this.procVisitor
+  }
+  val inputs = NameVisitInputs(
+      DebruijnLevelMap[VarSort](),
+      DebruijnLevelMap[VarSort]())
+  val nvar = new NameVar("x")
+
+  "NameQuote" should "compile to a quoted var if the var is bound" in {
+    val boundinputs = inputs.copy(env =
+      inputs.env.newBindings(List(("x", ProcSort)))._1)
+    val nqvar = new NameQuote(new PVar("x"))
+    val result = nqvar.accept(nameVisitor, boundinputs)
+    result.chan should be (Quote(Par().copy(expr = List(EVar(BoundVar(0))))))
+    result.knownFree should be (inputs.knownFree)
+  }
+
+  "NameQuote" should "return a free use if the quoted proc has a free var" in {
+    val nqvar = new NameQuote(new PVar("x"))
+    val result = nqvar.accept(nameVisitor, inputs)
+    result.chan should be (Quote(Par().copy(expr = List(EVar(FreeVar(0))))))
+    result.knownFree should be (inputs.knownFree.newBindings(List(("x", ProcSort)))._1)
+  }
+
+  "NameQuote" should "compile to a quoted ground" in {
+    val nqground = new NameQuote(new PGround(new GroundInt(7)))
+    val result = nqground.accept(nameVisitor, inputs)
+    result.chan should be (Quote(Par().copy(expr = List(GInt(7)))))
+    result.knownFree should be (inputs.knownFree)
+  }
+}
+
