0dcbc27da59e704f19899590e9781bfba0d56e12
Henry Till <henrytill@gmail.com>
2018-02-02 19:00:12 -0500

    rholang: enable test coverage measurement, sending reports to codecov

---
+++ rholang/src/test/scala/rholang/rosette/CompilerTests.scala
@@ -0,0 +1,24 @@
+package rholang.rosette
+
+import java.nio.file.{Files, Path, Paths}
+
+import coop.rchain.rho2rose.Rholang2RosetteCompiler
+import org.scalatest.FunSuite
+
+import scala.collection.JavaConverters._
+
+class CompilerTests extends FunSuite {
+
+  val testFiles: Iterable[Path] =
+    Files.newDirectoryStream(Paths.get("tests")).asScala
+
+  for (file <- testFiles if file.getFileName.toString.endsWith(".rho")) {
+    test(file.toString) {
+      assert(
+        Rholang2RosetteCompiler
+          .compile(file.toString)
+          .map(Rholang2RosetteCompiler.serialize)
+          .isDefined)
+    }
+  }
+}
