3add52c4e03d033296663b268e25710631119ef5
Kent Shikama <kent@kentshikama.com>
2018-02-06 15:21:11 -0800

    Change scored terms to keep track of score in a tree structure
    This allows us to not assume the maximum number of terms for the
    purposes of sorting.

--- rholang/src/main/scala/coop/rchain/rholang/interpreter/sort.scala
+++ rholang/src/main/scala/coop/rchain/rholang/interpreter/sort.scala
@@ -1,20 +1,40 @@
 package coop.rchain.rholang.interpreter
 
-case class ScoredTerm(term: Any, score: Seq[Int]) extends Ordered[ScoredTerm] {
-  def compare(that: ScoredTerm) : Int = {
-    def compareScore(s1: Seq[Int], s2: Seq[Int]) : Int = {
+import coop.rchain.rholang.intepreter._
+import coop.rchain.rholang.syntax.rholang_mercury.Absyn._
+
+sealed trait Tree[T] {
+  def size: Int
+}
+case class Leaf[T](item: T) extends Tree[T] {
+  def size = 1
+}
+case class Node[T] (children: Seq[Tree[T]]) extends Tree[T] {
+  def size = children.map(_.size).sum
+}
+
+object Leaves {
+  // Shortcut to be able to write Leaves(1,2,3) instead of Node(Seq(Leaf(1),Leaf(2),Leaf(3)))
+  def apply[T](children: T*) : Node[T] = Node(children.map(a => Leaf(a)))
+}
+
+case class ScoredTerm[T](term: T, score: Tree[Int]) extends Ordered[ScoredTerm[T]] {
+  def compare(that: ScoredTerm[T]) : Int = {
+    def compareScore(s1: Tree[Int], s2: Tree[Int]) : Int = {
       (s1, s2) match {
-        case (h1 :: t1, h2 :: t2) => {
-          h1 - h2 match {
-            case 0 => compareScore(t1,t2)
+        case (Leaf(a), Leaf(b)) => a.compare(b)
+        case (Node(h1 +: t1), Node(h2 +: t2)) =>
+          compareScore(h1, h2) match {
+            case 0 => compareScore(Node(t1), Node(t2))
             case other => other
           }
-        }
-        case (List(_), _) => 1
-        case (_, List(_)) => -1
-        case (_, _) => 0
+        case (Node(_), _) => -1
+        case (_, Node(_)) => 1
       }
     }
     compareScore(this.score, that.score)
   }
 }
+
+
+
--- rholang/src/test/scala/coop/rchain/rholang/interpreter/SortTest.scala
+++ rholang/src/test/scala/coop/rchain/rholang/interpreter/SortTest.scala
@@ -3,24 +3,26 @@ package coop.rchain.rholang.interpreter
 import org.scalatest._
 
 class ScoredTermSpec extends FlatSpec with Matchers {
-  "ScoredTerm" should "Sort so that shorter terms come first" in {
-    val unsortedTerms = Seq(ScoredTerm("foo", Seq(1,2,2,3)), ScoredTerm("bar", Seq(1,2,2)))
-    val sortedTerms = Seq(ScoredTerm("bar", Seq(1,2,2)), ScoredTerm("foo", Seq(1,2,2,3)))
+  "ScoredTerm" should "Sort so that shorter nodes come first" in {
+    val unsortedTerms = Seq(ScoredTerm("foo", Leaves(1,2,2,3)), ScoredTerm("bar", Leaves(1,2,2)))
+    val sortedTerms = Seq(ScoredTerm("bar", Leaves(1,2,2)), ScoredTerm("foo", Leaves(1,2,2,3)))
     unsortedTerms.sorted should be (sortedTerms)
   }
-  "ScoredTerm" should "Sort so that smaller scores stay first" in {
-    val unsortedTerms = Seq(ScoredTerm("foo", Seq(1)), ScoredTerm("bar", Seq(2)))
-    val sortedTerms = Seq(ScoredTerm("foo", Seq(1)), ScoredTerm("bar", Seq(2)))
+  "ScoredTerm" should "Sort so that smaller leafs stay first" in {
+    val unsortedTerms = Seq(ScoredTerm("foo", Leaf(1)), ScoredTerm("bar", Leaf(2)))
+    val sortedTerms = Seq(ScoredTerm("foo", Leaf(1)), ScoredTerm("bar", Leaf(2)))
     unsortedTerms.sorted should be (sortedTerms)
   }
-  "ScoredTerm" should "Sort so that smaller scores are put first" in {
-    val unsortedTerms = Seq(ScoredTerm("foo", Seq(2)), ScoredTerm("bar", Seq(1)))
-    val sortedTerms = Seq(ScoredTerm("bar", Seq(1)), ScoredTerm("foo", Seq(2)))
+  "ScoredTerm" should "Sort so that smaller leafs are put first" in {
+    val unsortedTerms = Seq(ScoredTerm("foo", Leaf(2)), ScoredTerm("bar", Leaf(1)))
+    val sortedTerms = Seq(ScoredTerm("bar", Leaf(1)), ScoredTerm("foo", Leaf(2)))
     unsortedTerms.sorted should be (sortedTerms)
   }
-  "ScoredTerm" should "Sort so that empty scores are put first" in {
-    val unsortedTerms = Seq(ScoredTerm("foo", Seq(2)), ScoredTerm("bar", Nil))
-    val sortedTerms = Seq(ScoredTerm("bar", Nil), ScoredTerm("foo", Seq(2)))
+  "ScoredTerm" should "Sort so that smaller nodes are put first" in {
+    val unsortedTerms = Seq(ScoredTerm("foo", Node(Seq(Leaves(1,2), Leaves(2,2)))),
+      ScoredTerm("bar", Node(Seq(Leaves(1,1), Leaves(2,2)))))
+    val sortedTerms = Seq(ScoredTerm("bar", Node(Seq(Leaves(1,1), Leaves(2,2)))),
+      ScoredTerm("foo", Node(Seq(Leaves(1,2), Leaves(2,2)))))
     unsortedTerms.sorted should be (sortedTerms)
   }
 }
