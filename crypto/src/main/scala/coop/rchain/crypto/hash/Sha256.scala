6640aa53045b4565d98a0114fbb02c88527c515c
Eitan Chatav <eitan.chatav@gmail.com>
2018-02-09 11:44:14 -0800

    Hash algorithms
    Blake2b256, Keccak256, Sha256

--- crypto/src/main/scala/coop/rchain/crypto/hash/Sha256.scala
+++ crypto/src/main/scala/coop/rchain/crypto/hash/Sha256.scala
@@ -3,9 +3,7 @@ package coop.rchain.crypto.hash
 import scorex.crypto.hash._
 import java.security.MessageDigest
 
-/**
-  * Hashing functions implementation with sha256 impl from Java SDK
-  */
+// Sha256 hashing algorithm
 object Sha256 extends CryptographicHash32 {
   override def hash(input: Array[Byte]): Digest32 = Digest32 @@ MessageDigest.getInstance("SHA-256").digest(input)
 }
