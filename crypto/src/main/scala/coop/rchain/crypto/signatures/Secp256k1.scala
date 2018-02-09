594b9118519a5e6a28c87582c355078345899b7c
Eitan Chatav <eitan.chatav@gmail.com>
2018-02-09 11:54:31 -0800

    documentation and secret key validation

--- crypto/src/main/scala/coop/rchain/crypto/signatures/Secp256k1.scala
+++ crypto/src/main/scala/coop/rchain/crypto/signatures/Secp256k1.scala
@@ -2,16 +2,41 @@ package coop.rchain.crypto.signatures
 
 import org.bitcoin._
 
-object Signatures {
+object Secp256k1 {
 
+  /**
+  * Verifies the given secp256k1 signature in native code.
+  * Calling when enabled == false is undefined (probably library not loaded)
+  *
+  * @param data The data which was signed, must be exactly 32 bytes
+  * @param signature The signature
+  * @param pub The public key which did the signing
+  */
   def verify(
     data: Array[Byte], signature: Array[Byte], pub: Array[Byte]
     ): Boolean =
     NativeSecp256k1.verify(data,signature,pub)
 
+  /**
+  * libsecp256k1 Create an ECDSA signature.
+  *
+  * @param data Message hash, 32 bytes
+  * @param sec Secret key, 32 bytes
+  *
+  * Return values
+  * @param sig byte array of signature
+  **/
   def sign(
     data: Array[Byte], sec: Array[Byte]
     ): Array[Byte] =
     NativeSecp256k1.sign(data,sec)
+
+  /**
+  * libsecp256k1 Seckey Verify - returns true if valid, false if invalid
+  *
+  * @param seckey ECDSA Secret key, 32 bytes
+  */
+  def secKeyVerify(seckey: Array[Byte]): Boolean =
+    NativeSecp256k1.secKeyVerify(seckey)
 
 }
