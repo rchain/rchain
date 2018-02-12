93ed555fe5d875f07863ba2037783e3a7eaec0ad
Kent Shikama <kent@kentshikama.com>
2018-02-12 13:21:09 -0800

    Change constants to val instead of var

--- rholang/src/main/scala/rholang/rosette/Roselang.scala
+++ rholang/src/main/scala/rholang/rosette/Roselang.scala
@@ -106,9 +106,9 @@ object RosetteOps {
   val _rx = "RX"
   val _run = "run"
   val _compile = "compile"
-  var _if = "if"
-  var _match = "match-pattern" // TODO: Adjust based on Rosette implementation. This operation checks for equality in the "match" implementation.
-  var _list = "list" // TODO: Extract into "temporary operations" set
+  val _if = "if"
+  val _match = "match-pattern" // TODO: Adjust based on Rosette implementation. This operation checks for equality in the "match" implementation.
+  val _list = "list" // TODO: Extract into "temporary operations" set
 }
 
 trait RholangASTToTerm 
