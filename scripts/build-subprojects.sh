3cce5738b052e79dbf98b8d92a3e5bfcfa2d3c0a
C.R. Kirkwood-Watts <kirkwood@totzeit.net>
2018-02-12 10:26:01 -0800

    No puedo deletrear.

--- scripts/build-subprojects.sh
+++ scripts/build-subprojects.sh
@@ -2,7 +2,7 @@
 
 if [ -f "build.sbt" ]; then
     echo "Building at top-level"
-    sbt -Dsbt.log.noformat=true < "scripts/sbt-subrojects"
+    sbt -Dsbt.log.noformat=true < "scripts/sbt-subprojects"
 else
     echo "No build/test files found!"
     exit 1
