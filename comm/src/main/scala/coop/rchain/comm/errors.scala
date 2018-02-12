7566e9250d39c8e8268671bdf0e4a43180393f02
Pawel Szulc <paul.szulc@gmail.com>
2018-02-12 11:09:37 +0100

    Remove empty lines

--- comm/src/main/scala/coop/rchain/comm/errors.scala
+++ comm/src/main/scala/coop/rchain/comm/errors.scala
@@ -1,11 +1,9 @@
 package coop.rchain.comm
 
 sealed trait CommError
-final case class UnknownCommError(msg: String) extends CommError
-
+final case class UnknownCommError(msg: String)       extends CommError
 final case class DatagramSizeError(size: Int)        extends CommError
 final case class DatagramFramingError(ex: Exception) extends CommError
 final case class DatagramException(ex: Exception)    extends CommError
-
-final case class ProtocolException(ex: Exception)  extends CommError
-final case class UnknownProtocolError(msg: String) extends CommError
+final case class ProtocolException(ex: Exception)    extends CommError
+final case class UnknownProtocolError(msg: String)   extends CommError
