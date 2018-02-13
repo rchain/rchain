ae54112956bdccc1fe0ed56f026eb6d686ccc77b
C.R. Kirkwood-Watts <kirkwood@totzeit.net>
2018-02-13 11:23:27 -0800

    Add coverage reports back in.

--- scripts/build-subprojects.sh
+++ scripts/build-subprojects.sh
@@ -6,24 +6,10 @@ if [ -d "${SUBPROJECT}" -a -f "${SUBPROJECT}/build.sh" ]; then
     echo "${SUBPROJECT}/build.sh"
     (cd "${SUBPROJECT}"; bash ./build.sh)
 elif [ -f "build.sbt" ]; then
-    # case ${SUBPROJECT} in
-    # 	rholang)
-    # 	    sbt_commands=";project ${SUBPROJECT} ;bnfc:generate ;compile ;assembly"
-    # 	    ;;
-    # 	comm|storage|roscala)
-    # 	    sbt_commands=";project ${SUBPROJECT} ;compile ;assembly"
-    # 	    ;;
-    # 	node)
-    # 	    sbt_commands=";project rholang ;bnfc:generate ;assembly ;project ${SUBPROJECT} ;assembly ;docker"
-    # 	    ;;
-    # esac
-    # if [ -z "$sbt_commands" ]; then
-    # 	exit 0
-    # fi
-    # Add coverage and any other common things here.
-    # sbt_commands="$sbt_commands ..."
-    # sbt -Dsbt.log.noformat=true "$sbt_commands"
     sbt -Dsbt.log.noformat=true "< scripts/sbt-subprojects"
+    for sub in comm rholang roscala storage node; do
+	(bash <(curl -s https://codecov.io/bash) -X gcov -s ./$sub -c -F $sub)
+    done
 else
     echo "No build/test files found!"
     exit 1
