f58d99b96d0161dbbb8caff9b8efb8b9847b3294
C.R. Kirkwood-Watts <kirkwood@totzeit.net>
2018-02-12 20:20:26 -0800

    This is what I meant to commit.

--- scripts/build-subprojects.sh
+++ scripts/build-subprojects.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
 if [ -f "build.sbt" ]; then
-    echo "Building at top-level"
-    sbt -Dsbt.log.noformat=true "< scripts/sbt-subprojects"
+    case ${SUBPROJECT} in
+	rholang)
+	    sbt_commands="bnfc:generate compile assembly"
+	    ;;
+	comm|storage)
+	    sbt_commands="compile assembly"
+	    ;;
+	node)
+	    sbt_commands="compile assembly docker"
+	    ;;
+    esac	
+    if [ -z "$sbt_commands" ]; then
+	exit 0
+    fi
+    # Add coverage and any other common things here.
+    # sbt_commands="$sbt_commands ..."
+    echo sbt -Dsbt.log.noformat=true project\ "${SUBPROJECT}" $sbt_commands
+    sbt -Dsbt.log.noformat=true project\ "${SUBPROJECT}" $sbt_commands
 else
     echo "No build/test files found!"
     exit 1
