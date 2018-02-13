98a9cfb79c73d09be8892f6ff001d08b85e3bb05
C.R. Kirkwood-Watts <kirkwood@totzeit.net>
2018-02-12 19:53:04 -0800

    Being deliberate about bnfc installation.
    Misc other changes to re-parallelize the CI build.

---
+++ scripts/install-dependencies.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# BNFC install for travis
+# Ubuntu 14.04 has an outdated BNFC package, so we build BNFC from source
+#
+# see also: ../.travis.yml
+set -e
+
+sudo apt install haskell-platform
+git clone https://github.com/BNFC/bnfc.git
+cd bnfc/source
+sudo cabal install --global
