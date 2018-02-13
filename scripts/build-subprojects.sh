7c14f17912d777709cc9155d799cc0ca3e904b53
C.R. Kirkwood-Watts <kirkwood@totzeit.net>
2018-02-13 13:14:49 -0800

    Simplified per @henrytill
    Using sbt magic to get subprojects built.

--- scripts/build-subprojects.sh
+++ scripts/build-subprojects.sh
@@ -6,7 +6,7 @@ if [ -d "${SUBPROJECT}" -a -f "${SUBPROJECT}/build.sh" ]; then
     echo "${SUBPROJECT}/build.sh"
     (cd "${SUBPROJECT}"; bash ./build.sh)
 elif [ -f "build.sbt" ]; then
-    sbt -Dsbt.log.noformat=true "< scripts/sbt-subprojects"
+    sbt -Dsbt.log.noformat=true clean coverage test coverageReport
     for sub in comm rholang roscala storage node; do
 	(bash <(curl -s https://codecov.io/bash) -X gcov -s ./$sub -c -F $sub)
     done
