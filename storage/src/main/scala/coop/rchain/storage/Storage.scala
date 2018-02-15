f38c9c6e880e9b57cc559e153c17a662321abfc4
Henry Till <henrytill@gmail.com>
2018-02-14 19:48:53 -0500

    CORE-185: storage: remove Key type

--- storage/src/main/scala/coop/rchain/storage/Storage.scala
+++ storage/src/main/scala/coop/rchain/storage/Storage.scala
@@ -8,46 +8,50 @@ import coop.rchain.storage.util._
 import org.lmdbjava.DbiFlags.MDB_CREATE
 import org.lmdbjava.{Dbi, Env, Txn}
 
-class Storage private (env: Env[ByteBuffer], db: Dbi[ByteBuffer])
-    extends IStorage
+class Storage[K, V] private (env: Env[ByteBuffer], db: Dbi[ByteBuffer])(implicit sa: Serialize[K],
+                                                                        sb: Serialize[V])
+    extends IStorage[K, V]
     with AutoCloseable {
 
-  def put[A](key: Key, value: A)(implicit s: Serialize[A]): Either[Error, Unit] =
+  def put(key: K, value: V): Either[Error, Unit] =
     Either
       .catchNonFatal {
-        val encoded = s.encode(value)
-        val keyBuff = ByteBuffer.allocateDirect(env.getMaxKeySize)
-        val valBuff = ByteBuffer.allocateDirect(encoded.length)
-        ignore { keyBuff.put(key.bytes).flip() }
-        ignore { valBuff.put(encoded).flip() }
+        val encodedKey   = sa.encode(key)
+        val encodedValue = sb.encode(value)
+        val keyBuff      = ByteBuffer.allocateDirect(env.getMaxKeySize)
+        val valBuff      = ByteBuffer.allocateDirect(encodedValue.length)
+        ignore { keyBuff.put(encodedKey).flip() }
+        ignore { valBuff.put(encodedValue).flip() }
         db.put(keyBuff, valBuff)
       }
       .leftMap(StorageError.apply)
 
-  def get[A](key: Key)(implicit s: Serialize[A]): Either[Error, A] =
+  def get(key: K): Either[Error, V] =
     Either
-      .catchNonFatal {
-        val keyBuff = ByteBuffer.allocateDirect(env.getMaxKeySize)
-        ignore { keyBuff.put(key.bytes).flip() }
+      .catchNonFatal[Either[Error, V]] {
+        val encodedKey = sa.encode(key)
+        val keyBuff    = ByteBuffer.allocateDirect(env.getMaxKeySize)
+        ignore { keyBuff.put(encodedKey).flip() }
         withResource(env.txnRead()) { (txn: Txn[ByteBuffer]) =>
           if (db.get(txn, keyBuff) != null) {
             val fetchedBuff = txn.`val`()
             val fetched     = new Array[Byte](fetchedBuff.remaining())
             ignore { fetchedBuff.get(fetched) }
-            s.decode(fetched)
+            sb.decode(fetched)
           } else {
-            Left[Error, A](NotFound)
+            Left[Error, V](NotFound)
           }
         }
       }
       .leftMap(StorageError.apply)
       .joinRight
 
-  def remove(key: Key): Either[Error, Boolean] =
+  def remove(key: K): Either[Error, Boolean] =
     Either
       .catchNonFatal {
-        val keyBuff = ByteBuffer.allocateDirect(env.getMaxKeySize)
-        ignore { keyBuff.put(key.bytes).flip() }
+        val encodedKey = sa.encode(key)
+        val keyBuff    = ByteBuffer.allocateDirect(env.getMaxKeySize)
+        ignore { keyBuff.put(encodedKey).flip() }
         db.delete(keyBuff)
       }
       .leftMap(StorageError.apply)
@@ -67,10 +71,11 @@ object Storage {
     * @param name Name of the database
     * @param mapSize Maximum size of the database, in bytes
     */
-  def create(path: Path, name: String, mapSize: Long): Storage = {
+  def create[K, V](path: Path, name: String, mapSize: Long)(implicit sk: Serialize[K],
+                                                            sv: Serialize[V]): Storage[K, V] = {
     val env: Env[ByteBuffer] =
       Env.create().setMapSize(mapSize).setMaxDbs(1).open(path.toFile)
     val db: Dbi[ByteBuffer] = env.openDbi(name, MDB_CREATE)
-    new Storage(env, db)
+    new Storage[K, V](env, db)
   }
 }
