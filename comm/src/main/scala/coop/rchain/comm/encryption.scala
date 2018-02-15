895a3817d690aad300c3b2b66c46c696050080bc
Pawel Szulc <paul.szulc@gmail.com>
2018-02-15 11:49:20 +0100

    Extract to32ByteArr as hashIt, introduce sign, rename object to encryption

--- comm/src/main/scala/coop/rchain/comm/encryption.scala
+++ comm/src/main/scala/coop/rchain/comm/encryption.scala
@@ -1,28 +1,30 @@
 package coop.rchain.comm
 
 import coop.rchain.catscontrib.Capture
+import com.roundeights.hasher.Implicits._
+import com.google.common.io.BaseEncoding
 
 import cats._, cats.data._, cats.implicits._
-import monix.eval.Task
 
 final case class PublicPrivateKeys(pub: Array[Byte], priv: Array[Byte])
 
-object PublicPrivateKeys {
+object encryption {
 
-  /** TODO this should use finctionality from cryptography subproject
+  /** FIX-ME this should use finctionality from cryptography subproject
     * once it is here, see https://github.com/rchain/rchain/pull/247/filescode
     * for detail
     */
-  def generate: PublicPrivateKeys = {
+  def generate: PublicPrivateKeys =
+    PublicPrivateKeys(hashIt("public"), hashIt("private"))
 
-    import com.roundeights.hasher.Implicits._
-    import com.google.common.io.BaseEncoding
+  /** FIX-ME This will sign any hashed 32 byte message */
+  /** TODO secp256k1 requires message to be 32 bytes, this should be checked and
+    * visible in type signature
+    */
+  def sign(priv: Array[Byte], hash: Array[Byte]): Array[Byte] = hash
 
-    val to32ByteArr: String => Array[Byte] =
-      (str: String) => BaseEncoding.base16().decode(str.sha256)
-
-    PublicPrivateKeys(to32ByteArr("public"), to32ByteArr("private"))
-  }
+  val hashIt: String => Array[Byte] = (str: String) =>
+    BaseEncoding.base16().lowerCase.decode(str.sha256.hex)
 
   /** FIX-ME this should store public and private keys on disk or internal DB */
   def storeKeys[F[_]: Capture: Applicative](keys: PublicPrivateKeys): F[Unit] = ().pure[F]
