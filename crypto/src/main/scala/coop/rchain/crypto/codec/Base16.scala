9b3b758352c35aa0f1ef7d396bacc3b767dd460b
Eitan Chatav <eitan.chatav@gmail.com>
2018-02-15 19:28:11 -0800

    cleaning

--- crypto/src/main/scala/coop/rchain/crypto/codec/Base16.scala
+++ crypto/src/main/scala/coop/rchain/crypto/codec/Base16.scala
@@ -8,7 +8,7 @@ object Base16 {
   private def bytes2hex(bytes: Array[Byte], sep: Option[String]): String =
     bytes.map("%02x".format(_)).mkString(sep.getOrElse(""))
 
-  private def hex2bytes(hex: String): Array[Byte] = {
-    hex.replaceAll("[^0-9A-Fa-f]", "").sliding(2, 2).toArray.map(Integer.parseInt(_, 16).toByte)
-  }
+  private def hex2bytes(hex: String): Array[Byte] =
+    hex.replaceAll("[^0-9A-Fa-f]", "").sliding(2, 2)
+      .toArray.map(Integer.parseInt(_, 16).toByte)
 }
