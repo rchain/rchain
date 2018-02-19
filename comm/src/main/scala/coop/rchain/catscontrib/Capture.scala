ec82010d07a9eb4aad2dc2e559bf2032f7a24c64
Pawel Szulc <paul.szulc@gmail.com>
2018-02-19 11:28:02 +0100

    Fix missing partial application
    Because Scala is not Haskell

--- comm/src/main/scala/coop/rchain/catscontrib/Capture.scala
+++ comm/src/main/scala/coop/rchain/catscontrib/Capture.scala
@@ -15,7 +15,7 @@ trait Capture[F[_]] {
   def capture[A](a: => A): F[A]
 
   /** Alias for `capture`. */
-  def apply[A] = capture[A]
+  def apply[A] = capture[A] _
 
   /** Construct a failed computation described by the given `Throwable`. */
   @SuppressWarnings(Array("org.wartremover.warts.Throw"))
