e1c681e52ccb6a89f109a9b5214ab58a8af6a6d9
Pawel Szulc <paul.szulc@gmail.com>
2018-02-20 15:24:00 +0100

    Add forever to monad operations

--- comm/src/main/scala/coop/rchain/catscontrib/monadOps.scala
+++ comm/src/main/scala/coop/rchain/catscontrib/monadOps.scala
@@ -1,9 +1,16 @@
 package coop.rchain.catscontrib
 
+import ski._
 import cats._, cats.data._, cats.implicits._
 
 final class MonadOps[F[_], A](val self: F[A])(implicit val F: Monad[F]) {
   def liftM[G[_[_], _]](implicit G: MonadTrans[G]): G[F, A] = G.liftM(self)
+  def forever: F[A]                                         = MonadOps.forever(self)
+}
+
+object MonadOps {
+  def forever[F[_]: Monad, A](fa: F[A]): F[A] =
+    fa >>= κ(forever(fa))
 }
 
 trait ToMonadOps {
