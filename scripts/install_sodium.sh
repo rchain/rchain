6d1d74658b29971f2c6402918ffc1191e4ac3add
Eitan Chatav <eitan.chatav@gmail.com>
2018-02-21 09:32:40 -0800

    asdf

--- scripts/install_sodium.sh
+++ scripts/install_sodium.sh
@@ -2,12 +2,8 @@
 
 set -e
 
-if [ ! -d "$HOME/libsodium/lib" ]; then
-    git clone https://github.com/jedisct1/libsodium.git --branch=stable
-    cd libsodium
-    ./configure --prefix=$HOME/libsodium
-    make
-    make install
-else
-    echo 'Using cached directory.'
-fi
+cd crypto
+git clone https://github.com/jedisct1/libsodium.git
+cd libsodium
+./configure --prefix=$HOME/libsodium
+make
