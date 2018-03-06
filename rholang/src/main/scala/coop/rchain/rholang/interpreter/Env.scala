d95729a157025d0497c6d216df3fd880ca484a14
JosDenmark <josephrileydenman@gmail.com>
2018-03-06 15:14:35 -0500

    Renamed Env[Par] in DeBruijn to HashMap[Int,Par]

--- rholang/src/main/scala/coop/rchain/rholang/interpreter/Env.scala
+++ rholang/src/main/scala/coop/rchain/rholang/interpreter/Env.scala
@@ -1,32 +1,33 @@
 package coop.rchain.rholang.interpreter
 
-import monix.eval.Task
-
 import scala.collection.immutable.HashMap
+
+/* Env reifies the Env[A] = HashMap[Int,A] type alias.
+   It extends HashMap with functions that manipulate
+   the map based on level indices. */
 
 object Env {
 
   def empty = HashMap.empty[Int, Par]
 
-  implicit class DeBruijn(env: Env[Par]) {
+  implicit class DeBruijn(env: HashMap[Int,Par]) {
 
     def get(level: Int): Option[Par] = env.get(level)
 
-    def put(level: Int)(data: Par): Env[Par] = env + (level -> data)
+    def put(level: Int)(data: Par): HashMap[Int,Par] = env + (level -> data)
 
     /* Since indexing starts from 0, # of bound variables = level of
-    environment associated with the term.*/
+       environment associated with the term.*/
     def level: Int = env.keys.max + 1
 
-    def rename(j: Int): Env[Par] = env map { case (k, data) => (k + j, data) }
+    def rename(j: Int): HashMap[Int,Par] = env map { case (k, data) => (k + j, data) }
 
-    /* In [env] allocate [k.size] new slots */
-    def alloc(k: List[Par]): Env[Par] = (env /: k) { (_env, data) =>
-      _env.put(_env.level)(data)
-    }
+      /* In [env] allocate [k.size] new slots */
+    def alloc(k: List[Par]): HashMap[Int,Par] = (env /: k) { (_env, data) =>
+        _env.put(_env.level)(data)
+      }
 
-    def merge(_env: Env[Par]): Env[Par] = env ++: _env.rename(env.level)
+    def merge(_env: HashMap[Int,Par]): HashMap[Int,Par] = env ++: _env.rename(env.level)
 
   }
-
 }
