c536dfeb00e8d30f43c0720940357beba5c995e7
JosDenmark <josephrileydenman@gmail.com>
2018-03-07 14:54:15 -0500

    Tests for Environment

--- rholang/src/test/scala/coop/rchain/rholang/interpreter/SubstituteTest.scala
+++ rholang/src/test/scala/coop/rchain/rholang/interpreter/SubstituteTest.scala
@@ -2,18 +2,17 @@ package coop.rchain.rholang.interpreter
 
 import org.scalatest.{FlatSpec, Matchers}
 import Substitute._
-import scala.collection.immutable.HashMap
 
 class VarSubSpec extends FlatSpec with Matchers {
 
   "FreeVar" should "throw an error" in {
-    val env = HashMap(0 -> Par(GPrivate()))
+    val env = Env(Par(GPrivate()))
     an[Error] should be thrownBy subOrDec(FreeVar(0))(env)
   }
 
   "BoundVar" should "be substituted for process" in {
     val source = Par(GPrivate())
-    val env    = HashMap(0 -> source)
+    val env    = Env(source)
     val result = subOrDec(BoundVar(0))(env)
     result should be(Right(source))
   }
@@ -21,18 +20,19 @@ class VarSubSpec extends FlatSpec with Matchers {
   "BoundVar" should "be substituted with renamed expression" in {
     val source  = Par(Send(ChanVar(BoundVar(0)), List(Par()), false, 0))
     val _source = Par(Send(ChanVar(BoundVar(1)), List(Par()), false, 0))
-    val env     = HashMap(0 -> source)
+    val env     = Env(source)
     val result  = subOrDec(BoundVar(0))(env)
     result should be(Right(_source))
   }
 
   "BoundVar" should "throw an error" in {
-    val env = HashMap(1 -> Par(GPrivate()))
+    val env = Env[Par](1 -> Par(GPrivate()))
+    println(env)
     an[Error] should be thrownBy subOrDec(BoundVar(0))(env)
   }
 
   "BoundVar" should "be decremented by environment level" in {
-    val env    = HashMap(0 -> Par(GPrivate()), 1 -> Par(GPrivate()))
+    val env    = Env(Par(GPrivate()), Par(GPrivate()))
     val result = subOrDec(BoundVar(2))(env)
     result should be(Left(BoundVar(0)))
   }
@@ -41,13 +41,13 @@ class VarSubSpec extends FlatSpec with Matchers {
 class ChannelSubSpec extends FlatSpec with Matchers {
 
   "ChanVar" should "be decremented by environment level" in {
-    val env    = HashMap(0 -> Par(GPrivate()), 1 -> Par(GPrivate()))
+    val env    = Env(Par(GPrivate()), Par(GPrivate()))
     val result = substitute(ChanVar(BoundVar(2)))(env)
     result should be(ChanVar(BoundVar(0)))
   }
 
   "Quote" should "substitute quoted process" in {
-    val env    = HashMap(0 -> Par(GPrivate()))
+    val env    = Env(Par(GPrivate()))
     val par    = Par(Send(ChanVar(BoundVar(1)), List(Par()), false, 0))
     val target = Quote(par)
     val result = substitute(target)(env)
@@ -56,7 +56,7 @@ class ChannelSubSpec extends FlatSpec with Matchers {
 
   "Channel" should "be substituted for a Quote" in {
     val source = Par(GPrivate())
-    val env    = HashMap(0 -> source)
+    val env    = Env(source)
     val result = substitute(ChanVar(BoundVar(0)))(env)
     result should be(Quote(source))
   }
@@ -65,15 +65,14 @@ class ChannelSubSpec extends FlatSpec with Matchers {
 class SendSubSpec extends FlatSpec with Matchers {
 
   "Send" should "decrement subject Channel and substitute object processes" in {
-    val env    = HashMap(0 -> Par(GPrivate()), 1 -> Par(GPrivate()))
+    val env    = Env(Par(GPrivate()), Par(GPrivate()))
     val result = substitute(Send(ChanVar(BoundVar(2)), List(Par()), false, 0))(env)
     result should be(Send(ChanVar(BoundVar(0)), List(Par()), false, 0))
   }
 
   "Send" should "substitute Channel for Quote" in {
     val source0 = Par(GPrivate())
-    val source1 = Par(GPrivate())
-    val env     = HashMap(0 -> source0, 1 -> source1)
+    val env     = Env(source0, Par(GPrivate()))
     val result = substitute(
       Send(Quote(Send(ChanVar(BoundVar(0)), List(Par()), false, 0)), List(Par()), false, 0))(env)
     result should be(
@@ -94,7 +93,7 @@ class SendSubSpec extends FlatSpec with Matchers {
                       0)
     val _target =
       Send(Quote(source), List(Par(Send(Quote(source), List(Par()), false, 0))), false, 0)
-    val env    = HashMap(0 -> source)
+    val env    = Env(source)
     val result = substitute(target)(env)
     result should be(_target)
   }
@@ -104,7 +103,7 @@ class SendSubSpec extends FlatSpec with Matchers {
     val chan1  = ChanVar(BoundVar(1))
     val source = Par(Send(chan0, List(Par()), false, 0))
     val target = Send(chan0, List(Par(Send(chan0, List(Par()), false, 0))), false, 0)
-    val env    = HashMap(0 -> source)
+    val env    = Env(source)
     val result = substitute(target)(env)
     result should be(
       Send(
@@ -130,7 +129,7 @@ class NewSubSpec extends FlatSpec with Matchers {
 
   "New" should "only substitute body of expression" in {
     val source = Par(GPrivate())
-    val env    = HashMap(0 -> source)
+    val env    = Env(source)
     val target = New(1, Par(Send(ChanVar(BoundVar(0)), List(Par()), false, 0)))
     val result = substitute(target)(env)
     result should be(
@@ -141,7 +140,7 @@ class NewSubSpec extends FlatSpec with Matchers {
   "New" should "only substitute all Channels in body of express" in {
     val source0 = Par(GPrivate())
     val source1 = Par(GPrivate())
-    val env     = HashMap(0 -> source0, 1 -> source1)
+    val env     = Env(source0, source1)
     val target = New(2,
                      Par(
                        Send(ChanVar(BoundVar(0)),
