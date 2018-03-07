b307b16f42cacd800434898a1b8d21bfc084d285
Jeremy Busk <jeremybusk@gmail.com>
2018-03-07 16:32:15 -0700

    removed exmaple scripts

--- docker/node/add-metric-to-pushgateway.sh
+++
@@ -1,3 +0,0 @@
-echo "some_metric 10" | curl --data-binary @- http://127.0.0.1:9091/metrics/job/job1
-echo "some_metric_2 30" | curl --data-binary @- http://127.0.0.1:9091/metrics/job/job2
-curl http://127.0.0.1:9091/metrics
