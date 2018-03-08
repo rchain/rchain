d04f2464bfeb51fc4b5f94d8e98576add8617fb5
Pawel Szulc <paul.szulc@gmail.com>
2018-03-08 16:01:38 +0100

    Add Effect instances for tests (stubs)

---
+++ comm/src/test/scala/coop/rchain/p2p/EffectsTestInstances.scala
@@ -0,0 +1,52 @@
+package coop.rchain.p2p
+
+import coop.rchain.comm._, CommError._
+import scala.concurrent.duration.{Duration, MILLISECONDS}
+import coop.rchain.catscontrib._
+import cats._, cats.data._, cats.implicits._
+
+/** Eagerly evaluated instances to do reasoning about applied effects */
+object EffectsTestInstances {
+
+  class LogicalTime[F[_]: Capture] extends Time[F] {
+    var clock: Long = 0
+
+    def currentMillis: F[Long] = Capture[F].capture {
+      this.clock = clock + 1
+      clock
+    }
+  }
+
+  class CommunicationStub[F[_]: Capture: Applicative](src: ProtocolNode) extends Communication[F] {
+
+    type Responses = ProtocolNode => (ProtocolMessage => CommErr[ProtocolMessage])
+    var reqresp: Option[Responses] = None
+
+    def setResponses(responses: Responses): Unit =
+      reqresp = Some(responses)
+
+    def roundTrip(msg: ProtocolMessage,
+                  remote: ProtocolNode,
+                  timeout: Duration = Duration(500, MILLISECONDS)): F[CommErr[ProtocolMessage]] =
+      Capture[F].capture {
+        reqresp.get.apply(remote).apply(msg)
+      }
+
+    def local: F[ProtocolNode]                                      = src.pure[F]
+    def commSend(data: Seq[Byte], peer: PeerNode): F[CommErr[Unit]] = ???
+    def addNode(node: PeerNode): F[Unit]                            = ???
+    def broadcast(msg: ProtocolMessage): F[Seq[CommErr[Unit]]]      = ???
+    def findMorePeers(limit: Int): F[Seq[PeerNode]]                 = ???
+    def countPeers: F[Int]                                          = ???
+    def receiver: F[Unit]                                           = ???
+  }
+
+  import Encryption._
+
+  class EncryptionStub[F[_]: Applicative](keys: PublicPrivateKeys, nonce: Nonce)
+      extends Encryption[F] {
+    def fetchKeys: F[PublicPrivateKeys] = keys.pure[F]
+    def generateNonce: F[Nonce]         = nonce.pure[F]
+  }
+
+}
