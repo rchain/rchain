72f49bf4e7de117daa1f561625185ac604db2491
Kyle Butt <kyle@pyrofex.net>
2018-03-14 23:16:45 -0600

    Rholang: Substitute: Use Debruijn Indices.
    Debruijn indices for substitution don't require re-naming. It does require
    actually creating an environment class. We need to be able to shift the keys of
    the map in 2 different cases. The first is when we add something to the
    environment. The second is as we substitute and move inside of a binding
    construct.

--- rholang/src/main/scala/coop/rchain/rholang/interpreter/Env.scala
+++ rholang/src/main/scala/coop/rchain/rholang/interpreter/Env.scala
@@ -6,6 +6,7 @@ import scala.collection.mutable
    It extends HashMap with functions that manipulate
    the map based on level indices. */
 
+/*
 object Env {
 
   type Env[A] = mutable.LinkedHashMap[Int, A]
@@ -36,5 +37,53 @@ object Env {
     def merge(_env: Env[A]): Env[A] =
       env ++= _env.rename(env.level)
 
+  }
+}
+ */
+
+case class Env[A](val envMap: mutable.LinkedHashMap[Int, A], var level: Int, var shift: Int) {
+  def this() = this(mutable.LinkedHashMap.empty[Int, A], 0, 0)
+  def put(a: A): Unit = {
+    envMap += (level -> a)
+    level += 1
+  }
+
+  def put(a: A, b: A, k: A*): Unit = {
+    put(a)
+    put(b)
+    for {
+      newVal <- k
+    } put(newVal)
+  }
+
+  // Still slightly uncertain about this.
+  def merge(_env: Env[A]): Unit = {
+    val renamed = _env.rename(level)
+    envMap ++= renamed.envMap
+    level = renamed.level
+    shift += _env.shift
+  }
+
+  def rename(j: Int): Env[A] =
+    Env(envMap = envMap map {
+      case (k, data) => (k + j, data)
+    }, level = level + j, shift = shift)
+
+  def get(k: Int): Option[A] =
+    envMap.get((level + shift) - k - 1)
+
+  def shift(j: Int): Unit =
+    shift += j
+}
+
+object Env {
+  def apply[A]() = new Env[A]()
+
+  def makeEnv[A](k: A*): Env[A] = {
+    val result = Env[A]()
+    for {
+      newVal <- k
+    } result.put(newVal)
+    result
   }
 }
--- rholang/src/test/scala/coop/rchain/rholang/interpreter/EnvTest.scala
+++ rholang/src/test/scala/coop/rchain/rholang/interpreter/EnvTest.scala
@@ -1,8 +1,9 @@
 package coop.rchain.rholang.interpreter
 
 import org.scalatest.{FlatSpec, Matchers}
+import scala.collection.mutable
+
 import implicits._
-import Env._
 import coop.rchain.models.Par
 
 class EnvSpec extends FlatSpec with Matchers {
@@ -14,19 +15,31 @@ class EnvSpec extends FlatSpec with Matchers {
   val source4: Par = GPrivate()
 
   "Data" should "always be inserted at the next available level index" in {
-    val result: Env[Par] = Env(source0, source1, source2)
-    result should be(Env[Par](0 -> source0, 1 -> source1, 2 -> source2))
+    val result: Env[Par] = Env()
+    result.put(source0, source1, source2)
+    result should be(
+      Env[Par](mutable.LinkedHashMap(0 -> source0, 1 -> source1, 2 -> source2), 3, 0))
   }
 
   "Level indices" should "be incremented by value of rename argument" in {
-    val result: Env[Par] = Env(source0, source1, source2) rename 1
-    result should be(Env[Par](1 -> source0, 2 -> source1, 3 -> source2))
+    val srcMap: Env[Par] = Env()
+    srcMap.put(source0, source1, source2)
+    val result = srcMap.rename(1)
+    result should be(
+      Env[Par](mutable.LinkedHashMap(1 -> source0, 2 -> source1, 3 -> source2), 4, 0))
   }
 
   "that.Env indices" should "be incremented by level of this.Env indices" in {
-    val target1: Env[Par] = Env(source0, source1, source2)
-    val target2: Env[Par] = Env(source3, source4)
-    val result: Env[Par]  = target1 merge target2
-    result should be(Env[Par](0 -> source0, 1 -> source1, 2 -> source2, 3 -> source3, 4 -> source4))
+    val target1: Env[Par] = Env()
+    target1.put(source0, source1, source2)
+    val target2: Env[Par] = Env()
+    target2.put(source3, source4)
+    val result = target1
+    target1.merge(target2)
+    result should be(
+      Env[Par](
+        mutable.LinkedHashMap(0 -> source0, 1 -> source1, 2 -> source2, 3 -> source3, 4 -> source4),
+        5,
+        0))
   }
 }
