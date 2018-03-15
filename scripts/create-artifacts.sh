61a762b5d789dbfd84ba30af1904e21bca392f9f
Henry Till <henrytill@gmail.com>
2018-03-15 19:53:24 -0400

    scripts/create-artifacts.sh: do it clean

--- scripts/create-artifacts.sh
+++ scripts/create-artifacts.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 SBT_OPTS="${SBT_OPTS:-} -Dsbt.log.noformat=true"
 
-sbt ${SBT_OPTS} node/debian:packageBin node/rpm:packageBin
+sbt ${SBT_OPTS} clean node/debian:packageBin node/rpm:packageBin
