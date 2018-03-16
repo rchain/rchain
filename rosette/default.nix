df8bf4c136926da9a19e66ae37bcf75a1b8c4e9e
Henry Till <henrytill@gmail.com>
2018-03-16 14:01:29 -0400

    rosette: add default.nix

---
+++ rosette/default.nix
@@ -0,0 +1,22 @@
+{ nixpkgs ? import <nixpkgs> {} }:
+
+let
+
+  pkgs = nixpkgs.pkgs.pkgsi686Linux;
+
+in pkgs.stdenv.mkDerivation {
+
+  name = "rosette";
+
+  src = ./.;
+
+  nativeBuildInputs = with pkgs;
+    [ cmake
+      doxygen
+      pkgconfig
+    ];
+
+  buildInputs =
+    [ # deps go here
+    ];
+}
