9c5e5abffa56f176ccca0e343e8ecfc44be55adc
Pawel Szulc <paul.szulc@gmail.com>
2018-03-20 11:19:42 +0100

    Reformat crypto module with sclafmt

--- crypto/src/main/scala/coop/rchain/crypto/hash/Blake2b256.scala
+++ crypto/src/main/scala/coop/rchain/crypto/hash/Blake2b256.scala
@@ -5,15 +5,15 @@ import org.bouncycastle.crypto.digests.Blake2bDigest
 /**
 Blake2b256 hashing algorithm
 
- * {{{
- * >>> import coop.rchain.crypto.codec._
- *
- * >>> Base16.encode(Blake2b256.hash("".getBytes))
- * 0e5751c026e543b2e8ab2eb06099daa1d1e5df47778f7787faab45cdf12fe3a8
- *
- * >>> Base16.encode(Blake2b256.hash("abc".getBytes))
- * bddd813c634239723171ef3fee98579b94964e3bb1cb3e427262c8c068d52319
- * }}}
+  * {{{
+  * >>> import coop.rchain.crypto.codec._
+  *
+  * >>> Base16.encode(Blake2b256.hash("".getBytes))
+  * 0e5751c026e543b2e8ab2eb06099daa1d1e5df47778f7787faab45cdf12fe3a8
+  *
+  * >>> Base16.encode(Blake2b256.hash("abc".getBytes))
+  * bddd813c634239723171ef3fee98579b94964e3bb1cb3e427262c8c068d52319
+  * }}}
 **/
 object Blake2b256 {
 
--- crypto/src/main/scala/coop/rchain/crypto/hash/Keccak256.scala
+++ crypto/src/main/scala/coop/rchain/crypto/hash/Keccak256.scala
@@ -5,15 +5,15 @@ import org.bouncycastle.crypto.digests.KeccakDigest
 /**
 Keccak256 hashing algorithm
 
- * {{{
- * >>> import coop.rchain.crypto.codec._
- *
- * >>> Base16.encode(Keccak256.hash("".getBytes))
- * c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470
- *
- * >>> Base16.encode(Keccak256.hash("abc".getBytes))
- * 4e03657aea45a94fc7d47ba826c8d667c0d1e6e33a64a036ec44f58fa12d6c45
- * }}}
+  * {{{
+  * >>> import coop.rchain.crypto.codec._
+  *
+  * >>> Base16.encode(Keccak256.hash("".getBytes))
+  * c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470
+  *
+  * >>> Base16.encode(Keccak256.hash("abc".getBytes))
+  * 4e03657aea45a94fc7d47ba826c8d667c0d1e6e33a64a036ec44f58fa12d6c45
+  * }}}
 **/
 object Keccak256 {
 
