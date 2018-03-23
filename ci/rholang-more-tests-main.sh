a94823920570bceed33e5aa0b494ef5d640f4426
Jeremy Busk <jeremybusk@gmail.com>
2018-03-23 11:51:19 -0600

    removed uneeded script

--- ci/rholang-more-tests-main.sh
+++ ci/rholang-more-tests-main.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 source ci/header.sh
 
-# ${project_root_dir}/ci/prep-ubuntu-with-deps.sh
 ${project_root_dir}/ci/build-rosette.sh
 ${project_root_dir}/ci/build-rhoscala-rbls.sh
 ${project_root_dir}/ci/test-rhoscala-rbls.sh
