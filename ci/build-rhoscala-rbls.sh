1df4becd1da63f3d1cb949fa05c960ad1fe2c597
Jeremy Busk <jeremybusk@gmail.com>
2018-03-22 18:36:38 -0600

    fixed sbt build

--- ci/build-rhoscala-rbls.sh
+++ ci/build-rhoscala-rbls.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 source ci/header.sh
-sbt -Dsbt.log.noformat=true clean bnfc:generate
-sbt -Dsbt.log.noformat=true <<EOL
-rholang/compile
-rholang/assembly 
-exit
-EOL
 
+sbt -Dsbt.log.noformat=true clean bnfc:generate
+sbt -Dsbt.log.noformat=true rholang/compile 
+sbt -Dsbt.log.noformat=true rholang/assembly 
 
 jar=$(ls -t ${rholang_root_dir}/target/scala*/*.jar | head -1)
 # the above is usually file like rholang/target/scala-2.12/rholang-assembly-0.1.0-SNAPSHOT.jar
--- ci/rhoscala-additional-tests-main.sh
+++ ci/rhoscala-additional-tests-main.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 source ci/header.sh
 
-sbt -Dsbt.log.noformat=true clean bnfc:generate coverage test coverageReport rpm:packageBin debian:packageBin
 # ${project_root_dir}/ci/prep-ubuntu-with-deps.sh
 ${project_root_dir}/ci/build-rosette.sh
 ${project_root_dir}/ci/build-rhoscala-rbls.sh
