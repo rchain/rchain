c95fc0208215fea98329ea42c677c6ac69492650
Jeremy Busk <jeremybusk@gmail.com>
2018-03-23 15:10:44 -0600

    removed rbl removal lines. Handled by clean-up after all rbl use needs have been met.

--- ci/build-rhoscala-rbls.sh
+++ ci/build-rhoscala-rbls.sh
@@ -12,17 +12,14 @@ jar=$(ls -t ${rholang_root_dir}/target/scala*/*.jar | head -1)
 for rho_file in $(ls ${rholang_root_dir}/tests/*.rho); do
     rbl_file=$(echo ${rho_file} | cut -f 1 -d '.').rbl
     java -jar ${jar} ${rho_file}
-    #rm ${rbl_file}
 done
 
 for rho_file in $(ls ${rholang_root_dir}/failure_tests/*.rho); do
     rbl_file=$(echo ${rho_file} | cut -f 1 -d '.').rbl
 if ! java -jar ${jar} ${rho_file} ; then
         echo "[success] with ${rho_file} failure test"
-        rm -f ${rbl_file}
     else
         echo "[error] Test failure. Failure test returned true."
-        rm -f ${rbl_file}
         exit 1
     fi
 done
