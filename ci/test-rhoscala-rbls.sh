2ec8d152e74f022b321989f3f3e99bd5a061ceab
Jeremy Busk <jeremybusk@gmail.com>
2018-03-22 23:15:47 -0600

    fix rbl tests

--- ci/test-rhoscala-rbls.sh
+++ ci/test-rhoscala-rbls.sh
@@ -2,17 +2,17 @@
 source ci/header.sh
 
 for rbl_file in $(ls ${rholang_root_dir}/tests/*.rbl); do
- [[ $( ulimit -s unlimited && export ESS_SYSDIR=${rosette_root_dir}/rbl/rosette && ${rosette_root_dir}/build.out/src/rosette --quiet --boot-dir=${rosette_root_dir}/rbl/rosette --boot=boot.rbl ${rbl_file} | grep ^Pass ) ]] && echo true || echo false
-    if [[ $out = "true" ]]; then
+    out=$([[ $( ulimit -s unlimited && export ESS_SYSDIR=${rosette_root_dir}/rbl/rosette && ${rosette_root_dir}/build.out/src/rosette --quiet --boot-dir=${rosette_root_dir}/rbl/rosette --boot=boot.rbl ${rbl_file} | grep ^Pass ) ]] && echo true || echo false)
+    if [[ ! $out = "true" ]]; then
         echo "[error] - rbl file ${rbl_file} did not return \"Pass\""
         exit 1
     fi
 done
 
 for rbl_file in $(ls ${rholang_root_dir}/failure_tests/*.rbl); do
- [[ $( ulimit -s unlimited && export ESS_SYSDIR=${rosette_root_dir}/rbl/rosette && ${rosette_root_dir}/build.out/src/rosette --quiet --boot-dir=${rosette_root_dir}/rbl/rosette --boot=boot.rbl ${rbl_file} | grep ^Pass ) ]] && echo true || echo false
+    out=$([[ $( ulimit -s unlimited && export ESS_SYSDIR=${rosette_root_dir}/rbl/rosette && ${rosette_root_dir}/build.out/src/rosette --quiet --boot-dir=${rosette_root_dir}/rbl/rosette --boot=boot.rbl ${rbl_file} | grep ^Pass ) ]] && echo true || echo false)
     if [[ $out = "true" ]]; then
-        echo "[error] - rbl file ${rbl_file} did not return \"Pass\""
+        echo "[error] - rbl failure test file ${rbl_file} returned \"Pass\""
         exit 1
     fi
 done
