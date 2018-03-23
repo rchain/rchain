a0f0ebf20946efede28fe9a1c7ae1b1892f30ce6
Henry Till <henrytill@gmail.com>
2018-03-23 16:36:58 -0400

    scripts/create-artifacts.sh: only create artifacts on tagged releases

--- scripts/create-artifacts.sh
+++ scripts/create-artifacts.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
-set -euo pipefail
+set -eo pipefail
 
-if [ $SUBPROJECT = "core" ]
+if [ "$SUBPROJECT" = "core" -a -n "$TRAVIS_TAG" ]
 then
     sbt -Dsbt.log.noformat=true clean node/debian:packageBin node/rpm:packageBin
 fi
