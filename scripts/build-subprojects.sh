f377865586e19cfb7aaa9eadafa5a5284f9d37ae
Henry Till <henrytill@gmail.com>
2018-03-24 13:53:41 -0400

    scripts/build-subprojects.sh: retab

--- scripts/build-subprojects.sh
+++ scripts/build-subprojects.sh
@@ -15,7 +15,7 @@ case "$SUBPROJECT" in
 
     for sub in crypto comm rholang roscala storage node
     do
-	    (bash <(curl -s https://codecov.io/bash) -X gcov -s ./$sub -c -F $sub)
+        (bash <(curl -s https://codecov.io/bash) -X gcov -s ./$sub -c -F $sub)
     done
     ;;
 
