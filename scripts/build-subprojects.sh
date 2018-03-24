c7e7ee9c9835195a4e6e6e1e0cbe2de0942e2c51
Henry Till <henrytill@gmail.com>
2018-03-23 20:54:55 -0400

    use Nix for building Rosette in CI

--- scripts/build-subprojects.sh
+++ scripts/build-subprojects.sh
@@ -1,18 +1,30 @@
-#!/bin/bash
+#! /usr/bin/env bash
 
 set -e
 
-if [ -d "${SUBPROJECT}" -a -f "${SUBPROJECT}/build.sh" ]; then
-    echo "${SUBPROJECT}/build.sh"
-    (cd "${SUBPROJECT}"; bash ./build.sh)
-elif [ "$SUBPROJECT" = "test_artifact_creation" ]; then
-    sbt -Dsbt.log.noformat=true clean bnfc:generate node/rpm:packageBin node/debian:packageBin
-elif [ -f "build.sbt" ]; then
+case "$SUBPROJECT" in
+
+"rosette")
+    cd rosette
+    nix-build
+    ./run.sh rbl/rosette/tests/simple_add.rbl
+    ;;
+
+"core")
     sbt -Dsbt.log.noformat=true clean bnfc:generate coverage test coverageReport
-    for sub in crypto comm rholang roscala storage node; do
-	(bash <(curl -s https://codecov.io/bash) -X gcov -s ./$sub -c -F $sub)
+
+    for sub in crypto comm rholang roscala storage node
+    do
+	    (bash <(curl -s https://codecov.io/bash) -X gcov -s ./$sub -c -F $sub)
     done
-else
-    echo "No build/test files found!"
+    ;;
+
+"test_artifact_creation")
+    sbt -Dsbt.log.noformat=true clean bnfc:generate node/rpm:packageBin node/debian:packageBin
+    ;;
+
+*)
+    echo "$SUBPROJECT build instructions are missing"
     exit 1
-fi
+
+esac
