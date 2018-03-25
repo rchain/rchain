4929e343f7f7b28b6b194c7747dd8eaefdb4c6e9
Jeremy Busk <jeremybusk@gmail.com>
2018-03-25 01:20:20 -0600

    fix rosette run so it will work in nix

--- rosette/run.sh
+++ rosette/run.sh
@@ -1,6 +1,9 @@
+#! /usr/bin/env bash
+
 # Increase stack limit so that Rosette can load all files
 ulimit -s unlimited
 
 # Execute Rosette instance
-export ESS_SYSDIR=rbl/rosette
-./build.out/src/rosette --quiet --interactive-repl --boot-dir=rbl/rosette --boot=boot.rbl $@
+export ESS_SYSDIR="$(readlink -f $(pwd)/result/lib/rosette)"
+
+./result/bin/rosette --verbose --boot-dir ${ESS_SYSDIR} --boot boot.rbl $@
