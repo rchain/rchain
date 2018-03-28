99396810b78e7ee8461d71e8fac7ff3a64bf2cac
Kyle Butt <kyle@pyrofex.net>
2018-03-27 18:25:46 -0600

    Node: Add a flag for evaluating rholang from a file.
    Add --eval option to the node so that evaluating a file is an option. Repl's
    are great, but it's nice to be able to save your work and keep trying.

--- node/src/main/scala/coop/rchain/node/main.scala
+++ node/src/main/scala/coop/rchain/node/main.scala
@@ -1,6 +1,7 @@
 package coop.rchain.node
 
 import org.rogach.scallop._
+import java.io.FileReader
 import java.util.UUID
 import java.net.{InetAddress, NetworkInterface}
 
@@ -30,11 +31,12 @@ import java.util.concurrent.TimeoutException
 
 import coop.rchain.models.{Channel, ListChannel, Par}
 import coop.rchain.rholang.interpreter._
+import coop.rchain.rholang.interpreter.Reduce.DebruijnInterpreter
 import coop.rchain.rholang.interpreter.RholangCLI.{lexer, normalizeTerm, parser}
 import coop.rchain.rholang.interpreter.storage.StoragePrinter
 import coop.rchain.rholang.syntax.rholang_mercury.Absyn.Proc
 import coop.rchain.rholang.syntax.rholang_mercury.{parser, Yylex}
-import coop.rchain.storage.{LMDBStore, Serialize}
+import coop.rchain.storage.{IStore, LMDBStore, Serialize}
 import kamon._
 
 import scala.annotation.tailrec
@@ -69,6 +71,8 @@ final case class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
   val repl = opt[Boolean](default = Some(false),
                           short = 'r',
                           descr = "Start node with REPL (but without P2P network)")
+  val eval = opt[String](default = None,
+                         descr = "Start node to evaluate rholang in file (but without P2P network)")
 
   verify()
 }
@@ -106,9 +110,21 @@ object Main {
     }
   }
 
+  private def reader(fileName: String): FileReader = new FileReader(fileName)
+  private def lexer(fileReader: Reader): Yylex     = new Yylex(fileReader)
+  private def parser(lexer: Yylex): parser         = new parser(lexer, lexer.getSymbolFactory())
+
   def main(args: Array[String]): Unit = {
     val conf = Conf(args)
-    conf.repl().fold(executeP2p(conf), executeREPL(conf))
+    conf.eval.toOption match {
+      case Some(fileName) => {
+        val source                  = reader(fileName)
+        val sortedTerm: Option[Par] = buildNormalizedTerm(source)
+        evaluate(sortedTerm.get)
+      }
+      case None =>
+        conf.repl().fold(executeP2p(conf), executeREPL(conf))
+    }
   }
 
   private def executeREPL(conf: Conf): Unit = {
@@ -116,58 +132,72 @@ object Main {
     repl
   }
 
+  private def evaluate(sortedTerm: Par): Unit = {
+    val persistentStore: LMDBStore[Channel, Seq[Channel], Seq[Channel], Par] = buildStore
+    val interp                                                               = Reduce.makeInterpreter(persistentStore)
+    evaluate(interp, persistentStore, sortedTerm)
+  }
+
   private def repl = {
-    implicit val serializer = Serialize.mkProtobufInstance(Channel)
-    implicit val serializer2 = new Serialize[Seq[Channel]] {
-
-      override def encode(a: Seq[Channel]): Array[Byte] =
-        ListChannel.toByteArray(ListChannel(a))
-
-      override def decode(bytes: Array[Byte]): Either[Throwable, Seq[Channel]] =
-        Either.catchNonFatal(ListChannel.parseFrom(bytes).channels.toList)
-    }
-
-    implicit val serializer3 = Serialize.mkProtobufInstance(Par)
-
-    val dbDir = Files.createTempDirectory("rchain-storage-test-")
-    val persistentStore =
-      LMDBStore.create[Channel, Seq[Channel], Seq[Channel], Par](dbDir, 1024 * 1024 * 1024)
-    val interp = Reduce.makeInterpreter(persistentStore)
+    val persistentStore: LMDBStore[Channel, Seq[Channel], Seq[Channel], Par] = buildStore
+    val interp                                                               = Reduce.makeInterpreter(persistentStore)
 
     for (ln <- Source.stdin.getLines) {
       if (ln.isEmpty) {
         print("> ")
       } else {
         val normalizedTerm = buildNormalizedTerm(new StringReader(ln)).get
-        val evaluatorTask = for {
-          _ <- printTask(normalizedTerm)
-          _ <- interp.inj(normalizedTerm)
-        } yield ()
-        val evaluatorFuture: CancelableFuture[Unit] = evaluatorTask.runAsync
-        @tailrec
-        def keepTrying(): Unit =
-          Await.ready(evaluatorFuture, 5.seconds).value match {
-            case Some(Success(_)) =>
-              println()
-              println("Storage Contents:")
-              StoragePrinter.prettyPrint(persistentStore)
-              print("\n> ")
-            case Some(Failure(e: TimeoutException)) =>
-              println("This is taking a long time. Feel free to ^C and quit.")
-              keepTrying()
-            case Some(Failure(e)) => throw e
-            case None             => throw new Error("Error: Future claimed to be ready, but value was None")
-          }
-        keepTrying()
+        evaluate(interp, persistentStore, normalizedTerm)
       }
     }
+  }
+
+  private def evaluate(interpreter: DebruijnInterpreter,
+                       store: IStore[Channel, Seq[Channel], Seq[Channel], Par],
+                       normalizedTerm: Par): Unit = {
+    val evaluatorTask = for {
+      _ <- printTask(normalizedTerm)
+      _ <- interpreter.inj(normalizedTerm)
+    } yield ()
+    val evaluatorFuture: CancelableFuture[Unit] = evaluatorTask.runAsync
+    keepTrying(evaluatorFuture, store)
   }
 
   private def printTask(normalizedTerm: Par): Task[Unit] =
     Task {
       print("Evaluating:\n")
       println(PrettyPrinter().buildString(normalizedTerm))
+      print("\n> ")
     }
+
+  @tailrec
+  private def keepTrying(evaluatorFuture: CancelableFuture[Unit],
+                         persistentStore: IStore[Channel, Seq[Channel], Seq[Channel], Par]): Unit =
+    Await.ready(evaluatorFuture, 5.seconds).value match {
+      case Some(Success(_)) =>
+        print("Storage Contents:\n")
+        StoragePrinter.prettyPrint(persistentStore)
+        print("\n> ")
+      case Some(Failure(e: TimeoutException)) =>
+        println("This is taking a long time. Feel free to ^C and quit.")
+        keepTrying(evaluatorFuture, persistentStore)
+      case Some(Failure(e)) => throw e
+      case None             => throw new Error("Error: Future claimed to be ready, but value was None")
+    }
+  private def buildStore = {
+    implicit val serializer = Serialize.mkProtobufInstance(Channel)
+    implicit val serializer2 = new Serialize[Seq[Channel]] {
+      override def encode(a: Seq[Channel]): Array[Byte] =
+        ListChannel.toByteArray(ListChannel(a))
+
+      override def decode(bytes: Array[Byte]): Either[Throwable, Seq[Channel]] =
+        Either.catchNonFatal(ListChannel.parseFrom(bytes).channels.toList)
+    }
+    implicit val serializer3 = Serialize.mkProtobufInstance(Par)
+
+    val dbDir = Files.createTempDirectory("rchain-storage-test-")
+    LMDBStore.create[Channel, Seq[Channel], Seq[Channel], Par](dbDir, 1024 * 1024 * 1024)
+  }
 
   private def buildNormalizedTerm(source: Reader): Option[Par] = {
     val term = buildAST(source)
@@ -182,9 +212,6 @@ object Main {
     val ast = parser(lxr)
     ast.pProc()
   }
-
-  private def lexer(fileReader: Reader): Yylex = new Yylex(fileReader)
-  private def parser(lexer: Yylex): parser     = new parser(lexer, lexer.getSymbolFactory())
 
   private def normalizeTerm(term: Proc, inputs: ProcVisitInputs) = {
     val normalizedTerm = ProcNormalizeMatcher.normalizeMatch(term, inputs)
