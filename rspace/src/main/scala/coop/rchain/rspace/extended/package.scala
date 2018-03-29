2bcb635998bdb27f585b5d9a97bcb107922a7181
Henry Till <henrytill@gmail.com>
2018-03-29 01:58:48 -0400

    rspace: remove unused functions

--- rspace/src/main/scala/coop/rchain/rspace/extended/package.scala
+++ rspace/src/main/scala/coop/rchain/rspace/extended/package.scala
@@ -16,48 +16,4 @@ package object extended {
     */
   def runKs[T](t: List[Option[(Function1[T, Unit], T)]]): Unit =
     t.foreach { case Some((k, data)) => k(data); case None => () }
-
-  /** Consume all
-    */
-  def consumeAll[C, P, A, K](
-      store: IStore[C, P, A, K],
-      channels: List[C],
-      patterns: List[P],
-      continuation: K)(implicit m: Match[P, A]): List[Option[(K, List[A])]] = {
-    @tailrec
-    def loop(acc: List[Option[(K, List[A])]]): List[Option[(K, List[A])]] =
-      consume(store, channels, patterns, continuation, persist = false) match {
-        case r @ Some(_) => loop(r :: acc)
-        case None        => acc
-      }
-    loop(List.empty[Option[(K, List[A])]])
-  }
-
-  /** Persistent consume
-    */
-  def pconsume[C, P, A, K](store: IStore[C, P, A, K],
-                           channels: List[C],
-                           patterns: List[P],
-                           continuation: K)(implicit m: Match[P, A]): List[(K, List[A])] = {
-    @tailrec
-    def loop(acc: List[(K, List[A])]): List[(K, List[A])] =
-      consume(store, channels, patterns, continuation, persist = true) match {
-        case Some(res) => loop(res :: acc)
-        case None      => acc
-      }
-    loop(List.empty[(K, List[A])])
-  }
-
-  /** Persistent produce
-    */
-  def pproduce[C, P, A, K](store: IStore[C, P, A, K], channel: C, data: A)(
-      implicit m: Match[P, A]): List[(K, List[A])] = {
-    @tailrec
-    def loop(acc: List[(K, List[A])]): List[(K, List[A])] =
-      produce(store, channel, data, persist = true) match {
-        case Some(res) => loop(res :: acc)
-        case None      => acc
-      }
-    loop(List.empty[(K, List[A])])
-  }
 }
