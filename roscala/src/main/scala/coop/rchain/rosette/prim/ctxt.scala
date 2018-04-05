a894a1eb02eced80635f00f2644be926c6fd3841
Timm Schäuble <timm.schaeuble@gmail.com>
2018-04-05 19:10:51 +0200

    Change pattern match to mapN

--- roscala/src/main/scala/coop/rchain/rosette/prim/ctxt.scala
+++ roscala/src/main/scala/coop/rchain/rosette/prim/ctxt.scala
@@ -18,16 +18,13 @@ object ctxt {
         optArg0 = ctxt.arg(0).map(_.asInstanceOf[Ctxt])
         optArg1 = ctxt.arg(1)
 
-        result <- (optArg0, optArg1) match {
-                   // Both ARG(0) and ARG(1) are available
-                   case (Some(arg0), Some(arg1)) =>
-                     for (returnRes <- returnResultToCtxt(arg0, arg1))
-                       yield Right[PrimError, Ob](if (returnRes) Ob.INVALID else Ob.NIV)
-
-                   case _ =>
-                     pureCtxt[PrimResult](Left(TypeMismatch(0, Ctxt.getClass.getName)))
-                 }
-
+        result <- (optArg0, optArg1)
+                   .mapN(
+                     // Both ARG(0) and ARG(1) are available
+                     (arg0, arg1) =>
+                       for (returnRes <- returnResultToCtxt(arg0, arg1))
+                         yield Right[PrimError, Ob](if (returnRes) Ob.INVALID else Ob.NIV))
+                   .getOrElse(pureCtxt[PrimResult](Left(TypeMismatch(0, Ctxt.getClass.getName))))
       } yield result
 
     private def returnResultToCtxt(k: Ctxt, result: Ob): CtxtTransition[Boolean] =
