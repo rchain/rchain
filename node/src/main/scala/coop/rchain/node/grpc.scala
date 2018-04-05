74fa5ca88ab2a1b96ee5a545fdbf193b6920e007
Pawel Szulc <paul.szulc@gmail.com>
2018-04-05 09:04:11 +0200

    Provide real implementation of Repl gRPC service

--- node/src/main/scala/coop/rchain/node/grpc.scala
+++ node/src/main/scala/coop/rchain/node/grpc.scala
@@ -4,7 +4,11 @@ import io.grpc.{Server, ServerBuilder}
 import scala.concurrent.{ExecutionContext, Future}
 import cats._, cats.data._, cats.implicits._
 import coop.rchain.node.repl._
-import coop.rchain.rholang.interpreter.Runtime
+import coop.rchain.rholang.interpreter.{RholangCLI, Runtime}
+import coop.rchain.rholang.interpreter.storage.StoragePrinter
+import monix.execution.Scheduler
+
+import java.io.StringReader
 
 class GrpcServer(executionContext: ExecutionContext, port: Int, runtime: Runtime) { self =>
 
@@ -13,7 +17,7 @@ class GrpcServer(executionContext: ExecutionContext, port: Int, runtime: Runtime
   def start(): Unit = {
     server = ServerBuilder
       .forPort(port)
-      .addService(ReplGrpc.bindService(new ReplImpl, executionContext))
+      .addService(ReplGrpc.bindService(new ReplImpl(runtime), executionContext))
       .build
       .start
       .some
@@ -29,9 +33,28 @@ class GrpcServer(executionContext: ExecutionContext, port: Int, runtime: Runtime
   def stop(): Unit =
     server.foreach(_.shutdown())
 
-  class ReplImpl extends ReplGrpc.Repl {
-    def run(request: ReplRequest): Future[ReplResponse] =
-      Future.successful(ReplResponse(s"running ${request.line}"))
+  class ReplImpl(runtime: Runtime) extends ReplGrpc.Repl {
+    import RholangCLI._
+    // TODO we need to handle this better
+    import monix.execution.Scheduler.Implicits.global
+
+    def run(request: ReplRequest): Future[ReplResponse] = {
+      val line           = request.line
+      val normalizedTerm = buildNormalizedTerm(new StringReader(line))
+
+      normalizedTerm match {
+        case None => Future.successful(ReplResponse(s"Could not build a term from '$line'"))
+        case Some(term) =>
+          evaluator(runtime.reducer, term).attempt
+            .map {
+              case Left(ex) => s"Caught boxed exception: $ex"
+              case Right(_) => s"Storage Contents:\n ${StoragePrinter.prettyPrint(runtime.store)}"
+            }
+            .map(ReplResponse(_))
+            .runAsync
+      }
+
+    }
   }
 
 }
