d331827d38adfb98be9452c1e505992daad07adb
Jeremy Busk <jeremybusk@gmail.com>
2018-04-10 17:14:37 -0600

    typo fix

--- ci/docker/rchain-docker-build-push.sh
+++ ci/docker/rchain-docker-build-push.sh
@@ -109,7 +109,7 @@ if [[ ${docker_dst_repo} ]]; then
         "${TRAVIS_BRANCH}" = "ops-test" ]] ; then
         #echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
         echo "Travis branch matched. Pushing rnode to Docker Repo"
-        docker login -u "${DOCKER_USERNAME}" -u "${DOCKER_PASSWORD}"
+        docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
         docker tag  ${DOCKER_SRC_REPO}:${DOCKER_SRC_TAG} ${docker_dst_repo}
         docker push ${docker_dst_repo}
     elif [[ "${TRAVIS}" != "true" ]]; then
