1936b251d00ddf03c40f288a9478204a777e31bf
Kent Shikama <kent@kentshikama.com>
2018-04-20 20:32:11 -0700

    Update block generator to take creator, bonds, and justifications

--- casper/src/test/scala/coop/rchain/casper/util/CasperUtilTest.scala
+++ casper/src/test/scala/coop/rchain/casper/util/CasperUtilTest.scala
@@ -5,7 +5,7 @@ import coop.rchain.casper.BlockGenerator
 import coop.rchain.casper.internals._
 import coop.rchain.casper.protocol._
 import org.scalatest.{FlatSpec, Matchers}
-import scalaz.{State}
+import scalaz.State
 
 import scala.collection.immutable.HashMap
 
@@ -56,5 +56,45 @@ class CasperUtilTest extends FlatSpec with Matchers with BlockGenerator {
     isInMainChain(chain.hashToBlocks, genesis, b4) should be(true)
     isInMainChain(chain.hashToBlocks, b2, b4) should be(true)
     isInMainChain(chain.hashToBlocks, b3, b4) should be(false)
+  }
+
+  // See https://docs.google.com/presentation/d/1znz01SF1ljriPzbMoFV0J127ryPglUYLFyhvsb-ftQk/edit?usp=sharing slide 29 for diagram
+  "isInMainChain" should "classify complicated chains appropriately" in {
+    val v1 = ByteString.copyFromUtf8("Validator One")
+    val v2 = ByteString.copyFromUtf8("Validator Two")
+    val createChain =
+      for {
+        genesis <- createBlock[StateWithChain](Seq(), ByteString.EMPTY)
+        b2      <- createBlock[StateWithChain](Seq(genesis.blockHash), v2)
+        _       <- createBlock[StateWithChain](Seq(genesis.blockHash), v1)
+        b4      <- createBlock[StateWithChain](Seq(b2.blockHash), v2)
+        _       <- createBlock[StateWithChain](Seq(b2.blockHash), v1)
+        _       <- createBlock[StateWithChain](Seq(b4.blockHash), v2)
+        b7      <- createBlock[StateWithChain](Seq(b4.blockHash), v1)
+        b8      <- createBlock[StateWithChain](Seq(b7.blockHash), v1)
+      } yield b8
+
+    val initState =
+      Chain(HashMap.empty[Int, BlockMessage], HashMap.empty[ByteString, BlockMessage], 0)
+    val (chain: Chain, lastBlock: BlockMessage) = createChain.run(initState)
+
+    val genesis = chain.idToBlocks(1)
+    val b2      = chain.idToBlocks(2)
+    val b3      = chain.idToBlocks(3)
+    val b4      = chain.idToBlocks(4)
+    val b5      = chain.idToBlocks(5)
+    val b6      = chain.idToBlocks(6)
+    val b7      = chain.idToBlocks(7)
+    val b8      = chain.idToBlocks(8)
+    isInMainChain(chain.hashToBlocks, genesis, b2) should be(true)
+    isInMainChain(chain.hashToBlocks, b2, b3) should be(false)
+    isInMainChain(chain.hashToBlocks, b3, b4) should be(false)
+    isInMainChain(chain.hashToBlocks, b4, b5) should be(false)
+    isInMainChain(chain.hashToBlocks, b5, b6) should be(false)
+    isInMainChain(chain.hashToBlocks, b6, b7) should be(false)
+    isInMainChain(chain.hashToBlocks, b7, b8) should be(true)
+    isInMainChain(chain.hashToBlocks, b2, b6) should be(true)
+    isInMainChain(chain.hashToBlocks, b2, b8) should be(true)
+    isInMainChain(chain.hashToBlocks, b4, b2) should be(false)
   }
 }
