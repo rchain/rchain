cc5563601793a7886e291f07d349d12f7371f5dc
birchmd <birch@pyrofex.net>
2018-04-25 10:28:02 -0400

    Clean up functional style

--- casper/src/main/scala/coop/rchain/casper/util/comm/CommUtil.scala
+++ casper/src/main/scala/coop/rchain/casper/util/comm/CommUtil.scala
@@ -1,24 +1,27 @@
 package coop.rchain.casper.util.comm
 
-import cats.Applicative, cats.implicits._
+import cats.Monad, cats.implicits._
 
 import coop.rchain.casper.protocol.BlockMessage
 
+import coop.rchain.comm.CommError.CommErr
 import coop.rchain.comm.ProtocolMessage
 import coop.rchain.comm.protocol.rchain.Packet
-import coop.rchain.p2p.effects.Communication
+import coop.rchain.p2p.effects.{Communication, Log}
 
 object CommUtil {
-  def sendBlock[F[_]: Applicative: Communication](b: BlockMessage): F[Unit] = {
+  def sendBlock[F[_]: Monad: Communication: Log](b: BlockMessage): F[Unit] = {
     val packet = blockMessageToPacket(b)
     val msg    = framePacket(packet)
 
-    val peers = Communication[F].peers
-    peers.map(ps => {
-      val sends = ps.map(peer => Communication[F].commSend(msg, peer))
-      //TODO: Make this log errors in sends
-      sends.foldLeft(Applicative[F].pure(Unit))(_ <* _)
-    })
+    for {
+      peers <- Communication[F].peers
+      sends <- peers.toList.traverse(peer => Communication[F].commSend(msg, peer))
+      _ <- sends.traverse {
+            case Left(err) => Log[F].error(s"$err")
+            case Right(_)  => Monad[F].pure(())
+          }
+    } yield ()
   }
 
   private def blockMessageToPacket(msg: BlockMessage): Packet =
