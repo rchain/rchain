763fa679c6149985355d1978c0208c3bc47f2380
Kent Shikama <kent@kentshikama.com>
2018-04-25 12:27:50 -0700

    NFC: Address review changes by @goral09

--- casper/src/main/scala/coop/rchain/casper/PrettyPrinter.scala
+++ casper/src/main/scala/coop/rchain/casper/PrettyPrinter.scala
@@ -6,12 +6,33 @@ import coop.rchain.casper.protocol.Resource.ResourceClass.{
   ProduceResource,
   StoppedResource
 }
-import coop.rchain.casper.protocol.{BlockMessage, Resource, Stopped}
+import coop.rchain.casper.protocol.{BlockMessage, RChainState, Resource, Stopped}
 import scalapb.GeneratedMessage
 import coop.rchain.crypto.codec._
 
-class PrettyPrinter {
-  def buildString(b: ByteString): String = {
+object PrettyPrinter {
+  def buildString(t: GeneratedMessage): String =
+    t match {
+      case b: BlockMessage => buildString(b)
+      case _               => "Unknown consensus protocol message"
+    }
+  private def buildString(b: BlockMessage): String = {
+    val blockString = for {
+      header     <- b.header
+      mainParent <- header.parentsHashList.headOption
+      body       <- b.body
+      postState  <- body.postState
+    } yield
+      s"Block #${postState.blockNumber} (${buildString(b.blockHash)}) " +
+        s"-- Creator ID ${buildString(b.sig)} " +
+        s"-- M Parent Hash ${buildString(mainParent)} " +
+        s"-- Contents ${buildString(postState)}"
+    blockString match {
+      case Some(str) => str
+      case None      => "Block with missing elements"
+    }
+  }
+  private def buildString(b: ByteString): String = {
     val MAX_LENGTH = 10
     val str        = Base16.encode(b.toByteArray)
     if (str.length > MAX_LENGTH) {
@@ -20,32 +41,15 @@ class PrettyPrinter {
       str
     }
   }
-  def buildString(t: GeneratedMessage): String =
-    t match {
-      case b: BlockMessage =>
-        val blockString = for {
-          header     <- b.header
-          mainParent <- header.parentsHashList.headOption
-          body       <- b.body
-          postState  <- body.postState
-        } yield
-          s"Block #${postState.blockNumber} (${buildString(b.blockHash)}) " +
-            s"-- Creator ID ${buildString(b.sig)} " +
-            s"-- M Parent Hash ${buildString(mainParent)} " +
-            s"-- Contents ${postState.resources.toList
-              .map {
-                case Resource(resourceClass) =>
-                  resourceClass match {
-                    case ProduceResource(produce) => s"Produce ${produce.id}"
-                    case ConsumeResource(consume) => s"Consume ${consume.id}"
-                    case StoppedResource(_)       => ""
-                  }
-              }
-              .mkString("[", ",", "]")}"
-        blockString match {
-          case Some(str) => str
-          case None      => "Block with missing elements"
-        }
-      case _ => "Unknown consensus protocol message"
-    }
+  private def buildString(p: RChainState): String =
+    p.resources.toList
+      .map {
+        case Resource(resourceClass) =>
+          resourceClass match {
+            case ProduceResource(produce) => s"Produce ${produce.id}"
+            case ConsumeResource(consume) => s"Consume ${consume.id}"
+            case StoppedResource(_)       => ""
+          }
+      }
+      .mkString("[", ",", "]")
 }
