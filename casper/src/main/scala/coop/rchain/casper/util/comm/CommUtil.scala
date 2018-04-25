b44f9e96ce2c96e3f1b0c297c580d233507e7c51
birchmd <birch@pyrofex.net>
2018-04-25 11:15:01 -0400

    Add packet handler stub for Casper

--- casper/src/main/scala/coop/rchain/casper/util/comm/CommUtil.scala
+++ casper/src/main/scala/coop/rchain/casper/util/comm/CommUtil.scala
@@ -1,12 +1,16 @@
 package coop.rchain.casper.util.comm
 
-import cats.Monad, cats.implicits._
+import cats.{Applicative, Monad}, cats.implicits._
 
 import coop.rchain.casper.protocol.BlockMessage
 
 import coop.rchain.comm.ProtocolMessage
 import coop.rchain.comm.protocol.rchain.Packet
 import coop.rchain.p2p.effects.{Communication, Log}
+
+import coop.rchain.crypto.codec.Base16
+
+import scala.util.Try
 
 object CommUtil {
   def sendBlock[F[_]: Monad: Communication: Log](b: BlockMessage): F[Unit] = {
@@ -15,16 +19,34 @@ object CommUtil {
 
     for {
       peers <- Communication[F].peers
-      sends <- peers.toList.traverse(peer => Communication[F].commSend(msg, peer))
+      sends <- peers.toList.traverse(peer => Communication[F].commSend(msg, peer).map(_ -> peer))
       _ <- sends.traverse {
-            case Left(err) => Log[F].error(s"$err")
-            case Right(_)  => Monad[F].pure(())
+            case (Left(err), _)   => Log[F].error(s"$err")
+            case (Right(_), peer) => Log[F].info(s"Sent block ${hashString(b)} to $peer")
           }
     } yield ()
   }
 
+  def casperPacketHandler[F[_]: Applicative]: PartialFunction[Packet, F[String]] =
+    Function.unlift(packetToblockMessage).andThen {
+      case b: BlockMessage =>
+        /*
+         * TODO:
+         *  -add new block to internal Casper protocol state
+         *  -run fork-choice
+         *  -if this is a block that has not been recieved before then send to known peers
+         */
+        "I got a block :)".pure[F]
+    }
+
+  private def hashString(b: BlockMessage): String =
+    Base16.encode(b.blockHash.toByteArray)
+
   private def blockMessageToPacket(msg: BlockMessage): Packet =
     Packet().withContent(msg.toByteString)
+
+  private def packetToblockMessage(msg: Packet): Option[BlockMessage] =
+    Try(BlockMessage.parseFrom(msg.content.toByteArray)).toOption
 
   //TODO: Ask Pawel to provide a method somewhere in Comm that does the framing
   private def framePacket(p: Packet): ProtocolMessage = ???
