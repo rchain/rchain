a490572e66595b3028ce808727794ce17a3333fa
Pawel Szulc <paul.szulc@gmail.com>
2018-04-27 22:36:19 +0200

    Bring TaskOps back to normal

--- shared/src/main/scala/coop/rchain/catscontrib/taskOps.scala
+++ shared/src/main/scala/coop/rchain/catscontrib/taskOps.scala
@@ -2,29 +2,13 @@ package coop.rchain.catscontrib
 
 import monix.eval.Task
 import monix.execution.Scheduler
-import monix.execution.schedulers.SchedulerService
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import cats._, cats.data._, cats.implicits._
 
 object TaskContrib {
-  implicit class TaskOps[A](task: Task[A])(implicit scheduler: SchedulerService) {
-    def unsafeRunSync: A = {
-      println("running...")
-      val cancellable = task.runAsync
-      sys.addShutdownHook {
-        println("cancelling...")
-        cancellable.cancel()
-        println("cancelled")
-        import monix.execution.Scheduler.Implicits.global
-        println("shutting down")
-        scheduler.shutdown
-        println("awaiting termination")
-        val termination = scheduler.awaitTermination(30.seconds, global)
-        Await.result(termination, Duration.Inf)
-        println("done")
-      }
-      Await.result(cancellable, Duration.Inf)
-    }
+  implicit class TaskOps[A](task: Task[A])(implicit scheduler: Scheduler) {
+    def unsafeRunSync: A =
+      Await.result(task.runAsync, Duration.Inf)
   }
 }
