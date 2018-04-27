434c401180eec91091dbcb13cc4434602ea1f729
Kent Shikama <kent@kentshikama.com>
2018-04-26 18:00:13 -0700

    Test comm with simple MultiParentCasper instance
    Broadcasts an empty block every 5 seconds

--- casper/src/main/scala/coop/rchain/casper/Casper.scala
+++ casper/src/main/scala/coop/rchain/casper/Casper.scala
@@ -1,8 +1,12 @@
 package coop.rchain.casper
 
-import cats.Applicative, cats.implicits._
-
+import cats.{Applicative, Monad}
+import cats.implicits._
 import coop.rchain.casper.protocol.{BlockMessage, Resource}
+import coop.rchain.casper.util.comm.CommUtil
+import coop.rchain.catscontrib.{Capture, IOUtil}
+import coop.rchain.p2p.Network.{ErrorHandler, KeysStore}
+import coop.rchain.p2p.effects._
 
 trait Casper[F[_], A] {
   def addBlock(b: BlockMessage): F[Unit]
@@ -10,6 +14,7 @@ trait Casper[F[_], A] {
   def deploy(r: Resource): F[Unit]
   def estimator: F[A]
   def proposeBlock: F[Option[BlockMessage]]
+  def shouldSendBlock: F[Unit]
 }
 
 trait MultiParentCasper[F[_]] extends Casper[F, IndexedSeq[BlockMessage]]
@@ -19,12 +24,30 @@ object MultiParentCasper extends MultiParentCasperInstances {
 }
 
 sealed abstract class MultiParentCasperInstances {
-  def noCasper[F[_]: Applicative]: MultiParentCasper[F] = new MultiParentCasper[F] {
+  def noCasper[F[_]: Applicative]: MultiParentCasper[F] =
+    new MultiParentCasper[F] {
+      def addBlock(b: BlockMessage): F[Unit]    = ().pure[F]
+      def contains(b: BlockMessage): F[Boolean] = false.pure[F]
+      def deploy(r: Resource): F[Unit]          = ().pure[F]
+      def estimator: F[IndexedSeq[BlockMessage]] =
+        Applicative[F].pure[IndexedSeq[BlockMessage]](Vector(BlockMessage()))
+      def proposeBlock: F[Option[BlockMessage]] = Applicative[F].pure[Option[BlockMessage]](None)
+      def shouldSendBlock: F[Unit]              = ().pure[F]
+    }
+
+  def simpleCasper[
+      F[_]: Monad: Capture: Applicative: NodeDiscovery: TransportLayer: Log: Time: Encryption: KeysStore: ErrorHandler]
+    : MultiParentCasper[F] = new MultiParentCasper[F] {
     def addBlock(b: BlockMessage): F[Unit]    = ().pure[F]
     def contains(b: BlockMessage): F[Boolean] = false.pure[F]
     def deploy(r: Resource): F[Unit]          = ().pure[F]
     def estimator: F[IndexedSeq[BlockMessage]] =
       Applicative[F].pure[IndexedSeq[BlockMessage]](Vector(BlockMessage()))
     def proposeBlock: F[Option[BlockMessage]] = Applicative[F].pure[Option[BlockMessage]](None)
+    def shouldSendBlock: F[Unit] =
+      for {
+        _ <- IOUtil.sleep[F](5000L)
+        _ <- CommUtil.sendBlock[F](BlockMessage.defaultInstance)
+      } yield ()
   }
 }
--- casper/src/main/scala/coop/rchain/casper/util/comm/CommUtil.scala
+++ casper/src/main/scala/coop/rchain/casper/util/comm/CommUtil.scala
@@ -2,23 +2,28 @@ package coop.rchain.casper.util.comm
 
 import cats.{Applicative, Monad}
 import cats.implicits._
-
 import coop.rchain.casper.MultiParentCasper
 import coop.rchain.casper.protocol.BlockMessage
 import coop.rchain.comm.ProtocolMessage
 import coop.rchain.comm.protocol.rchain.Packet
-import coop.rchain.p2p.effects.{Log, NodeDiscovery, TransportLayer}
+import coop.rchain.p2p.effects._
 import coop.rchain.crypto.codec.Base16
+import coop.rchain.p2p.Network.{frameMessage, ErrorHandler, KeysStore}
+import coop.rchain.p2p.NetworkProtocol
+
 import scala.util.Try
 
 object CommUtil {
-  def sendBlock[F[_]: Monad: NodeDiscovery: TransportLayer: Log](b: BlockMessage): F[Unit] = {
-    val packet = blockMessageToPacket(b)
-    val msg    = framePacket(packet)
-
+  def sendBlock[
+      F[_]: Monad: NodeDiscovery: TransportLayer: Log: Time: Encryption: KeysStore: ErrorHandler](
+      b: BlockMessage): F[Unit] = {
+    val serializedBlock = b.toByteString
     for {
       peers <- NodeDiscovery[F].peers
-      sends <- peers.toList.traverse(peer => TransportLayer[F].commSend(msg, peer).map(_ -> peer))
+      sends <- peers.toList.traverse { peer =>
+                frameMessage[F](peer, nonce => NetworkProtocol.framePacket(peer, serializedBlock))
+                  .flatMap(msg => TransportLayer[F].commSend(msg, peer).map(_ -> peer))
+              }
       _ <- sends.traverse {
             case (Left(err), _)   => Log[F].error(s"$err")
             case (Right(_), peer) => Log[F].info(s"Sent block ${hashString(b)} to $peer")
@@ -26,7 +31,8 @@ object CommUtil {
     } yield ()
   }
 
-  def casperPacketHandler[F[_]: Monad: MultiParentCasper: NodeDiscovery: TransportLayer: Log]
+  def casperPacketHandler[
+      F[_]: Monad: MultiParentCasper: NodeDiscovery: TransportLayer: Log: Time: Encryption: KeysStore: ErrorHandler]
     : PartialFunction[Packet, F[String]] =
     Function.unlift(packetToBlockMessage).andThen {
       case b: BlockMessage =>
@@ -40,7 +46,8 @@ object CommUtil {
         } yield logMessage
     }
 
-  private def handleNewBlock[F[_]: Monad: MultiParentCasper: NodeDiscovery: TransportLayer: Log](
+  private def handleNewBlock[
+      F[_]: Monad: MultiParentCasper: NodeDiscovery: TransportLayer: Log: Time: Encryption: KeysStore: ErrorHandler](
       b: BlockMessage): F[String] =
     for {
       _          <- MultiParentCasper[F].addBlock(b)
@@ -51,12 +58,6 @@ object CommUtil {
   private def hashString(b: BlockMessage): String =
     Base16.encode(b.blockHash.toByteArray)
 
-  private def blockMessageToPacket(msg: BlockMessage): Packet =
-    Packet().withContent(msg.toByteString)
-
   private def packetToBlockMessage(msg: Packet): Option[BlockMessage] =
     Try(BlockMessage.parseFrom(msg.content.toByteArray)).toOption
-
-  //TODO: Ask Pawel to provide a method somewhere in Comm that does the framing
-  private def framePacket(p: Packet): ProtocolMessage = ???
 }
