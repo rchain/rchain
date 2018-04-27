7eaac18d2391da7436451eafdeec8abe36c02c01
Jeremy Busk <jeremybusk@gmail.com>
2018-04-27 11:54:01 -0600

    added script for local machine rnode docker test-net build using debian packages

---
+++ scripts/local-docker-rnode-p2p-test-net-builder.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+NETWORK_UID="1" # Unique identifier for network if you wanted to run multiple test networks
+
+if [[ $1 && $2 ]]; then
+    git_repo=$1
+    branch_name=$2
+    echo "Creating docker rnode test-net for ${git_repo} ${branch_name}"
+else
+    echo "Usage: $0 <repo url>> <branch name>"
+    echo "Usage: $0 https://github.com/rchain/rchain dev"
+    exit
+fi
+
+network_name="${NETWORK_UID}.rnode.test.net"
+
+sudo echo "" # Ask for sudo auth early
+
+# Create debian package from git repo via sbt
+git_dir=$(mktemp -d /tmp/rchain-git.XXXXXXXX)
+cd ${git_dir}
+git clone ${git_repo} 
+cd rchain
+git checkout ${branch_name}
+sbt -Dsbt.log.noformat=true clean rholang/bnfc:generate node/debian:packageBin
+
+# Create docker network if it doesn't exist
+if [[ "$(sudo docker network list | grep ${network_name})" == "" ]]; then
+    sudo docker network create \
+      --driver=bridge \
+      --subnet=10.1.1.0/24 \
+      --ip-range=10.1.1.0/24 \
+      --gateway=10.1.1.1 \
+      ${network_name}
+else
+    echo "The network ${network_name} already exists."
+fi
+
+for i in {0..2}; do
+    container_name="node${i}.${network_name}"
+    echo $container_name
+
+	# If container exists force remove it.
+	if [[ $(sudo docker ps -aq -f name=${container_name}) ]]; then
+		sudo docker rm -f ${container_name}
+	fi
+
+	sudo docker run -dit --name ${container_name} \
+        -v $git_dir/rchain/node/target/rnode_0.2.1_all.deb:/rnode_0.2.1_all.deb \
+        --network=${network_name} \
+        openjdk
+    
+    # Ret rnode run command depending on node nubmer
+    if [[ $i == 0 ]]; then
+        rnode_cmd="rnode --port 30304 --standalone --name 0f365f1016a54747b384b386b8e85352 > /var/log/rnode.log 2>&1 &"
+    else
+        rnode_cmd="rnode --bootstrap rnode://0f365f1016a54747b384b386b8e85352@10.1.1.2:30304 > /var/log/rnode.log 2>&1 &"
+    fi
+
+    branch_name="0.2.1"
+	sudo docker exec ${container_name} bash -c "apt -y update; apt -y iputils-ping bridge-utils iproute2; apt -y install ./rnode_${branch_name}_all.deb; mkdir /var/lib/rnode; ${rnode_cmd}" 
+done
+
+echo "############################COMPLETE###########################"
+echo "==============================================================="
+echo "Run below to display standalone bootstrap server rnode log:"
+echo "sudo docker exec  node0.1.rnode.test.net bash -c \"tail -f /var/log/rnode.log\""
+echo "==============================================================="
+echo "Run below to display node1 rnode log:"
+echo "sudo docker exec  node1.1.rnode.test.net bash -c \"tail -f /var/log/rnode.log\""
+echo "==============================================================="
+echo "Run below to go into standaloner server and do things:"
+echo "sudo docker exec -it node0.1.rnode.test.net /bin/bash"
