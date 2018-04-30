6ea84090b8f6810745f87ea0c40817eaa0b480e2
Jeremy Busk <jeremybusk@gmail.com>
2018-04-30 21:02:05 +0000

    added tests to p2p test net

--- scripts/p2p-test-network.sh
+++ scripts/p2p-test-network.sh
@@ -4,7 +4,7 @@
 # "local repo" as params builds from current repo you are in
 # "delete testnet" removes all testnet resources 
 
-set -eo pipefail
+#set -eo pipefail
 
 NETWORK_UID="1" # Unique identifier for network if you wanted to run multiple test networks
 network_name="testnet${NETWORK_UID}.rchain"
@@ -38,10 +38,24 @@ create_test_network_resources() {
       coop.rchain/rnode ${rnode_cmd}
   
     sudo docker exec ${container_name} sh -c "apk add curl"
+    sleep 3 # slow down 
   done
-
+  
+  echo "======================================================"
+  echo "P2P test network build complete. Converging network."
+  echo "======================================================"
+  echo ""
   echo "Test network build has completed but it might take a minute for start-up of network and metrics to be available."
+  echo ""
+  echo 'Run option "docker-help" to get more info on docker commands to interact with node containers'  
+  echo ""
+}
 
+docker_help_info() {
+  if [[ ! $1 ]]; then
+    echo "E: Requires network name as argument"
+    exit
+  fi
   echo "#########################DOCKER NOTES##########################"
   echo "==============================================================="
   echo "To display standalone bootstrap server rnode log:"
@@ -84,50 +98,132 @@ delete_test_network_resources() {
   fi
 }
 
+run_tests_on_network() {
+  all_pass=true
+  if [[ ! $1 ]]; then
+    echo "E: Requires network name as argument"
+    exit
+  fi
+
+  for container_name in $(docker container ls --all --format {{.Names}} | grep \.${network_name}$); do
+
+    echo "============================================="
+    echo "Running tests on ${container_name}"
+
+    if [[ $(sudo docker exec ${container_name} sh -c "curl -s 127.0.0.1:9095") ]]; then
+      echo "PASS: Could connect to metrics api" 
+    else
+      echo "FAIL: Could not connect to metrics api" 
+      all_pass=false
+    fi
+   
+    # Disabled for inconsistent value bug. Using log peers total until fixed
+    # metric_expected_peers_total="2.0"
+    # if [[ $(sudo docker exec ${container_name} sh -c "curl -s 127.0.0.1:9095 | grep 'peers_total ${metric_expected_peers_total}'") ]]; then
+    #   echo "PASS: Correct metric api total peers count" 
+    # else
+    #   echo "FAIL: Incorrect metric api total peers count" 
+    #   sudo docker exec ${container_name} sh -c "curl -s 127.0.0.1:9095 | grep 'peers_total*'"
+    #   exit
+    # fi
+    
+    if [[ $(sudo docker logs ${container_name} | grep 'Peers: 2.') ]]; then
+      echo "PASS: Correct log peers count" 
+    else
+      echo "FAIL: Incorrect log peers count" 
+      sudo docker logs ${container_name} | grep 'Peers:'
+      all_pass=false
+    fi
+
+    if [[ ! $(sudo docker logs ${container_name} | grep ERR) ]]; then
+      echo "PASS: No error messages contained in logs" 
+    else
+      echo "FAIL: ERROR messages contained in logs" 
+      sudo docker logs ${container_name} | grep ERR
+      all_pass=false
+    fi
+
+  done
+  
+  # Check for failures
+  echo "============================================="
+  if [[ $all_pass == false ]]; then
+    echo "ERROR: Not all network checks passed."
+    exit 1
+  elif [[ $all_pass == true ]]; then
+    echo "SUCCESS: All checks passed"
+  else
+    echo "Unsupported"
+  fi
+}
+
+create_docker_rnode_image() {
+  if [[ ! $1 ]]; then
+    echo "E: Requires git repo as argument"
+    exit
+  fi
+  echo "Creating RChain rnode docker image coop.rchain/rnode from git src via sbt"
+  if [[ "$1" == "local" ]]; then
+    sbt -Dsbt.log.noformat=true clean rholang/bnfc:generate node/docker
+  elif [[ $1 && $2 ]]; then
+    git_dir=$(mktemp -d /tmp/rchain-git.XXXXXXXX)
+    cd ${git_dir}
+    git clone $1 
+    cd rchain
+    git checkout $2
+    sbt -Dsbt.log.noformat=true clean rholang/bnfc:generate node/docker
+  else
+    echo "Unsupported"
+  fi
+}
+
+# ======================================================
 
 # Process params
 if [[ "${TRAVIS}" == "true" ]]; then
   echo "Running in TRAVIS CI"
-  git_repo="local"
-  branch_name="${TRAVIS_BRANCH}"
+  sbt -Dsbt.log.noformat=true clean rholang/bnfc:generate node/docker
+  create_test_network_resources "${network_name}"
+  echo "Running tests on network in 90 seconds after bootup and convergence"
+  echo "Please be patient"
+  sleep 90 # allow plenty of time for network to boot and converge
+  run_tests_on_network "${network_name}"
 elif [[ $1 == "local" ]]; then
-  git_repo="local"
+  sudo echo "" # Ask for sudo early
+  create_docker_rnode_image "local"
+  delete_test_network_resources "${network_name}"
+  create_test_network_resources "${network_name}"
+elif [[ $1 == "run-tests" ]]; then
+  sudo echo "" # Ask for sudo early
+  run_tests_on_network "${network_name}"
+  exit
 elif [[ $1 == "start" ]]; then
+  sudo echo "" # Ask for sudo early
   delete_test_network_resources "${network_name}"
   create_test_network_resources "${network_name}"
   exit
 elif [[ $1 == "stop" ]]; then
+  sudo echo "" # Ask for sudo early
   delete_test_network_resources "${network_name}"
   exit
+elif [[ $1 == "docker-help" ]]; then
+  docker_help_info "${network_name}"
+  exit
 elif [[ $1 && $2 ]]; then
+  sudo echo "" # Ask for sudo early
   git_repo=$1
   branch_name=$2
   echo "Creating docker rnode test-net for ${git_repo} ${branch_name}"
+  create_docker_rnode_image "${git_repo}" "${branch_name}"
+  delete_test_network_resources "${network_name}"
+  create_test_network_resources "${network_name}"
 else
   echo "Usage: $0 <repo url> <branch name>"
   echo "Usage: $0 https://github.com/rchain/rchain dev"
-  echo "Special Usage Commands:"
   echo "Usage: $0 local"
   echo "Usage: $0 start"
   echo "Usage: $0 stop"
-  echo "Usage: $0 delete testnet"
+  echo "Usage: $0 run-tests"
+  echo "Usage: $0 docker-help"
   exit
 fi
-
-# Remove test network resources before creating new network
-delete_test_network_resources "${network_name}"
-
-sudo echo "" # Ask for sudo early
-echo "Creating RChain rnode docker image coop.rchain/rnode from git src via sbt"
-if [[ "${git_repo}" == "local" ]]; then
-  sbt -Dsbt.log.noformat=true clean rholang/bnfc:generate node/docker
-else
-  git_dir=$(mktemp -d /tmp/rchain-git.XXXXXXXX)
-  cd ${git_dir}
-  git clone ${git_repo} 
-  cd rchain
-  git checkout ${branch_name}
-  sbt -Dsbt.log.noformat=true clean rholang/bnfc:generate node/docker
-fi
-
-create_test_network_resources "${network_name}"
