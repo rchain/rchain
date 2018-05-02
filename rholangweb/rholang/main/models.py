ae3de18eca392a732bf2cb00b987d00f19b21794
Dan Connolly <dckc@madmode.com>
2018-05-01 20:04:17 -0500

    rholangweb end of life in favor of rchain.cloud (RHOL-78)
    for reference:
    
    http://rchain.cloud/
    
    https://github.com/th3build/rchain.cloud
    Latest commit 7f251b3  Apr 24

--- rholangweb/rholang/main/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Log(models.Model):
-    input = models.TextField(blank=True)
-    output = models.TextField(blank=True)
-
-    def __str__(self):
-        return str(self.input) + str(self.output)
-
