7a213b5e320cf5a8448d697d22cb0d916fc68b6a
Kent Shikama <kent@kentshikama.com>
2018-05-02 09:46:49 -0700

    Replace ByteString usage with BlockHash

--- casper/src/main/scala/coop/rchain/casper/BlockDag.scala
+++ casper/src/main/scala/coop/rchain/casper/BlockDag.scala
@@ -1,13 +1,12 @@
 package coop.rchain.casper
 
-import com.google.protobuf.ByteString
 import coop.rchain.casper.Estimator.{BlockHash, Validator}
 import coop.rchain.casper.protocol.BlockMessage
 
 import scala.collection.immutable.{HashMap, HashSet}
 
 final case class BlockDag(idToBlocks: HashMap[Int, BlockMessage],
-                          blockLookup: HashMap[ByteString, BlockMessage],
+                          blockLookup: HashMap[BlockHash, BlockMessage],
                           childMap: HashMap[BlockHash, HashSet[BlockHash]],
                           latestMessages: HashMap[Validator, BlockHash],
                           currentId: Int)
