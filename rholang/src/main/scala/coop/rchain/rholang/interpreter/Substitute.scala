466ec597cc9161adbca0cbc7716ff90d2b8a3ea4
Mateusz Górski <gorski.mateusz@protonmail.ch>
2018-05-08 08:10:20 +0200

    Instead of throwing error raise it to MonadError

--- rholang/src/main/scala/coop/rchain/rholang/interpreter/Substitute.scala
+++ rholang/src/main/scala/coop/rchain/rholang/interpreter/Substitute.scala
@@ -1,7 +1,7 @@
 package coop.rchain.rholang.interpreter
 
 import cats.data.{Kleisli, ReaderT, StateT}
-import cats.{Applicative, Apply, Monad}
+import cats.{Applicative, Apply, Monad, MonadError}
 import coop.rchain.models.Channel.ChannelInstance._
 import coop.rchain.models.Expr.ExprInstance._
 import coop.rchain.models.Var.VarInstance._
@@ -28,7 +28,8 @@ object Substitute {
 
   def apply[M[_], A](implicit ev: Substitute[M, A]): Substitute[M, A] = ev
 
-  def maybeSubstitute[M[+ _]: Applicative](term: Var): Kleisli[M, Env[Par], Either[Var, Par]] =
+  def maybeSubstitute[M[+ _]](term: Var)(
+      implicit me: MonadError[M, Throwable]): Kleisli[M, Env[Par], Either[Var, Par]] =
     Kleisli { (env: Env[Par]) =>
       term.varInstance match {
         case BoundVar(index) =>
@@ -37,17 +38,19 @@ object Substitute {
             case None =>
               Applicative[M].pure(Left[Var, Par](BoundVar(index))) //scalac is not helping here
           }
-        case _ => throw new Error(s"Illegal Substitution [$term]")
+        case _ => me.raiseError(new Error(s"Illegal Substitution [$term]"))
       }
     }
 
-  def maybeSubstitute[M[_]: Applicative](term: EVar): Kleisli[M, Env[Par], Either[EVar, Par]] =
+  def maybeSubstitute[M[_]](term: EVar)(
+      implicit me: MonadError[M, Throwable]): Kleisli[M, Env[Par], Either[EVar, Par]] =
     maybeSubstitute[M](term.v.get).map {
       case Left(v)    => Left(EVar(v))
       case Right(par) => Right(par)
     }
 
-  def maybeSubstitute[M[_]: Monad](term: Eval): Kleisli[M, Env[Par], Either[Eval, Par]] =
+  def maybeSubstitute[M[_]](term: Eval)(
+      implicit me: MonadError[M, Throwable]): Kleisli[M, Env[Par], Either[Eval, Par]] =
     term.channel.get.channelInstance match {
       case Quote(p) => substitutePar[M].substitute(p).map(Right(_))
       case ChanVar(v) =>
@@ -57,12 +60,14 @@ object Substitute {
         }
     }
 
-  implicit def substituteQuote[M[_]: Monad]: Substitute[M, Quote] = new Substitute[M, Quote] {
-    override def substitute(term: Quote): Kleisli[M, Env[Par], Quote] =
-      substitutePar[M].substitute(term.value).map(Quote(_))
-  }
+  implicit def substituteQuote[M[_]](implicit ev: MonadError[M, Throwable]): Substitute[M, Quote] =
+    new Substitute[M, Quote] {
+      override def substitute(term: Quote): Kleisli[M, Env[Par], Quote] =
+        substitutePar[M].substitute(term.value).map(Quote(_))
+    }
 
-  implicit def substituteBundle[M[_]: Monad]: Substitute[M, Bundle] = new Substitute[M, Bundle] {
+  implicit def substituteBundle[M[_]](
+      implicit ev: MonadError[M, Throwable]): Substitute[M, Bundle] = new Substitute[M, Bundle] {
     import BundleOps._
 
     override def substitute(term: Bundle): Kleisli[M, Env[Par], Bundle] =
@@ -74,7 +79,8 @@ object Substitute {
       }
   }
 
-  implicit def substituteChannel[M[_]: Monad]: Substitute[M, Channel] = new Substitute[M, Channel] {
+  implicit def substituteChannel[M[_]](
+      implicit ev: MonadError[M, Throwable]): Substitute[M, Channel] = new Substitute[M, Channel] {
     override def substitute(term: Channel): Kleisli[M, Env[Par], Channel] =
       for {
         env <- Kleisli.ask[M, Env[Par]]
@@ -89,73 +95,76 @@ object Substitute {
       } yield ChannelSortMatcher.sortMatch(channelSubst).term
   }
 
-  implicit def substitutePar[M[_]: Monad]: Substitute[M, Par] = new Substitute[M, Par] {
-    def subExp(exprs: Seq[Expr]): Kleisli[M, Env[Par], Par] =
-      exprs.toList.foldM(VectorPar()) { (par, expr) =>
-        expr.exprInstance match {
-          case EVarBody(e @ EVar(_)) =>
-            maybeSubstitute[M](e).map {
-              case Left(_e)    => par.prepend(_e)
-              case Right(_par) => _par ++ par
-            }
-          case _ => substituteExpr[M].substitute(expr).map(par.prepend(_))
+  implicit def substitutePar[M[_]](implicit ev: MonadError[M, Throwable]): Substitute[M, Par] =
+    new Substitute[M, Par] {
+      def subExp(exprs: Seq[Expr]): Kleisli[M, Env[Par], Par] =
+        exprs.toList.foldM(VectorPar()) { (par, expr) =>
+          expr.exprInstance match {
+            case EVarBody(e @ EVar(_)) =>
+              maybeSubstitute[M](e).map {
+                case Left(_e)    => par.prepend(_e)
+                case Right(_par) => _par ++ par
+              }
+            case _ => substituteExpr[M].substitute(expr).map(par.prepend(_))
+          }
         }
-      }
 
-    def subEval(evals: Seq[Eval]): Kleisli[M, Env[Par], Par] =
-      evals.toList.foldM(VectorPar()) { (par, eval) =>
-        maybeSubstitute[M](eval).map {
-          case Left(plainEval)   => par.prepend(plainEval)
-          case Right(droppedPar) => droppedPar ++ par
+      def subEval(evals: Seq[Eval]): Kleisli[M, Env[Par], Par] =
+        evals.toList.foldM(VectorPar()) { (par, eval) =>
+          maybeSubstitute[M](eval).map {
+            case Left(plainEval)   => par.prepend(plainEval)
+            case Right(droppedPar) => droppedPar ++ par
+          }
         }
-      }
 
-    implicit override def substitute(term: Par): Kleisli[M, Env[Par], Par] =
-      for {
-        env            <- Kleisli.ask[M, Env[Par]]
-        exprs          <- subExp(term.exprs)
-        evals          <- subEval(term.evals)
-        sends          <- term.sends.toList.traverse(substituteSend[M].substitute(_))
-        bundles        <- term.bundles.toList.traverse(substituteBundle[M].substitute(_))
-        receives       <- term.receives.toList.traverse(substituteReceive[M].substitute(_))
-        news           <- term.news.toList.traverse(substituteNew[M].substitute(_))
-        matches        <- term.matches.toList.traverse(substituteMatch[M].substitute(_))
-        locallyFree    = term.locallyFree.until(env.shift)
-        connectiveUsed = term.connectiveUsed
-      } yield
-        exprs ++
-          evals ++
-          Par(
-            exprs = Nil,
-            evals = Nil,
-            sends = sends,
-            bundles = bundles,
-            receives = receives,
-            news = news,
-            matches = matches,
-            ids = term.ids,
-            locallyFree = locallyFree,
-            connectiveUsed = connectiveUsed
-          )
-  }
-
-  implicit def substituteSend[M[_]: Monad]: Substitute[M, Send] = new Substitute[M, Send] {
-    override def substitute(term: Send): Kleisli[M, Env[Par], Send] = Kleisli { implicit env =>
-      (for {
-        channelsSub <- substituteChannel[M].substitute(term.chan.get)
-        parsSub     <- term.data.toList.traverse(substitutePar[M].substitute(_))
-      } yield
-        Send(
-          chan = channelsSub,
-          data = parsSub,
-          persistent = term.persistent,
-          locallyFree = term.locallyFree.until(env.shift),
+      implicit override def substitute(term: Par): Kleisli[M, Env[Par], Par] =
+        for {
+          env            <- Kleisli.ask[M, Env[Par]]
+          exprs          <- subExp(term.exprs)
+          evals          <- subEval(term.evals)
+          sends          <- term.sends.toList.traverse(substituteSend[M].substitute(_))
+          bundles        <- term.bundles.toList.traverse(substituteBundle[M].substitute(_))
+          receives       <- term.receives.toList.traverse(substituteReceive[M].substitute(_))
+          news           <- term.news.toList.traverse(substituteNew[M].substitute(_))
+          matches        <- term.matches.toList.traverse(substituteMatch[M].substitute(_))
+          locallyFree    = term.locallyFree.until(env.shift)
           connectiveUsed = term.connectiveUsed
-        )).map(SendSortMatcher.sortMatch(_)).map(_.term).run(env)
+        } yield
+          exprs ++
+            evals ++
+            Par(
+              exprs = Nil,
+              evals = Nil,
+              sends = sends,
+              bundles = bundles,
+              receives = receives,
+              news = news,
+              matches = matches,
+              ids = term.ids,
+              locallyFree = locallyFree,
+              connectiveUsed = connectiveUsed
+            )
     }
-  }
 
-  implicit def substituteReceive[M[_]: Monad]: Substitute[M, Receive] = new Substitute[M, Receive] {
+  implicit def substituteSend[M[_]](implicit ev: MonadError[M, Throwable]): Substitute[M, Send] =
+    new Substitute[M, Send] {
+      override def substitute(term: Send): Kleisli[M, Env[Par], Send] = Kleisli { implicit env =>
+        (for {
+          channelsSub <- substituteChannel[M].substitute(term.chan.get)
+          parsSub     <- term.data.toList.traverse(substitutePar[M].substitute(_))
+        } yield
+          Send(
+            chan = channelsSub,
+            data = parsSub,
+            persistent = term.persistent,
+            locallyFree = term.locallyFree.until(env.shift),
+            connectiveUsed = term.connectiveUsed
+          )).map(SendSortMatcher.sortMatch(_)).map(_.term).run(env)
+      }
+    }
+
+  implicit def substituteReceive[M[_]](
+      implicit ev: MonadError[M, Throwable]): Substitute[M, Receive] = new Substitute[M, Receive] {
     override def substitute(term: Receive): Kleisli[M, Env[Par], Receive] =
       for {
         env <- Kleisli.ask[M, Env[Par]]
@@ -178,101 +187,104 @@ object Substitute {
         )
   }
 
-  implicit def substituteNew[M[_]: Monad]: Substitute[M, New] = new Substitute[M, New] {
-    override def substitute(term: New): Kleisli[M, Env[Par], New] = Kleisli { env =>
-      substitutePar[M].substitute(term.p.get)(env.shift(term.bindCount)).map { parSub =>
-        NewSortMatcher
-          .sortMatch(New(term.bindCount, parSub, term.locallyFree.until(env.shift)))
-          .term
+  implicit def substituteNew[M[_]](implicit ev: MonadError[M, Throwable]): Substitute[M, New] =
+    new Substitute[M, New] {
+      override def substitute(term: New): Kleisli[M, Env[Par], New] = Kleisli { env =>
+        substitutePar[M].substitute(term.p.get)(env.shift(term.bindCount)).map { parSub =>
+          NewSortMatcher
+            .sortMatch(New(term.bindCount, parSub, term.locallyFree.until(env.shift)))
+            .term
+        }
       }
     }
-  }
 
-  implicit def substituteMatch[M[_]: Monad]: Substitute[M, Match] = new Substitute[M, Match] {
-    override def substitute(term: Match): Kleisli[M, Env[Par], Match] =
-      for {
-        env       <- Kleisli.ask[M, Env[Par]]
-        targetSub <- substitutePar[M].substitute(term.target.get)
-        casesSub <- Kleisli.lift(term.cases.toList.traverse {
-                     case MatchCase(_case, Some(_par), freeCount) =>
-                       substitutePar[M].substitute(_par)(env.shift(freeCount)).map { par =>
-                         MatchCase(_case, par, freeCount)
+  implicit def substituteMatch[M[_]](implicit ev: MonadError[M, Throwable]): Substitute[M, Match] =
+    new Substitute[M, Match] {
+      override def substitute(term: Match): Kleisli[M, Env[Par], Match] =
+        for {
+          env       <- Kleisli.ask[M, Env[Par]]
+          targetSub <- substitutePar[M].substitute(term.target.get)
+          casesSub <- Kleisli.lift(term.cases.toList.traverse {
+                       case MatchCase(_case, Some(_par), freeCount) =>
+                         substitutePar[M].substitute(_par)(env.shift(freeCount)).map { par =>
+                           MatchCase(_case, par, freeCount)
+                         }
+                     })
+        } yield
+          MatchSortMatcher
+            .sortMatch(
+              Match(targetSub, casesSub, term.locallyFree.until(env.shift), term.connectiveUsed))
+            .term
+    }
+
+  implicit def substituteExpr[M[_]](implicit ev: MonadError[M, Throwable]): Substitute[M, Expr] =
+    new Substitute[M, Expr] {
+      override def substitute(term: Expr): Kleisli[M, Env[Par], Expr] =
+        term.exprInstance match {
+          case ENotBody(ENot(par)) => substitutePar[M].substitute(par.get).map(ENot(_))
+          case ENegBody(ENeg(par)) => substitutePar[M].substitute(par.get).map(ENeg(_))
+          case EMultBody(EMult(par1, par2)) =>
+            substitute2(par1.get, par2.get)(EMult(_, _))
+          case EDivBody(EDiv(par1, par2)) =>
+            substitute2(par1.get, par2.get)(EDiv(_, _))
+          case EPlusBody(EPlus(par1, par2)) =>
+            substitute2(par1.get, par2.get)(EPlus(_, _))
+          case EMinusBody(EMinus(par1, par2)) =>
+            substitute2(par1.get, par2.get)(EMinus(_, _))
+          case ELtBody(ELt(par1, par2)) =>
+            substitute2(par1.get, par2.get)(ELt(_, _))
+          case ELteBody(ELte(par1, par2)) =>
+            substitute2(par1.get, par2.get)(ELte(_, _))
+          case EGtBody(EGt(par1, par2)) =>
+            substitute2(par1.get, par2.get)(EGt(_, _))
+          case EGteBody(EGte(par1, par2)) =>
+            substitute2(par1.get, par2.get)(EGte(_, _))
+          case EEqBody(EEq(par1, par2)) =>
+            substitute2(par1.get, par2.get)(EEq(_, _))
+          case ENeqBody(ENeq(par1, par2)) =>
+            substitute2(par1.get, par2.get)(ENeq(_, _))
+          case EAndBody(EAnd(par1, par2)) =>
+            substitute2(par1.get, par2.get)(EAnd(_, _))
+          case EOrBody(EOr(par1, par2)) =>
+            substitute2(par1.get, par2.get)(EOr(_, _))
+          case EListBody(EList(ps, locallyFree, connectiveUsed, rem)) =>
+            for {
+              env <- Kleisli.ask[M, Env[Par]]
+              pss <- ps.toList
+                      .traverse(substitutePar[M].substitute(_))
+              newLocallyFree = locallyFree.until(env.shift)
+            } yield Expr(exprInstance = EListBody(EList(pss, newLocallyFree, connectiveUsed, rem)))
+
+          case ETupleBody(ETuple(ps, locallyFree, connectiveUsed)) =>
+            for {
+              env <- Kleisli.ask[M, Env[Par]]
+              pss <- ps.toList
+                      .traverse(substitutePar[M].substitute(_))
+              newLocallyFree = locallyFree.until(env.shift)
+            } yield Expr(exprInstance = ETupleBody(ETuple(pss, newLocallyFree, connectiveUsed)))
+
+          case ESetBody(ESet(ps, locallyFree, connectiveUsed)) =>
+            for {
+              env <- Kleisli.ask[M, Env[Par]]
+              pss <- ps.toList
+                      .traverse(substitutePar[M].substitute(_))
+              newLocallyFree = locallyFree.until(env.shift)
+            } yield Expr(exprInstance = ESetBody(ESet(pss, newLocallyFree, connectiveUsed)))
+
+          case EMapBody(EMap(kvs, locallyFree, connectiveUsed)) =>
+            for {
+              env <- Kleisli.ask[M, Env[Par]]
+              kvps <- kvs.toList
+                       .traverse {
+                         case KeyValuePair(p1, p2) =>
+                           for {
+                             pk1 <- substitutePar[M].substitute(p1.get)
+                             pk2 <- substitutePar[M].substitute(p2.get)
+                           } yield KeyValuePair(pk1, pk2)
                        }
-                   })
-      } yield
-        MatchSortMatcher
-          .sortMatch(
-            Match(targetSub, casesSub, term.locallyFree.until(env.shift), term.connectiveUsed))
-          .term
-  }
-
-  implicit def substituteExpr[M[_]: Monad]: Substitute[M, Expr] = new Substitute[M, Expr] {
-    override def substitute(term: Expr): Kleisli[M, Env[Par], Expr] =
-      term.exprInstance match {
-        case ENotBody(ENot(par)) => substitutePar[M].substitute(par.get).map(ENot(_))
-        case ENegBody(ENeg(par)) => substitutePar[M].substitute(par.get).map(ENeg(_))
-        case EMultBody(EMult(par1, par2)) =>
-          substitute2(par1.get, par2.get)(EMult(_, _))
-        case EDivBody(EDiv(par1, par2)) =>
-          substitute2(par1.get, par2.get)(EDiv(_, _))
-        case EPlusBody(EPlus(par1, par2)) =>
-          substitute2(par1.get, par2.get)(EPlus(_, _))
-        case EMinusBody(EMinus(par1, par2)) =>
-          substitute2(par1.get, par2.get)(EMinus(_, _))
-        case ELtBody(ELt(par1, par2)) =>
-          substitute2(par1.get, par2.get)(ELt(_, _))
-        case ELteBody(ELte(par1, par2)) =>
-          substitute2(par1.get, par2.get)(ELte(_, _))
-        case EGtBody(EGt(par1, par2)) =>
-          substitute2(par1.get, par2.get)(EGt(_, _))
-        case EGteBody(EGte(par1, par2)) =>
-          substitute2(par1.get, par2.get)(EGte(_, _))
-        case EEqBody(EEq(par1, par2)) =>
-          substitute2(par1.get, par2.get)(EEq(_, _))
-        case ENeqBody(ENeq(par1, par2)) =>
-          substitute2(par1.get, par2.get)(ENeq(_, _))
-        case EAndBody(EAnd(par1, par2)) =>
-          substitute2(par1.get, par2.get)(EAnd(_, _))
-        case EOrBody(EOr(par1, par2)) =>
-          substitute2(par1.get, par2.get)(EOr(_, _))
-        case EListBody(EList(ps, locallyFree, connectiveUsed, rem)) =>
-          for {
-            env <- Kleisli.ask[M, Env[Par]]
-            pss <- ps.toList
-                    .traverse(substitutePar[M].substitute(_))
-            newLocallyFree = locallyFree.until(env.shift)
-          } yield Expr(exprInstance = EListBody(EList(pss, newLocallyFree, connectiveUsed, rem)))
-
-        case ETupleBody(ETuple(ps, locallyFree, connectiveUsed)) =>
-          for {
-            env <- Kleisli.ask[M, Env[Par]]
-            pss <- ps.toList
-                    .traverse(substitutePar[M].substitute(_))
-            newLocallyFree = locallyFree.until(env.shift)
-          } yield Expr(exprInstance = ETupleBody(ETuple(pss, newLocallyFree, connectiveUsed)))
-
-        case ESetBody(ESet(ps, locallyFree, connectiveUsed)) =>
-          for {
-            env <- Kleisli.ask[M, Env[Par]]
-            pss <- ps.toList
-                    .traverse(substitutePar[M].substitute(_))
-            newLocallyFree = locallyFree.until(env.shift)
-          } yield Expr(exprInstance = ESetBody(ESet(pss, newLocallyFree, connectiveUsed)))
-
-        case EMapBody(EMap(kvs, locallyFree, connectiveUsed)) =>
-          for {
-            env <- Kleisli.ask[M, Env[Par]]
-            kvps <- kvs.toList
-                     .traverse {
-                       case KeyValuePair(p1, p2) =>
-                         for {
-                           pk1 <- substitutePar[M].substitute(p1.get)
-                           pk2 <- substitutePar[M].substitute(p2.get)
-                         } yield KeyValuePair(pk1, pk2)
-                     }
-            newLocallyFree = locallyFree.until(env.shift)
-          } yield Expr(exprInstance = EMapBody(EMap(kvps, newLocallyFree, connectiveUsed)))
-        case g @ _ => Kleisli.pure(term)
-      }
-  }
+              newLocallyFree = locallyFree.until(env.shift)
+            } yield Expr(exprInstance = EMapBody(EMap(kvps, newLocallyFree, connectiveUsed)))
+          case g @ _ => Kleisli.pure(term)
+        }
+    }
 }
--- rholang/src/test/scala/coop/rchain/rholang/interpreter/SubstituteTest.scala
+++ rholang/src/test/scala/coop/rchain/rholang/interpreter/SubstituteTest.scala
@@ -8,6 +8,8 @@ import coop.rchain.rholang.interpreter.Substitute._
 import coop.rchain.rholang.interpreter.implicits._
 import org.scalatest.{FlatSpec, Matchers}
 import cats.{Eval => CEval}
+import cats.implicits._
+import monix.eval.Coeval
 
 import scala.collection.immutable.BitSet
 
@@ -15,26 +17,26 @@ class VarSubSpec extends FlatSpec with Matchers {
   "FreeVar" should "throw an error" in {
     val source: Par   = GPrivate()
     val env: Env[Par] = Env.makeEnv(source)
-    an[Error] should be thrownBy maybeSubstitute[CEval](FreeVar(0)).run(env).value
+    an[Error] should be thrownBy maybeSubstitute[Coeval](FreeVar(0)).run(env).value
   }
 
   "BoundVar" should "be substituted for process" in {
     val source: Par   = GPrivate()
     val env: Env[Par] = Env.makeEnv(source)
-    val result        = maybeSubstitute[CEval](BoundVar(0)).run(env).value
+    val result        = maybeSubstitute[Coeval](BoundVar(0)).run(env).value
     result should be(Right(source))
   }
 
   "BoundVar" should "be substituted with expression" in {
     val source: Par   = Send(ChanVar(BoundVar(0)), List(Par()), false, BitSet(0))
     val env: Env[Par] = Env.makeEnv(source)
-    val result        = maybeSubstitute[CEval](BoundVar(0)).run(env).value
+    val result        = maybeSubstitute[Coeval](BoundVar(0)).run(env).value
     result should be(Right(source))
   }
 
   "BoundVar" should "be left unchanged" in {
     val env                 = Env.makeEnv(GPrivate(): Par, GPrivate(): Par).shift(1)
-    val result              = maybeSubstitute[CEval](BoundVar(0)).run(env).value
+    val result              = maybeSubstitute[Coeval](BoundVar(0)).run(env).value
     val expectedResult: Var = BoundVar(0)
     result should be(Left(expectedResult))
   }
@@ -43,7 +45,7 @@ class VarSubSpec extends FlatSpec with Matchers {
 class ChannelSubSpec extends FlatSpec with Matchers {
   "ChanVar" should "be left unchanged" in {
     val env                     = Env.makeEnv(GPrivate(): Par, GPrivate(): Par).shift(1)
-    val result                  = substitute[CEval, Channel](ChanVar(BoundVar(0))).run(env).value
+    val result                  = substitute[Coeval, Channel](ChanVar(BoundVar(0))).run(env).value
     val expectedResult: Channel = ChanVar(BoundVar(0))
     result should be(expectedResult)
   }
@@ -52,7 +54,7 @@ class ChannelSubSpec extends FlatSpec with Matchers {
     val env    = Env.makeEnv(GPrivate(): Par).shift(1)
     val par    = Send(ChanVar(BoundVar(0)), List(Par()), false, BitSet(0))
     val target = Quote(par)
-    val result = substitute[CEval, Channel](target).run(env).value
+    val result = substitute[Coeval, Channel](target).run(env).value
     val expectedResult: Channel =
       Quote(Send(ChanVar(BoundVar(0)), List(Par()), false, BitSet(0)))
     result should be(expectedResult)
@@ -61,7 +63,7 @@ class ChannelSubSpec extends FlatSpec with Matchers {
   "Channel" should "be substituted for a Quote" in {
     val source: Par             = GPrivate()
     val env                     = Env.makeEnv(source)
-    val result                  = substitute[CEval, Channel](ChanVar(BoundVar(0))).run(env).value
+    val result                  = substitute[Coeval, Channel](ChanVar(BoundVar(0))).run(env).value
     val expectedResult: Channel = Quote(source)
     result should be(expectedResult)
   }
@@ -72,7 +74,7 @@ class SendSubSpec extends FlatSpec with Matchers {
 
     val env = Env.makeEnv(GPrivate(): Par, GPrivate(): Par).shift(1)
     val result =
-      substitute[CEval, Send](Send(ChanVar(BoundVar(0)), List(Par()), false, BitSet(0)))
+      substitute[Coeval, Send](Send(ChanVar(BoundVar(0)), List(Par()), false, BitSet(0)))
         .run(env)
         .value
     result should be(Send(ChanVar(BoundVar(0)), List(Par()), false, BitSet(0)))
@@ -82,7 +84,7 @@ class SendSubSpec extends FlatSpec with Matchers {
     val source0: Par = GPrivate()
     val source1: Par = GPrivate()
     val env          = Env.makeEnv(source0, source1)
-    val result = substitute[CEval, Send](
+    val result = substitute[Coeval, Send](
       Send(Quote(Send(ChanVar(BoundVar(1)), List(Par()), false, BitSet(1))),
            List(Par()),
            false,
@@ -106,7 +108,7 @@ class SendSubSpec extends FlatSpec with Matchers {
                       BitSet(0))
     val _target =
       Send(Quote(source), List(Send(Quote(source), List(Par()), false, BitSet())), false, BitSet())
-    val result = substitute[CEval, Send](target).run(env).value
+    val result = substitute[Coeval, Send](target).run(env).value
     result should be(_target)
   }
 
@@ -116,7 +118,7 @@ class SendSubSpec extends FlatSpec with Matchers {
     val env         = Env.makeEnv(source)
     val target =
       Send(chan0, List(Send(chan0, List(Par()), false, BitSet(0))), false, BitSet(0))
-    val result = substitute[CEval, Send](target).run(env).value
+    val result = substitute[Coeval, Send](target).run(env).value
     result should be(
       Send(
         Quote(New(1, Send(chan0, List(Par()), false, BitSet(0)), BitSet())),
@@ -140,7 +142,7 @@ class NewSubSpec extends FlatSpec with Matchers {
     val source: Par = GPrivate()
     val env         = Env.makeEnv(source)
     val target      = New(1, Send(ChanVar(BoundVar(1)), List(Par()), false, BitSet(1)), BitSet(0))
-    val result      = substitute[CEval, New](target).run(env).value
+    val result      = substitute[Coeval, New](target).run(env).value
     result should be(New(1, Send(Quote(source), List(Par()), false, BitSet()), BitSet()))
   }
 
@@ -155,7 +157,7 @@ class NewSubSpec extends FlatSpec with Matchers {
                           BitSet(2, 3)),
                      BitSet(0, 1))
 
-    val result = substitute[CEval, New](target).run(env).value
+    val result = substitute[Coeval, New](target).run(env).value
     result should be(
       New(
         2,
@@ -172,7 +174,7 @@ class EvalSubSpec extends FlatSpec with Matchers {
   "Eval" should "remove Eval/Quote pairs." in {
     val env: Env[Par] = Env.makeEnv(GPrivate("one"), GPrivate("zero"))
     val target: Par   = Eval(ChanVar(BoundVar(1)))
-    val result        = substitute[CEval, Par](target).run(env).value
+    val result        = substitute[Coeval, Par](target).run(env).value
     val expected: Par = GPrivate("one")
 
     result should be(expected)
@@ -184,7 +186,7 @@ class BundleSubSpec extends FlatSpec with Matchers {
     val source: Par = GPrivate()
     val env         = Env.makeEnv(source)
     val target      = Bundle(Send(ChanVar(BoundVar(0)), List(Par()), false, BitSet(0)))
-    val result      = substitute[CEval, Bundle](target).run(env).value
+    val result      = substitute[Coeval, Bundle](target).run(env).value
     result should be(
       Bundle(Send(Quote(source), List(Par()), false, BitSet()))
     )
@@ -200,7 +202,7 @@ class BundleSubSpec extends FlatSpec with Matchers {
            false,
            BitSet(0, 1))
     )
-    val result = substitute[CEval, Bundle](target).run(env).value
+    val result = substitute[Coeval, Bundle](target).run(env).value
     result should be(
       Bundle(
         Send(Quote(source0),
@@ -214,7 +216,7 @@ class BundleSubSpec extends FlatSpec with Matchers {
     val r: Par              = Bundle(body = Expr(GString("stdout")), writeFlag = true, readFlag = false) // bundle+ { "stdout" }
     val env                 = Env.makeEnv(r)
     val bundle: Par         = Bundle(Expr(EVarBody(EVar(BoundVar(0)))), writeFlag = false, readFlag = true)
-    val result              = substitute[CEval, Par](bundle).run(env).value
+    val result              = substitute[Coeval, Par](bundle).run(env).value
     val expectedResult: Par = Bundle(Expr(GString("stdout")), writeFlag = false, readFlag = false)
 
     result should be(expectedResult)
