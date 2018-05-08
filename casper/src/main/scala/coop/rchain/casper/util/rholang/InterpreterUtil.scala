54b6bfd99b71ad4e367fbcf81ab1ec9ba990242d
birchmd <birch@pyrofex.net>
2018-05-08 09:35:31 -0400

    checkpoint tuplespace by copying mdb files

--- casper/src/main/scala/coop/rchain/casper/util/rholang/InterpreterUtil.scala
+++ casper/src/main/scala/coop/rchain/casper/util/rholang/InterpreterUtil.scala
@@ -5,22 +5,10 @@ import coop.rchain.models.Par
 import coop.rchain.rholang.interpreter.RholangCLI
 
 import java.io.StringReader
-import java.nio.file.{Files, Path, Paths, StandardCopyOption}
 
 object InterpreterUtil {
 
   def mkTerm(s: String): Either[Throwable, Par] =
     RholangCLI.buildNormalizedTerm(new StringReader(s))
-
-  def copyDB(source: Path, dest: Path): Unit = {
-    val srcLock = Paths.get(source.toString + "/lock.mdb")
-    val srcData = Paths.get(source.toString + "/data.mdb")
-
-    val destLock = Paths.get(dest.toString + "/lock.mdb")
-    val destData = Paths.get(dest.toString + "/data.mdb")
-
-    Files.copy(srcLock, destLock, StandardCopyOption.REPLACE_EXISTING)
-    Files.copy(srcData, destData, StandardCopyOption.REPLACE_EXISTING)
-  }
 
 }
--- casper/src/main/scala/coop/rchain/casper/util/rholang/Tuplespace.scala
+++ casper/src/main/scala/coop/rchain/casper/util/rholang/Tuplespace.scala
@@ -4,33 +4,51 @@ import com.google.protobuf.ByteString
 
 import coop.rchain.catscontrib.TaskContrib._
 import coop.rchain.crypto.hash.Sha256
+import coop.rchain.crypto.codec.Base16
 import coop.rchain.models.Par
 import coop.rchain.rholang.interpreter.Runtime
 import coop.rchain.rholang.interpreter.storage.StoragePrinter
 
-import java.nio.file.Path
+import java.nio.file.{Files, Path, StandardCopyOption}
 
 import monix.execution.Scheduler
 
-class Tuplespace(val location: Path, val size: Long) {
-  val runtime = Runtime.create(location, size)
+class Tuplespace(val name: String, val location: Path, val size: Long) {
+  val dbLocation: Path = location.resolve(name)
+  val runtime: Runtime = Runtime.create(dbLocation, size)
 
-  def withTerm(term: Par, dest: Path)(implicit scheduler: Scheduler): Tuplespace = {
-    InterpreterUtil.copyDB(location, dest)
-    val result = new Tuplespace(dest, size)
-    Tuplespace.updateInPlace(result, term)
-  }
+  def addTerm(term: Par)(implicit scheduler: Scheduler): Unit =
+    runtime.reducer.inj(term).unsafeRunSync
 
-  def hash: ByteString = {
+  def hash: Array[Byte] = {
     val bytes = ByteString.copyFromUtf8(StoragePrinter.prettyPrint(runtime.store))
-    val hsh   = Sha256.hash(bytes.toByteArray)
-    ByteString.copyFrom(hsh)
+    Sha256.hash(bytes.toByteArray)
   }
+
+  def checkpoint: Checkpoint = {
+    val hsh        = hash
+    val hashString = Base16.encode(hsh)
+    val dest       = location.resolve(hashString)
+
+    dest.toFile.mkdir()
+    Tuplespace.copyDB(dbLocation, dest)
+
+    new Checkpoint(ByteString.copyFrom(hsh), location, size)
+  }
+
+  def storageRepr: String =
+    StoragePrinter.prettyPrint(runtime.store)
 }
 
 object Tuplespace {
-  def updateInPlace(source: Tuplespace, term: Par)(implicit scheduler: Scheduler): Tuplespace = {
-    source.runtime.reducer.inj(term).unsafeRunSync
-    source
+  def copyDB(source: Path, dest: Path): Unit = {
+    val srcLock = source.resolve("lock.mdb")
+    val srcData = source.resolve("data.mdb")
+
+    val destLock = dest.resolve("lock.mdb")
+    val destData = dest.resolve("data.mdb")
+
+    Files.copy(srcLock, destLock, StandardCopyOption.REPLACE_EXISTING)
+    Files.copy(srcData, destData, StandardCopyOption.REPLACE_EXISTING)
   }
 }
