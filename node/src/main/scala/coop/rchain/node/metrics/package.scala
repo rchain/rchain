2c37c9b52902123c1a0d2af8069aa0a573a6aaf6
Sebastian Bach <sebastian@pyrofex.net>
2018-05-15 17:15:12 +0200

    JMX Metrics effect

--- node/src/main/scala/coop/rchain/node/metrics/package.scala
+++ node/src/main/scala/coop/rchain/node/metrics/package.scala
@@ -1,26 +1,106 @@
 package coop.rchain.node
 
+import java.lang.management.ManagementFactory
+
+import scala.collection.JavaConverters._
+
 import cats.Applicative
+import cats.implicits._
 
 package object metrics {
 
   def jmxMetrics[F[_]: Applicative]: JmxMetrics[F] =
     new JmxMetrics[F] {
 
-      import java.lang.management.ManagementFactory
-      import javax.management.{Attribute, ObjectName}
+      private def check[A](a: A)(implicit n: Numeric[A]): Option[A] = checkOpt(Some(a))
 
-      private val mBeanServer  = ManagementFactory.getOperatingSystemMXBean
-      private val operatingSystem = mBeanServer.getObjectInstance()
+      private def checkOpt[A](a: Option[A])(implicit n: Numeric[A]): Option[A] =
+        a.filter(n.gteq(_, n.zero))
 
-//      private val operatingSystem = ManagementFactory.getOperatingSystemMXBean
-//      val x = operatingSystem.getSystemLoadAverage
+      def processCpu: F[ProcessCpu] =
+        ManagementFactory.getOperatingSystemMXBean match {
+          case b: com.sun.management.OperatingSystemMXBean =>
+            ProcessCpu(check(b.getProcessCpuLoad), check(b.getProcessCpuTime)).pure[F]
+          case _ => ProcessCpu().pure[F]
+        }
 
-      def processCpu: F[ProcessCpu] = ???
-      def memoryUsage: F[Memory] = ???
-      def garbageCollectors: F[Seq[GarbageCollector]] = ???
-      def threadPools: F[Seq[MemoryPool]] = ???
-      def threads: F[Threads] = ???
+      def memoryUsage: F[MemoryUsage] = {
+        val b       = ManagementFactory.getMemoryMXBean
+        val heap    = b.getHeapMemoryUsage
+        val nonHeap = b.getNonHeapMemoryUsage
+        MemoryUsage(
+          Memory(
+            committed = heap.getCommitted,
+            init = heap.getInit,
+            max = heap.getMax,
+            used = heap.getUsed
+          ),
+          Memory(
+            committed = nonHeap.getCommitted,
+            init = nonHeap.getInit,
+            max = nonHeap.getMax,
+            used = nonHeap.getUsed
+          )
+        ).pure[F]
+      }
+
+      def garbageCollectors: F[Seq[GarbageCollector]] =
+        ManagementFactory.getGarbageCollectorMXBeans.asScala
+          .map {
+            case b: com.sun.management.GarbageCollectorMXBean =>
+              val last = Option(b.getLastGcInfo)
+              GarbageCollector(
+                name = b.getName,
+                totalCollections = b.getCollectionCount,
+                totalCollectionTime = b.getCollectionTime,
+                startTime = checkOpt(last.map(_.getStartTime)),
+                endTime = checkOpt(last.map(_.getEndTime)),
+                duration = checkOpt(last.map(_.getDuration))
+              )
+            case b =>
+              GarbageCollector(
+                name = b.getName,
+                totalCollections = b.getCollectionCount,
+                totalCollectionTime = b.getCollectionTime
+              )
+          }
+          .toSeq
+          .pure[F]
+
+      def memoryPools: F[Seq[MemoryPool]] =
+        ManagementFactory.getMemoryPoolMXBeans.asScala
+          .map { b =>
+            val usage     = b.getUsage
+            val peakUsage = b.getPeakUsage
+            MemoryPool(
+              name = b.getName,
+              poolType = b.getType.toString,
+              usage = Memory(
+                committed = usage.getCommitted,
+                init = usage.getInit,
+                max = usage.getMax,
+                used = usage.getUsed
+              ),
+              peakUsage = Memory(
+                committed = peakUsage.getCommitted,
+                init = peakUsage.getInit,
+                max = peakUsage.getMax,
+                used = peakUsage.getUsed
+              )
+            )
+          }
+          .toSeq
+          .pure[F]
+
+      def threads: F[Threads] = {
+        val b = ManagementFactory.getThreadMXBean
+        Threads(
+          threadCount = b.getThreadCount,
+          daemonThreadCount = b.getDaemonThreadCount,
+          peakThreadCount = b.getPeakThreadCount,
+          totalStartedThreadCount = b.getTotalStartedThreadCount
+        ).pure[F]
+      }
     }
 
 }
