ea376e7db454a64d10d36ff8267448d60299f5fd
Sebastian Bach <sebastian@pyrofex.net>
2018-05-25 14:45:15 +0200

    Change description of certificate option

--- node/src/main/scala/coop/rchain/node/conf.scala
+++ node/src/main/scala/coop/rchain/node/conf.scala
@@ -15,7 +15,10 @@ final case class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
   val diagnostics = opt[Boolean](default = Some(false), short = 'd', descr = "Node diagnostics")
 
   val certificate =
-    opt[String](default = None, short = 'c', descr = "Read node key from a X.509 certificate file")
+    opt[String](default = None,
+                short = 'c',
+                descr =
+                  "Path to node's X.509 certificate file, that is being used for identification")
 
   val port =
     opt[Int](default = Some(30304),
