b5c1da334b5aea7e5d9db473f392289c53a25e97
Sebastian Bach <sebastian@pyrofex.net>
2018-05-25 17:03:26 +0200

    Add TLS to GRPC TransportLayer with public key checks

--- node/src/main/scala/coop/rchain/node/effects/TcpTransportLayer.scala
+++ node/src/main/scala/coop/rchain/node/effects/TcpTransportLayer.scala
@@ -1,28 +1,37 @@
 package coop.rchain.node.effects
 
-import scala.concurrent.duration._
-import java.net.SocketAddress
+import java.io.File
+
 import coop.rchain.comm._, CommError._
 import coop.rchain.comm.protocol.routing._
 import coop.rchain.p2p.effects._
 import coop.rchain.metrics.Metrics
-import io.grpc.{ManagedChannel, ManagedChannelBuilder, Server, ServerBuilder}
 
 import cats._, cats.data._, cats.implicits._
 import coop.rchain.catscontrib._, Catscontrib._, ski._, TaskContrib._
 
-import scala.concurrent.duration.{Duration, MILLISECONDS}
+import scala.concurrent.duration._
 import scala.util.Try
-import scala.concurrent.{Await, Promise}
+import scala.concurrent.Await
 import scala.concurrent.{ExecutionContext, Future}
+import io.grpc.netty._
+import io.netty.handler.ssl.{ClientAuth, SslContext}
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory
 
 class TcpTransportLayer[F[_]: Monad: Capture: Metrics: Futurable](host: String, port: Int)(
     src: PeerNode)(implicit executionContext: ExecutionContext)
     extends TransportLayer[F] {
 
-  private def client(endpoint: Endpoint) =
-    TransportLayerGrpc.stub(
-      ManagedChannelBuilder.forAddress(endpoint.host, endpoint.tcpPort).usePlaintext(true).build)
+  private def client(endpoint: Endpoint) = {
+    val channel = NettyChannelBuilder
+      .forAddress(host, port)
+      .negotiationType(NegotiationType.TLS)
+      .sslContext(buildClientSslContext("", ""))
+      .intercept(new SslSessionClientInterceptor())
+      .build()
+
+    TransportLayerGrpc.stub(channel)
+  }
 
   def roundTrip(msg: ProtocolMessage,
                 remote: ProtocolNode,
@@ -58,13 +67,29 @@ class TcpTransportLayer[F[_]: Monad: Capture: Metrics: Futurable](host: String, 
 
   def receive(dispatch: ProtocolMessage => F[Option[ProtocolMessage]]): F[Unit] =
     Capture[F].capture {
-      ServerBuilder
+      NettyServerBuilder
         .forPort(port)
+        .sslContext(buildServerSslContext("", ""))
         .addService(
           TransportLayerGrpc.bindService(new TranportLayerImpl[F](dispatch), executionContext))
+        .intercept(new SslSessionServerInterceptor())
         .build
         .start
     }
+
+  def buildServerSslContext(certChainFile: String, privateKeyFile: String): SslContext =
+    GrpcSslContexts
+      .forServer(new File(certChainFile), new File(privateKeyFile))
+      .trustManager(InsecureTrustManagerFactory.INSTANCE)
+      .clientAuth(ClientAuth.OPTIONAL)
+      .build()
+
+  def buildClientSslContext(certChainFile: String, privateKeyFile: String): SslContext = {
+    val builder = GrpcSslContexts.forClient
+    builder.trustManager(InsecureTrustManagerFactory.INSTANCE)
+    builder.keyManager(new File(certChainFile), new File(privateKeyFile))
+    builder.build
+  }
 }
 
 class TranportLayerImpl[F[_]: Monad: Capture: Metrics: Futurable](
@@ -72,7 +97,7 @@ class TranportLayerImpl[F[_]: Monad: Capture: Metrics: Futurable](
     extends TransportLayerGrpc.TransportLayer {
 
   def send(request: TLRequest): Future[TLResponse] =
-    (request.protocol
+    request.protocol
       .fold(internalServerError("protocol not available in request").pure[F]) { protocol =>
         ProtocolMessage.toProtocolMessage(protocol) match {
           case Left(error) => internalServerError(error.toString).pure[F]
@@ -82,7 +107,7 @@ class TranportLayerImpl[F[_]: Monad: Capture: Metrics: Futurable](
               case Some(pm) => returnProtocol(pm.proto).pure[F]
             }
         }
-      })
+      }
       .toFuture
 
   private def returnProtocol(protocol: Protocol): TLResponse =
