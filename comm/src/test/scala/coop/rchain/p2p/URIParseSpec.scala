ba0e53bc048174a42df1a01c215391147ab841c2
Sebastian Bach <sebastian@pyrofex.net>
2018-05-28 14:13:02 +0200

    Fix comm unit test

--- comm/src/test/scala/coop/rchain/p2p/URIParseSpec.scala
+++ comm/src/test/scala/coop/rchain/p2p/URIParseSpec.scala
@@ -8,9 +8,11 @@ class URIParseSpec extends FlatSpec with Matchers {
     Left(ParseError(s"bad address: $s"))
 
   "A well formed rnode URI" should "parse into a PeerNode" in {
-    val uri = "rnode://abcde@localhost:12345"
+    val uri = "rnode://abcdef@localhost:12345"
     NetworkAddress.parse(uri) should be(
-      Right(PeerNode(NodeIdentifier("abcde".getBytes), Endpoint("localhost", 12345, 12345))))
+      Right(
+        PeerNode(NodeIdentifier("abcdef".sliding(2, 2).toArray.map(Integer.parseInt(_, 16).toByte)),
+          Endpoint("localhost", 12345, 12345))))
   }
 
   "A non-rnode URI" should "parse as an error" in {
