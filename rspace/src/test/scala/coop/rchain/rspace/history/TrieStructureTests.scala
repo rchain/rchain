b5ac7969af4216c0b2a57afe523ef26ba3332e3f
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2018-05-30 21:20:52 +0200

    [WIP] checke levels after inserting 2 elements

--- rspace/src/test/scala/coop/rchain/rspace/history/TrieStructureTests.scala
+++ rspace/src/test/scala/coop/rchain/rspace/history/TrieStructureTests.scala
@@ -55,9 +55,8 @@ class TrieStructureTests
 
   it should "have two levels after inserting one element" in {
     withTestTrieStore { store =>
-      // Root before: 0xc575260cf13e36f179a50b0882bd64fc0466ecd25bdd7bc88766c2cc2e4c0dfe
       insert(store, key1, val1)
-      // Root after: 0x538b82b41d4360492c17a112864ffb989571504b73b1281677a692e9fd2ee4cc
+
       store.withTxn(store.createTxnRead()) { txn =>
         val trie = store.get(txn, store.workingRootHash.get)
         trie match {
@@ -78,4 +77,80 @@ class TrieStructureTests
     }
   }
 
+  it should "have four levels after inserting second element with same hash prefix" in {
+    withTestTrieStore { store =>
+      insert(store, key1, val1)
+      insert(store, key2, val2)
+
+      store.withTxn(store.createTxnRead()) { txn =>
+        val trie = store.get(txn, store.workingRootHash.get)
+        trie match {
+          case Some(Node(PointerBlock(vector))) =>
+            vector should have size 256
+            val expectedHash = Blake2b256Hash
+              .fromHex("0xc67f3d92de6f9e75b9561274fbcc4c8efe8d1161554e3b0f66307a111ddd11ce")
+              .get
+
+            val maybeExpectedHash = Some(expectedHash)
+            vector(1) shouldBe maybeExpectedHash
+            vector.filterNot(_ == maybeExpectedHash) should contain only None
+
+            store.get(txn, expectedHash) match {
+              case Some(Node(PointerBlock(vector))) =>
+                vector should have size 256
+                val expectedHash = Blake2b256Hash
+                  .fromHex("0xdb61c95e4ea234de4fe154861f86d090af2029da3cfca83cf210c71c539f1942")
+                  .get
+
+                val maybeExpectedHash = Some(expectedHash)
+                vector(0) shouldBe maybeExpectedHash
+                vector.filterNot(_ == maybeExpectedHash) should contain only None
+
+                store.get(txn, expectedHash) match {
+
+                  case Some(Node(PointerBlock(vector))) =>
+                    vector should have size 256
+                    val expectedHash = Blake2b256Hash
+                      .fromHex("0x681aff745729ccec1d5e3f23f5b56796416a6604a8a07b646fc16f1b0e8c70c8")
+                      .get
+
+                    val maybeExpectedHash = Some(expectedHash)
+                    vector(0) shouldBe maybeExpectedHash
+                    vector.filterNot(_ == maybeExpectedHash) should contain only None
+                    store.get(txn, expectedHash) match {
+
+                      case Some(Node(PointerBlock(vector))) =>
+                        vector should have size 256
+                        val expectedHash1 = Blake2b256Hash
+                          .fromHex(
+                            "0x8d329ed700f130f40b15b73b1bd4f7b70d982acb9dce55e58f58425038f5db1c")
+                          .get
+                        val expectedHash2 = Blake2b256Hash
+                          .fromHex(
+                            "0xf22c71982cf8663fb1ea77a444233c99d8c00cd187b0253cfc4213228fea6625")
+                          .get
+
+                        val maybeExpectedHash1 = Some(expectedHash1)
+                        val maybeExpectedHash2 = Some(expectedHash2)
+
+                        vector(0) shouldBe maybeExpectedHash1
+                        vector(1) shouldBe maybeExpectedHash2
+                        vector
+                          .filterNot(_ == maybeExpectedHash1)
+                          .filterNot(_ == maybeExpectedHash2) should contain only None
+
+                        store.get(txn, expectedHash1) shouldBe Some(Leaf(key1, val1))
+                        store.get(txn, expectedHash2) shouldBe Some(Leaf(key2, val2))
+                    }
+                }
+
+              case _ =>
+            }
+
+          case _ => fail("expected a node")
+        }
+      }
+
+    }
+  }
 }
