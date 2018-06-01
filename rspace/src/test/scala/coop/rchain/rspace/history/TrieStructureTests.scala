c51cda1a934ef3bbaa762510589c4914ad3e53fb
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2018-06-01 11:55:59 +0200

    Test retention after delete

--- rspace/src/test/scala/coop/rchain/rspace/history/TrieStructureTests.scala
+++ rspace/src/test/scala/coop/rchain/rspace/history/TrieStructureTests.scala
@@ -77,14 +77,61 @@ class TrieStructureTests
       insert(store, key1, val1)
       insert(store, key2, val2)
 
-      val rootHex   = "0xae608338e532497ea0844d9efd6008251451538497d30d3d4dbfb9a032fc8feb"
-      val level1Hex = "0xc67f3d92de6f9e75b9561274fbcc4c8efe8d1161554e3b0f66307a111ddd11ce"
-      val level2Hex = "0xdb61c95e4ea234de4fe154861f86d090af2029da3cfca83cf210c71c539f1942"
-      val level3Hex = "0x681aff745729ccec1d5e3f23f5b56796416a6604a8a07b646fc16f1b0e8c70c8"
-      val leaf1Hex  = "0x8d329ed700f130f40b15b73b1bd4f7b70d982acb9dce55e58f58425038f5db1c"
-      val leaf2Hex  = "0xf22c71982cf8663fb1ea77a444233c99d8c00cd187b0253cfc4213228fea6625"
+      store.withTxn(store.createTxnRead()) { implicit txn =>
+        val rootHex   = "0xae608338e532497ea0844d9efd6008251451538497d30d3d4dbfb9a032fc8feb"
+        val level1Hex = "0xc67f3d92de6f9e75b9561274fbcc4c8efe8d1161554e3b0f66307a111ddd11ce"
+        val level2Hex = "0xdb61c95e4ea234de4fe154861f86d090af2029da3cfca83cf210c71c539f1942"
+        val level3Hex = "0x681aff745729ccec1d5e3f23f5b56796416a6604a8a07b646fc16f1b0e8c70c8"
+        val leaf1Hex  = "0x8d329ed700f130f40b15b73b1bd4f7b70d982acb9dce55e58f58425038f5db1c"
+        val leaf2Hex  = "0xf22c71982cf8663fb1ea77a444233c99d8c00cd187b0253cfc4213228fea6625"
+
+        expectNode(rootHex, Seq((1, level1Hex)))
+        expectNode(level1Hex, Seq((0, level2Hex)))
+        expectNode(level2Hex, Seq((0, level3Hex)))
+        expectNode(level3Hex, Seq((0, leaf1Hex), (1, leaf2Hex)))
+
+        val expectedLeaf1Hash = Blake2b256Hash
+          .fromHex(leaf1Hex)
+          .get
+        val expectedLeaf2Hash = Blake2b256Hash
+          .fromHex(leaf2Hex)
+          .get
+
+        store.get(txn, expectedLeaf1Hash) shouldBe Some(Leaf(key1, val1))
+        store.get(txn, expectedLeaf2Hash) shouldBe Some(Leaf(key2, val2))
+      }
+
+    }
+  }
+
+  it should "retain previous structure after delete" in {
+    withTestTrieStore { implicit store =>
+      insert(store, key1, val1)
+      insert(store, key2, val2)
+
+      delete(store, key2, val2)
 
       store.withTxn(store.createTxnRead()) { implicit txn =>
+        val rootHex = "0x538b82b41d4360492c17a112864ffb989571504b73b1281677a692e9fd2ee4cc"
+        val leafHex = "0x8d329ed700f130f40b15b73b1bd4f7b70d982acb9dce55e58f58425038f5db1c"
+
+        expectNode(rootHex, Seq((1, leafHex)))
+
+        val expectedLeafHash = Blake2b256Hash
+          .fromHex(leafHex)
+          .get
+
+        store.get(txn, expectedLeafHash) shouldBe Some(Leaf(key1, val1))
+      }
+
+      store.withTxn(store.createTxnRead()) { implicit txn =>
+        val rootHex   = "0xae608338e532497ea0844d9efd6008251451538497d30d3d4dbfb9a032fc8feb"
+        val level1Hex = "0xc67f3d92de6f9e75b9561274fbcc4c8efe8d1161554e3b0f66307a111ddd11ce"
+        val level2Hex = "0xdb61c95e4ea234de4fe154861f86d090af2029da3cfca83cf210c71c539f1942"
+        val level3Hex = "0x681aff745729ccec1d5e3f23f5b56796416a6604a8a07b646fc16f1b0e8c70c8"
+        val leaf1Hex  = "0x8d329ed700f130f40b15b73b1bd4f7b70d982acb9dce55e58f58425038f5db1c"
+        val leaf2Hex  = "0xf22c71982cf8663fb1ea77a444233c99d8c00cd187b0253cfc4213228fea6625"
+
         expectNode(rootHex, Seq((1, level1Hex)))
         expectNode(level1Hex, Seq((0, level2Hex)))
         expectNode(level2Hex, Seq((0, level3Hex)))
