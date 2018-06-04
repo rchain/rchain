8098e661aa327f6aa5787fa01a47eeded5724690
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2018-06-04 11:10:59 +0200

    Change fixtures to data objects

--- rspace/src/test/scala/coop/rchain/rspace/history/TrieStructureTests.scala
+++ rspace/src/test/scala/coop/rchain/rspace/history/TrieStructureTests.scala
@@ -34,11 +34,30 @@ class TrieStructureTests
       }
     }
 
+  private[this] val SingleElementData = new {
+    val key1    = TestData.key1
+    val val1    = TestData.val1
+    val rootHex = "538b82b41d4360492c17a112864ffb989571504b73b1281677a692e9fd2ee4cc"
+    val leafHex = "8d329ed700f130f40b15b73b1bd4f7b70d982acb9dce55e58f58425038f5db1c"
+  }
+
+  private[this] val CommonPrefixData = new {
+    val key1 = TestData.key1
+    val val1 = TestData.val1
+    val key2 = TestData.key2
+    val val2 = TestData.val2
+
+    val rootHex   = "ae608338e532497ea0844d9efd6008251451538497d30d3d4dbfb9a032fc8feb"
+    val level1Hex = "c67f3d92de6f9e75b9561274fbcc4c8efe8d1161554e3b0f66307a111ddd11ce"
+    val level2Hex = "db61c95e4ea234de4fe154861f86d090af2029da3cfca83cf210c71c539f1942"
+    val level3Hex = "681aff745729ccec1d5e3f23f5b56796416a6604a8a07b646fc16f1b0e8c70c8"
+    val leaf1Hex  = "8d329ed700f130f40b15b73b1bd4f7b70d982acb9dce55e58f58425038f5db1c"
+    val leaf2Hex  = "f22c71982cf8663fb1ea77a444233c99d8c00cd187b0253cfc4213228fea6625"
+  }
+
   "insert's effect" should "be visible in the outer read transaction" ignore {
     withTrieTxnAndStore { (store, txn, trie) =>
-      val f = singleElementFixture
-      import f._
-
+      import SingleElementData._
       insert(store, key1, val1)
       // Insert was made in a nested transaction, so it's effect should be visible
       store.get(txn, store.getRoot(txn).get) should not be None
@@ -54,32 +73,9 @@ class TrieStructureTests
       case _ => fail("expected a node")
     }
 
-  private[this] def singleElementFixture = new {
-    val key1    = TestData.key1
-    val val1    = TestData.val1
-    val rootHex = "538b82b41d4360492c17a112864ffb989571504b73b1281677a692e9fd2ee4cc"
-    val leafHex = "8d329ed700f130f40b15b73b1bd4f7b70d982acb9dce55e58f58425038f5db1c"
-  }
-
-  private[this] def commonPrefixFixture = new {
-    val key1 = TestData.key1
-    val val1 = TestData.val1
-    val key2 = TestData.key2
-    val val2 = TestData.val2
-
-    val rootHex   = "ae608338e532497ea0844d9efd6008251451538497d30d3d4dbfb9a032fc8feb"
-    val level1Hex = "c67f3d92de6f9e75b9561274fbcc4c8efe8d1161554e3b0f66307a111ddd11ce"
-    val level2Hex = "db61c95e4ea234de4fe154861f86d090af2029da3cfca83cf210c71c539f1942"
-    val level3Hex = "681aff745729ccec1d5e3f23f5b56796416a6604a8a07b646fc16f1b0e8c70c8"
-    val leaf1Hex  = "8d329ed700f130f40b15b73b1bd4f7b70d982acb9dce55e58f58425038f5db1c"
-    val leaf2Hex  = "f22c71982cf8663fb1ea77a444233c99d8c00cd187b0253cfc4213228fea6625"
-
-  }
-
   it should "have two levels after inserting one element" in {
     withTestTrieStore { implicit store =>
-      val f = singleElementFixture
-      import f._
+      import SingleElementData._
       insert(store, key1, val1)
 
       assertSingleElementTrie
@@ -88,8 +84,7 @@ class TrieStructureTests
 
   it should "have four levels after inserting second element with same hash prefix" in {
     withTestTrieStore { implicit store =>
-      val f = commonPrefixFixture
-      import f._
+      import CommonPrefixData._
       insert(store, key1, val1)
       insert(store, key2, val2)
 
@@ -99,8 +94,7 @@ class TrieStructureTests
 
   it should "retain previous structure after delete" in {
     withTestTrieStore { implicit store =>
-      val f = commonPrefixFixture
-      import f._
+      import CommonPrefixData._
 
       insert(store, key1, val1)
       insert(store, key2, val2)
@@ -114,8 +108,7 @@ class TrieStructureTests
 
   it should "retain previous structure after rollback" in {
     withTestTrieStore { implicit store =>
-      val f1 = commonPrefixFixture
-      import f1._
+      import CommonPrefixData._
 
       insert(store, key1, val1)
       insert(store, key2, val2)
@@ -123,7 +116,7 @@ class TrieStructureTests
       store.withTxn(store.createTxnWrite()) { txn =>
         store.putRoot(txn,
                       Blake2b256Hash
-                        .fromHex(singleElementFixture.rootHex))
+                        .fromHex(SingleElementData.rootHex))
       }
 
       assertSingleElementTrie
@@ -133,8 +126,7 @@ class TrieStructureTests
 
   private[this] def assertSingleElementTrie(
       implicit store: ITrieStore[Txn[ByteBuffer], TestKey, ByteVector]) = {
-    val f = singleElementFixture
-    import f._
+    import SingleElementData._
     store.withTxn(store.createTxnRead()) { implicit txn =>
       expectNode(rootHex, Seq((1, leafHex)))
       val expectedLeafHash = Blake2b256Hash
@@ -147,8 +139,7 @@ class TrieStructureTests
 
   private[this] def assertCommonPrefixTrie(
       implicit store: ITrieStore[Txn[ByteBuffer], TestKey, ByteVector]) = {
-    val f = commonPrefixFixture
-    import f._
+    import CommonPrefixData._
     store.withTxn(store.createTxnRead()) { implicit txn =>
       expectNode(rootHex, Seq((1, level1Hex)))
       expectNode(level1Hex, Seq((0, level2Hex)))
