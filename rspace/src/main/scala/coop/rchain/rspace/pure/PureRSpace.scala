743c5ab521b7b18201aeebec8dd7004df111a34f
Henry Till <henrytill@gmail.com>
2018-06-07 12:06:48 -0400

    CORE-592: rspace: fix PureRSpace#reset

--- rspace/src/main/scala/coop/rchain/rspace/pure/PureRSpace.scala
+++ rspace/src/main/scala/coop/rchain/rspace/pure/PureRSpace.scala
@@ -24,7 +24,7 @@ class PureRSpace[F[_], C, P, A, K](space: ISpace[C, P, A, K]) {
 
   def getCheckpoint()(c: Capture[F]): F[Blake2b256Hash] = c.capture(space.getCheckpoint())
 
-  def reset(hash: Blake2b256Hash)(c: Capture[F]): F[Unit] = c.capture(space.getCheckpoint())
+  def reset(hash: Blake2b256Hash)(c: Capture[F]): F[Unit] = c.capture(space.reset(hash))
 
   def close()(c: Capture[F]): F[Unit] = c.capture(space.close())
 }
