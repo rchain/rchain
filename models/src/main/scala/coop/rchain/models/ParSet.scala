a2d14eb68da9db089f133e5af9af5ff3e1d350a4
Mateusz Górski <gorski.mateusz@protonmail.ch>
2018-06-07 16:12:21 +0200

    Override equals and hashCode methods on ParSet

--- models/src/main/scala/coop/rchain/models/ParSet.scala
+++ models/src/main/scala/coop/rchain/models/ParSet.scala
@@ -4,6 +4,18 @@ import scala.collection.immutable.BitSet
 
 case class ParSet(ps: SortedHashSet[Par], connectiveUsed: Boolean = false) {
   lazy val locallyFree: BitSet = ParSet.updateLocallyFree(ps)
+
+  override def equals(o: scala.Any): Boolean = o match {
+    case parSet: ParSet =>
+      this.ps == parSet.ps && this.connectiveUsed == parSet.connectiveUsed && this.locallyFree == parSet.locallyFree
+    case _ => false
+  }
+
+  override def hashCode(): Int = (
+    37 * this.ps.hashCode() +
+      37 * this.connectiveUsed.hashCode() +
+      37 * this.locallyFree.hashCode()
+  )
 }
 
 object ParSet {
