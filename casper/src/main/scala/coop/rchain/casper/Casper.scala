f8369262552aea494950a180424e558fe3733bae
Kent Shikama <kent@kentshikama.com>
2018-06-10 21:39:28 -0700

    Fail with error on impossible cases for BlockStatus

--- casper/src/main/scala/coop/rchain/casper/Casper.scala
+++ casper/src/main/scala/coop/rchain/casper/Casper.scala
@@ -266,8 +266,7 @@ sealed abstract class MultiParentCasperInstances {
           case InvalidBlock =>
             Log[F].info(
               s"CASPER: Did not add invalid block ${PrettyPrinter.buildString(block.blockHash)}")
-          case InvalidBlockSignature => ().pure[F]
-          case Undecided             => ().pure[F]
+          case _ => throw new Error("Should never reach")
         }
 
       private def canAdd(block: BlockMessage): BlockStatus = {
