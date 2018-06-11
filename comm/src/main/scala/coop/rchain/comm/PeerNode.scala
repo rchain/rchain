44c250baaec3c9b1e6e7f9d0c37d1dd0583e27ad
Pawel Szulc <paul.szulc@gmail.com>
2018-06-11 12:06:16 +0200

    Rename comm.scala to PeerNode.scala

--- comm/src/main/scala/coop/rchain/comm/PeerNode.scala
+++ comm/src/main/scala/coop/rchain/comm/PeerNode.scala
@@ -2,11 +2,6 @@ package coop.rchain.comm
 
 import java.net.InetSocketAddress
 
-trait Comm[A] {
-  def send(data: Seq[Byte], p: PeerNode): Either[CommError, Unit]
-  def recv: Either[CommError, (A, Seq[Byte])]
-}
-
 // TODO: Add Show instance
 final case class NodeIdentifier(key: Seq[Byte]) {
   override def toString: String = key.map("%02x".format(_)).mkString
@@ -17,14 +12,13 @@ object NodeIdentifier {
     NodeIdentifier(name.sliding(2, 2).toArray.map(Integer.parseInt(_, 16).toByte))
 }
 
+// TODO: Add Show instance
 final case class Endpoint(host: String, tcpPort: Int, udpPort: Int) {
   val tcpSocketAddress = new InetSocketAddress(host, tcpPort)
   val udpSocketAddress = new InetSocketAddress(host, udpPort)
 }
 
-/**
-  * A PeerNode is (at least) an identifier and a network configuration.
-  */
+// TODO: Add Show instance
 final case class PeerNode(id: NodeIdentifier, endpoint: Endpoint) {
 
   def key  = id.key
@@ -36,9 +30,4 @@ final case class PeerNode(id: NodeIdentifier, endpoint: Endpoint) {
   def toAddress: String =
     s"rnode://$sKey@${endpoint.host}:${endpoint.udpPort}"
 
-}
-
-trait Notary {
-  def sign(data: Seq[Byte]): Seq[Byte]
-  def checkSignature(sig: Seq[Byte]): Boolean
 }
