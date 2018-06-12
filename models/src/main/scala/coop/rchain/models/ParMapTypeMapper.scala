a954ab0bb802a701d02285ab57964717a87d3d21
Mateusz Górski <gorski.mateusz@protonmail.ch>
2018-06-12 15:08:36 +0200

    Refactor ParMapTypeMapper

--- models/src/main/scala/coop/rchain/models/ParMapTypeMapper.scala
+++ models/src/main/scala/coop/rchain/models/ParMapTypeMapper.scala
@@ -6,13 +6,15 @@ object ParMapTypeMapper {
   implicit val parMapEMapTypeMapper: TypeMapper[EMap, ParMap] =
     TypeMapper(emapToParMap)(parMapToEMap)
 
-  private[models] def emapToParMap(emap: EMap): ParMap = {
-    def unzip(kvp: KeyValuePair): (Par, Par) = (kvp.key, kvp.value)
+  private[models] def emapToParMap(emap: EMap): ParMap =
     ParMap(emap.kvs.map(unzip), emap.connectiveUsed, emap.locallyFree)
-  }
 
-  private[models] def parMapToEMap(parMap: ParMap): EMap = {
-    def toKeyValuePair(tuple: (Par, Par)): KeyValuePair = KeyValuePair(tuple._1, tuple._2)
-    EMap(parMap.ps.sortedMap.map(toKeyValuePair), parMap.locallyFree.value, parMap.connectiveUsed)
-  }
+  private[models] def parMapToEMap(parMap: ParMap): EMap =
+    EMap(parMap.ps.sortedMap.map(t => zip(t._1, t._2)),
+         parMap.locallyFree.value,
+         parMap.connectiveUsed)
+
+  private[models] def unzip(kvp: KeyValuePair): (Par, Par) = (kvp.key, kvp.value)
+
+  private[models] def zip(k: Par, v: Par): KeyValuePair = KeyValuePair(k, v)
 }
