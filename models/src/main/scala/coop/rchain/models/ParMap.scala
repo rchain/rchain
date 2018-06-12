48cfdceba6fe024ac5a68998cc95f06116607539
Mateusz Górski <gorski.mateusz@protonmail.ch>
2018-06-12 15:04:55 +0200

    Add auxiliary constructors to ParMap

--- models/src/main/scala/coop/rchain/models/ParMap.scala
+++ models/src/main/scala/coop/rchain/models/ParMap.scala
@@ -27,4 +27,13 @@ object ParMap {
   def apply(seq: Seq[(Par, Par)], connectiveUsed: Boolean, locallyFree: BitSet): ParMap =
     new ParMap(SortedParMap(seq), connectiveUsed, Coeval.delay(locallyFree).memoize)
 
+  def apply(seq: Seq[(Par, Par)], connectiveUsed: Boolean): ParMap =
+    ParMap(seq, connectiveUsed, Coeval.delay(updateLocallyFree(seq)))
+
+  def updateLocallyFree(ps: Seq[(Par, Par)]): BitSet =
+    ps.foldLeft(BitSet()) {
+      case (acc, (key, value)) =>
+        acc | key.locallyFree | value.locallyFree
+    }
+
 }
