f327e60e5122e8a007a235563bc4fd81f75da6da
Mateusz Górski <gorski.mateusz@protonmail.ch>
2018-06-12 20:29:33 +0200

    RM dead code

--- models/src/main/scala/coop/rchain/models/HasLocallyFree.scala
+++ models/src/main/scala/coop/rchain/models/HasLocallyFree.scala
@@ -6,7 +6,3 @@ trait HasLocallyFree[T] {
   def connectiveUsed(source: T): Boolean
   def locallyFree(source: T): BitSet
 }
-
-object HasLocallyFree {
-  def of[T](implicit ev: HasLocallyFree[T]): HasLocallyFree[T] = ev
-}
