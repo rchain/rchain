8e95831fa52c70052156fd19c91f39c3f8c528df
ys-pyrofex <yaraslau@pyrofex.net>
2018-06-13 20:25:53 +0300

    Fixed comment text.

--- rspace/src/main/scala/coop/rchain/rspace/util/package.scala
+++ rspace/src/main/scala/coop/rchain/rspace/util/package.scala
@@ -45,18 +45,18 @@ package object util {
   }
 
   /**
-    * Extracts a continuation from
+    * Extracts a continuation from a produce result
     */
   def getK[A, K](t: Option[(K, A)]): K =
     t.map(_._1).get
 
   /** Runs a continuation with the accompanying data
     */
-  def runK[T](t: Option[(Function1[T, Unit], T)]): Unit =
+  def runK[T](t: Option[((T) => Unit, T)]): Unit =
     t.foreach { case (k, data) => k(data) }
 
   /** Runs a list of continuations with the accompanying data
     */
-  def runKs[T](t: Seq[Option[(Function1[T, Unit], T)]]): Unit =
+  def runKs[T](t: Seq[Option[((T) => Unit, T)]]): Unit =
     t.foreach { case Some((k, data)) => k(data); case None => () }
 }
