ef1902c527a4d54faf2040e9b713146f7c89a7a2
Ovidiu Deac <ovidiu@pyrofex.net>
2018-06-14 16:38:29 +0300

    fix build - second attempt

--- node/src/main/scala/coop/rchain/node/grpc.scala
+++ node/src/main/scala/coop/rchain/node/grpc.scala
@@ -8,27 +8,23 @@ import cats._
 import cats.data._
 import cats.implicits._
 import com.google.protobuf.empty.Empty
-import coop.rchain.casper.{MultiParentCasper, PrettyPrinter}
+import coop.rchain.casper.{MultiParentCasper}
 import coop.rchain.casper.protocol._
-import coop.rchain.casper.util.ProtoUtil
 import coop.rchain.casper.protocol.{Deploy, DeployServiceGrpc, DeployServiceResponse, DeployString}
 import coop.rchain.casper.util.rholang.InterpreterUtil
 import coop.rchain.catscontrib._
 import Catscontrib._
-import coop.rchain.crypto.codec.Base16
 import coop.rchain.node.model.repl._
 import coop.rchain.node.model.diagnostics._
-import coop.rchain.rholang.interpreter.{PrettyPrinter, RholangCLI, Runtime, errors}
+import coop.rchain.rholang.interpreter.{errors, PrettyPrinter, Runtime}
 import coop.rchain.rholang.interpreter.storage.StoragePrinter
 import monix.eval.Task
 import monix.execution.Scheduler
-import com.google.protobuf.ByteString
 import java.io.{Reader, StringReader}
 
 import coop.rchain.casper.api.BlockAPI
 import coop.rchain.node.diagnostics.{JvmMetrics, NodeMetrics, StoreMetrics}
 import coop.rchain.rholang.interpreter.errors.InterpreterError
-import coop.rchain.comm.transport._
 import coop.rchain.comm.discovery._
 import coop.rchain.models.Par
 import coop.rchain.rholang.interpreter.Interpreter._
@@ -122,9 +118,9 @@ object GrpcServer {
     def eval(request: EvalRequest): Future[ReplResponse] =
       exec(new StringReader(request.program))
 
-    def runEvaluate(runtime:Runtime, term : Par) : Task[Vector[errors.InterpreterError]] =
+    def runEvaluate(runtime: Runtime, term: Par): Task[Vector[errors.InterpreterError]] =
       for {
-        _ <- Task.now(printNormalizedTerm(term))
+        _      <- Task.now(printNormalizedTerm(term))
         result <- evaluate(runtime, term)
       } yield (result)
 
