b6e1dc474fb70315c7d15f2e1b17b81d328b8b8a
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2018-07-11 13:13:09 +0200

    Support install in ReplayRSpace

--- rspace/src/main/scala/coop/rchain/rspace/ReplayRSpace.scala
+++ rspace/src/main/scala/coop/rchain/rspace/ReplayRSpace.scala
@@ -33,6 +33,12 @@ class ReplayRSpace[C, P, A, R, K](val store: IStore[C, P, A, K], val branch: Bra
     sv
   }
 
+  private[this] val installs: SyncVar[Installs[C, K, R, A, P]] = {
+    val installs = new SyncVar[Installs[C, K, R, A, P]]()
+    installs.put(Map.empty)
+    installs
+  }
+
   def consume(channels: Seq[C], patterns: Seq[P], continuation: K, persist: Boolean)(
       implicit m: Match[P, A, R]): Option[(K, Seq[R])] =
     store.eventsCounter.registerConsume {
@@ -147,8 +153,58 @@ class ReplayRSpace[C, P, A, R, K](val store: IStore[C, P, A, K], val branch: Bra
       }
     }
 
+  private[this] def install(txn: store.T, channels: Seq[C], patterns: Seq[P], continuation: K)(
+      implicit m: Match[P, A, R]): Option[(K, Seq[R])] = {
+    if (channels.length =!= patterns.length) {
+      val msg = "channels.length must equal patterns.length"
+      logger.error(msg)
+      throw new IllegalArgumentException(msg)
+    }
+    logger.debug(s"""|install: searching for data matching <patterns: $patterns>
+                     |at <channels: $channels>""".stripMargin.replace('\n', ' '))
+
+    val consumeRef = Consume.create(channels, patterns, continuation, true)
+    installs.update(_.updated(channels, Install(patterns, continuation, m)))
+
+    /*
+     * Here, we create a cache of the data at each channel as `channelToIndexedData`
+     * which is used for finding matches.  When a speculative match is found, we can
+     * remove the matching datum from the remaining data candidates in the cache.
+     *
+     * Put another way, this allows us to speculatively remove matching data without
+     * affecting the actual store contents.
+     */
+
+    val channelToIndexedData = channels.map { (c: C) =>
+      c -> Random.shuffle(store.getData(txn, Seq(c)).zipWithIndex)
+    }.toMap
+
+    val options: Option[Seq[DataCandidate[C, R]]] =
+      extractDataCandidates(channels.zip(patterns), channelToIndexedData, Nil).sequence
+
+    options match {
+      case None =>
+        store.removeAll(txn, channels)
+        store.installWaitingContinuation(
+          txn,
+          channels,
+          WaitingContinuation(patterns, continuation, persist = true, consumeRef))
+        for (channel <- channels) store.addJoin(txn, channel, channels)
+        logger.debug(s"""|consume: no data found,
+                         |storing <(patterns, continuation): ($patterns, $continuation)>
+                         |at <channels: $channels>""".stripMargin.replace('\n', ' '))
+        None
+      case Some(_) =>
+        throw new RuntimeException("Installing can be done only on startup")
+    }
+
+  }
+
   def install(channels: Seq[C], patterns: Seq[P], continuation: K)(
-      implicit m: Match[P, A, R]): Option[(K, Seq[R])] = None
+      implicit m: Match[P, A, R]): Option[(K, Seq[R])] =
+    store.withTxn(store.createTxnWrite()) { txn =>
+      install(txn, channels, patterns, continuation)
+    }
 
   def produce(channel: C, data: A, persist: Boolean)(
       implicit m: Match[P, A, R]): Option[(K, Seq[R])] =
@@ -317,6 +373,13 @@ class ReplayRSpace[C, P, A, R, K](val store: IStore[C, P, A, K], val branch: Bra
     // update the replay data
     replayData.update(const(rigs))
   }
+
+  override protected[this] def restoreInstalls(txn: store.T): Unit =
+    installs.get.foreach {
+      case (channels, Install(patterns, continuation, _match)) =>
+        install(txn, channels, patterns, continuation)(_match)
+    }
+
 }
 
 object ReplayRSpace {
