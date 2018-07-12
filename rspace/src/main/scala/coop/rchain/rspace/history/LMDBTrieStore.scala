39cc4ad87c832f1e6e3a9406ae9ed05548b360c9
Dominik Zajkowski <dominik@pyrofex.net>
2018-07-12 17:08:15 +0200

    CORE-760 LMDB factor out common elements.

--- rspace/src/main/scala/coop/rchain/rspace/history/LMDBTrieStore.scala
+++ rspace/src/main/scala/coop/rchain/rspace/history/LMDBTrieStore.scala
@@ -2,7 +2,7 @@ package coop.rchain.rspace.history
 
 import java.nio.ByteBuffer
 
-import coop.rchain.rspace.Blake2b256Hash
+import coop.rchain.rspace.{Blake2b256Hash, LMDBStorage}
 import coop.rchain.shared.AttemptOps._
 import coop.rchain.shared.ByteVectorOps._
 import coop.rchain.shared.Resources.withResource
@@ -20,41 +20,14 @@ class LMDBTrieStore[K, V] private (val env: Env[ByteBuffer],
                                    _dbPastRoots: Dbi[ByteBuffer])(implicit
                                                                   codecK: Codec[K],
                                                                   codecV: Codec[V])
-    extends ITrieStore[Txn[ByteBuffer], K, V] {
+    extends ITrieStore[Txn[ByteBuffer], K, V]
+    with LMDBStorage {
 
-  private[rspace] def createTxnRead(): Txn[ByteBuffer] = env.txnRead
+  private[rspace] def put(txn: Txn[ByteBuffer], key: Blake2b256Hash, value: Trie[K, V]): Unit =
+    _dbTrie.put(txn, key, value)
 
-  private[rspace] def createTxnWrite(): Txn[ByteBuffer] = env.txnWrite
-
-  private[rspace] def withTxn[R](txn: Txn[ByteBuffer])(f: Txn[ByteBuffer] => R): R =
-    try {
-      val ret: R = f(txn)
-      txn.commit()
-      ret
-    } catch {
-      case ex: Throwable =>
-        txn.abort()
-        throw ex
-    } finally {
-      txn.close()
-    }
-
-  private[rspace] def put(txn: Txn[ByteBuffer], key: Blake2b256Hash, value: Trie[K, V]): Unit = {
-    val encodedKey   = Codec[Blake2b256Hash].encode(key).get
-    val encodedValue = Codec[Trie[K, V]].encode(value).get
-    val keyBuff      = encodedKey.bytes.toDirectByteBuffer
-    val valBuff      = encodedValue.bytes.toDirectByteBuffer
-    _dbTrie.put(txn, keyBuff, valBuff)
-  }
-
-  private[rspace] def get(txn: Txn[ByteBuffer], key: Blake2b256Hash): Option[Trie[K, V]] = {
-    val encodedKey = Codec[Blake2b256Hash].encode(key).get
-    val keyBuff    = encodedKey.bytes.toDirectByteBuffer
-    Option(_dbTrie.get(txn, keyBuff)).map { (buffer: ByteBuffer) =>
-      // ht: Yes, I want to throw an exception if deserialization fails
-      Codec[Trie[K, V]].decode(BitVector(buffer)).map(_.value).get
-    }
-  }
+  private[rspace] def get(txn: Txn[ByteBuffer], key: Blake2b256Hash): Option[Trie[K, V]] =
+    _dbTrie.get(txn, key)(Codec[Trie[K, V]])
 
   private[rspace] def toMap: Map[Blake2b256Hash, Trie[K, V]] =
     withTxn(createTxnRead()) { txn =>
@@ -80,13 +53,8 @@ class LMDBTrieStore[K, V] private (val env: Env[ByteBuffer],
     _dbPastRoots.drop(txn)
   }
 
-  private[rspace] def getRoot(txn: Txn[ByteBuffer], branch: Branch): Option[Blake2b256Hash] = {
-    val encodedBranch     = Codec[Branch].encode(branch).get
-    val encodedBranchBuff = encodedBranch.bytes.toDirectByteBuffer
-    Option(_dbRoot.get(txn, encodedBranchBuff)).map { (buffer: ByteBuffer) =>
-      Codec[Blake2b256Hash].decode(BitVector(buffer)).map(_.value).get
-    }
-  }
+  private[rspace] def getRoot(txn: Txn[ByteBuffer], branch: Branch): Option[Blake2b256Hash] =
+    _dbRoot.get(txn, branch)(Codec[Branch], Codec[Blake2b256Hash])
 
   private[rspace] def persistAndGetRoot(txn: Txn[ByteBuffer],
                                         branch: Branch): Option[Blake2b256Hash] =
@@ -97,23 +65,12 @@ class LMDBTrieStore[K, V] private (val env: Env[ByteBuffer],
       }
       .map {
         case (currentRoot, updatedPastRoots) =>
-          val encodedBranch        = Codec[Branch].encode(branch).get
-          val encodedBranchBuff    = encodedBranch.bytes.toDirectByteBuffer
-          val encodedPastRoots     = Codec[Seq[Blake2b256Hash]].encode(updatedPastRoots).get
-          val encodedPastRootsBuff = encodedPastRoots.bytes.toDirectByteBuffer
-          _dbPastRoots.put(txn, encodedBranchBuff, encodedPastRootsBuff)
+          _dbPastRoots.put(txn, branch, updatedPastRoots)
           currentRoot
       }
 
-  private[rspace] def putRoot(txn: Txn[ByteBuffer], branch: Branch, hash: Blake2b256Hash): Unit = {
-    val encodedBranch     = Codec[Branch].encode(branch).get
-    val encodedBranchBuff = encodedBranch.bytes.toDirectByteBuffer
-    val encodedHash       = Codec[Blake2b256Hash].encode(hash).get
-    val encodedHashBuff   = encodedHash.bytes.toDirectByteBuffer
-    if (!_dbRoot.put(txn, encodedBranchBuff, encodedHashBuff)) {
-      throw new Exception(s"could not persist: $hash")
-    }
-  }
+  private[rspace] def putRoot(txn: Txn[ByteBuffer], branch: Branch, hash: Blake2b256Hash): Unit =
+    _dbRoot.put(txn, branch, hash)(Codec[Branch], Codec[Blake2b256Hash])
 
   private[this] def getAllPastRoots(txn: Txn[ByteBuffer]): Seq[Blake2b256Hash] =
     withResource(_dbPastRoots.iterate(txn)) { (it: CursorIterator[ByteBuffer]) =>
@@ -123,15 +80,8 @@ class LMDBTrieStore[K, V] private (val env: Env[ByteBuffer],
     }
 
   private[this] def getPastRootsInBranch(txn: Txn[ByteBuffer],
-                                         branch: Branch): Seq[Blake2b256Hash] = {
-    val encodedBranch     = Codec[Branch].encode(branch).get
-    val encodedBranchBuff = encodedBranch.bytes.toDirectByteBuffer
-    Option(_dbPastRoots.get(txn, encodedBranchBuff))
-      .map { bytes =>
-        Codec[Seq[Blake2b256Hash]].decode(BitVector(bytes)).map(_.value).get
-      }
-      .getOrElse(Seq.empty)
-  }
+                                         branch: Branch): Seq[Blake2b256Hash] =
+    _dbPastRoots.get(txn, branch)(Codec[Branch], Codec[Seq[Blake2b256Hash]]).getOrElse(Seq.empty)
 
   private[rspace] def validateAndPutRoot(txn: Txn[ByteBuffer],
                                          branch: Branch,
