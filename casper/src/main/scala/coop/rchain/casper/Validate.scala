27f6301453165c3650b7d8603b3060bc5de9fe37
Kent Shikama <kent@kentshikama.com>
2018-07-16 05:52:03 -0700

    Add failure case for when block is missing tuplespace hash

--- casper/src/main/scala/coop/rchain/casper/Validate.scala
+++ casper/src/main/scala/coop/rchain/casper/Validate.scala
@@ -337,20 +337,27 @@ object Validate {
   def bondsCache[F[_]: Applicative: Log](
       b: BlockMessage,
       runtimeManager: RuntimeManager): F[Either[InvalidBlock, ValidBlock]] = {
-    val bonds          = ProtoUtil.bonds(b)
-    val tuplespaceHash = ProtoUtil.tuplespace(b).get
-    Try(runtimeManager.computeBonds(tuplespaceHash)) match {
-      case Success(computedBonds) =>
-        if (bonds == computedBonds) {
-          Applicative[F].pure(Right(Valid))
-        } else {
-          for {
-            _ <- Log[F].warn("Bonds in proof of stake contract do not match block's bond cache.")
-          } yield Left(InvalidBondsCache)
+    val bonds = ProtoUtil.bonds(b)
+    ProtoUtil.tuplespace(b) match {
+      case Some(tuplespaceHash) =>
+        Try(runtimeManager.computeBonds(tuplespaceHash)) match {
+          case Success(computedBonds) =>
+            if (bonds == computedBonds) {
+              Applicative[F].pure(Right(Valid))
+            } else {
+              for {
+                _ <- Log[F].warn(
+                      "Bonds in proof of stake contract do not match block's bond cache.")
+              } yield Left(InvalidBondsCache)
+            }
+          case Failure(_) =>
+            for {
+              _ <- Log[F].warn("Failed to compute bonds from tuplespace hash.")
+            } yield Left(InvalidBondsCache)
         }
-      case Failure(_) =>
+      case None =>
         for {
-          _ <- Log[F].warn("Failed to compute bonds from tuplespace hash.")
+          _ <- Log[F].warn("Block is missing a tuplespace hash.")
         } yield Left(InvalidBondsCache)
     }
   }
