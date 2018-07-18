f35fc2aebd232687f328f6218ea372ecb1331189
Mateusz Górski <gorski.mateusz@protonmail.ch>
2018-07-18 15:37:08 +0200

    RHOL-504: Add convenience method on CostAccount for merging two CostAccount instances

--- rholang/src/main/scala/coop/rchain/rholang/interpreter/accounting/CostAccount.scala
+++ rholang/src/main/scala/coop/rchain/rholang/interpreter/accounting/CostAccount.scala
@@ -2,7 +2,9 @@ package coop.rchain.rholang.interpreter.accounting
 
 case class CostAccount(idx: Int, cost: Cost) {
   def charge(value: Cost): CostAccount = copy(idx = this.idx + 1, cost = this.cost + value)
-  def +(cost: Cost): CostAccount       = charge(cost)
+  def charge(other: CostAccount): CostAccount =
+    copy(idx = this.idx + other.idx, cost = this.cost + other.cost)
+  def +(cost: Cost): CostAccount = charge(cost)
 }
 
 object CostAccount {
