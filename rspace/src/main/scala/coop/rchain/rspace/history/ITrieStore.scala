79201fe7a7f9171d8509c060d2bfdafd27220d7b
Henry Till <henrytill@gmail.com>
2018-07-19 13:26:17 -0400

    ITrieStore: add getAllPastRoots

--- rspace/src/main/scala/coop/rchain/rspace/history/ITrieStore.scala
+++ rspace/src/main/scala/coop/rchain/rspace/history/ITrieStore.scala
@@ -22,6 +22,8 @@ trait ITrieStore[T, K, V] {
 
   private[rspace] def putRoot(txn: T, branch: Branch, hash: Blake2b256Hash): Unit
 
+  private[rspace] def getAllPastRoots(txn: T): Seq[Blake2b256Hash]
+
   private[rspace] def validateAndPutRoot(txn: T, branch: Branch, hash: Blake2b256Hash): Unit
 
   private[rspace] def put(txn: T, key: Blake2b256Hash, value: Trie[K, V]): Unit
