1a015b9198365a8dd3bec1ebf496e9890c994c80
Timm Schäuble <timm.schaeuble@gmail.com>
2018-07-25 19:09:56 +0200

    Remove misplaced comment

--- roscala/src/main/scala/coop/rchain/roscala/prim/ob.scala
+++ roscala/src/main/scala/coop/rchain/roscala/prim/ob.scala
@@ -3,12 +3,6 @@ package coop.rchain.roscala.prim
 import coop.rchain.roscala.ob.{Ctxt, Fixnum}
 
 object ob {
-
-  /**
-    * Define the tuple-cons primitive.
-    * This prepends an Ob to the specified Tuple
-    * e.g. (tuple-cons 1 [2 3]) ==> [1 2 3]
-    */
   object objectIndexedSize extends Prim {
     override val name: String = "prim-size"
     override val minArgs: Int = 1
@@ -17,5 +11,4 @@ object ob {
     override def fnSimple(ctxt: Ctxt): Either[PrimError, Fixnum] =
       Right(ctxt.arg(0).indexedSize())
   }
-
 }
