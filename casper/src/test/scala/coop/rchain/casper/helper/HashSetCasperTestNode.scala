86153dd51352bc2438ce212362ecf76885a5b73d
Pawel Szulc <paul.szulc@gmail.com>
2018-08-02 09:17:29 +0200

    Fix Capser test import

--- casper/src/test/scala/coop/rchain/casper/helper/HashSetCasperTestNode.scala
+++ casper/src/test/scala/coop/rchain/casper/helper/HashSetCasperTestNode.scala
@@ -16,7 +16,7 @@ import coop.rchain.crypto.signatures.Ed25519
 import coop.rchain.metrics.Metrics
 import coop.rchain.p2p.EffectsTestInstances._
 import coop.rchain.p2p.effects.PacketHandler
-import coop.rchain.comm.rp.Connect.handle
+import coop.rchain.comm.rp.HandleMessages.handle
 import coop.rchain.comm.protocol.routing._
 import coop.rchain.rholang.interpreter.Runtime
 import java.nio.file.Files
