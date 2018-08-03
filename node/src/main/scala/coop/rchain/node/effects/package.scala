976d4b59ff616ff06fdf4bfd79ceeebf61c9838b
Pawel Szulc <paul.szulc@gmail.com>
2018-08-03 14:25:01 +0200

    Loving Scala... :(

--- node/src/main/scala/coop/rchain/node/effects/package.scala
+++ node/src/main/scala/coop/rchain/node/effects/package.scala
@@ -76,5 +76,6 @@ package object effects {
   def consoleIO(consoleReader: ConsoleReader): ConsoleIO[Task] = new JLineConsoleIO(consoleReader)
 
   def tcpConnections: Task[Cell[Task, TransportState]] = Cell.mvarCell(TransportState.empty)
-  def rpConnections: Task[ConnectionsCell[Task]]       = Cell.const(Connections.empty).pure[Task] // noop for now
+  def rpConnections: Task[ConnectionsCell[Task]] =
+    Cell.const[Task, Connections](Connections.empty).pure[Task] // noop for now
 }
