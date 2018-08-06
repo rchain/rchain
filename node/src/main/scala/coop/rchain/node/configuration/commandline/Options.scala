ee9b3b3ab50258a99847ec24924b08704a22c29e
Mateusz Górski <gorski.mateusz@protonmail.ch>
2018-08-06 13:21:28 +0200

    NO-TICKET:NODE: Add required constraint to deploy's cmd flags.

--- node/src/main/scala/coop/rchain/node/configuration/commandline/Options.scala
+++ node/src/main/scala/coop/rchain/node/configuration/commandline/Options.scala
@@ -183,19 +183,23 @@ final case class Options(arguments: Seq[String]) extends ScallopConf(arguments) 
       addr.startsWith("0x") && addr.drop(2).matches("[0-9a-fA-F]+")
     val from = opt[String](
       descr = "Purse address that will be used to pay for the deployment.",
+      required = true,
       validate = addressCheck
     )
 
     val phloLimit = opt[Int](
-      descr = "The amount of phlo to use for the transaction (unused phlo is refunded)."
+      descr = "The amount of phlo to use for the transaction (unused phlo is refunded).",
+      required = true
     )
 
     val phloPrice = opt[Int](
-      descr = "The price of phlo for this transaction in units dust/phlo."
+      descr = "The price of phlo for this transaction in units dust/phlo.",
+      required = true
     )
 
     val nonce = opt[Int](
-      descr = "This allows to overwrite your own pending transactions that use the same nonce."
+      descr = "This allows to overwrite your own pending transactions that use the same nonce.",
+      required = true
     )
 
     val location = trailArg[String](required = true)
