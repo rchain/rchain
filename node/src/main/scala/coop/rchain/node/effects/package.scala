c4fb3dc5c717a4df8c2d6e83be1f2beb0f979c22
Sebastian Bach <sebastian@pyrofex.net>
2018-08-14 13:00:10 +0200

    Bring back integration tests for TransportLayer

--- node/src/main/scala/coop/rchain/node/effects/package.scala
+++ node/src/main/scala/coop/rchain/node/effects/package.scala
@@ -75,11 +75,14 @@ package object effects {
         } yield r
     }
 
-  def tcpTransportLayer(host: String, port: Int, cert: File, key: File)(
+  def tcpTransportLayer(host: String, port: Int, certPath: Path, keyPath: Path)(
       implicit scheduler: Scheduler,
       connections: TcpTransportLayer.TransportCell[Task],
-      log: Log[Task]): TcpTransportLayer =
+      log: Log[Task]): TcpTransportLayer = {
+    val cert = Resources.withResource(Source.fromFile(certPath.toFile))(_.mkString)
+    val key  = Resources.withResource(Source.fromFile(keyPath.toFile))(_.mkString)
     new TcpTransportLayer(host, port, cert, key)
+  }
 
   def consoleIO(consoleReader: ConsoleReader): ConsoleIO[Task] = new JLineConsoleIO(consoleReader)
 
