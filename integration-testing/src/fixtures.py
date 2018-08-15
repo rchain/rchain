36d39f188c331d1d3acbda6c5cb976be1d67c7d3
Ovidiu Deac <ovidiu@pyrofex.net>
2018-08-16 01:18:49 +0300

    refactor fixtures

--- integration-testing/src/fixtures.py
+++ integration-testing/src/fixtures.py
@@ -3,57 +3,14 @@ import pytest
 import docker
 import tools.random as random
 
-docker_client = docker.from_env()
+from tools.rnode import create_bootstrap_node, get_rnode_address, create_peer_nodes
+from tools.wait import wait_for, contains, container_logs, network_converged
 
 import collections
+
+docker_client = docker.from_env()
+
 RChain = collections.namedtuple("RChain", ["network", "bootstrap", "peers"])
-
-
-def create_network():
-    network_name = f"rchain-{random.random_string(5)}"
-
-    docker_client.networks.create(network_name, driver="bridge")
-
-    return network_name
-
-def remove_resources_by_network(network_name):
-    """Remove resources by network name."""
-
-    containers = docker_client.containers.list(all=True, filters={"name":f".{network_name}"})
-    for container in containers:
-        logging.info("=" * 100)
-        logging.info(f"Docker container logs for {container.name}:")
-        logging.info("=" * 100)
-        logs = container.logs().decode('utf-8').splitlines()
-        for log_line in logs:
-            logging.info(f"{container.name}: {log_line}")
-
-        logging.info("=" * 100)
-
-    logging.info(f"Removing resources for docker network {network_name}")
-
-    for container in containers:
-        logging.info(f"Remove container {container.name}")
-        container.remove(force=True, v=True)
-
-    for network in docker_client.networks.list():
-        if network_name == network.name:
-            print(f"removing {network.name}")
-            network.remove()
-
-    logging.info("Remove unused volumes")
-    docker_client.volumes.prune()
-
-@pytest.fixture
-def docker_network():
-    logging.info("Setup")
-
-    network_name = create_network()
-
-    yield network_name
-
-    logging.info("Teardown")
-    remove_resources_by_network(network_name)
 
 Config = collections.namedtuple( "Config",
                                  [
@@ -68,3 +25,58 @@ def config(request):
                 bootstrap_startup_timeout = 60,
                 network_converge_timeout = 200
                 )
+
+
+@pytest.fixture
+def docker_network():
+    network_name = f"rchain-{random.random_string(5)}"
+
+    docker_client.networks.create(network_name, driver="bridge")
+
+    yield network_name
+
+    for network in docker_client.networks.list():
+        if network_name == network.name:
+            print(f"removing {network.name}")
+            network.remove()
+
+@pytest.fixture
+def converged_network(config, docker_network):
+    logging.debug(f"Docker network = {docker_network}")
+
+    bootstrap = create_bootstrap_node(docker_network)
+
+    assert wait_for( contains( container_logs(bootstrap),
+                               "coop.rchain.node.NodeRuntime - Starting stand-alone node."),
+                     config.bootstrap_startup_timeout, 2), \
+        "Bootstrap node didn't start correctly"
+
+    bootstrap_address  = get_rnode_address(bootstrap)
+
+    peers = create_peer_nodes(config.peer_count, bootstrap_address, docker_network)
+
+    assert wait_for( network_converged(bootstrap, config.peer_count),
+                     config.network_converge_timeout, 10), \
+        "The network did NOT converge. Check container logs for issues. One or more containers might have failed to start or connect."
+
+
+    yield RChain(docker_network, bootstrap, peers)
+
+    containers = [bootstrap] + peers
+
+    for container in containers:
+        logging.info("=" * 100)
+        logging.info(f"Docker container logs for {container.name}:")
+        logging.info("=" * 100)
+        logs = container.logs().decode('utf-8').splitlines()
+        for log_line in logs:
+            logging.info(f"{container.name}: {log_line}")
+
+        logging.info("=" * 100)
+
+    for container in containers:
+        logging.info(f"Remove container {container.name}")
+        container.remove(force=True, v=True)
+
+    logging.info("Remove unused volumes")
+    docker_client.volumes.prune()
--- integration-testing/test/network/test_convergence.py
+++ integration-testing/test/network/test_convergence.py
@@ -1,26 +1,9 @@
 
 import logging
 from fixtures import *
-from tools.rnode import create_bootstrap_node, get_rnode_address, create_peer_nodes
-from tools.wait import wait_for, contains, container_logs, network_converged
 
-def test_convergence(config, docker_network):
+def test_convergence(config, converged_network):
     """
     This test represents an integration test that starts a network and checks if it converges after the de.
     """
-    logging.debug(f"Docker network = {docker_network}")
-
-    bootstrap = create_bootstrap_node(docker_network)
-
-    assert wait_for( contains( container_logs(bootstrap),
-                               "coop.rchain.node.NodeRuntime - Starting stand-alone node."),
-                     config.bootstrap_startup_timeout, 2),\
-        "Bootstrap node didn't start correctly"
-
-    bootstrap_address  = get_rnode_address(bootstrap)
-
-    peers = create_peer_nodes(config.peer_count, bootstrap_address, docker_network)
-
-    assert wait_for( network_converged(bootstrap, config.peer_count),
-                     config.network_converge_timeout, 10),\
-        "The network did NOT converge. Check container logs for issues. One or more containers might have failed to start or connect."
+    pass
