d349e5efea8d02ae296c511be5b05b4cb57173ba
Ovidiu Deac <ovidiu@pyrofex.net>
2018-08-16 11:31:06 +0300

    add test for RuntimeException

--- integration-testing/test/network/test_basics.py
+++ integration-testing/test/network/test_basics.py
@@ -2,10 +2,11 @@
 import logging
 from delayed_assert import expect, assert_expectations
 
-def test_metrics_api_socket(started_rchain_network):
-    containers = [started_rchain_network.bootstrap] + started_rchain_network.peers
+def containers(rchain):
+    return [rchain.bootstrap] + rchain.peers
 
-    for container  in containers:
+def test_metrics_api_socket(started_rchain_network):
+    for container  in containers(started_rchain_network):
         logging.info(f"Test metrics api socket for {container.name}")
         cmd = f"nmap -sS -n -p T:40403 -oG - {container.name}"
         r = container.exec_run(cmd=cmd, user='root').output.decode("utf-8")
@@ -15,9 +16,7 @@ def test_metrics_api_socket(started_rchain_network):
 
 
 def test_node_logs_for_errors(converged_network):
-    containers = [converged_network.bootstrap] + converged_network.peers
-
-    for container in containers:
+    for container in containers(converged_network):
         logging.info(f"Testing {container.name} node logs for errors.")
         logs = container.logs().decode('utf-8')
 
@@ -26,5 +25,19 @@ def test_node_logs_for_errors(converged_network):
                 if "ERROR" in line:
                     logging.error(f"Error: {line}")
             expect(not "ERROR" in line, f"Container {container.name} error in log line: {line}")
+
+    assert_expectations()
+
+def test_node_logs_for_RuntimeException(converged_network):
+    for container in containers(converged_network):
+        logging.info(f"Testing {container.name} node logs for \"java RuntimeException\".")
+        logs = container.logs().decode('utf-8')
+
+
+        if "RuntimeException" in logs:
+            for line in logs.splitlines():
+                if "RuntimeException" in line:
+                    logging.error(f"Error: {line}")
+            expect(not "RuntimeException" in line, f"Container {container.name} error in log line: {line}")
 
     assert_expectations()
