20e1384d3049b29ca37ec4ab6ad6a8edb5c0aba2
Kent Shikama <kent@kentshikama.com>
2018-08-17 08:32:56 -0700

    Update AtomicMVar to use SyncVar and rename to AtomicSyncVarF

--- shared/src/main/scala/coop/rchain/shared/AtomicMVar.scala
+++ shared/src/main/scala/coop/rchain/shared/AtomicMVar.scala
@@ -1,29 +1,34 @@
 package coop.rchain.shared
 
-import cats.MonadError
 import cats.implicits._
-import cats.effect.Async
-import cats.effect.concurrent.MVar
+import cats.effect.Sync
+import scala.concurrent.SyncVar
 
-class AtomicMVar[F[_], A] private (private val underlying: MVar[F, A]) {
-  def modify[B](f: A => F[(A, B)])(implicit monadError: MonadError[F, Throwable]): F[B] =
+class AtomicSyncVarF[F[_], A] private (private val underlying: SyncVar[A]) {
+  def get(implicit sync: Sync[F]): F[A] =
+    sync.delay { underlying.take }
+
+  def modify[B](f: A => F[(A, B)])(implicit sync: Sync[F]): F[B] =
     for {
-      value  <- underlying.take
+      value  <- sync.delay { underlying.take }
       fValue <- f(value).attempt
       result <- fValue match {
                  case Right((newValue, b)) =>
-                   underlying.put(newValue).map(_ => b)
+                   sync.delay { underlying.put(newValue) }.map(_ => b)
 
                  case Left(err) =>
-                   underlying.put(value) *> MonadError[F, Throwable].raiseError(err)
+                   sync.delay { underlying.put(value) } *> sync.raiseError(err)
                }
     } yield result
 
-  def update(f: A => F[A])(implicit monadError: MonadError[F, Throwable]): F[Unit] =
+  def update(f: A => F[A])(implicit sync: Sync[F]): F[Unit] =
     modify(a => f(a).map((_, ())))
 }
 
-object AtomicMVar {
-  def of[F[_]: Async, A](initial: A): F[AtomicMVar[F, A]] =
-    MVar.uncancelableOf[F, A](initial).map(new AtomicMVar(_))
+object AtomicSyncVarF {
+  def of[F[_], A](initial: A): AtomicSyncVarF[F, A] = {
+    val underlying = new SyncVar[A]()
+    underlying.put(initial)
+    new AtomicSyncVarF[F, A](underlying)
+  }
 }
