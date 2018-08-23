bcd2647dee6aac862472653802203eca22728eef
Ovidiu Deac <ovidiu@pyrofex.net>
2018-08-23 14:49:11 +0300

    better name for the property

--- crypto/src/test/scala/coop/rchain/crypto/codec/Base16Spec.scala
+++ crypto/src/test/scala/coop/rchain/crypto/codec/Base16Spec.scala
@@ -4,7 +4,7 @@ import org.scalatest._
 import prop._
 
 class Base16Spec extends PropSpec with GeneratorDrivenPropertyChecks with Matchers {
-  property("decode works on any encoded string") {
+  property("decode after encode returns the original input") {
     forAll { (input: Array[Byte]) =>
       val encoded = Base16.encode(input)
       val decoded = Base16.decode(encoded)
