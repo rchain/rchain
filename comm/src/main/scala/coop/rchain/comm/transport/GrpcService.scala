4cd2eafa7a144c40eec70981ee9056d537419bc7
Sebastian Bach <sebastian@pyrofex.net>
2018-08-27 16:12:02 +0200

    Add comments in GrpcService

--- comm/src/main/scala/coop/rchain/comm/transport/GrpcService.scala
+++ comm/src/main/scala/coop/rchain/comm/transport/GrpcService.scala
@@ -12,6 +12,10 @@ import io.grpc.stub.{ServerCalls, StreamObserver}
 import monix.eval.Task
 import monix.execution.Scheduler
 
+/*
+  This is a replacement object for TransportLayerGrpc and allows a fine-grained
+  control over the service request-response cycle in gRPC.
+ */
 object GrpcService {
   def bindService(transportLayer: TransportLayer)(implicit scheduler: Scheduler,
                                                   log: Log[Task],
@@ -27,6 +31,12 @@ object GrpcService {
               .materialize
               .flatMap(response =>
                 Try(scalapb.grpc.Grpc.completeObserver(observer)(response)) match {
+                  /*
+                      When the peer node gets shutdown before the service can respond
+                      grpc-java throws an Exception which gets printed to the console.
+                      This exception shouldn't appear on console because it's a very
+                      common situation in a P2P network.
+                   */
                   case Failure(e: StatusRuntimeException)
                       if e.getStatus.getCode == Status.Code.CANCELLED =>
                     log.debug("Couldn't send response: Channel was cancelled")
