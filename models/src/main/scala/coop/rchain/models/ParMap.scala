ce432f2a5c00c7ddf6e94b436cf5376b3ac31e62
Artur Gajowy <artur.gajowy@gmail.com>
2018-08-28 10:42:23 +0200

    Simplify a test constructor for ParMap

--- models/src/main/scala/coop/rchain/models/ParMap.scala
+++ models/src/main/scala/coop/rchain/models/ParMap.scala
@@ -24,14 +24,14 @@ object ParMap {
   def apply(seq: Seq[(Par, Par)], connectiveUsed: Boolean, locallyFree: BitSet): ParMap =
     new ParMap(SortedParMap(seq), connectiveUsed, Coeval.pure(locallyFree).memoize)
 
-  def apply(seq: Seq[(Par, Par)], connectiveUsed: Boolean): ParMap =
-    ParMap(seq, connectiveUsed, Coeval.pure(updateLocallyFree(seq)))
+  def apply(seq: Seq[(Par, Par)]): ParMap =
+    ParMap(seq, connectiveUsed(seq), Coeval.pure(updateLocallyFree(seq)))
 
   def apply(map: SortedParMap): ParMap =
-    ParMap(map, connectiveUsed(map), Coeval.pure(updateLocallyFree(map.toSeq)))
+    ParMap(map, connectiveUsed(map.toSeq), Coeval.pure(updateLocallyFree(map.toSeq)))
 
-  private def connectiveUsed(map: SortedParMap): Boolean =
-    map.sortedMap.exists { case (k, v) => k.connectiveUsed || v.connectiveUsed }
+  private def connectiveUsed(map: Seq[(Par, Par)]): Boolean =
+    map.exists { case (k, v) => k.connectiveUsed || v.connectiveUsed }
 
   private def updateLocallyFree(ps: Seq[(Par, Par)]): BitSet =
     ps.foldLeft(BitSet()) {
