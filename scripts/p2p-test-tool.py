babd65ad77fb5fd72f81a577118d52777096e039
Jeremy Busk <jeremybusk@gmail.com>
2018-08-29 12:36:58 -0600

    remove test duplication in -T option list

--- scripts/p2p-test-tool.py
+++ scripts/p2p-test-tool.py
@@ -113,7 +113,7 @@ parser.add_argument("-T", "--tests-to-run",
                     dest='tests_to_run',
                     type=str,
                     nargs='+',
-                    default=['network_sockets', 'count', 'eval', 'count', 'repl', 'propose', 'errors', 'RuntimeException'],
+                    default=['network_sockets', 'count', 'eval', 'repl', 'propose', 'errors', 'RuntimeException'],
                     help="run these tests in this order")
 parser.add_argument("--thread-pool-size",
                     dest='thread_pool_size',
