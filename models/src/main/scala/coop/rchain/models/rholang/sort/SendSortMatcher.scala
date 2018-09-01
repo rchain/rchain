28a824e7be9c21d3badd29678bc5c2c598365def
Artur Gajowy <artur.gajowy@gmail.com>
2018-09-01 23:25:04 +0200

    SendSortMatcher returns via F

--- models/src/main/scala/coop/rchain/models/rholang/sort/SendSortMatcher.scala
+++ models/src/main/scala/coop/rchain/models/rholang/sort/SendSortMatcher.scala
@@ -1,23 +1,25 @@
 package coop.rchain.models.rholang.sort
 
+import cats.effect.Sync
 import coop.rchain.models.Send
 import coop.rchain.models.rholang.implicits._
+import cats.implicits._
 
 private[sort] object SendSortMatcher extends Sortable[Send] {
-  def sortMatch(s: Send): ScoredTerm[Send] = {
-    val sortedChan = Sortable.sortMatch(s.chan)
-    val sortedData = s.data.toList.map(Sortable.sortMatch(_))
-    val sortedSend = Send(
-      chan = sortedChan.term,
-      data = sortedData.map(_.term.get),
-      persistent = s.persistent,
-      locallyFree = s.locallyFree,
-      connectiveUsed = s.connectiveUsed
-    )
-    val persistentScore = if (s.persistent) 1 else 0
-    val sendScore = Node(
-      Score.SEND,
-      Seq(Leaf(persistentScore)) ++ Seq(sortedChan.score) ++ sortedData.map(_.score): _*)
-    ScoredTerm(sortedSend, sendScore)
-  }
+  def sortMatch[F[_]: Sync](s: Send): F[ScoredTerm[Send]] =
+    for {
+      sortedChan <- Sortable.sortMatch(s.chan)
+      sortedData <- s.data.toList.map(Sortable.sortMatch(_)).sequence
+      sortedSend = Send(
+        chan = sortedChan.term,
+        data = sortedData.map(_.term.get),
+        persistent = s.persistent,
+        locallyFree = s.locallyFree,
+        connectiveUsed = s.connectiveUsed
+      )
+      persistentScore = if (s.persistent) 1 else 0
+      sendScore = Node(
+        Score.SEND,
+        Seq(Leaf(persistentScore)) ++ Seq(sortedChan.score) ++ sortedData.map(_.score): _*)
+    } yield ScoredTerm(sortedSend, sendScore)
 }
