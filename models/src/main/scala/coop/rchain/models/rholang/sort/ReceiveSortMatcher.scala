ac554818af2490a60dd9e761e0552bf44ed77c89
Artur Gajowy <artur.gajowy@gmail.com>
2018-09-01 23:23:06 +0200

    ReceiveSortMatcher returns via F

--- models/src/main/scala/coop/rchain/models/rholang/sort/ReceiveSortMatcher.scala
+++ models/src/main/scala/coop/rchain/models/rholang/sort/ReceiveSortMatcher.scala
@@ -1,41 +1,47 @@
 package coop.rchain.models.rholang.sort
 
-import coop.rchain.models.{Receive, ReceiveBind}
+import cats.effect.Sync
+import coop.rchain.models.{Receive, ReceiveBind, Var}
+import cats.implicits._
 
 object ReceiveSortMatcher extends Sortable[Receive] {
 
-  def sortBind(bind: ReceiveBind): ScoredTerm[ReceiveBind] = {
-    val patterns       = bind.patterns
-    val source         = bind.source
-    val sortedPatterns = patterns.toList.map(channel => Sortable.sortMatch(channel))
-    val sortedChannel  = Sortable.sortMatch(source)
-    val sortedRemainder = bind.remainder match {
-      case Some(bindRemainder) =>
-        val scoredVar = Sortable.sortMatch(bindRemainder)
-        ScoredTerm(Some(scoredVar.term), scoredVar.score)
-      case None => ScoredTerm(None, Leaf(Score.ABSENT))
-    }
-    ScoredTerm(
-      ReceiveBind(sortedPatterns.map(_.term), sortedChannel.term, bind.remainder, bind.freeCount),
-      Node(Seq(sortedChannel.score) ++ sortedPatterns.map(_.score) ++ Seq(sortedRemainder.score))
-    )
+  def sortBind[F[_]: Sync](bind: ReceiveBind): F[ScoredTerm[ReceiveBind]] = {
+    val patterns = bind.patterns
+    val source   = bind.source
+    for {
+      sortedPatterns <- patterns.toList.map(channel => Sortable.sortMatch(channel)).sequence
+      sortedChannel  <- Sortable.sortMatch(source)
+      sortedRemainder <- bind.remainder match {
+                          case Some(bindRemainder) =>
+                            Sortable.sortMatch(bindRemainder).map { scoredVar =>
+                              ScoredTerm(Some(scoredVar.term), scoredVar.score)
+                            }
+                          case None => ScoredTerm(None, Leaf(Score.ABSENT)).pure[F]
+                        }
+    } yield
+      ScoredTerm(
+        ReceiveBind(sortedPatterns.map(_.term), sortedChannel.term, bind.remainder, bind.freeCount),
+        Node(Seq(sortedChannel.score) ++ sortedPatterns.map(_.score) ++ Seq(sortedRemainder.score))
+      )
   }
 
   // The order of the binds must already be presorted by the time this is called.
   // This function will then sort the insides of the preordered binds.
-  def sortMatch(r: Receive): ScoredTerm[Receive] = {
-    val sortedBinds     = r.binds.toList.map(bind => sortBind(bind))
-    val persistentScore = if (r.persistent) 1 else 0
-    val sortedBody      = Sortable.sortMatch(r.body)
-    ScoredTerm(
-      Receive(sortedBinds.map(_.term),
-              sortedBody.term,
-              r.persistent,
-              r.bindCount,
-              r.locallyFree,
-              r.connectiveUsed),
-      Node(Score.RECEIVE,
-           Seq(Leaf(persistentScore)) ++ sortedBinds.map(_.score) ++ Seq(sortedBody.score): _*)
-    )
-  }
+  def sortMatch[F[_]: Sync](r: Receive): F[ScoredTerm[Receive]] =
+    for {
+      sortedBinds     <- r.binds.toList.map(bind => sortBind(bind)).sequence
+      persistentScore = if (r.persistent) 1 else 0
+      sortedBody      <- Sortable.sortMatch(r.body)
+    } yield
+      ScoredTerm(
+        Receive(sortedBinds.map(_.term),
+                sortedBody.term,
+                r.persistent,
+                r.bindCount,
+                r.locallyFree,
+                r.connectiveUsed),
+        Node(Score.RECEIVE,
+             Seq(Leaf(persistentScore)) ++ sortedBinds.map(_.score) ++ Seq(sortedBody.score): _*)
+      )
 }
