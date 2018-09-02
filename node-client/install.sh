b1c5aa7c05f338f87f2084d24b266d3afd971035
Will Qiu <157747074@qq.com>
2018-09-02 16:36:29 +0200

    create virtualen, add gitignore and correct spell mistake

--- node-client/install.sh
+++ node-client/install.sh
@@ -3,15 +3,19 @@ BASEPATH=$(cd `dirname $0`; pwd)
 RCHAINPATH=$(dirname ${BASEPATH})
 mkdir -p ${BASEPATH}/protobuf
 
-echo ${BASEPATH}
-# install the python dependencies
-pip install -r ${BASEPATH}/requirements.txt
+# create the Python environment
+python3 -m venv ./.virtualenv
+
+.virtualenv/bin/pip install --upgrade pip
+
+# install the Python dependencies
+.virtualenv/bin/pip install -r ${BASEPATH}/requirements.txt
 
 # remove all the original protobuf files
-rm -f ${BASEPATH}/protobuf/*.proto
+rm ${BASEPATH}/protobuf/*.proto
 cp ${RCHAINPATH}/models/src/main/protobuf/*.proto ${BASEPATH}/protobuf
 
-Regerate(){
+Regenerate(){
 filename=$(basename $@)
 cat $@ | sed 's/import \"scalapb\/scalapb\.proto\"\;//g' | sed 's/\[(scalapb\.field)\..*/;/g' | sed '/^option (scalapb\.options)/,/^};$/s/.*//g'> ${BASEPATH}/protobuf/${filename}
 }
@@ -19,14 +23,14 @@ cat $@ | sed 's/import \"scalapb\/scalapb\.proto\"\;//g' | sed 's/\[(scalapb\.fi
 # remove the scala code in the protofiles
 for file in ${RCHAINPATH}/models/src/main/protobuf/*.proto;
 do
-Regerate ${file}
+Regenerate ${file}
 done
 
 for file in ${RCHAINPATH}/comm/src/main/protobuf/coop/rchain/comm/protocol/*.proto
 do
-Regerate ${file}
+Regenerate ${file}
 done
 
 
-# compile the proto files to python files
-python -m grpc_tools.protoc -I ${BASEPATH}/protobuf --python_out=${BASEPATH} --grpc_python_out=${BASEPATH} ${BASEPATH}/protobuf/*.proto
+# compile the proto files to Python files
+.virtualenv/bin/python -m grpc_tools.protoc -I ${BASEPATH}/protobuf --python_out=${BASEPATH} --grpc_python_out=${BASEPATH} ${BASEPATH}/protobuf/*.proto
