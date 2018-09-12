ea554f3b67047662e9588476b548accc97654488
Timm Schäuble <timm.schaeuble@gmail.com>
2018-09-12 18:41:14 +0200

    Add tests and cleanup decompression function

--- shared/src/main/scala/coop/rchain/shared/ByteStringOps.scala
+++ shared/src/main/scala/coop/rchain/shared/ByteStringOps.scala
@@ -1,6 +1,7 @@
 package coop.rchain.shared
 
 import java.io._
+import java.util.concurrent.{Callable, Executors}
 
 import com.google.protobuf.ByteString
 import coop.rchain.shared.Resources._
@@ -10,19 +11,14 @@ import org.apache.commons.compress.compressors.lz4.{
   FramedLZ4CompressorOutputStream
 }
 
-import scala.util.control.NonFatal
+import scala.util.{Failure, Success, Try}
 
 object ByteStringOps {
 
   implicit class RichByteString(bs: ByteString) {
     def compress: ByteString = compressLZ4(bs)
 
-    def decompress: Option[ByteString] =
-      try {
-        Some(decompressLZ4(bs))
-      } catch {
-        case e: IOException => None
-      }
+    def decompress: Option[ByteString] = decompressLZ4(bs)
   }
 
   private def compressLZ4(bs: ByteString): ByteString =
@@ -49,36 +45,49 @@ object ByteStringOps {
       }
     }
 
-  private def decompressLZ4(bs: ByteString): ByteString = {
+  private def decompressLZ4(bs: ByteString): Option[ByteString] = {
     val pos = new PipedOutputStream()
+    val pis = new PipedInputStream(pos)
+    val is  = bs.newInput()
 
-    withResource(bs.newInput()) { is =>
-      withResource(new PipedInputStream(pos)) { pis =>
-        new Thread {
-          override def run(): Unit =
-            withResource(new FramedLZ4CompressorInputStream(is)) { lz4is =>
-              // `pos` has to be closed from within this thread
-              var exception: Throwable = null
-              try {
-                val data  = new Array[Byte](8192)
-                var count = lz4is.read(data)
+    // Writes decompressed data into `pos`
+    val producer = new Callable[Try[Unit]] {
+      override def call(): Try[Unit] =
+        try {
+          val lz4is = new FramedLZ4CompressorInputStream(is)
+          val data  = new Array[Byte](8192)
+          var count = lz4is.read(data)
 
-                while (count != -1) {
-                  pos.write(data, 0, count)
-                  count = lz4is.read(data)
-                }
-              } catch {
-                case NonFatal(e) =>
-                  exception = e
-                  throw e
-              } finally {
-                closeAndAddSuppressed(exception, pos)
-              }
-            }
-        }.start()
+          while (count != -1) {
+            pos.write(data, 0, count)
+            count = lz4is.read(data)
+          }
 
-        ByteString.readFrom(pis)
+          lz4is.close()
+          Success(())
+        } catch {
+          case e: IOException => Failure(e)
+        } finally {
+          is.close()
+          pos.close()
+        }
+    }
+
+    // Reads into [[ByteString]] from `pis`
+    val consumer = new Callable[ByteString] {
+      override def call(): ByteString = {
+        val bs = ByteString.readFrom(pis)
+        pis.close()
+        bs
       }
     }
+
+    // Needs a thread pool with at least two threads
+    val es = Executors.newFixedThreadPool(2)
+    val t0 = es.submit(producer)
+    val t1 = es.submit(consumer)
+    es.shutdown()
+
+    t0.get().toOption.map(_ => t1.get())
   }
 }
