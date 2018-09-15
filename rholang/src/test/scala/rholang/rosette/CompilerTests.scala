42d00ce7489c16c5ca06cfe34ba895e323ba7aad
Mateusz Górski <gorski.mateusz@protonmail.ch>
2018-09-15 13:22:37 +0200

    Rholang: Fix how CompilerTests detect errors in the execution.
    Signed-off-by: Mateusz Górski <gorski.mateusz@protonmail.ch>

--- rholang/src/test/scala/rholang/rosette/CompilerTests.scala
+++ rholang/src/test/scala/rholang/rosette/CompilerTests.scala
@@ -5,14 +5,14 @@ import java.nio.file.{Files, Path, Paths}
 
 import coop.rchain.rholang.interpreter.{Interpreter, Runtime}
 import monix.execution.Scheduler.Implicits.global
-import org.scalatest.FunSuite
+import org.scalatest.{FunSuite, Matchers}
 
 import scala.collection.JavaConverters._
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.util.Try
 
-class CompilerTests extends FunSuite {
+class CompilerTests extends FunSuite with Matchers {
   val mapSize     = 1024L * 1024L * 10
   val tmpPrefix   = "rspace-store-"
   val maxDuration = 5.seconds
@@ -28,23 +28,20 @@ class CompilerTests extends FunSuite {
   for (file <- testFiles if file.getFileName.toString.endsWith(".rho")) {
     test(file.toString.reverse.takeWhile(_ != '/').reverse) {
       val result = execute(file)
-      assert(result.isSuccess)
+      result shouldBe Right(runtime)
     }
   }
 
   for (file <- failureTestFiles if file.getFileName.toString.endsWith(".rho")) {
     test(file.toString.reverse.takeWhile(_ != '/').reverse) {
       val result = execute(file)
-      assert(result.isFailure)
+      assert(result.isLeft)
     }
   }
 
-  private def execute(file: Path) = {
-    val future = Interpreter.execute(runtime, new FileReader(file.toString)).runAsync
-
-    Try {
-      Await.result(future, maxDuration)
-    }
+  private def execute(file: Path): Either[Throwable, Runtime] = {
+    val future = Interpreter.execute(runtime, new FileReader(file.toString)).attempt.runAsync
+    Await.result(future, maxDuration)
   }
 
 }
