b621d52db7b3a12053f12189031a29a6cefdb791
Mateusz Górski <gorski.mateusz@protonmail.ch>
2018-09-24 12:06:24 +0200

    Rholang: Test what we stop the computation when RSpace return `OutOfPhlogistonError`.

--- rholang/src/test/scala/coop/rchain/rholang/interpreter/CostAccountingReducerTest.scala
+++ rholang/src/test/scala/coop/rchain/rholang/interpreter/CostAccountingReducerTest.scala
@@ -1,13 +1,21 @@
 package coop.rchain.rholang.interpreter
 
+import coop.rchain.crypto.hash.Blake2b512Random
+import coop.rchain.models.Channel.ChannelInstance
+import coop.rchain.models.Channel.ChannelInstance.Quote
 import coop.rchain.models.Expr.ExprInstance.{EVarBody, GString}
 import coop.rchain.models.Var.VarInstance.{BoundVar, FreeVar}
 import coop.rchain.models._
 import coop.rchain.models.rholang.implicits._
+import coop.rchain.rholang.interpreter.Reduce.DebruijnInterpreter
 import coop.rchain.rholang.interpreter.accounting.{Chargeable, Cost, CostAccount, CostAccountingAlg}
-import monix.eval.Coeval
+import coop.rchain.rholang.interpreter.errors.OutOfPhlogistonsError
+import coop.rchain.rholang.interpreter.storage.TuplespaceAlg
+import monix.eval.{Coeval, Task}
 import org.scalactic.TripleEqualsSupport
 import org.scalatest.{FlatSpec, Matchers}
+import scala.concurrent.duration._
+import monix.execution.Scheduler.Implicits.global
 
 class CostAccountingReducerTest extends FlatSpec with Matchers with TripleEqualsSupport {
 
@@ -39,7 +47,27 @@ class CostAccountingReducerTest extends FlatSpec with Matchers with TripleEquals
   }
 
   it should "stop if OutOfPhloError is returned from RSpace" in {
-    pending
+    val tuplespaceAlg = new TuplespaceAlg[Task] {
+      override def produce(
+          chan: Channel,
+          data: ListChannelWithRandom,
+          persistent: Boolean
+      ): Task[Unit] =
+        Task.raiseError(OutOfPhlogistonsError)
+      override def consume(
+          binds: Seq[(BindPattern, ChannelInstance.Quote)],
+          body: ParWithRandom,
+          persistent: Boolean
+      ): Task[Unit] = Task.raiseError(OutOfPhlogistonsError)
+    }
+
+    implicit val errorLog = new ErrorLog()
+    implicit val rand     = Blake2b512Random(128)
+    implicit val costAlg  = CostAccountingAlg.unsafe[Task](CostAccount(1000))
+    val reducer           = new DebruijnInterpreter[Task, Task.Par](tuplespaceAlg, Map.empty)
+    val send              = Send(Channel(Quote(GString("x"))), Seq(Par()))
+    val test              = reducer.inj(send).attempt.runSyncUnsafe(1.second)
+    assert(test === Left(OutOfPhlogistonsError))
   }
 
   it should "update the cost account after going back from RSpace" in {
