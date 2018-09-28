9ac76aea0d079cc7ef9dd32a819eb10f98702e8d
Kyle Butt <kyle@pyrofex.net>
2018-09-28 11:37:45 -0600

    Models: Add basic test for IsForgeable
    This could be more involved, but a basic sanity test that an unforgeable name
    shows up as unforgeable is essential.

---
+++ models/src/test/scala/coop/rchain/models/IsForgeableSpec.scala
@@ -0,0 +1,39 @@
+package coop.rchain.models
+
+import coop.rchain.models.Channel.ChannelInstance
+import coop.rchain.models.Channel.ChannelInstance._
+import coop.rchain.models.Expr.ExprInstance
+import coop.rchain.models.Expr.ExprInstance._
+import coop.rchain.models._
+import coop.rchain.models.rholang.implicits._
+
+import org.scalatest.{Assertion, FlatSpec, Matchers}
+
+class IsForgeableSpec extends FlatSpec with Matchers {
+
+  "A GPrivate" should "not be forgeable" in {
+    val unforgeable: Par = GPrivateBuilder("unforgeable")
+
+    ParIsForgeable(unforgeable) should be(false)
+  }
+  "A ground type" should "be forgeable" in {
+    val unforgeable: Par = GString("forgeable")
+
+    ParIsForgeable(unforgeable) should be(true)
+  }
+  "A send of a ground type on a ground type" should "be forgeable" in {
+    val unforgeable: Par = Send(Quote(GString("forge1")), List(GString("forge2")))
+
+    ParIsForgeable(unforgeable) should be(true)
+  }
+  "A send of a ground type on an unforgeable name" should "be unforgeable" in {
+    val unforgeable: Par = Send(Quote(GPrivateBuilder("unforge")), List(GString("forge2")))
+
+    ParIsForgeable(unforgeable) should be(false)
+  }
+  "A send of an unforgeable on a ground type" should "be unforgeable" in {
+    val unforgeable: Par = Send(Quote(GString("forge1")), List(GPrivateBuilder("unforge")))
+
+    ParIsForgeable(unforgeable) should be(false)
+  }
+}
