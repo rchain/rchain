fb4618aab7929c357e689729169d373b1b6d90da
Timm Schäuble <timm.schaeuble@gmail.com>
2018-10-06 23:32:20 +0200

    Change to minimal test cases and normalized input

--- rholang/src/test/scala/coop/rchain/rholang/interpreter/RuntimeSpec.scala
+++ rholang/src/test/scala/coop/rchain/rholang/interpreter/RuntimeSpec.scala
@@ -1,10 +1,15 @@
 package coop.rchain.rholang.interpreter
-import java.io.StringReader
 import java.nio.file.Files
 
+import coop.rchain.models.Expr.ExprInstance.EVarBody
+import coop.rchain.models.Var.VarInstance.{BoundVar, FreeVar}
+import coop.rchain.models._
+import coop.rchain.rholang.interpreter.Interpreter.EvaluateResult
+import coop.rchain.rholang.interpreter.errors.ReduceError
 import monix.execution.Scheduler.Implicits.global
 import org.scalatest.{FlatSpec, Matchers}
 
+import scala.collection.immutable.BitSet
 import scala.concurrent.duration._
 
 class RuntimeSpec extends FlatSpec with Matchers {
@@ -14,42 +19,362 @@ class RuntimeSpec extends FlatSpec with Matchers {
 
   val runtime = Runtime.create(Files.createTempDirectory(tmpPrefix), mapSize)
 
-  val nonReadableErr = "ReduceError: Trying to read from non-readable channel."
+  val nonReadableErr = ReduceError("Trying to read from non-readable channel.")
 
   "rho:io:stdout" should "not get intercepted" in {
-    val intercept =
-      """new stdout(`rho:io:stdout`) in { stdout!("foo", "bar") | for (...@x <- stdout) { stdout!(x) } }"""
+    // new s(`rho:io:stdout`) in { for (x <- s) { Nil } }
+    val interceptNormalized = Par(
+      List(),
+      List(),
+      List(
+        New(
+          1,
+          Par(
+            List(),
+            List(
+              Receive(
+                List(
+                  ReceiveBind(
+                    List(
+                      Par(
+                        List(),
+                        List(),
+                        List(),
+                        List(Expr(EVarBody(EVar(Var(FreeVar(0)))))),
+                        List(),
+                        List(),
+                        List(),
+                        List(),
+                        AlwaysEqual(BitSet()),
+                        true
+                      )
+                    ),
+                    Par(
+                      List(),
+                      List(),
+                      List(),
+                      List(Expr(EVarBody(EVar(Var(BoundVar(0)))))),
+                      List(),
+                      List(),
+                      List(),
+                      List(),
+                      AlwaysEqual(BitSet(0)),
+                      false
+                    ),
+                    None,
+                    1
+                  )
+                ),
+                Par(
+                  List(),
+                  List(),
+                  List(),
+                  List(),
+                  List(),
+                  Vector(),
+                  List(),
+                  List(),
+                  AlwaysEqual(BitSet()),
+                  false
+                ),
+                false,
+                1,
+                AlwaysEqual(BitSet(0)),
+                false
+              )
+            ),
+            List(),
+            List(),
+            List(),
+            Vector(),
+            List(),
+            List(),
+            AlwaysEqual(BitSet(0)),
+            false
+          ),
+          Vector("rho:io:stdout"),
+          AlwaysEqual(BitSet())
+        )
+      ),
+      List(),
+      List(),
+      Vector(),
+      List(),
+      List(),
+      AlwaysEqual(BitSet()),
+      false
+    )
 
-    failure(intercept).getMessage.stripLineEnd should endWith(nonReadableErr)
+    failure(interceptNormalized) should contain(nonReadableErr)
   }
 
   "rho:io:stdoutAck" should "not get intercepted" in {
-    val intercept =
-      """new ackCh, stdoutAck(`rho:io:stdoutAck`) in { stdoutAck!("foo") | for (@x <- stdoutAck) { stdoutAck!(x, *ackCh) } }"""
+    // new s(`rho:io:stdoutAck`) in { for (x <- s) { Nil } }
+    val interceptNormalized = Par(
+      List(),
+      List(),
+      List(
+        New(
+          1,
+          Par(
+            List(),
+            List(
+              Receive(
+                List(
+                  ReceiveBind(
+                    List(
+                      Par(
+                        List(),
+                        List(),
+                        List(),
+                        List(Expr(EVarBody(EVar(Var(FreeVar(0)))))),
+                        List(),
+                        List(),
+                        List(),
+                        List(),
+                        AlwaysEqual(BitSet()),
+                        true
+                      )
+                    ),
+                    Par(
+                      List(),
+                      List(),
+                      List(),
+                      List(Expr(EVarBody(EVar(Var(BoundVar(0)))))),
+                      List(),
+                      List(),
+                      List(),
+                      List(),
+                      AlwaysEqual(BitSet(0)),
+                      false
+                    ),
+                    None,
+                    1
+                  )
+                ),
+                Par(
+                  List(),
+                  List(),
+                  List(),
+                  List(),
+                  List(),
+                  Vector(),
+                  List(),
+                  List(),
+                  AlwaysEqual(BitSet()),
+                  false
+                ),
+                false,
+                1,
+                AlwaysEqual(BitSet(0)),
+                false
+              )
+            ),
+            List(),
+            List(),
+            List(),
+            Vector(),
+            List(),
+            List(),
+            AlwaysEqual(BitSet(0)),
+            false
+          ),
+          Vector("rho:io:stdoutAck"),
+          AlwaysEqual(BitSet())
+        )
+      ),
+      List(),
+      List(),
+      Vector(),
+      List(),
+      List(),
+      AlwaysEqual(BitSet()),
+      false
+    )
 
-    failure(intercept).getMessage.stripLineEnd should endWith(nonReadableErr)
+    failure(interceptNormalized) should contain(nonReadableErr)
   }
 
   "rho:io:stderr" should "not get intercepted" in {
-    val intercept =
-      """new stderr(`rho:io:stderr`) in { stderr!("foo", "bar") | for (...@x <- stderr) { stderr!(x) } }"""
+    // new s(`rho:io:stderr`) in { for(x <- s) { Nil } }
+    val interceptNormalized = Par(
+      List(),
+      List(),
+      List(
+        New(
+          1,
+          Par(
+            List(),
+            List(
+              Receive(
+                List(
+                  ReceiveBind(
+                    List(
+                      Par(
+                        List(),
+                        List(),
+                        List(),
+                        List(Expr(EVarBody(EVar(Var(FreeVar(0)))))),
+                        List(),
+                        List(),
+                        List(),
+                        List(),
+                        AlwaysEqual(BitSet()),
+                        true
+                      )
+                    ),
+                    Par(
+                      List(),
+                      List(),
+                      List(),
+                      List(Expr(EVarBody(EVar(Var(BoundVar(0)))))),
+                      List(),
+                      List(),
+                      List(),
+                      List(),
+                      AlwaysEqual(BitSet(0)),
+                      false
+                    ),
+                    None,
+                    1
+                  )
+                ),
+                Par(
+                  List(),
+                  List(),
+                  List(),
+                  List(),
+                  List(),
+                  Vector(),
+                  List(),
+                  List(),
+                  AlwaysEqual(BitSet()),
+                  false
+                ),
+                false,
+                1,
+                AlwaysEqual(BitSet(0)),
+                false
+              )
+            ),
+            List(),
+            List(),
+            List(),
+            Vector(),
+            List(),
+            List(),
+            AlwaysEqual(BitSet(0)),
+            false
+          ),
+          Vector("rho:io:stderr"),
+          AlwaysEqual(BitSet())
+        )
+      ),
+      List(),
+      List(),
+      Vector(),
+      List(),
+      List(),
+      AlwaysEqual(BitSet()),
+      false
+    )
 
-    failure(intercept).getMessage.stripLineEnd should endWith(nonReadableErr)
+    failure(interceptNormalized) should contain(nonReadableErr)
   }
 
   "rho:io:stderrAck" should "not get intercepted" in {
-    val intercept =
-      """new ackCh, stderrAck(`rho:io:stderrAck`) in { stderrAck!("foo") | for (@x <- stderrAck) { stderrAck!(x, *ackCh) } }"""
+    // new s(`rho:io:stderrAck`) in { for (x <- s) { Nil } }
+    val interceptNormalized = Par(
+      List(),
+      List(),
+      List(
+        New(
+          1,
+          Par(
+            List(),
+            List(
+              Receive(
+                List(
+                  ReceiveBind(
+                    List(
+                      Par(
+                        List(),
+                        List(),
+                        List(),
+                        List(Expr(EVarBody(EVar(Var(FreeVar(0)))))),
+                        List(),
+                        List(),
+                        List(),
+                        List(),
+                        AlwaysEqual(BitSet()),
+                        true
+                      )
+                    ),
+                    Par(
+                      List(),
+                      List(),
+                      List(),
+                      List(Expr(EVarBody(EVar(Var(BoundVar(0)))))),
+                      List(),
+                      List(),
+                      List(),
+                      List(),
+                      AlwaysEqual(BitSet(0)),
+                      false
+                    ),
+                    None,
+                    1
+                  )
+                ),
+                Par(
+                  List(),
+                  List(),
+                  List(),
+                  List(),
+                  List(),
+                  Vector(),
+                  List(),
+                  List(),
+                  AlwaysEqual(BitSet()),
+                  false
+                ),
+                false,
+                1,
+                AlwaysEqual(BitSet(0)),
+                false
+              )
+            ),
+            List(),
+            List(),
+            List(),
+            Vector(),
+            List(),
+            List(),
+            AlwaysEqual(BitSet(0)),
+            false
+          ),
+          Vector("rho:io:stderrAck"),
+          AlwaysEqual(BitSet())
+        )
+      ),
+      List(),
+      List(),
+      Vector(),
+      List(),
+      List(),
+      AlwaysEqual(BitSet()),
+      false
+    )
 
-    failure(intercept).getMessage.stripLineEnd should endWith(nonReadableErr)
+    failure(interceptNormalized) should contain(nonReadableErr)
   }
 
-  private def failure(rho: String): Throwable =
-    execute(rho).swap.getOrElse(fail(s"Expected $rho to fail - it didn't."))
+  private def failure(normalized: Par): Vector[Throwable] =
+    evaluate(normalized).right.get.errors
 
-  private def execute(source: String): Either[Throwable, Runtime] =
+  private def evaluate(normalized: Par): Either[Throwable, EvaluateResult] =
     Interpreter
-      .execute(runtime, new StringReader(source))
+      .evaluate(runtime, normalized)
       .attempt
       .runSyncUnsafe(maxDuration)
 }
