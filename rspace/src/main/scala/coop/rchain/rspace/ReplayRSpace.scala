71993737addc27667ebaf5441d42aafef632b045
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2018-10-09 13:57:09 +0200

    Add doc for rig

--- rspace/src/main/scala/coop/rchain/rspace/ReplayRSpace.scala
+++ rspace/src/main/scala/coop/rchain/rspace/ReplayRSpace.scala
@@ -23,6 +23,14 @@ trait IReplaySpace[F[_], C, P, E, A, R, K] extends ISpace[F, C, P, E, A, R, K] {
 
   protected[rspace] var replayData: ReplayData = ReplayData.empty
 
+  /** Rigs this ReplaySpace with the initial state and a log of permitted operations.
+    * During replay, whenever a COMM event that is not available in the log occurs, an error is going to be raised.
+    *
+    * This method is not thread safe.
+    *
+    *  @param startRoot A [Blake2b256Hash] representing the intial state
+    *  @param log A [Log] with permitted operations
+    */
   def rig(startRoot: Blake2b256Hash, log: trace.Log): Unit = {
     // create a set of the "new" IOEvents
     val newStuff: Set[Event] = log.filter {
