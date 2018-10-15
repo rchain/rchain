1a17fcce375a45dabe98624a12443035cdb71c8d
Mateusz Górski <gorski.mateusz@protonmail.ch>
2018-10-16 00:39:07 +0200

    Casper: Order the elements in the Log.

--- casper/src/main/scala/coop/rchain/casper/util/rholang/ProcessedDeployUtil.scala
+++ casper/src/main/scala/coop/rchain/casper/util/rholang/ProcessedDeployUtil.scala
@@ -4,6 +4,7 @@ import coop.rchain.casper.util.EventConverter
 import coop.rchain.casper.protocol._
 import coop.rchain.models.PCost
 import coop.rchain.rspace.trace
+import scodec.Codec
 
 case class InternalProcessedDeploy(
     deploy: Deploy,
@@ -13,12 +14,18 @@ case class InternalProcessedDeploy(
 )
 
 object ProcessedDeployUtil {
+  implicit val byteArrOrdering = coop.rchain.rspace.util.ordArrayByte
 
   def toInternal(pd: ProcessedDeploy): Option[InternalProcessedDeploy] =
     for {
       d <- pd.deploy
       c <- pd.cost
-      l = pd.log.map(EventConverter.toRspaceEvent)
+      l = pd.log
+        .map(EventConverter.toRspaceEvent)
+        .sortBy(
+          e =>
+            Codec.encode(e).getOrElse(throw new Exception("RSpace log is corrupted.")).toByteArray
+        )
       s = if (pd.errored) UnknownFailure else Succeeded
     } yield InternalProcessedDeploy(d, c, l, s)
 
