1002b079d2b7a8a02dbaf9a243c1628f213cee52
Artur Gajowy <artur.gajowy@gmail.com>
2018-10-15 15:00:16 +0200

    Use GeneratedMessageCompanion as a typeclass

--- models/src/main/scala/coop/rchain/models/serialization/implicits.scala
+++ models/src/main/scala/coop/rchain/models/serialization/implicits.scala
@@ -8,24 +8,27 @@ import scodec.bits.ByteVector
 import scalapb.{GeneratedMessage, GeneratedMessageCompanion, Message}
 
 object implicits {
-  def mkProtobufInstance[T <: GeneratedMessage with Message[T]](
-      comp: GeneratedMessageCompanion[T]
-  ) = new Serialize[T] {
+  def mkProtobufInstance[T <: GeneratedMessage with Message[T]: GeneratedMessageCompanion] =
+    new Serialize[T] {
 
-    override def encode(a: T): ByteVector =
-      ByteVector.view(comp.toByteArray(a))
+      override def encode(a: T): ByteVector = {
+        val comp: GeneratedMessageCompanion[T] = implicitly
+        ByteVector.view(comp.toByteArray(a))
+      }
 
-    override def decode(bytes: ByteVector): Either[Throwable, T] =
-      Either.catchNonFatal(comp.parseFrom(bytes.toArray))
-  }
+      override def decode(bytes: ByteVector): Either[Throwable, T] = {
+        val comp: GeneratedMessageCompanion[T] = implicitly
+        Either.catchNonFatal(comp.parseFrom(bytes.toArray))
+      }
+    }
 
-  implicit val serializePar: Serialize[Par]         = mkProtobufInstance(Par)
-  implicit val serializeVar: Serialize[Var]         = mkProtobufInstance(Var)
-  implicit val serializeSend: Serialize[Send]       = mkProtobufInstance(Send)
-  implicit val serializeReceive: Serialize[Receive] = mkProtobufInstance(Receive)
-  implicit val serializeNew: Serialize[New]         = mkProtobufInstance(New)
-  implicit val serializeExpr: Serialize[Expr]       = mkProtobufInstance(Expr)
-  implicit val serializeMatch: Serialize[Match]     = mkProtobufInstance(Match)
-  implicit val serializeESet: Serialize[ESet]       = mkProtobufInstance(ESet)
-  implicit val serializeEMap: Serialize[EMap]       = mkProtobufInstance(EMap)
+  implicit val serializePar: Serialize[Par]         = mkProtobufInstance
+  implicit val serializeVar: Serialize[Var]         = mkProtobufInstance
+  implicit val serializeSend: Serialize[Send]       = mkProtobufInstance
+  implicit val serializeReceive: Serialize[Receive] = mkProtobufInstance
+  implicit val serializeNew: Serialize[New]         = mkProtobufInstance
+  implicit val serializeExpr: Serialize[Expr]       = mkProtobufInstance
+  implicit val serializeMatch: Serialize[Match]     = mkProtobufInstance
+  implicit val serializeESet: Serialize[ESet]       = mkProtobufInstance
+  implicit val serializeEMap: Serialize[EMap]       = mkProtobufInstance
 }
