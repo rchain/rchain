39cd93dab103b2656eb46d92f6dd8b5e24d1d173
Kent Shikama <kent@kentshikama.com>
2018-10-16 14:29:18 -0700

    Add more log messages to Casper to debug block adding
    Change "Initialize protocol handshake" to debug

--- comm/src/main/scala/coop/rchain/comm/rp/Connect.scala
+++ comm/src/main/scala/coop/rchain/comm/rp/Connect.scala
@@ -124,7 +124,7 @@ object Connect {
       peerAddr = peer.toAddress
       _        <- Log[F].debug(s"Connecting to $peerAddr")
       _        <- Metrics[F].incrementCounter("connects")
-      _        <- Log[F].info(s"Initialize protocol handshake to $peerAddr")
+      _        <- Log[F].debug(s"Initialize protocol handshake to $peerAddr")
       local    <- RPConfAsk[F].reader(_.local)
       ph       = protocolHandshake(local)
       phsresp  <- TransportLayer[F].roundTrip(peer, ph, timeout * 2) >>= ErrorHandler[F].fromEither
