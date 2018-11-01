965637d44595369a2a6a3572457a47842b0bfb63
Ovidiu Deac <ovidiu@pyrofex.net>
2018-11-01 09:19:20 +0200

    optimize imports

--- rholang/src/test/scala/coop/rchain/rholang/interpreter/accounting/CostAccountingPropertyTest.scala
+++ rholang/src/test/scala/coop/rchain/rholang/interpreter/accounting/CostAccountingPropertyTest.scala
@@ -1,24 +1,23 @@
 package coop.rchain.rholang.interpreter.accounting
 import java.nio.file.Files
 
-import cats.Id
-import cats.effect.Sync
-import coop.rchain.crypto.hash.Blake2b512Random
 import cats.implicits._
+import coop.rchain.crypto.hash.Blake2b512Random
 import coop.rchain.models._
 import coop.rchain.rholang.interpreter.Runtime.{RhoContext, RhoISpace}
-import coop.rchain.rholang.interpreter.errors.OutOfPhlogistonsError
-import coop.rchain.rholang.{PrettyPrinted, ProcGen}
 import coop.rchain.rholang.interpreter._
+import coop.rchain.rholang.interpreter.errors.OutOfPhlogistonsError
 import coop.rchain.rholang.syntax.rholang_mercury.Absyn.{PPar, Proc}
 import coop.rchain.rholang.syntax.rholang_mercury.PrettyPrinter
+import coop.rchain.rholang.{GenTools, PrettyPrinted, ProcGen}
 import coop.rchain.rspace.history.Branch
 import coop.rchain.rspace.{Context, RSpace}
 import monix.eval.Task
+import monix.execution.Scheduler.Implicits.global
 import org.scalacheck.Arbitrary
 import org.scalacheck.Test.Parameters
-import org.scalatest.{FlatSpec, Matchers}
 import org.scalatest.prop.PropertyChecks
+import org.scalatest.{FlatSpec, Matchers}
 
 import scala.concurrent.duration._
 
@@ -81,12 +80,11 @@ object CostAccountingPropertyTest {
       .runSyncUnsafe(duration)
 
   def createRhoISpace(): RhoISpace[Task] = {
-    import cats.implicits._
     import coop.rchain.catscontrib.TaskContrib._
     import coop.rchain.rholang.interpreter.storage.implicits._
     val dbDir               = Files.createTempDirectory("cost-accounting-property-test-")
     val context: RhoContext = Context.create(dbDir, 1024L * 1024L * 4)
-    val space: RhoISpace[Task] = (RSpace
+    val space: RhoISpace[Task] = RSpace
       .create[
         Task,
         Par,
@@ -95,7 +93,7 @@ object CostAccountingPropertyTest {
         ListParWithRandom,
         ListParWithRandomAndPhlos,
         TaggedContinuation
-      ](context, Branch("test")))
+      ](context, Branch("test"))
       .unsafeRunSync
     space
   }
