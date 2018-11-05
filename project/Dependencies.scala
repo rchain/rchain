5eb039f21b5c2759a9669916bfd03b2451cb270b
Scala steward <scala-steward@timepit.eu>
2018-11-05 21:29:31 +0100

    Update kamon-core to 1.1.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
 
   val circeVersion  = "0.9.1"
   val http4sVersion = "0.19.0-M1"
-  val kamonVersion  = "1.1.0"
+  val kamonVersion  = "1.1.3"
 
   // format: off
   val bitcoinjCore        = "org.bitcoinj"                % "bitcoinj-core"             % "0.14.6"
