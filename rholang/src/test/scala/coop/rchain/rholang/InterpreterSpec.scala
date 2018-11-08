cc059f71123397f9f5b2ad1404521eeb921764eb
Ovidiu Deac <ovidiu@pyrofex.net>
2018-11-08 05:20:28 +0200

    fix compilation errors in InterpreterSpec

--- rholang/src/test/scala/coop/rchain/rholang/InterpreterSpec.scala
+++ rholang/src/test/scala/coop/rchain/rholang/InterpreterSpec.scala
@@ -10,6 +10,7 @@ import monix.eval.Task
 import org.scalatest.{FlatSpec, Matchers}
 
 import scala.concurrent.duration._
+import scala.util.Try
 
 class InterpreterSpec extends FlatSpec with Matchers {
   private val mapSize     = 10L * 1024L * 1024L
@@ -19,48 +20,77 @@ class InterpreterSpec extends FlatSpec with Matchers {
   behavior of "Interpreter"
 
   it should "restore RSpace to its prior state after evaluation error" in {
-    val initStorage = storageContents()
-    val send        = "@{0}!(0)"
-    success(send)
-    val beforeError = storageContents()
-    assert(beforeError.contains(send))
-    failure("@1!(1) | @2!(3.noSuchMethod())")
-    assert(storageContents() == beforeError)
-    success("new stdout(`rho:io:stdout`) in { stdout!(42) }")
-    assert(storageContents() == beforeError)
-    success("for (_ <- @0) { Nil }")
-    assert(storageContents() == initStorage)
+    import coop.rchain.catscontrib.effect.implicits.bracketTry
+
+    val sendRho = "@{0}!(0)"
+
+    val (initStorage, beforeError, afterError, afterSend, finalContent) =
+      mkRuntime[Try](tmpPrefix, mapSize)
+      .use { runtime =>
+        Try {
+          val initStorage = storageContents(runtime)
+          success(runtime, sendRho).runSyncUnsafe(maxDuration)
+          val beforeError = storageContents(runtime)
+          failure(runtime, "@1!(1) | @2!(3.noSuchMethod())").runSyncUnsafe(maxDuration)
+          val afterError = storageContents(runtime)
+          success(runtime, "new stdout(`rho:io:stdout`) in { stdout!(42) }").runSyncUnsafe(maxDuration)
+          val afterSend = storageContents(runtime)
+          success(runtime, "for (_ <- @0) { Nil }").runSyncUnsafe(maxDuration)
+          val finalContent = storageContents(runtime)
+
+          (initStorage, beforeError, afterError, afterSend, finalContent)
+        }
+      }
+      .get
+
+    assert(beforeError.contains(sendRho))
+    assert(afterError == beforeError)
+    assert(afterSend == beforeError)
+    assert(finalContent == initStorage)
   }
 
   it should "yield correct results for the PrimeCheck contract" in {
-    success("""
-        |new loop, primeCheck, stdoutAck(`rho:io:stdoutAck`) in {
-        |            contract loop(@x) = {
-        |              match x {
-        |                [] => Nil
-        |                [head ...tail] => {
-        |                  new ret in {
-        |                    for (_ <- ret) {
-        |                      loop!(tail)
-        |                    } | primeCheck!(head, *ret)
-        |                  }
-        |                }
-        |              }
-        |            } |
-        |            contract primeCheck(@x, ret) = {
-        |              match x {
-        |                Nil => { stdoutAck!("Nil", *ret) | @0!("Nil") }
-        |                ~{~Nil | ~Nil} => { stdoutAck!("Prime", *ret) | @0!("Pr") }
-        |                _ => { stdoutAck!("Composite", *ret) |  @0!("Co") }
-        |              }
-        |            } |
-        |            loop!([Nil, 7, 7 | 8, 9 | Nil, 9 | 10, Nil, 9])
-        |  }
-      """.stripMargin)
+    import coop.rchain.catscontrib.effect.implicits.bracketTry
+    val contents = mkRuntime[Try](tmpPrefix, mapSize)
+        .use { runtime =>
+          Try {
+            success(
+              runtime,
+              """
+              |new loop, primeCheck, stdoutAck(`rho:io:stdoutAck`) in {
+              |            contract loop(@x) = {
+              |              match x {
+              |                [] => Nil
+              |                [head ...tail] => {
+              |                  new ret in {
+              |                    for (_ <- ret) {
+              |                      loop!(tail)
+              |                    } | primeCheck!(head, *ret)
+              |                  }
+              |                }
+              |              }
+              |            } |
+              |            contract primeCheck(@x, ret) = {
+              |              match x {
+              |                Nil => { stdoutAck!("Nil", *ret) | @0!("Nil") }
+              |                ~{~Nil | ~Nil} => { stdoutAck!("Prime", *ret) | @0!("Pr") }
+              |                _ => { stdoutAck!("Composite", *ret) |  @0!("Co") }
+              |              }
+              |            } |
+              |            loop!([Nil, 7, 7 | 8, 9 | Nil, 9 | 10, Nil, 9])
+              |  }
+            """.stripMargin
+            ).runSyncUnsafe(maxDuration)
+
+            storageContents(runtime)
+          }
+        }
+        .get
+
     // TODO: this is not the way we should be testing execution results,
     // yet strangely it works - and we don't have a better way for now
     assert(
-      storageContents().startsWith(
+      contents.startsWith(
         Seq(
           """@{0}!("Nil") |""",
           """@{0}!("Pr") |""",
@@ -77,17 +107,15 @@ class InterpreterSpec extends FlatSpec with Matchers {
   private def storageContents(runtime : Runtime): String =
     StoragePrinter.prettyPrint(runtime.space.store)
 
-  private def success(rho: String): Unit =
-    execute(rho).swap.foreach(error => fail(s"""Execution failed for: $rho
+  private def success(runtime : Runtime, rho: String): Task[Unit] =
+    execute(runtime, rho).map(_.swap.foreach(error => fail(s"""Execution failed for: $rho
                                                |Cause:
-                                               |$error""".stripMargin))
+                                               |$error""".stripMargin)))
 
-  private def failure(rho: String): Throwable =
-    execute(rho).swap.getOrElse(fail(s"Expected $rho to fail - it didn't."))
+  private def failure(runtime : Runtime, rho: String): Task[Throwable] =
+    execute(runtime, rho).map(_.swap.getOrElse(fail(s"Expected $rho to fail - it didn't.")))
 
-  private def execute(source: String): Either[Throwable, Runtime] =
-    mkRuntime[Task](tmpPrefix, mapSize)
-      .use{runtime => Interpreter.execute(runtime, new StringReader(source)).attempt}
-      .runSyncUnsafe(maxDuration)
+  private def execute(runtime:Runtime, source: String): Task[Either[Throwable, Runtime]] =
+    Interpreter.execute(runtime, new StringReader(source)).attempt
 
 }
