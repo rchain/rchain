67115c3d11c19ee346f70f196811194c9c915043
Adam Szkoda <adaszko@gmail.com>
2018-11-08 18:45:47 +0100

    Formatting fixes

--- integration-testing/rnode_testing/util.py
+++ integration-testing/rnode_testing/util.py
@@ -1,14 +1,18 @@
-import logging
-import random
-import tempfile
-import rnode_testing.resources as resources
 import os
+import random
 import pprint
-from contextlib import contextmanager
+import logging
+import tempfile
+import contextlib
 import collections
 
+import docker as docker_py
 
-@contextmanager
+import rnode_testing.resources as resources
+
+
+
+@contextlib.contextmanager
 def log_box(log_function, title="", char="*", length=150):
     full_title = f" {title} " if title else ""
     title_stars_len = int((length - len(full_title)) / 2)
@@ -31,15 +35,14 @@ def make_tempdir(prefix):
     return tempfile.mkdtemp(dir="/tmp", prefix=prefix)
 
 
-Config = collections.namedtuple("Config",
-                                [
-                                    "peer_count",
-                                    "node_startup_timeout",
-                                    "network_converge_timeout",
-                                    "receive_timeout",
-                                    "rnode_timeout",
-                                    "blocks"
-                                ])
+Config = collections.namedtuple("Config", [
+    "peer_count",
+    "node_startup_timeout",
+    "network_converge_timeout",
+    "receive_timeout",
+    "rnode_timeout",
+    "blocks",
+])
 
 
 KeyPair = collections.namedtuple("KeyPair", ["private_key", "public_key"])
@@ -58,13 +61,14 @@ def parse_config(request):
 
     def make_timeout(value, base, peer_factor=10): return value if value > 0 else base + peer_count * peer_factor
 
-    config = Config(peer_count=peer_count,
-                    node_startup_timeout=make_timeout(start_timeout, 30, 10),
-                    network_converge_timeout=make_timeout(converge_timeout, 200, 10),
-                    receive_timeout=make_timeout(receive_timeout, 10, 10),
-                    rnode_timeout=rnode_timeout,
-                    blocks=blocks
-                    )
+    config = Config(
+        peer_count=peer_count,
+        node_startup_timeout=make_timeout(start_timeout, 30, 10),
+        network_converge_timeout=make_timeout(converge_timeout, 200, 10),
+        receive_timeout=make_timeout(receive_timeout, 10, 10),
+        rnode_timeout=rnode_timeout,
+        blocks=blocks,
+    )
 
     with log_box(logging.info):
         s = pprint.pformat(dict(config._asdict()), indent=4)
@@ -73,29 +77,20 @@ def parse_config(request):
     return config
 
 
-@contextmanager
+@contextlib.contextmanager
 def docker():
-    import docker
-
-    docker_client = docker.from_env()
-
+    docker_client = docker_py.from_env()
     try:
         yield docker_client
     finally:
-        logging.info("Remove unused volumes")
         docker_client.volumes.prune()
-
-        logging.info("Remove unused networks")
         docker_client.networks.prune()
 
 
-@contextmanager
+@contextlib.contextmanager
 def bonds_file(validator_keys):
     (fd, file) = tempfile.mkstemp(prefix="rchain-bonds-file-", suffix=".txt", dir="/tmp")
-
     try:
-        logging.info(f"Using bonds file: `{file}`")
-
         with os.fdopen(fd, "w") as f:
             for pair in validator_keys:
                 bond = random.randint(1, 100)
@@ -103,10 +98,9 @@ def bonds_file(validator_keys):
         yield file
     finally:
         os.unlink(file)
-        logging.info(f"Bonds file `{file}` deleted")
 
 
-@contextmanager
+@contextlib.contextmanager
 def validators_data(config):
     # Using pre-generated validator key pairs by rnode. We do this because warning below  with python generated keys
     # WARN  coop.rchain.casper.Validate$ - CASPER: Ignoring block 2cb8fcc56e... because block creator 3641880481... has 0 weight
