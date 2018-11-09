48df62657c8843417d68d08a46026e8fc2ace8e5
Ovidiu Deac <ovidiu@pyrofex.net>
2018-11-09 21:42:36 +0200

    rewrite ErrorLog in terms of Ref

--- rholang/src/main/scala/coop/rchain/rholang/interpreter/ErrorLog.scala
+++ rholang/src/main/scala/coop/rchain/rholang/interpreter/ErrorLog.scala
@@ -1,39 +1,28 @@
 package coop.rchain.rholang.interpreter
 
 import cats.Functor
+import cats.implicits._
+import cats.effect.Sync
+import cats.effect.concurrent.Ref
 import cats.mtl.FunctorTell
-import monix.eval.Task
 
-class ErrorLog extends FunctorTell[Task, Throwable] {
-  private var errorVector: Vector[Throwable] = Vector.empty
-  val functor                                = implicitly[Functor[Task]]
-  override def tell(e: Throwable): Task[Unit] =
-    Task.now {
-      this.synchronized {
-        errorVector = errorVector :+ e
-      }
-    }
+class ErrorLog[F[_] : Sync] (private val errors : Ref[F, Vector[Throwable]]) extends FunctorTell[F, Throwable] {
+  override  val functor: Functor[F] = implicitly[Functor[F]]
 
-  override def writer[A](a: A, e: Throwable): Task[A] =
-    Task.now {
-      this.synchronized {
-        errorVector = errorVector :+ e
-      }
-      a
-    }
+  override def tell(e: Throwable): F[Unit] = errors.update( es => es :+ e )
 
-  override def tuple[A](ta: (Throwable, A)): Task[A] =
-    Task.now {
-      this.synchronized {
-        errorVector = errorVector :+ ta._1
-      }
-      ta._2
-    }
+  override def writer[A](a: A, e: Throwable): F[A] = tell(e) *> a.pure[F]
 
-  def readAndClearErrorVector(): Vector[Throwable] =
-    this.synchronized {
-      val ret = errorVector
-      errorVector = Vector.empty
-      ret
-    }
+  override def tuple[A](ta: (Throwable, A)): F[A] = tell(ta._1) *> ta._2.pure[F]
+
+  def readAndClearErrorVector(): F[Vector[Throwable]] =
+    for {
+      previousErrors <- errors.get
+      _ <- errors.set(Vector.empty)
+    } yield previousErrors
+}
+
+object ErrorLog {
+  def create[F[_] : Sync]: F[ErrorLog[F]] =
+    Ref.of[F, Vector[Throwable]](Vector.empty[Throwable]).map(new ErrorLog(_))
 }
