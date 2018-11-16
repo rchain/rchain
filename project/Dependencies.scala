d29bb00c3f7fad4cd4b986ce28c874da24b62bf1
Scala steward <scala-steward@timepit.eu>
2018-11-16 08:56:14 +0100

    Update scala-uri to 1.1.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -40,7 +40,7 @@ object Dependencies {
   val lz4                 = "org.lz4"                     % "lz4-java"                  % "1.5.0"
   val monix               = "io.monix"                   %% "monix"                     % "3.0.0-RC2"
   val scalaLogging        = "com.typesafe.scala-logging" %% "scala-logging"             % "3.7.2"
-  val scalaUri            = "io.lemonlabs"               %% "scala-uri"                 % "1.1.4"
+  val scalaUri            = "io.lemonlabs"               %% "scala-uri"                 % "1.1.5"
   val scalacheck          = "org.scalacheck"             %% "scalacheck"                % "1.13.5" % "test"
   val scalacheckNoTest    = "org.scalacheck"             %% "scalacheck"                % "1.13.5"
   val scalacheckShapeless = "com.github.alexarchambault" %% "scalacheck-shapeless_1.13" % "1.1.8" % "test"
