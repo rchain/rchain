6c6ce817a831c51c3f2d921c75a7e05922b3f7b8
Artur Gajowy <artur.gajowy@gmail.com>
2018-11-16 12:10:58 +0100

    Derive `Arbitrary[PrettyPrinted[T]]` for `T: Arbitrary: Pretty`

--- models/src/test/scala/coop/rchain/models/PrettyPrinted.scala
+++ models/src/test/scala/coop/rchain/models/PrettyPrinted.scala
@@ -1,6 +1,6 @@
 package coop.rchain.models
 
-import org.scalacheck.Shrink
+import org.scalacheck.{Arbitrary, Shrink}
 
 case class PrettyPrinted[T](value: T, toStr: T => String) {
   override def toString = toStr(value)
@@ -8,8 +8,14 @@ case class PrettyPrinted[T](value: T, toStr: T => String) {
 
 object PrettyPrinted {
 
-  implicit def shrinkPretty[T](implicit shrinker: Shrink[T]): Shrink[PrettyPrinted[T]] = Shrink {
-    p =>
-      shrinker.shrink(p.value).map(PrettyPrinted[T](_, p.toStr))
+  implicit def arbitraryPrettyPrintedFromPretty[T: Arbitrary: Pretty]
+    : Arbitrary[PrettyPrinted[T]] = {
+    val gen = implicitly[Arbitrary[T]].arbitrary
+    Arbitrary(gen.map(PrettyPrinted(_, Pretty[T].pretty(_, 0))))
   }
+
+  implicit def shrinkPrettyPrinted[T](implicit shrinker: Shrink[T]): Shrink[PrettyPrinted[T]] =
+    Shrink { p =>
+      shrinker.shrink(p.value).map(PrettyPrinted[T](_, p.toStr))
+    }
 }
