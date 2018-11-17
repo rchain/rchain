6fd468012f3aeaf8f8a405f7ef332a2985a65c26
Ovidiu Deac <ovidiu@pyrofex.net>
2018-11-17 16:30:25 +0200

    fix SyncInstances compilation errors

--- shared/src/main/scala/coop/rchain/catscontrib/SyncInstances.scala
+++ shared/src/main/scala/coop/rchain/catscontrib/SyncInstances.scala
@@ -1,5 +1,6 @@
 package coop.rchain.catscontrib
 
+import cats.implicits._
 import cats.data.EitherT
 import cats.effect.{ExitCase, Sync}
 import cats.{Applicative, FlatMap}
@@ -29,12 +30,44 @@ object SyncInstances {
 
       def bracketCase[A, B](
           acquire: Effect[A]
-      )(use: A => Effect[B])(release: (A, ExitCase[Throwable]) => Effect[Unit]): Effect[B] =
+      )(use: A => Effect[B])(release: (A, ExitCase[Throwable]) => Effect[Unit]): Effect[B] = {
+        def useTask(eitherResource: Either[Throwable, Either[E, A]]): Task[Either[Throwable, B]] = {
+          def go(eitherA: Either[E, A]): Task[Either[E, B]] =
+            eitherA
+              .traverse(use(_).value)
+              .map(_.joinRight)
+
+          eitherResource
+            .leftMap(fromThrowable(_))
+            .traverse(go)
+            .map(_.joinRight.leftMap(toThrowable))
+        }
+
+        def releaseTask(
+            exitValue: Either[Throwable, Either[E, A]],
+            exitCase: ExitCase[Throwable]
+        ): Task[Unit] = {
+          def go(a: A): Task[Unit] =
+            release(a, exitCase).value
+              .flatMap {
+                case Left(ex)  => Task.raiseError(toThrowable(ex))
+                case Right(()) => Task.unit
+              }
+
+          exitValue
+            .map(x => x.leftMap(toThrowable))
+            .joinRight
+            .traverse(go)
+            .rethrow
+        }
+
         EitherT(
-          acquire.value
-            .bracketCase(
-              resource => use(resource).value){
-              case (a, exitCase) => release(a, exitCase).value.map(_.right)})
+          acquire.value.attempt
+            .bracketCase(useTask)(releaseTask)
+            .map(_.leftMap(fromThrowable))
+        )
+
+      }
 
       def handleErrorWith[A](fa: Effect[A])(f: Throwable => Effect[A]): Effect[A] =
         EitherT(fa.value flatMap {
