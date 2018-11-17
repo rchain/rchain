6d634f49379b1bb8a703888d9fd70260c1af0a5f
Ovidiu Deac <ovidiu@pyrofex.net>
2018-11-17 16:56:20 +0200

    add tests for Sync[Effect]

---
+++ shared/src/test/scala/coop/rchain/shared/SyncInstancesSpec.scala
@@ -0,0 +1,76 @@
+package coop.rchain.shared
+
+import cats.implicits._
+import cats.data.EitherT
+import cats.effect.{ExitCase, Sync}
+import cats.effect.concurrent.Ref
+import coop.rchain.catscontrib.SyncInstances.syncEffect
+import monix.eval.Task
+import monix.execution.Scheduler.Implicits.global
+import org.scalatest.{FunSpec, Matchers}
+
+import scala.concurrent.duration._
+
+class SyncInstancesSpec extends FunSpec with Matchers {
+
+  type Effect[A] = EitherT[Task, Throwable, A]
+  def mkEffect[A](t: Task[A]): Effect[A] = EitherT[Task, Throwable, A](t.attempt)
+
+  implicit val sync: Sync[Effect] = syncEffect[Throwable](identity, identity)
+
+  describe("Sync[Effect].brackedCase") {
+
+    it("should call release in case of error") {
+      val ex = new Exception()
+
+      val (result, exitValue, exitCase) =
+        test[Int](
+          1,
+          _ => Task.raiseError[Int](ex)
+        ).runSyncUnsafe(1.second)
+
+      result should be(Left(ex))
+      exitValue should be(Some(1))
+      exitCase should be(Some(ExitCase.error(ex)))
+    }
+
+    it("should call release with the return value in case of success") {
+      val (result, exitValue, exitCase) =
+        test[Int](1, x => Task.delay(x + 1))
+          .runSyncUnsafe(1.second)
+
+      result should be(Right(2))
+      exitCase should be(Some(ExitCase.complete))
+      exitValue should be(Some(1))
+    }
+  }
+
+  def test[A](
+      init: A,
+      body: A => Task[A]
+  ): Task[(Either[Throwable, A], Option[A], Option[ExitCase[Throwable]])] = {
+    val acquireEff = mkEffect(Task.delay(init))
+
+    def bodyEff(a: A) = mkEffect(body(a))
+
+    for {
+      exitValueRef <- Ref.of[Task, Option[A]](None)
+      exitCaseRef  <- Ref.of[Task, Option[ExitCase[Throwable]]](None)
+
+      r <- Sync[Effect]
+            .attempt(
+              Sync[Effect].bracketCase(acquireEff)(bodyEff)(
+                (v: A, x: ExitCase[Throwable]) =>
+                  mkEffect(for {
+                    _ <- exitValueRef.set(Some(v))
+                    _ <- exitCaseRef.set(Some(x))
+                  } yield ())
+              )
+            )
+            .value
+
+      ev <- exitValueRef.get
+      ec <- exitCaseRef.get
+    } yield (r.joinRight, ev, ec)
+  }
+}
