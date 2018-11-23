853a8b359bcc6f1921d423f3a0f1c2dcda92235d
Artur Gajowy <artur.gajowy@gmail.com>
2018-11-23 22:14:43 +0100

    Add MonadTrans instance for StateT

--- shared/src/main/scala/coop/rchain/catscontrib/package.scala
+++ shared/src/main/scala/coop/rchain/catscontrib/package.scala
@@ -3,6 +3,7 @@ package coop.rchain
 package object catscontrib
     extends CaptureInstances
     with EitherTInstances
+    with StateTInstances
     with WriterTInstances
     with ApplicativeError_Instances
     with SeqInstances
