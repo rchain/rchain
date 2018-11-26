26beeec76b200bb3e08b1df304c555f1656d9b16
Sebastian Bach <sebastian.bach@rchain.coop>
2018-11-26 16:23:29 +0100

    Add error method with Exception cause to Log

--- shared/src/main/scala/coop/rchain/shared/Log.scala
+++ shared/src/main/scala/coop/rchain/shared/Log.scala
@@ -2,12 +2,14 @@ package coop.rchain.shared
 
 import cats._
 import cats.data._
+import cats.effect.Sync
 import cats.implicits._
 import coop.rchain.catscontrib._
 import Catscontrib._
 
 import scala.language.experimental.macros
 import scala.reflect.macros.blackbox
+import coop.rchain.catscontrib.effect.implicits._
 
 trait LogSource {
   val clazz: Class[_]
@@ -41,6 +43,7 @@ trait Log[F[_]] {
   def info(msg: String)(implicit ev: LogSource): F[Unit]
   def warn(msg: String)(implicit ev: LogSource): F[Unit]
   def error(msg: String)(implicit ev: LogSource): F[Unit]
+  def error(msg: String, cause: Throwable)(implicit ev: LogSource): F[Unit]
 }
 
 object Log extends LogInstances {
@@ -54,15 +57,18 @@ object Log extends LogInstances {
       def info(msg: String)(implicit ev: LogSource): T[F, Unit]  = L.info(msg)(ev).liftM[T]
       def warn(msg: String)(implicit ev: LogSource): T[F, Unit]  = L.warn(msg)(ev).liftM[T]
       def error(msg: String)(implicit ev: LogSource): T[F, Unit] = L.error(msg)(ev).liftM[T]
+      def error(msg: String, cause: Throwable)(implicit ev: LogSource): T[F, Unit] =
+        L.error(msg, cause)(ev).liftM[T]
     }
 
   class NOPLog[F[_]: Applicative] extends Log[F] {
-    def isTraceEnabled(implicit ev: LogSource): F[Boolean]  = false.pure[F]
-    def trace(msg: String)(implicit ev: LogSource): F[Unit] = ().pure[F]
-    def debug(msg: String)(implicit ev: LogSource): F[Unit] = ().pure[F]
-    def info(msg: String)(implicit ev: LogSource): F[Unit]  = ().pure[F]
-    def warn(msg: String)(implicit ev: LogSource): F[Unit]  = ().pure[F]
-    def error(msg: String)(implicit ev: LogSource): F[Unit] = ().pure[F]
+    def isTraceEnabled(implicit ev: LogSource): F[Boolean]                    = false.pure[F]
+    def trace(msg: String)(implicit ev: LogSource): F[Unit]                   = ().pure[F]
+    def debug(msg: String)(implicit ev: LogSource): F[Unit]                   = ().pure[F]
+    def info(msg: String)(implicit ev: LogSource): F[Unit]                    = ().pure[F]
+    def warn(msg: String)(implicit ev: LogSource): F[Unit]                    = ().pure[F]
+    def error(msg: String)(implicit ev: LogSource): F[Unit]                   = ().pure[F]
+    def error(msg: String, cause: Throwable)(implicit ev: LogSource): F[Unit] = ().pure[F]
   }
 
 }
@@ -71,21 +77,23 @@ sealed abstract class LogInstances {
   implicit def eitherTLog[E, F[_]: Monad: Log[?[_]]]: Log[EitherT[F, E, ?]] =
     Log.forTrans[F, EitherT[?[_], E, ?]]
 
-  def log[F[_]: Capture]: Log[F] = new Log[F] {
+  def log[F[_]: Sync]: Log[F] = new Log[F] {
     import com.typesafe.scalalogging.Logger
 
     def isTraceEnabled(implicit ev: LogSource): F[Boolean] =
-      Capture[F].capture(Logger(ev.clazz).underlying.isTraceEnabled())
+      Sync[F].delay(Logger(ev.clazz).underlying.isTraceEnabled())
     def trace(msg: String)(implicit ev: LogSource): F[Unit] =
-      Capture[F].capture(Logger(ev.clazz).trace(msg))
+      Sync[F].delay(Logger(ev.clazz).trace(msg))
     def debug(msg: String)(implicit ev: LogSource): F[Unit] =
-      Capture[F].capture(Logger(ev.clazz).debug(msg))
+      Sync[F].delay(Logger(ev.clazz).debug(msg))
     def info(msg: String)(implicit ev: LogSource): F[Unit] =
-      Capture[F].capture(Logger(ev.clazz).info(msg))
+      Sync[F].delay(Logger(ev.clazz).info(msg))
     def warn(msg: String)(implicit ev: LogSource): F[Unit] =
-      Capture[F].capture(Logger(ev.clazz).warn(msg))
+      Sync[F].delay(Logger(ev.clazz).warn(msg))
     def error(msg: String)(implicit ev: LogSource): F[Unit] =
-      Capture[F].capture(Logger(ev.clazz).error(msg))
+      Sync[F].delay(Logger(ev.clazz).error(msg))
+    def error(msg: String, cause: Throwable)(implicit ev: LogSource): F[Unit] =
+      Sync[F].delay(Logger(ev.clazz).error(msg, cause))
   }
 
   val logId: Log[Id] = log
