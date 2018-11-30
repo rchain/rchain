d9f67b52cd97ef10a6aeb95992c6b28bfe5f9330
Adam Szkoda <adaszko@gmail.com>
2018-11-30 15:16:37 +0100

    Fix test_eval

--- integration-testing/test/test_eval.py
+++ integration-testing/test/test_eval.py
@@ -1,13 +1,14 @@
 import os
 
-from typing import TYPE_CHECKING
+import conftest
 
-if TYPE_CHECKING:
-    from rnode_testing.rnode import Node
+from rnode_testing.rnode import docker_network_with_started_bootstrap
 
 
-def test_eval(bootstrap_node: "Node") -> None:
-    relative_paths = bootstrap_node.shell_out('sh', '-c', 'ls /opt/docker/examples/*.rho').splitlines()
-    for relative_path in relative_paths:
-        full_path = os.path.join('/opt/docker/examples', relative_path)
-        bootstrap_node.eval(full_path)
+def test_eval(command_line_options_fixture, docker_client_fixture):
+    with conftest.testing_context(command_line_options_fixture, docker_client_fixture) as context:
+        with docker_network_with_started_bootstrap(context=context) as bootstrap_node:
+            relative_paths = bootstrap_node.shell_out('sh', '-c', 'ls /opt/docker/examples/*.rho').splitlines()
+            for relative_path in relative_paths:
+                full_path = os.path.join('/opt/docker/examples', relative_path)
+                bootstrap_node.eval(full_path)
