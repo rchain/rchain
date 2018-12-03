75d0f9785d4cad59cc8e3c4aa101b64c010c00b3
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2018-12-03 20:35:58 +0100

    First draft of Consume creation benchmark

---
+++ rspace-bench/src/test/scala/coop/rchain/rspace/bench/hashing/TraceEventCreationBench.scala
@@ -0,0 +1,78 @@
+package coop.rchain.rspace.hashing
+
+import java.util.concurrent.TimeUnit
+
+import scala.collection.immutable.Seq
+
+import coop.rchain.models._
+import coop.rchain.models.testImplicits._
+import coop.rchain.rholang.interpreter.storage.implicits._
+import coop.rchain.rspace._
+import coop.rchain.rspace.bench.serialization.ModelSerializerBenchState
+import coop.rchain.rspace.internal._
+import coop.rchain.rspace.trace.Consume
+import org.openjdk.jmh.annotations.{State => BenchState, _}
+import org.openjdk.jmh.infra.Blackhole
+import org.scalacheck.Arbitrary._
+import org.scalacheck.{Arbitrary, Gen}
+
+@Fork(value = 1)
+@Warmup(iterations = 1)
+@Measurement(iterations = 10)
+@OperationsPerInvocation(value = 100)
+@BenchState(Scope.Benchmark)
+class TraceEventCreationBench extends ModelSerializerBenchState {
+
+  def arbitraryConsumeData[C, P, K](
+      implicit
+      arbC: Arbitrary[C],
+      arbP: Arbitrary[P],
+      arbK: Arbitrary[K]
+  ): Arbitrary[(Seq[C], Seq[P], K)] =
+    Arbitrary(Gen.sized { size =>
+      val constrainedSize = Math.max(1, size)
+      for {
+        chans        <- Gen.containerOfN[List, C](constrainedSize, arbC.arbitrary)
+        pats         <- Gen.containerOfN[List, P](chans.length, arbP.arbitrary)
+        continuation <- arbK.arbitrary
+      } yield (chans, pats, continuation)
+    })
+
+  val light =
+    generate[(Seq[Par], Seq[BindPattern], ListParWithRandom)](1)(
+      arbitraryConsumeData[Par, BindPattern, ListParWithRandom]
+    )
+  val medium =
+    generate[(Seq[Par], Seq[BindPattern], ListParWithRandom)](5)(
+      arbitraryConsumeData[Par, BindPattern, ListParWithRandom]
+    )
+  val heavy =
+    generate[(Seq[Par], Seq[BindPattern], ListParWithRandom)](10)(
+      arbitraryConsumeData[Par, BindPattern, ListParWithRandom]
+    )
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.AverageTime))
+  @OutputTimeUnit(TimeUnit.NANOSECONDS)
+  def lightConsume(bh: Blackhole): Unit =
+    doBench(bh, light)
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.AverageTime))
+  @OutputTimeUnit(TimeUnit.NANOSECONDS)
+  def mediumConsume(bh: Blackhole): Unit =
+    doBench(bh, medium)
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.AverageTime))
+  @OutputTimeUnit(TimeUnit.MILLISECONDS)
+  def heavyConsume(bh: Blackhole): Unit =
+    doBench(bh, heavy)
+
+  @inline
+  def doBench(bh: Blackhole, seq: Seq[(Seq[Par], Seq[BindPattern], ListParWithRandom)]): Unit =
+    seq.map {
+      case (channels, patterns, continuation) =>
+        bh.consume(Consume.create(channels, patterns, continuation, false))
+    }
+}
