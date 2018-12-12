d71c1e382e0d62299a77d4162c563190b4164768
Dominik Zajkowski <dzajkowski@invisibl.es>
2018-12-12 20:44:45 +0100

    CORE-1521 missing import

--- integration-tests/test/test_storage.py
+++ integration-tests/test/test_storage.py
@@ -5,7 +5,7 @@ from .rnode import docker_network_with_started_bootstrap
 from .wait import (
     wait_for_approved_block_received_handler_state,
 )
-
+import pytest
 
 @pytest.mark.xfail
 def test_non_transactional_data_is_stored_and_served_by_node(command_line_options_fixture, docker_client_fixture):
