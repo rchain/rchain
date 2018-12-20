4b397f06686b8bf5465c11e9dbb6aea071371bf2
Pawel Szulc <paul.szulc@gmail.com>
2018-12-21 00:10:30 +0100

    Add initial set of tests for StreamHandler.handleStream

---
+++ comm/src/test/scala/coop/rchain/comm/transport/StreamHandlerSpec.scala
@@ -0,0 +1,94 @@
+package coop.rchain.comm.transport
+
+import java.io.File
+import java.nio.file._
+import org.scalatest._
+import coop.rchain.comm._
+import coop.rchain.shared.Log
+import coop.rchain.comm.protocol.routing._
+import scala.util.Random
+import monix.eval.Task
+import monix.reactive.Observable
+import coop.rchain.catscontrib.TaskContrib._
+import cats._, cats.data._, cats.implicits._
+import monix.execution.Scheduler.Implicits.global
+import com.google.protobuf.ByteString
+
+class StreamHandlerSpec extends FunSpec with Matchers with BeforeAndAfterEach {
+
+  implicit val log = new Log.NOPLog[Task]()
+
+  var tempFolder: Path = null
+
+  override def beforeEach(): Unit =
+    tempFolder = Files.createTempDirectory("rchain")
+
+  override def afterEach(): Unit =
+    tempFolder.toFile.delete()
+
+  describe("StreamHandler.handleStream result") {
+    it("should contain file path which parent is temp folder") {
+      // given
+      val stream = createStream()
+      // when
+      val msg: StreamMessage = handleStream(stream)
+      // then
+      msg.path.getParent shouldBe tempFolder
+    }
+
+    it("should contain sender and message type information") {
+      // given
+      val stream = createStream(
+        sender = "sender",
+        typeId = BlockMessage.id
+      )
+      // when
+      val msg: StreamMessage = handleStream(stream)
+      // then
+      msg.sender shouldBe peerNode("sender")
+      msg.typeId shouldBe BlockMessage.id
+    }
+
+    it("should contain content length of the stored file") {
+      // given
+      val messageSize   = 10 * 1024 // 10 kb
+      val contentLength = messageSize * 3 + (messageSize / 2)
+      val stream = createStream(
+        messageSize = messageSize,
+        contentLength = contentLength
+      )
+      // when
+      val msg: StreamMessage = handleStream(stream)
+      // then
+      msg.contentLength shouldBe contentLength
+    }
+  }
+
+  private def handleStream(stream: Observable[Chunk]): StreamMessage =
+    (StreamHandler.handleStream(tempFolder, stream) >>= {
+      case Left(ex) =>
+        Task.delay {
+          ex.printStackTrace
+          Left(ex)
+        }
+      case Right(msg) => Right(msg).pure[Task]
+    }).unsafeRunSync.right.get
+
+  private def createStream(
+      messageSize: Int = 10 * 1024,
+      contentLength: Int = 30 * 1024,
+      sender: String = "sender",
+      typeId: String = BlockMessage.id
+  ): Observable[Chunk] = {
+
+    val content = Array.fill(contentLength)((Random.nextInt(256) - 128).toByte)
+    val packet  = Packet(BlockMessage.id, ByteString.copyFrom(content))
+    val sender  = peerNode("sender")
+    val blob    = Blob(sender, packet)
+    Observable.fromIterator(Chunker.chunkIt(blob, messageSize))
+  }
+
+  private def peerNode(name: String): PeerNode =
+    PeerNode(NodeIdentifier(name.getBytes), Endpoint("", 80, 80))
+
+}
