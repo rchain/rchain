31f76fa210d8e8b11cda2a7e81ca8ed26b7c1ceb
Artur Gajowy <artur.gajowy@gmail.com>
2018-12-31 09:07:35 +0100

    Remove unused variants of ski.kp*

--- shared/src/main/scala/coop/rchain/catscontrib/ski/package.scala
+++ shared/src/main/scala/coop/rchain/catscontrib/ski/package.scala
@@ -13,10 +13,8 @@ package object ski {
   /**
     * Returns a function that takes number of arguments and always returns the last argument as a result
     */
-  def kp[A, B](x: => B): A => B                               = _ => x
-  def kp2[A, B, C](x: => C): (A, B) => C                      = (_, _) => x
-  def kp3[A, B, C, D](x: => D): (A, B, C) => D                = (_, _, _) => x
-  def kp6[A, B, C, D, E, F, G](x: G): (A, B, C, D, E, F) => G = (_, _, _, _, _, _) => x
+  def kp[A, B](x: => B): A => B          = _ => x
+  def kp2[A, B, C](x: => C): (A, B) => C = (_, _) => x
 
   /**
     * Returns a functiont that always returns its argument
