3de5a3db1a5c685a68b298e7920ad034265dbce7
Pawel Szulc <paul.szulc@gmail.com>
2019-01-03 14:00:50 +0100

    Introduce show-justification-lines flag
    And refactoring/renaming some parts of the code, mainly /s/visualizeBlock/visualizeDag

--- node/src/main/scala/coop/rchain/node/configuration/model.scala
+++ node/src/main/scala/coop/rchain/node/configuration/model.scala
@@ -62,15 +62,15 @@ case class Deploy(
     nonce: Int,
     location: String
 ) extends Command
-case object DeployDemo                   extends Command
-case object Propose                      extends Command
-case class ShowBlock(hash: String)       extends Command
-case class ShowBlocks(depth: Int)        extends Command
-case class VisualizeBlocks(depth: Int)   extends Command
-case object Run                          extends Command
-case object Help                         extends Command
-case class DataAtName(name: Name)        extends Command
-case class ContAtName(names: List[Name]) extends Command
+case object DeployDemo                                               extends Command
+case object Propose                                                  extends Command
+case class ShowBlock(hash: String)                                   extends Command
+case class ShowBlocks(depth: Int)                                    extends Command
+case class VisualizeDag(depth: Int, showJustificationLines: Boolean) extends Command
+case object Run                                                      extends Command
+case object Help                                                     extends Command
+case class DataAtName(name: Name)                                    extends Command
+case class ContAtName(names: List[Name])                             extends Command
 case class BondingDeployGen(
     bondKey: String,
     ethAddress: String,
