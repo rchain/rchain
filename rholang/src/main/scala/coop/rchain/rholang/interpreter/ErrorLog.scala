76fbdc9f10d9af03f26b283f629eb4ec77f8108c
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2019-01-04 15:58:57 +0100

    Delay execution in ErrorLog

--- rholang/src/main/scala/coop/rchain/rholang/interpreter/ErrorLog.scala
+++ rholang/src/main/scala/coop/rchain/rholang/interpreter/ErrorLog.scala
@@ -1,35 +1,36 @@
 package coop.rchain.rholang.interpreter
 
 import cats._
+import cats.effect._
 import cats.implicits._
 import cats.mtl.FunctorTell
 
-class ErrorLog[F[_]: Applicative] extends FunctorTell[F, Throwable] {
+class ErrorLog[F[_]: Sync] extends FunctorTell[F, Throwable] {
   private var errorVector: Vector[Throwable] = Vector.empty
   val functor                                = implicitly[Functor[F]]
-  override def tell(e: Throwable): F[Unit] =
-    (this
-      .synchronized {
-        errorVector = errorVector :+ e
-      })
-      .pure[F]
+  override def tell(e: Throwable): F[Unit] = Sync[F].delay {
+    this.synchronized {
+      errorVector = errorVector :+ e
+    }
+  }
 
-  override def writer[A](a: A, e: Throwable): F[A] = {
+  override def writer[A](a: A, e: Throwable): F[A] = Sync[F].delay {
     this.synchronized {
       errorVector = errorVector :+ e
     }; a
-  }.pure[F]
+  }
 
-  override def tuple[A](ta: (Throwable, A)): F[A] = {
+  override def tuple[A](ta: (Throwable, A)): F[A] = Sync[F].delay {
     this.synchronized {
       errorVector = errorVector :+ ta._1
     }; ta._2
-  }.pure[F]
+  }
 
-  def readAndClearErrorVector(): F[Vector[Throwable]] =
+  def readAndClearErrorVector(): F[Vector[Throwable]] = Sync[F].delay {
     this.synchronized {
       val ret = errorVector
       errorVector = Vector.empty
-      ret.pure[F]
+      ret
     }
+  }
 }
