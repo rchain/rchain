7b10516209cf684e8757d699eb6f60d03f210adc
Pawel Szulc <paul.szulc@gmail.com>
2019-01-07 12:57:26 +0100

    Lint test_p2p tests

--- integration-tests/test/test_p2p.py
+++ integration-tests/test/test_p2p.py
@@ -1,22 +1,21 @@
-import os
 import pytest
 
 @pytest.mark.xfail
 def test_connecting_to_existing_node(command_line_options_fixture, docker_client_fixture):
-    fail 
+    assert False
 
 @pytest.mark.xfail
 def test_connecting_to_non_existing_node(command_line_options_fixture, docker_client_fixture):
-    fail 
+    assert False
 
 @pytest.mark.xfail
 def test_discover_other_nodes(command_line_options_fixture, docker_client_fixture):
-    fail
+    assert False
 
 @pytest.mark.xfail
 def test_number_of_protocol_peers(command_line_options_fixture, docker_client_fixture):
-    fail
+    assert False
 
 @pytest.mark.xfail
 def test_number_of_discovery_peers(command_line_options_fixture, docker_client_fixture):
-    fail
+    assert False
