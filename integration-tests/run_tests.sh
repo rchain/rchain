109b7f6b144abb85c128a4e6bb97fe48fb912da5
Adam Szkoda <adaszko@gmail.com>
2019-01-07 17:00:16 +0100

    Build integration tests docker image quietly

--- integration-tests/run_tests.sh
+++ integration-tests/run_tests.sh
@@ -2,13 +2,12 @@
 
 tag=latest
 if [[ -n $DRONE_BUILD_NUMBER ]]; then
-	# Mind our own business on Drone CI with concurrent jobs
-	tag=DRONE-$DRONE_BUILD_NUMBER
+    # Mind our own business on Drone CI with concurrent jobs
+    tag=DRONE-$DRONE_BUILD_NUMBER
 fi
 
 export DEFAULT_IMAGE=rchain-integration-tests:$tag
 
-sed "s/rnode:latest/rnode:$tag/" Dockerfile |\
-	docker build -t $DEFAULT_IMAGE -f - .
+sed "s/rnode:latest/rnode:$tag/" Dockerfile | docker build --quiet --tag "$DEFAULT_IMAGE" --file - .
 
 pipenv run py.test -v "$@"
