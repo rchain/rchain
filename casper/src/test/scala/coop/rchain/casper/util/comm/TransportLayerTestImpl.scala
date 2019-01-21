c41c0adc1e323319b6feb608bfa6a3c34f8ebfdf
Dominik Zajkowski <dzajkowski@invisibl.es>
2019-01-21 15:41:27 +0100

    Add a TestNetwork structure for TransportLayerTestImpl (#RCHAIN-2831)

--- casper/src/test/scala/coop/rchain/casper/util/comm/TransportLayerTestImpl.scala
+++ casper/src/test/scala/coop/rchain/casper/util/comm/TransportLayerTestImpl.scala
@@ -1,18 +1,64 @@
 package coop.rchain.casper.util.comm
 
-import cats.{Id, Monad}
+import cats.Monad
 import cats.effect.concurrent.Ref
 import cats.implicits._
+import cats.mtl.MonadState
 import coop.rchain.comm.protocol.routing._
 import coop.rchain.catscontrib._
-import coop.rchain.comm.CommError.{peerNodeNotFound, CommErr}
+import coop.rchain.comm.CommError.CommErr
 import coop.rchain.comm.PeerNode
-import coop.rchain.comm.rp.ProtocolHelper
 import coop.rchain.comm.rp.ProtocolHelper.protocol
 
 import scala.concurrent.duration.FiniteDuration
 import scala.collection.mutable
 import coop.rchain.comm.transport._
+import coop.rchain.shared.AtomicMonadState
+import monix.execution.atomic.AtomicAny
+
+import scala.collection.immutable.Queue
+
+object TestNetwork {
+  type NodeMessageQueues = Map[PeerNode, Queue[Protocol]]
+  type TestNetwork[F[_]] = MonadState[F, NodeMessageQueues]
+
+  def apply[F[_]](implicit ev: TestNetwork[F]): TestNetwork[F] = ev
+
+  def addPeer[F[_]: TestNetwork](peer: PeerNode): F[Unit] =
+    TestNetwork[F].modify(t => t + (peer -> Queue.empty))
+
+  def peerQueue[F[_]: TestNetwork: Monad](peer: PeerNode): F[Queue[Protocol]] =
+    TestNetwork[F].inspect(_(peer))
+
+  def send[F[_]: TestNetwork](peer: PeerNode, msg: Protocol): F[Unit] =
+    TestNetwork[F].modify(t => {
+      t + (peer -> t(peer).enqueue(msg))
+    })
+
+  def clear[F[_]: TestNetwork](peer: PeerNode): F[Unit] =
+    TestNetwork[F].modify(t => {
+      t + (peer -> Queue.empty)
+    })
+
+  def handleQueue[F[_]: TestNetwork: Monad](
+      dispatch: Protocol => F[CommunicationResponse],
+      peer: PeerNode
+  ): F[Unit] =
+    for {
+      topology     <- TestNetwork[F].get
+      q            = topology(peer)
+      maybeMessage = q.dequeueOption
+      _ <- maybeMessage.fold(().pure[F]) {
+            case (p, nq) =>
+              TestNetwork[F].set(topology + (peer -> nq)) *>
+                dispatch(p) *>
+                handleQueue(dispatch, peer)
+          }
+    } yield ()
+
+  def empty[F[_]](implicit captureF: Capture[F], monadF: Monad[F]): TestNetwork[F] =
+    new AtomicMonadState[F, NodeMessageQueues](AtomicAny(Map.empty))
+}
 
 class TransportLayerTestImpl[F[_]: Monad](
     identity: PeerNode,
