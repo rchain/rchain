f70873d968207fa0acc966a0ea4af03645cb5236
Dan Connolly <dckc@madmode.com>
2019-01-25 22:07:36 -0600

    MakeRev: make RhoSpec test out of exampleUsage (WIP)

---
+++ casper/src/test/scala/coop/rchain/casper/genesis/contracts/RevSpec.scala
@@ -0,0 +1,19 @@
+package coop.rchain.casper.genesis.contracts
+
+import coop.rchain.casper.genesis.contracts
+import coop.rchain.casper.helper.RhoSpec
+import coop.rchain.rholang.wallet.RevTest
+
+import scala.concurrent.duration._
+
+class RevSpec
+    extends RhoSpec(
+      RevTest,
+      Seq(
+        StandardDeploys.nonNegativeNumber,
+        StandardDeploys.makeMint,
+        StandardDeploys.basicWallet,
+        StandardDeploys.walletCheck
+      ),
+      10.seconds
+    )
