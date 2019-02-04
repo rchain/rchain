35dea0f4ad7e20b0b2b8fdf7129da68e0d68949b
Daniyar Itegulov <daniyar.itegulov@rchain.coop>
2019-02-04 16:57:23 +1100

    Create helper function for handling IO and get rid of EitherT wrappers

--- block-storage/src/main/scala/coop/rchain/blockstorage/util/io/RandomAccessIO.scala
+++ block-storage/src/main/scala/coop/rchain/blockstorage/util/io/RandomAccessIO.scala
@@ -3,69 +3,54 @@ package coop.rchain.blockstorage.util.io
 import java.io.{FileNotFoundException, IOException, RandomAccessFile}
 import java.nio.file.Path
 
-import cats.data.EitherT
 import cats.effect.Sync
 import cats.implicits._
-import IOError.IOErrT
+import IOError.IOErr
 
 import scala.language.higherKinds
 
 final case class RandomAccessIO[F[_]: Sync](file: RandomAccessFile) {
-  def close(): IOErrT[F, Unit] =
-    EitherT(Sync[F].delay { file.close() }.attempt).leftMap[IOError] {
-      case e: IOException =>
-        ClosingFailed(e)
-      case t =>
-        UnexpectedIOError(t)
+  private def handleIo[A](io: => A, handleIoException: IOException => IOError): F[IOErr[A]] =
+    Sync[F].delay { io }.attempt.map[IOErr[A]] {
+      case Left(e: IOException) => Left(handleIoException(e))
+      case Left(e)              => Left(UnexpectedIOError(e))
+      case Right(v)             => Right(v)
     }
 
-  def write(bytes: Array[Byte]): IOErrT[F, Unit] =
-    EitherT(Sync[F].delay { file.write(bytes) }.attempt).leftMap[IOError] {
-      case e: IOException => ByteArrayWriteFailed(e)
-      case e              => UnexpectedIOError(e)
-    }
+  def close(): F[IOErr[Unit]] =
+    handleIo(file.close(), ClosingFailed.apply)
 
-  def writeInt(v: Int): IOErrT[F, Unit] =
-    EitherT(Sync[F].delay { file.writeInt(v) }.attempt).leftMap[IOError] {
-      case e: IOException => IntWriteFailed(e)
-      case e              => UnexpectedIOError(e)
-    }
+  def write(bytes: Array[Byte]): F[IOErr[Unit]] =
+    handleIo(file.write(bytes), ByteArrayWriteFailed.apply)
 
-  def readInt: IOErrT[F, Int] =
-    EitherT(Sync[F].delay { file.readInt() }.attempt).leftMap[IOError] {
-      case e: IOException => IntReadFailed(e)
-      case e              => UnexpectedIOError(e)
-    }
+  def writeInt(v: Int): F[IOErr[Unit]] =
+    handleIo(file.writeInt(v), IntWriteFailed.apply)
 
-  def readFully(buffer: Array[Byte]): IOErrT[F, Unit] =
-    EitherT(Sync[F].delay { file.readFully(buffer) }.attempt).leftMap[IOError] {
-      case e: IOException => ByteArrayReadFailed(e)
-      case e              => UnexpectedIOError(e)
-    }
+  def readInt: F[IOErr[Int]] =
+    handleIo(file.readInt(), IntReadFailed.apply)
 
-  def length: IOErrT[F, Long] =
-    EitherT(Sync[F].delay { file.length() }.attempt).leftMap(UnexpectedIOError.apply)
+  def readFully(buffer: Array[Byte]): F[IOErr[Unit]] =
+    handleIo(file.readFully(buffer), ByteArrayReadFailed.apply)
 
-  def seek(offset: Long): IOErrT[F, Unit] =
-    EitherT(Sync[F].delay { file.seek(offset) }.attempt).leftMap[IOError] {
-      case e: IOException => FileSeekFailed(e)
-      case e              => UnexpectedIOError(e)
-    }
+  def length: F[IOErr[Long]] =
+    handleIo(file.length(), UnexpectedIOError.apply)
 
-  def setLength(length: Long): IOErrT[F, Unit] =
-    EitherT(Sync[F].delay { file.setLength(length) }.attempt).leftMap[IOError] {
-      case e: IOException => ClearFileFailed(e)
-      case t              => UnexpectedIOError(t)
-    }
+  def seek(offset: Long): F[IOErr[Unit]] =
+    handleIo(file.seek(offset), FileSeekFailed.apply)
+
+  def setLength(length: Long): F[IOErr[Unit]] =
+    handleIo(file.setLength(length), ClearFileFailed.apply)
 }
 
 object RandomAccessIO {
-  def open[F[_]: Sync](path: Path): IOErrT[F, RandomAccessIO[F]] =
-    EitherT(Sync[F].delay { new RandomAccessFile(path.toFile, "rw") }.attempt)
-      .leftMap[IOError] {
-        case e: FileNotFoundException => FileNotFound(e)
-        case e: SecurityException     => FileSecurityViolation(e)
-        case t                        => UnexpectedIOError(t)
+  def open[F[_]: Sync](path: Path): F[IOErr[RandomAccessIO[F]]] =
+    Sync[F]
+      .delay { new RandomAccessFile(path.toFile, "rw") }
+      .attempt
+      .map[IOErr[RandomAccessIO[F]]] {
+        case Left(e: FileNotFoundException) => Left(FileNotFound(e))
+        case Left(e: SecurityException)     => Left(FileSecurityViolation(e))
+        case Left(t)                        => Left(UnexpectedIOError(t))
+        case Right(file)                    => Right(RandomAccessIO[F](file))
       }
-      .map(file => RandomAccessIO[F](file))
 }
