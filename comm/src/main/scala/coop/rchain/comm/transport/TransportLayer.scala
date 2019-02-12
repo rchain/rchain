2c38ae7c75cb0cbe992abb06ed0aa733cb8d9c1b
Sebastian Bach <sebastian.bach@rchain.coop>
2019-02-12 17:51:11 +0100

    Use new GrpcTransport client and server in NodeRuntime

--- comm/src/main/scala/coop/rchain/comm/transport/TransportLayer.scala
+++ comm/src/main/scala/coop/rchain/comm/transport/TransportLayer.scala
@@ -14,13 +14,8 @@ trait TransportLayer[F[_]] {
   def roundTrip(peer: PeerNode, msg: Protocol, timeout: FiniteDuration): F[CommErr[Protocol]]
   def send(peer: PeerNode, msg: Protocol): F[CommErr[Unit]]
   def broadcast(peers: Seq[PeerNode], msg: Protocol): F[Seq[CommErr[Unit]]]
-  def receive(
-      dispatch: Protocol => F[CommunicationResponse],
-      handleStreamed: Blob => F[Unit]
-  ): F[Unit]
   def stream(peers: Seq[PeerNode], blob: Blob): F[Unit]
   def disconnect(peer: PeerNode): F[Unit]
-  def shutdown(msg: Protocol): F[Unit]
 }
 
 object TransportLayer extends TransportLayerInstances {
@@ -29,11 +24,6 @@ object TransportLayer extends TransportLayerInstances {
 }
 
 sealed abstract class TransportLayerInstances {
-
-  import CommunicationResponse._
-
-  private implicit val logSource: LogSource = LogSource(this.getClass)
-
   implicit def eitherTTransportLayer[F[_]: Monad: Log](
       implicit evF: TransportLayer[F]
   ): TransportLayer[EitherT[F, CommError, ?]] =
@@ -58,30 +48,7 @@ sealed abstract class TransportLayerInstances {
       def stream(peers: Seq[PeerNode], blob: Blob): EitherT[F, CommError, Unit] =
         EitherT.liftF(evF.stream(peers, blob))
 
-      def receive(
-          dispatch: Protocol => EitherT[F, CommError, CommunicationResponse],
-          handleStreamed: Blob => EitherT[F, CommError, Unit]
-      ): EitherT[F, CommError, Unit] = {
-        val dis: Protocol => F[CommunicationResponse] = msg =>
-          dispatch(msg).value.flatMap {
-            case Left(err) =>
-              Log[F].error(s"Error while handling message. Error: ${err.message}") *> notHandled(
-                err
-              ).pure[F]
-            case Right(m) => m.pure[F]
-          }
-        val hb: Blob => F[Unit] = (b) =>
-          handleStreamed(b).value.flatMap {
-            case Left(err) =>
-              Log[F].error(s"Error while handling streamed Packet message. Error: ${err.message}")
-            case Right(_) => ().pure[F]
-          }
-        EitherT.liftF(evF.receive(dis, hb))
-      }
-
       def disconnect(peer: PeerNode): EitherT[F, CommError, Unit] =
         EitherT.liftF(evF.disconnect(peer))
-
-      def shutdown(msg: Protocol): EitherT[F, CommError, Unit] = EitherT.liftF(evF.shutdown(msg))
     }
 }
