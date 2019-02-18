d1ae3806a88e3f28aa36f7d1c44a1892b6ace53e
Sebastian Bach <sebastian.bach@rchain.coop>
2019-02-18 12:45:39 +0100

    Implement grpc Either message and helpers

---
+++ models/src/main/scala/coop/rchain/models/either/syntax/AllSyntax.scala
@@ -0,0 +1,3 @@
+package coop.rchain.models.either.syntax
+
+trait AllSyntax extends EitherSyntax
--- shared/src/main/scala/coop/rchain/shared/ThrowableOps.scala
+++ shared/src/main/scala/coop/rchain/shared/ThrowableOps.scala
@@ -1,5 +1,7 @@
 package coop.rchain.shared
 
+import scala.annotation.tailrec
+
 object ThrowableOps {
   implicit class RichThrowable(th: Throwable) {
     def containsMessageWith(str: String): Boolean =
@@ -8,5 +10,24 @@ object ThrowableOps {
         case (Some(cause), _)                    => cause.containsMessageWith(str)
         case _                                   => false
       }
+
+    def fold[B](z: B)(f: (B, Throwable) => B): B = {
+      @tailrec
+      def loop(t0: Throwable, acc: B): B =
+        Option(t0) match {
+          case None     => acc
+          case Some(t1) => loop(t1.getCause, f(acc, t1))
+        }
+
+      loop(th, z)
+    }
+
+    def toMessageList(prefix: String = "Caused by: "): Seq[String] =
+      fold(Seq.empty[String]) { (ms, t) =>
+        Option(t.getMessage)
+          .filter(_.trim.length > 0)
+          .fold(ms)(msg => s"${if (ms.isEmpty) "" else prefix}$msg" +: ms)
+      }.reverse
+
   }
 }
