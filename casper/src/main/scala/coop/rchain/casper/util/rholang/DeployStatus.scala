7a38776ce6b963c8aed5bbbf1a8c2604d45c1053
Joseph Denman <josephrileydenman@gmail.com>
2019-02-26 15:18:27 -0500

    add InvalidDeployError trait
    Signed-off-by: Joseph Denman <josephrileydenman@gmail.com>

--- casper/src/main/scala/coop/rchain/casper/util/rholang/DeployStatus.scala
+++ casper/src/main/scala/coop/rchain/casper/util/rholang/DeployStatus.scala
@@ -21,13 +21,22 @@ final case class ReplayStatusMismatch(replay: DeployStatus, orig: DeployStatus) 
 final case object UnknownFailure                                                extends Failed
 final case class UserErrors(errors: Vector[Throwable])                          extends Failed
 final case class InternalErrors(errors: Vector[Throwable])                      extends Failed
+
+sealed trait InvalidDeployError                     extends Throwable
+final case class InsufficientRevError(msg: String)  extends InvalidDeployError
+final case class InvalidSequenceError(msg: String)  extends InvalidDeployError
+final case class InsufficientPhloError(msg: String) extends InvalidDeployError
+final case class InvalidSignatureError(msg: String) extends InvalidDeployError
+final case class MissingFieldError(msg: String)     extends InvalidDeployError
+
 //TODO add fatal error related to rspace closed after https://github.com/rchain/rchain/pull/1339 is merged
 
 object DeployStatus {
   def fromErrors(errors: Vector[Throwable]): DeployStatus = {
     val (userErrors, internalErrors) = errors.partition {
-      case _: InterpreterError => true
-      case _                   => false
+      case _: InvalidDeployError => true
+      case _: InterpreterError   => true
+      case _                     => false
     }
 
     internalErrors
