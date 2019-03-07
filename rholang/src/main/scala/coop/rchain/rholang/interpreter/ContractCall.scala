05695ae8a9fec386fc3b8c3afdb31e68d6658b74
Ovidiu Deac <ovidiu.deac@rchain.coop>
2019-03-07 15:14:23 +0200

    add documentation for ContractCall

--- rholang/src/main/scala/coop/rchain/rholang/interpreter/ContractCall.scala
+++ rholang/src/main/scala/coop/rchain/rholang/interpreter/ContractCall.scala
@@ -9,6 +9,24 @@ import coop.rchain.rholang.interpreter.storage.implicits.matchListPar
 import coop.rchain.rspace.util.unpackCont
 import cats.implicits._
 
+/**
+  * This is a tool for unapplying the messages sent to the system contracts.
+  *
+  * The unapply returns (Producer, Seq[Par]).
+  *
+  * The Producer is the function with the signature (Seq[Par], Par) => F[Unit] which can be used to send a message
+  * through a channel. The first argument with type Seq[Par] is the content of the message and the second argument is
+  * the channel.
+  *
+  * Note that the random generator and the sequence number extracted from the incoming message are required for sending
+  * messages back to the caller so they are given as the first argument list to the produce function.
+  *
+  * The Seq[Par] returned by unapply contains the message content and can be further unapplied as needed to match the
+  * required signature.
+  *
+  * @param space the rspace instance
+  * @param dispatcher the dispatcher
+  */
 class ContractCall[F[_]: Sync](
     space: RhoISpace[F],
     dispatcher: Dispatch[F, ListParWithRandomAndPhlos, TaggedContinuation]
