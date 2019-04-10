57b522b03248d49ecb422b324a213e4e08d7dfc9
Kent Shikama <kent@kentshikama.com>
2019-04-10 15:31:21 -0700

    Use Genesis constructor in buildGenesis for slowcooker

--- casper/src/slowcooker/scala/coop.rchain.casper/HashSetCasperSpecification.scala
+++ casper/src/slowcooker/scala/coop.rchain.casper/HashSetCasperSpecification.scala
@@ -4,7 +4,8 @@ import cats.data.EitherT
 import cats.effect.Sync
 import cats.implicits._
 import coop.rchain.casper.MultiParentCasper.ignoreDoppelgangerCheck
-import coop.rchain.casper.genesis.contracts.{Faucet, PreWallet}
+import coop.rchain.casper.genesis.Genesis
+import coop.rchain.casper.genesis.contracts._
 import coop.rchain.casper.helper.HashSetCasperTestNode
 import coop.rchain.casper.helper.HashSetCasperTestNode.Effect
 import coop.rchain.casper.protocol.{BlockMessage, DeployData}
@@ -19,6 +20,7 @@ import coop.rchain.catscontrib._
 import coop.rchain.catscontrib.TaskContrib._
 import coop.rchain.comm.CommError
 import coop.rchain.crypto.{PrivateKey, PublicKey}
+import coop.rchain.rholang.interpreter.util.RevAddress
 import monix.eval.Task
 import org.scalacheck._
 import org.scalacheck.commands.Commands
@@ -56,9 +58,26 @@ object HashSetCasperActions {
     val wallets     = ethAddresses.map(addr => PreWallet(addr, BigInt(10001)))
     val bonds       = bondsGen(validators)
     val minimumBond = 100L
-    val genesis = buildGenesis(
-      buildGenesisParameters(4, validators, createBonds(validators))
-    )
+    val genesis =
+      buildGenesis(
+        Genesis(
+          shardId = "HashSetCasperSpecification",
+          wallets = wallets,
+          proofOfStake = ProofOfStake(
+            minimumBond = minimumBond,
+            maximumBond = Long.MaxValue,
+            validators = bonds.toSeq.map(Validator.tupled)
+          ),
+          faucet = true,
+          genesisPk = Ed25519.newKeyPair._2,
+          timestamp = 0L,
+          vaults = bonds.toList.map {
+            case (pk, stake) =>
+              RevAddress.fromPublicKey(pk).map(Vault(_, stake))
+          }.flattenOption,
+          supply = Long.MaxValue
+        )
+      )
     (genesis, validatorKeys)
   }
 
