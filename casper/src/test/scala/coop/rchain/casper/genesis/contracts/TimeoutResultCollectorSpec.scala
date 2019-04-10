1dda931b4bb77a77975ca3b40662b795b07aeffd
Kent Shikama <kent@kentshikama.com>
2019-04-10 15:22:02 -0700

    Increase timeout in TimeoutResultCollectorSpec

--- casper/src/test/scala/coop/rchain/casper/genesis/contracts/TimeoutResultCollectorSpec.scala
+++ casper/src/test/scala/coop/rchain/casper/genesis/contracts/TimeoutResultCollectorSpec.scala
@@ -2,6 +2,7 @@ package coop.rchain.casper.genesis.contracts
 import coop.rchain.casper.helper.RhoSpec
 import coop.rchain.rholang.build.CompiledRholangSource
 import org.scalatest.{AppendedClues, FlatSpec, Matchers}
+import coop.rchain.catscontrib.TaskContrib._
 
 import scala.concurrent.duration._
 import monix.execution.Scheduler.Implicits.global
@@ -10,7 +11,7 @@ class TimeoutResultCollectorSpec extends FlatSpec with AppendedClues with Matche
   it should "testFinished should be false if execution hasn't finished within timeout" in {
     RhoSpec
       .getResults(CompiledRholangSource("TimeoutResultCollectorTest.rho"), Seq.empty)
-      .runSyncUnsafe(15.seconds)
+      .unsafeRunSync
       .hasFinished should be(false)
   }
 }
