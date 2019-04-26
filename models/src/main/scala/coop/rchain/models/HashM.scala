34419040179690e6fb11e07d9cd731547389404c
Pawel Szulc <paul.szulc@gmail.com>
2019-04-26 16:50:07 +0200

    Remove obsolete MaybeBlockMessage

--- models/src/main/scala/coop/rchain/models/HashM.scala
+++ models/src/main/scala/coop/rchain/models/HashM.scala
@@ -143,7 +143,6 @@ object HashM extends HashMDerivation {
   implicit val DeployDataHash             = gen[DeployData]
   implicit val FindDeployInBlockQueryHash = gen[FindDeployInBlockQuery]
   implicit val HeaderHash                 = gen[Header]
-  implicit val MaybeBlockMessageHash      = gen[MaybeBlockMessage]
   implicit val ProcessedDeployHash        = gen[ProcessedDeploy]
   implicit val RChainStateHash            = gen[RChainState]
   implicit val UnapprovedBlockHash        = gen[UnapprovedBlock]
