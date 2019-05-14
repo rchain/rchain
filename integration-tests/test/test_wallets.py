54e7eda66323b1fe33bbe9d9259b535b7400adf1
will <zsluedem06@gmail.com>
2019-05-14 16:11:55 +0800

    add transfer failing test

--- integration-tests/test/test_wallets.py
+++ integration-tests/test/test_wallets.py
@@ -25,6 +25,7 @@ from .wait import (
 
 ALICE_KEY = conftest.KeyPair(private_key='b2527b00340a83e302beae2a8daf6d654e8e57541acfa261cc1b5635eb16aa15', public_key='12a764e25517b99c35db937115f480a5067f6a809dd0c1f382ad2a9212a012c3')
 BOB_KEY = conftest.KeyPair(private_key='9a801debae8bb97fe54c99389cafa576c60612503348578125b65ab182ff5850', public_key='155244935e80a72b4f3e83fb2fcfbb8d07c5bfdbc91e3b6c96371583139acad8')
+CHARLIE_KEY = conftest.KeyPair(private_key='567ea426deaeb8233f134c3a266149fb196d6eea7d28b447dfefff92002cb400', public_key='17815c1cc861ce91f8959e7948730f6bd3d2054716a600123f5cf6da515e14ca')
 
 def create_genesis_vault(context: TestingContext, node: Node, rev_addr: str, init_vault_value: int, private_key: str) -> None:
     log_marker = random_string(context, 10)
@@ -43,7 +44,7 @@ def transfer_funds(context: TestingContext, node: Node, from_rev_addr: str, to_r
     If the transfer fail to be processed, it would return the reason why it failed like "Invalid AuthKey".
     """
     log_marker = random_string(context, 10)
-    transfer_funds_result_pattern = re.compile('"{} (Successfully|Failing) reason: (?P<reason>[a-zA-Z0-9]*)"'.format(log_marker))
+    transfer_funds_result_pattern = re.compile('"{} (Successfully|Failing) reason: (?P<reason>[a-zA-Z0-9 ]*)"'.format(log_marker))
     node.deploy_contract_with_substitution(
         substitute_dict={"%FROM": from_rev_addr, "%TO": to_rev_addr, "%AMOUNT": str(amount), "%LOG_MARKER": log_marker},
         rho_file_path="resources/wallets/transfer_funds.rho",
@@ -106,3 +107,23 @@ def test_alice_pay_bob(command_line_options: CommandLineOptions, docker_client: 
 
             bob_balance = get_vault_balance(context, bootstrap_node, bob_rev_address, BOB_KEY.private_key)
             assert bob_balance == 0
+
+
+def test_transfer_failed_with_invalid_key(command_line_options: CommandLineOptions, docker_client: DockerClient, random_generator: Random) -> None:
+    with testing_context(command_line_options, random_generator, docker_client) as context:
+        with docker_network_with_started_bootstrap(context=context) as bootstrap_node:
+            charlie_init_vault_value = 5000
+
+            alice_rev_address = get_rev_address_by_public_key(context, bootstrap_node, ALICE_KEY.public_key, ALICE_KEY.private_key)
+            charlie_rev_address = get_rev_address_by_public_key(context, bootstrap_node, BOB_KEY.public_key, CHARLIE_KEY.private_key)
+
+            create_genesis_vault(context, bootstrap_node, charlie_rev_address, charlie_init_vault_value, ALICE_KEY.private_key)
+            alice_balance = get_vault_balance(context, bootstrap_node, alice_rev_address, ALICE_KEY.private_key)
+            charlie_balance = get_vault_balance(context, bootstrap_node, charlie_rev_address, BOB_KEY.private_key)
+
+            assert charlie_balance == 5000
+            assert alice_balance == 0
+
+            result = transfer_funds(context, bootstrap_node, charlie_rev_address, alice_rev_address, 100, ALICE_KEY.private_key)
+
+            assert result == 'Invalid AuthKey'
