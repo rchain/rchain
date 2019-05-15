5a1a31cccfd8ffc9d88e5ba820e9f3c2171043d6
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2019-05-15 16:06:57 +0200

    Increase storage size for genesis

--- casper/src/test/scala/coop/rchain/casper/MultiParentCasperTestUtil.scala
+++ casper/src/test/scala/coop/rchain/casper/MultiParentCasperTestUtil.scala
@@ -95,7 +95,7 @@ object MultiParentCasperTestUtil {
 
   def buildGenesis(genesisParameters: Genesis): BlockMessage = {
     val storageDirectory                   = Files.createTempDirectory(s"hash-set-casper-test-genesis")
-    val storageSize: Long                  = 3024L * 1024
+    val storageSize: Long                  = 3024L * 1024 * 10
     implicit val log: Log.NOPLog[Task]     = new Log.NOPLog[Task]
     implicit val metricsEff: Metrics[Task] = new metrics.Metrics.MetricsNOP[Task]
     (for {
