38b295eada5823c17962a76d5ddefa09bd26a126
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2019-05-30 10:35:30 +0200

    Re-enable RholangBuildTest

--- casper/src/test/scala/coop/rchain/casper/RholangBuildTest.scala
+++ casper/src/test/scala/coop/rchain/casper/RholangBuildTest.scala
@@ -6,6 +6,7 @@ import coop.rchain.casper.helper.HashSetCasperTestNode
 import coop.rchain.casper.helper.HashSetCasperTestNode._
 import coop.rchain.casper.util.ConstructDeploy
 import coop.rchain.crypto.signatures.Secp256k1
+import coop.rchain.casper.scalatestcontrib._
 import coop.rchain.rholang.interpreter.accounting
 import monix.execution.Scheduler.Implicits.global
 import org.scalatest.{FlatSpec, Matchers}
@@ -18,12 +19,14 @@ class RholangBuildTest extends FlatSpec with Matchers {
 
   //put a new casper instance at the start of each
   //test since we cannot reset it
-  "Our build system" should "allow import of rholang sources into scala code" in {
-    HashSetCasperTestNode.standaloneEff(genesis, validatorKeys.last).use { node =>
-      import node._
+  "Our build system" should "allow import of rholang sources into scala code" in effectTest {
+    HashSetCasperTestNode
+      .standaloneEff(genesis, validatorKeys.last)
+      .use { node =>
+        import node._
 
-      val code =
-        """new double, dprimes, rl(`rho:registry:lookup`), ListOpsCh, time(`rho:block:timestamp`), timeRtn, timeStore, stdout(`rho:io:stdout`) in {
+        val code =
+          """new double, dprimes, rl(`rho:registry:lookup`), ListOpsCh, time(`rho:block:timestamp`), timeRtn, timeStore, stdout(`rho:io:stdout`) in {
           |  contract double(@x, ret) = { ret!(2 * x) } |
           |  rl!(`rho:lang:listOps`, *ListOpsCh) |
           |  for(@(_, ListOps) <- ListOpsCh) {
@@ -34,18 +37,18 @@ class RholangBuildTest extends FlatSpec with Matchers {
           |    timeStore!("The timestamp is ${timestamp}" %% {"timestamp" : timestamp})
           |  }
           |}""".stripMargin
-      val deploy = ConstructDeploy.sourceDeploy(code, 1L, accounting.MAX_VALUE)
-      for {
-        createBlockResult <- MultiParentCasper[Effect]
-                              .deploy(deploy) *> MultiParentCasper[Effect].createBlock
-        Created(signedBlock) = createBlockResult
-        _                    <- MultiParentCasper[Effect].addBlock(signedBlock, ignoreDoppelgangerCheck[Effect])
-        storage              <- MultiParentCasperTestUtil.blockTuplespaceContents(signedBlock)
-        _                    = logEff.warns should be(Nil)
-        _                    = storage.contains("!([4, 6, 10, 14])") should be(true)
-        result               = storage.contains("!(\"The timestamp is 1\")") should be(true)
-      } yield result
-    }
+        val deploy = ConstructDeploy.sourceDeploy(code, 1L, accounting.MAX_VALUE)
+        for {
+          createBlockResult <- MultiParentCasper[Effect]
+                                .deploy(deploy) *> MultiParentCasper[Effect].createBlock
+          Created(signedBlock) = createBlockResult
+          _                    <- MultiParentCasper[Effect].addBlock(signedBlock, ignoreDoppelgangerCheck[Effect])
+          storage              <- MultiParentCasperTestUtil.blockTuplespaceContents(signedBlock)
+          _                    = logEff.warns should be(Nil)
+          _                    = storage.contains("!([4, 6, 10, 14])") should be(true)
+          result               = storage.contains("!(\"The timestamp is 1\")") should be(true)
+        } yield result
+      }
   }
 
 }
