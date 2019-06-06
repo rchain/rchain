6bf0cf37492bd4c22a00c7c2f0147c59c167c87e
Artur Gajowy <artur.gajowy@gmail.com>
2019-06-06 09:06:53 +0200

    WIP FIX TEST EstimatorHelperTest.scala

--- casper/src/test/scala/coop/rchain/casper/EstimatorHelperTest.scala
+++ casper/src/test/scala/coop/rchain/casper/EstimatorHelperTest.scala
@@ -1,7 +1,9 @@
 package coop.rchain.casper
 
 import cats.implicits._
+import coop.rchain.blockstorage.BlockStore
 import coop.rchain.casper.EstimatorHelper.conflicts
+import coop.rchain.casper.genesis.contracts.TestUtil
 import coop.rchain.casper.helper.BlockGenerator.{
   computeBlockCheckpoint,
   injectPostStateHash,
@@ -50,11 +52,27 @@ class EstimatorHelperTest
         for {
 
           deploys <- (0 until 6).toList.traverse(i => basicProcessedDeploy[Task](i))
-          genesis <- createGenesis[Task]()
-          b2      <- createBlock[Task](Seq(genesis.blockHash), genesis, deploys = Seq(deploys(0)))
-          b3      <- createBlock[Task](Seq(genesis.blockHash), genesis, deploys = Seq(deploys(1)))
-          b4      <- createBlock[Task](Seq(b2.blockHash), genesis, deploys = Seq(deploys(2)))
-          b5      <- createBlock[Task](Seq(b3.blockHash), genesis, deploys = Seq(deploys(2)))
+          genesis <- TestUtil.defaultGenesisSetup[Task](runtimeManager)
+
+          _ <- BlockStore[Task].put(genesis.blockHash, genesis)
+
+          b1 <- createBlock[Task](
+                 Seq(genesis.blockHash),
+                 genesis,
+                 deploys = Seq.empty
+               )
+          b2 <- createBlock[Task](
+                 Seq(b1.blockHash),
+                 genesis,
+                 deploys = Seq(deploys(0))
+               )
+          b3 <- createBlock[Task](
+                 Seq(b1.blockHash),
+                 genesis,
+                 deploys = Seq(deploys(1))
+               )
+          b4 <- createBlock[Task](Seq(b2.blockHash), genesis, deploys = Seq(deploys(2)))
+          b5 <- createBlock[Task](Seq(b3.blockHash), genesis, deploys = Seq(deploys(2)))
           b6 <- createBlock[Task](
                  Seq(b2.blockHash, b3.blockHash),
                  genesis,
@@ -66,18 +84,17 @@ class EstimatorHelperTest
           b10 <- createBlock[Task](Seq(b8.blockHash), genesis, deploys = Seq(deploys(4)))
 
           dag <- blockDagStorage.getRepresentation
-
           computeBlockCheckpointResult <- computeBlockCheckpoint(
-                                           genesis,
-                                           genesis,
+                                           b1,
+                                           b1,
                                            dag,
                                            runtimeManager
                                          )
           (postGenStateHash, postGenProcessedDeploys) = computeBlockCheckpointResult
           _ <- injectPostStateHash[Task](
                 0,
-                genesis,
-                genesis,
+                b1,
+                b1,
                 postGenStateHash,
                 postGenProcessedDeploys
               )
