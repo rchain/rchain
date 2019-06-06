0a111e83d557568b789f24dbaecdc5f5049a9ca7
Artur Gajowy <artur.gajowy@gmail.com>
2019-06-06 11:26:52 +0200

    Remove unused method

--- casper/src/test/scala/coop/rchain/casper/util/rholang/Resources.scala
+++ casper/src/test/scala/coop/rchain/casper/util/rholang/Resources.scala
@@ -11,19 +11,6 @@ import monix.execution.Scheduler
 
 object Resources {
 
-  def mkRuntimeAndManager(
-      prefix: String,
-      storageSize: Int = 1024 * 1024,
-      storeType: StoreType = StoreType.LMDB
-  )(implicit scheduler: Scheduler): Resource[Task, (Runtime[Task], RuntimeManager[Task])] = {
-    implicit val log: Log[Task]            = Log.log[Task]
-    implicit val metricsEff: Metrics[Task] = new metrics.Metrics.MetricsNOP[Task]
-    for {
-      runtime        <- mkRuntime[Task, Task.Par](prefix)
-      runtimeManager <- Resource.liftF(RuntimeManager.fromRuntime[Task](runtime))
-    } yield ((runtime, runtimeManager))
-  }
-
   def mkRuntimeManager(
       prefix: String,
       storageSize: Long = 1024 * 1024L,
