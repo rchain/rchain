08cab181d2833dd35b0521e8c665d3b151d641c2
Artur Gajowy <artur.gajowy@gmail.com>
2019-06-07 13:26:11 +0200

    Remove nested for-comprehensions in {EstimatorHelper,Validate}Test

--- casper/src/test/scala/coop/rchain/casper/EstimatorHelperTest.scala
+++ casper/src/test/scala/coop/rchain/casper/EstimatorHelperTest.scala
@@ -46,55 +46,59 @@ class EstimatorHelperTest
    */
   "Blocks" should "conflict if they use the same deploys in different histories" in withStorage {
     implicit blockStore => implicit blockDagStorage =>
-      for {
-        deploys <- (0 until 6).toList.traverse(i => basicProcessedDeploy[Task](i))
-        genesis <- createGenesis[Task]()
-        b2      <- createBlock[Task](Seq(genesis.blockHash), genesis, deploys = Seq(deploys(0)))
-        b3      <- createBlock[Task](Seq(genesis.blockHash), genesis, deploys = Seq(deploys(1)))
-        b4      <- createBlock[Task](Seq(b2.blockHash), genesis, deploys = Seq(deploys(2)))
-        b5      <- createBlock[Task](Seq(b3.blockHash), genesis, deploys = Seq(deploys(2)))
-        b6      <- createBlock[Task](Seq(b2.blockHash, b3.blockHash), genesis, deploys = Seq(deploys(2)))
-        b7      <- createBlock[Task](Seq(b6.blockHash), genesis, deploys = Seq(deploys(3)))
-        b8      <- createBlock[Task](Seq(b6.blockHash), genesis, deploys = Seq(deploys(5)))
-        b9      <- createBlock[Task](Seq(b7.blockHash), genesis, deploys = Seq(deploys(5)))
-        b10     <- createBlock[Task](Seq(b8.blockHash), genesis, deploys = Seq(deploys(4)))
+      mkRuntimeManager("casper-util-test").use { runtimeManager =>
+        for {
 
-        dag <- blockDagStorage.getRepresentation
-        result <- mkRuntimeManager("casper-util-test").use { runtimeManager =>
-                   for {
-                     computeBlockCheckpointResult <- computeBlockCheckpoint(
-                                                      genesis,
-                                                      genesis,
-                                                      dag,
-                                                      runtimeManager
-                                                    )
-                     (postGenStateHash, postGenProcessedDeploys) = computeBlockCheckpointResult
-                     _ <- injectPostStateHash[Task](
-                           0,
-                           genesis,
-                           genesis,
-                           postGenStateHash,
-                           postGenProcessedDeploys
-                         )
-                     _ <- updateChainWithBlockStateUpdate[Task](1, genesis, runtimeManager)
-                     _ <- updateChainWithBlockStateUpdate[Task](2, genesis, runtimeManager)
-                     _ <- updateChainWithBlockStateUpdate[Task](3, genesis, runtimeManager)
-                     _ <- updateChainWithBlockStateUpdate[Task](4, genesis, runtimeManager)
-                     _ <- updateChainWithBlockStateUpdate[Task](5, genesis, runtimeManager)
-                     _ <- updateChainWithBlockStateUpdate[Task](6, genesis, runtimeManager)
-                     _ <- updateChainWithBlockStateUpdate[Task](7, genesis, runtimeManager)
-                     _ <- updateChainWithBlockStateUpdate[Task](8, genesis, runtimeManager)
-                     _ <- updateChainWithBlockStateUpdate[Task](9, genesis, runtimeManager)
+          deploys <- (0 until 6).toList.traverse(i => basicProcessedDeploy[Task](i))
+          genesis <- createGenesis[Task]()
+          b2      <- createBlock[Task](Seq(genesis.blockHash), genesis, deploys = Seq(deploys(0)))
+          b3      <- createBlock[Task](Seq(genesis.blockHash), genesis, deploys = Seq(deploys(1)))
+          b4      <- createBlock[Task](Seq(b2.blockHash), genesis, deploys = Seq(deploys(2)))
+          b5      <- createBlock[Task](Seq(b3.blockHash), genesis, deploys = Seq(deploys(2)))
+          b6 <- createBlock[Task](
+                 Seq(b2.blockHash, b3.blockHash),
+                 genesis,
+                 deploys = Seq(deploys(2))
+               )
+          b7  <- createBlock[Task](Seq(b6.blockHash), genesis, deploys = Seq(deploys(3)))
+          b8  <- createBlock[Task](Seq(b6.blockHash), genesis, deploys = Seq(deploys(5)))
+          b9  <- createBlock[Task](Seq(b7.blockHash), genesis, deploys = Seq(deploys(5)))
+          b10 <- createBlock[Task](Seq(b8.blockHash), genesis, deploys = Seq(deploys(4)))
 
-                     _      <- conflicts[Task](b2, b3, dag) shouldBeF false
-                     _      <- conflicts[Task](b4, b5, dag) shouldBeF true
-                     _      <- conflicts[Task](b6, b6, dag) shouldBeF false
-                     _      <- conflicts[Task](b6, b9, dag) shouldBeF false
-                     _      <- conflicts[Task](b7, b8, dag) shouldBeF false
-                     _      <- conflicts[Task](b7, b10, dag) shouldBeF false
-                     result <- conflicts[Task](b9, b10, dag) shouldBeF true
-                   } yield result
-                 }
-      } yield result
+          dag <- blockDagStorage.getRepresentation
+
+          computeBlockCheckpointResult <- computeBlockCheckpoint(
+                                           genesis,
+                                           genesis,
+                                           dag,
+                                           runtimeManager
+                                         )
+          (postGenStateHash, postGenProcessedDeploys) = computeBlockCheckpointResult
+          _ <- injectPostStateHash[Task](
+                0,
+                genesis,
+                genesis,
+                postGenStateHash,
+                postGenProcessedDeploys
+              )
+          _ <- updateChainWithBlockStateUpdate[Task](1, genesis, runtimeManager)
+          _ <- updateChainWithBlockStateUpdate[Task](2, genesis, runtimeManager)
+          _ <- updateChainWithBlockStateUpdate[Task](3, genesis, runtimeManager)
+          _ <- updateChainWithBlockStateUpdate[Task](4, genesis, runtimeManager)
+          _ <- updateChainWithBlockStateUpdate[Task](5, genesis, runtimeManager)
+          _ <- updateChainWithBlockStateUpdate[Task](6, genesis, runtimeManager)
+          _ <- updateChainWithBlockStateUpdate[Task](7, genesis, runtimeManager)
+          _ <- updateChainWithBlockStateUpdate[Task](8, genesis, runtimeManager)
+          _ <- updateChainWithBlockStateUpdate[Task](9, genesis, runtimeManager)
+
+          _      <- conflicts[Task](b2, b3, dag) shouldBeF false
+          _      <- conflicts[Task](b4, b5, dag) shouldBeF true
+          _      <- conflicts[Task](b6, b6, dag) shouldBeF false
+          _      <- conflicts[Task](b6, b9, dag) shouldBeF false
+          _      <- conflicts[Task](b7, b8, dag) shouldBeF false
+          _      <- conflicts[Task](b7, b10, dag) shouldBeF false
+          result <- conflicts[Task](b9, b10, dag) shouldBeF true
+        } yield result
+      }
   }
 }
