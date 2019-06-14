7c81a545ab413d8fa592ef7df213b9c6fc89dbb1
kayvan kazeminejad <kayvan@q2io.com>
2019-06-14 12:14:42 -0700

    Implement the new side-effect-free AttemptOpsF

--- rspace/src/main/scala/coop/rchain/rspace/nextgenrspace/history/RootsStore.scala
+++ rspace/src/main/scala/coop/rchain/rspace/nextgenrspace/history/RootsStore.scala
@@ -6,7 +6,7 @@ import java.nio.charset.StandardCharsets
 import cats.implicits._
 import cats.effect.Sync
 import coop.rchain.rspace.Blake2b256Hash
-import coop.rchain.shared.AttemptOps._
+import coop.rchain.shared.AttemptOpsF.RichAttempt
 import coop.rchain.shared.ByteVectorOps._
 import scodec.bits.{BitVector, ByteVector}
 
@@ -25,25 +25,39 @@ object RootsStoreInstances {
       ByteVector("current-root".getBytes(StandardCharsets.UTF_8)).toDirectByteBuffer
 
     override def currentRoot(): F[Option[Blake2b256Hash]] =
-      store
-        .get(currentRootName)
-        .map(_.map(bytes => Blake2b256Hash.codecBlake2b256Hash.decode(BitVector(bytes)).get.value))
-
-    override def validateRoot(key: Blake2b256Hash): F[Option[Blake2b256Hash]] = {
-      val bytes = Blake2b256Hash.codecBlake2b256Hash.encode(key).get.toByteVector.toDirectByteBuffer
       for {
-        bytesMaybe <- store.get(bytes)
-        result     <- bytesMaybe.traverse(_ => store.put(currentRootName, bytes).map(_ => key))
+        byteBuf ← store.get(currentRootName)
+        maybeDecoded ← byteBuf
+                        .map(
+                          bytes ⇒
+                            Blake2b256Hash.codecBlake2b256Hash
+                              .decode(BitVector(bytes))
+                              .get
+                        )
+                        .sequence
+        maybeHash = maybeDecoded.map(_.value)
+      } yield (maybeHash)
+
+    override def validateRoot(key: Blake2b256Hash): F[Option[Blake2b256Hash]] =
+      for {
+        bits    ← Blake2b256Hash.codecBlake2b256Hash.encode(key).get
+        bytes   = bits.toByteVector.toDirectByteBuffer
+        byteBuf ← store.get(bytes)
+        result ← byteBuf.traverse(
+                  _ ⇒
+                    store
+                      .put(currentRootName, bytes)
+                      .map(_ ⇒ key)
+                )
       } yield result
-    }
 
-    override def recordRoot(key: Blake2b256Hash): F[Unit] = {
-      val bytes = Blake2b256Hash.codecBlake2b256Hash.encode(key).get.toByteVector.toDirectByteBuffer
+    override def recordRoot(key: Blake2b256Hash): F[Unit] =
       for {
-        _ <- store.put(bytes, tag)
-        _ <- store.put(currentRootName, bytes)
+        bits  ← Blake2b256Hash.codecBlake2b256Hash.encode(key).get
+        bytes = bits.toByteVector.toDirectByteBuffer
+        _     ← store.put(bytes, tag)
+        _     ← store.put(currentRootName, bytes)
       } yield ()
-    }
 
     override def close(): F[Unit] = store.close()
   }
