1b9adb14df9cd2d00adaf3227cdf3193405c8a5f
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2019-07-04 11:51:56 +0200

    Remove obsolete maybeRoot from SoftCheckpoint

--- rspace/src/main/scala/coop/rchain/rspace/Checkpoint.scala
+++ rspace/src/main/scala/coop/rchain/rspace/Checkpoint.scala
@@ -2,7 +2,6 @@ package coop.rchain.rspace
 
 final case class SoftCheckpoint[C, P, A, K](
     cacheSnapshot: Snapshot[C, P, A, K],
-    log: trace.Log,
-    maybeRoot: Option[Blake2b256Hash] = None
-) //TODO: remove maybeRoot along with old RSpace
+    log: trace.Log
+)
 final case class Checkpoint(root: Blake2b256Hash, log: trace.Log)
