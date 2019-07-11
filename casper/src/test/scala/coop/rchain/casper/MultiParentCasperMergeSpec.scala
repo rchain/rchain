9d1d83a6c5d8fba6d83b450fdba211bd7d777f9e
Kent Shikama <kent@kentshikama.com>
2019-07-10 18:29:59 -0700

    Add test cases starting with !X

--- casper/src/test/scala/coop/rchain/casper/MultiParentCasperMergeSpec.scala
+++ casper/src/test/scala/coop/rchain/casper/MultiParentCasperMergeSpec.scala
@@ -66,17 +66,39 @@ class MultiParentCasperMergeSpec extends FlatSpec with Matchers with Inspectors 
   it should "handle multi-parent blocks correctly when they operate on volatile produce/consume pairs" in effectTest {
     // 4! 4!
     checkConflicts("@0!(0)", "for (_ <- @0) { 0 }", "for (_ <- @0) { 0 }") >>
+      // 4X 4X (TODO: Eventually should merge)
+      checkConflicts("0", "for (_ <- @0) { 0 }", "for (_ <- @0) { 0 }") >>
+      //
+      // !X !X (TODO: Eventually should merge)
+      checkConflicts("0", "@0!(0)", "@0!(0)") >>
+      // !X !4
+      checkMerge("for (_ <- @0) { 0 }", "@1!(0)", "@0!(0)") >>
+      // !X !C
+      checkMerge("contract @0(id) = { 0 }", "@1!(0)", "@0!(0)") >>
+      // !X 4X
+      checkConflicts("0", "@0!(0)", "for (_ <- @0) { 0 }") >>
+      // !X 4!
+      checkMerge("@0!(0)", "@1!(0)", "for (_ <- @0) { 0 }") >>
+      // !X 4!!
+      checkMerge("@0!!(0)", "@1!(0)", "for (_ <- @0) { 0 }") >>
+      // !X !!X (TODO: Eventually should merge)
+      checkConflicts("0", "@0!(0)", "@0!!(0)") >>
+      // !X !!4
+      checkMerge("for (_ <- @0) { 0 }", "@1!(0)", "@0!!(0)") >>
+      // !X CX
+      checkConflicts("0", "@0!(0)", "contract @0(id) = { 0 }") >>
+      // !X C!
+      checkMerge("@0!(0)", "@1!(0)", "contract @0(id) = { 0 }") >>
       // (!4) (!4)
       checkMerge("0", "for (_ <- @0) { 0 } | @0!(1)", "for (_ <- @0) { 0 } | @0!(2)") >>
       // !4 !4
       checkConflicts("for (_ <- @0) { 0 }", "@0!(1)", "@0!(2)") >>
       // !4 !4
       checkMerge("for (@1 <- @0) { 0 }", "@0!(1)", "for (@2 <- @0) { 0 } | @0!(2)") >>
-      // Cases that are conflicting now but should not once we have more merge support
-      // !X !X
-      checkConflicts("0", "@0!(0)", "@0!(0)") >>
-      // 4X 4X
-      checkConflicts("0", "for (_ <- @0) { 0 }", "for (_ <- @0) { 0 }")
+      // !4 !C
+      checkMerge("for (@1 <- @0) { 0 } | contract @1(id) = { 0 }", "@0!(1)", "@1!(1)") >>
+      // !4 4X
+      checkMerge("for (_ <- @0) { 0 }", "@0!(1)", "for (_ <- @1) { 0 }")
   }
 
   private def checkConflicts(base: String, b1: String, b2: String): Effect[Unit] =
