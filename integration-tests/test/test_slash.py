19c250be6c8416bb340e9d49844bf93be018373f
will <zsluedem06@gmail.com>
2019-07-19 10:20:04 +0800

    change protocol blockrequest and make exception explicit

--- integration-tests/test/test_slash.py
+++ integration-tests/test/test_slash.py
@@ -1,7 +1,7 @@
 import base64
 import hashlib
 import re
-# import sys
+import sys
 from random import Random
 
 import pytest
@@ -35,15 +35,14 @@ def hex_block_hash(block_hash: bytes) -> bytes:
     return base64.b16encode(block_hash).lower()
 
 
-# @pytest.mark.skipif(sys.platform in ('win32', 'cygwin', 'darwin'), reason="Only Linux docker support connection between host and container which node client needs")
-@pytest.mark.xfail
+@pytest.mark.skipif(sys.platform in ('win32', 'cygwin', 'darwin'), reason="Only Linux docker support connection between host and container which node client needs")
 def test_simple_slash(command_line_options: CommandLineOptions, random_generator: Random, docker_client: DockerClient) -> None:
     peers_keypairs = [BONDED_VALIDATOR_KEY_1, BONDED_VALIDATOR_KEY_2]
     with conftest.testing_context(command_line_options, random_generator, docker_client, bootstrap_key=BOOTSTRAP_NODE_KEYS, peers_keys=peers_keypairs) as context, \
         docker_network_with_started_bootstrap(context=context) as bootstrap_node, \
         bootstrap_connected_peer(context=context, bootstrap=bootstrap_node, name='bonded-validator-1', private_key=BONDED_VALIDATOR_KEY_1) as validator1, \
         bootstrap_connected_peer(context=context, bootstrap=bootstrap_node, name='bonded-validator-2', private_key=BONDED_VALIDATOR_KEY_2) as validator2, \
-        node_protocol_client(bootstrap_node.network, docker_client) as client:
+        node_protocol_client(bootstrap_node.network, docker_client, context) as client:
             contract = '/opt/docker/examples/tut-hello.rho'
 
             validator1.deploy(contract, BONDED_VALIDATOR_KEY_1)
