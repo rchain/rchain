7ac1e8dd26dbc4483adb77f39be7275d2a5d9a84
Kent Shikama <kent@kentshikama.com>
2019-07-25 00:39:02 -0700

    Memoize scoring of terms in SortedParMap

--- models/src/main/scala/coop/rchain/models/SortedParMap.scala
+++ models/src/main/scala/coop/rchain/models/SortedParMap.scala
@@ -1,13 +1,12 @@
 package coop.rchain.models
 
-import coop.rchain.models.rholang.sorter.Sortable
-
+import coop.rchain.models.rholang.sorter
 import monix.eval.Coeval
 
-import scala.collection.GenTraversableOnce
+import scala.collection.{mutable, GenTraversableOnce}
 import scala.collection.immutable.TreeMap
-
 import coop.rchain.models.rholang.sorter.ScoredTerm.{ordering => ScoredTermOrdering}
+import coop.rchain.models.rholang.sorter.Sortable
 
 final case class SortedParMap(ps: TreeMap[Par, Par]) extends Iterable[(Par, Par)] {
 
@@ -45,19 +44,27 @@ final case class SortedParMap(ps: TreeMap[Par, Par]) extends Iterable[(Par, Par)
 }
 
 object SortedParMap {
-  implicit def ordering: Ordering[Par] = (x: Par, y: Par) => {
-    (for {
-      termX <- Sortable[Par].sortMatch[Coeval](x)
-      termY <- Sortable[Par].sortMatch[Coeval](y)
-    } yield ScoredTermOrdering.compare(termX, termY)).value()
+
+  // From https://stackoverflow.com/a/36960228/2750819
+  def memoize[I, O](f: I => O): I => O = new mutable.HashMap[I, O]() {
+    override def apply(key: I): O = getOrElseUpdate(key, f(key))
+  }
+
+  lazy val scoredTerm: Par => sorter.ScoredTerm[Par] = memoize(
+    par => Sortable[Par].sortMatch[Coeval](par).value()
+  )
+
+  val parOrdering: Ordering[Par] = new Ordering[Par] {
+    override def compare(x: Par, y: Par): Int =
+      ScoredTermOrdering[Par].compare(scoredTerm(x), scoredTerm(y))
   }
 
   def apply(treeMap: TreeMap[Par, Par]): SortedParMap = new SortedParMap(treeMap)
 
   def apply(map: Map[Par, Par]): SortedParMap =
-    SortedParMap(TreeMap.empty[Par, Par](ordering) ++ map)
+    SortedParMap(TreeMap.empty[Par, Par](parOrdering) ++ map)
 
   def apply(seq: Seq[(Par, Par)]): SortedParMap = SortedParMap(seq.toMap)
 
-  def empty: SortedParMap = SortedParMap(TreeMap.empty[Par, Par](ordering))
+  def empty: SortedParMap = SortedParMap(TreeMap.empty[Par, Par](parOrdering))
 }
