4505dcde5833af7d9e5aee83f84a0a24d7e1516f
JosDenmark <josephrileydenman@gmail.com>
2019-08-04 22:22:34 -0400

    Add random initial balance check to genesis vault construction test.

--- casper/src/test/scala/coop/rchain/casper/RholangBuildTest.scala
+++ casper/src/test/scala/coop/rchain/casper/RholangBuildTest.scala
@@ -16,6 +16,7 @@ import coop.rchain.crypto.signatures.Secp256k1
 import coop.rchain.rholang.interpreter.util.RevAddress
 import monix.execution.Scheduler.Implicits.global
 import org.scalatest.{FlatSpec, Matchers}
+import scala.util.Random
 
 class RholangBuildTest extends FlatSpec with Matchers {
 
@@ -67,14 +68,43 @@ class RholangBuildTest extends FlatSpec with Matchers {
     val vaults = (1 to REV_ADDRESS_COUNT)
       .map(i => (Secp256k1.newKeyPair, i))
       .map { case ((_, publicKey), i) => Vault(RevAddress.fromPublicKey(publicKey).get, i.toLong) }
-      .toSeq
+
+    val vault               = vaults(Random.nextInt(REV_ADDRESS_COUNT))
     val (keyPairs, genesis) = buildGenesisParameters()
     val genesisParams       = (keyPairs, genesis.copy(vaults = vaults))
-    HashSetCasperTestNode
-      .standaloneEff(buildGenesis(genesisParams))
-      .use { node =>
-        import node._
-        (logEff.warns should be(Nil)).pure[Effect]
-      }
+
+    HashSetCasperTestNode.standaloneEff(buildGenesis(genesisParams)).use { node =>
+      import node._
+
+      val balanceCheckSource: String =
+        s"""  new testRet, RevVaultCh, vaultCh, rl(`rho:registry:lookup`) in {
+            #   rl!(`rho:rchain:revVault`, *RevVaultCh) |
+            #   for (@(_, RevVault) <- RevVaultCh) {
+            #     @RevVault!("findOrCreate", "${vault.revAddress.toBase58}", *vaultCh) |
+            #     for(@(true, vault) <- vaultCh){
+            #        @vault!("balance", *testRet)
+            #     }
+            #   }
+            # }
+            # """.stripMargin('#')
+
+      for {
+        balanceCheckDeploy <- ConstructDeploy.sourceDeployNowF(balanceCheckSource)
+        createBlockResult <- MultiParentCasper[Effect]
+                              .deploy(balanceCheckDeploy) >> MultiParentCasper[Effect].createBlock
+        Created(signedBlock) = createBlockResult
+        _                    <- MultiParentCasper[Effect].addBlock(signedBlock, ignoreDoppelgangerCheck[Effect])
+        _                    = logEff.warns should be(Nil)
+        _ <- getDataAtPrivateChannel[Effect](
+              signedBlock,
+              Base16.encode(
+                RegistrySigGen.generateUnforgeableNameId(
+                  PublicKey(balanceCheckDeploy.deployer.toByteArray),
+                  balanceCheckDeploy.timestamp
+                )
+              )
+            ).map(_ shouldBe Seq(s"${vault.initialBalance}"))
+      } yield ()
+    }
   }
 }
