2ef2a154c4a5584ab113fe7a23dac5897a550f47
JosDenmark <josephrileydenman@gmail.com>
2019-08-04 23:14:54 -0400

    Remove unused `CompiledRholangSource` class members from `Validator`.

--- casper/src/main/scala/coop/rchain/casper/genesis/contracts/Validator.scala
+++ casper/src/main/scala/coop/rchain/casper/genesis/contracts/Validator.scala
@@ -6,23 +6,4 @@ import coop.rchain.rholang.build.CompiledRholangSource
 import coop.rchain.rholang.interpreter.{NormalizerEnv, ParBuilder}
 import monix.eval.Coeval
 
-// TODO: Eliminate public key argument if unnecessary
-final case class Validator(pk: PublicKey, stake: Long) extends CompiledRholangSource {
-
-  val path: String = "<synthetic in Validator.scala>"
-
-  val code: String =
-    s"""
-       | new rl(`rho:registry:lookup`), PoSCh in {
-       |   rl!(`rho:rchain:pos`, *PoSCh)
-       |   | for (@(_, PoS) <- PoSCh) {
-       |     @PoS!("bond", $stake, Nil)
-       |   }
-       | }
-     """.stripMargin
-
-  val normalizerEnv: NormalizerEnv = NormalizerEnv.empty
-
-  lazy val term: Par = ParBuilder[Coeval].buildNormalizedTerm(code, normalizerEnv).value()
-
-}
+final case class Validator(pk: PublicKey, stake: Long)
