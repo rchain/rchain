6a647e1d8072f5c718b52d2da3f0021d0de95ac5
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2019-08-06 10:47:01 +0200

    Rephrase and fix the "conflicts in different histories test"
    In the EstimatorHelperTest

--- casper/src/test/scala/coop/rchain/casper/EstimatorHelperTest.scala
+++ casper/src/test/scala/coop/rchain/casper/EstimatorHelperTest.scala
@@ -10,7 +10,7 @@ import coop.rchain.casper.helper.{BlockDagStorageFixture, BlockGenerator, HashSe
 import coop.rchain.casper.protocol.Event.EventInstance.{Consume, Produce}
 import coop.rchain.casper.protocol.{ConsumeEvent, Event, ProcessedDeploy, ProduceEvent}
 import coop.rchain.casper.scalatestcontrib._
-import coop.rchain.casper.util.ConstructDeploy.{basicDeployData, basicProcessedDeploy}
+import coop.rchain.casper.util.ConstructDeploy._
 import coop.rchain.casper.util.GenesisBuilder
 import coop.rchain.casper.util.rholang.{Resources, RuntimeManager}
 import coop.rchain.metrics
@@ -52,44 +52,50 @@ class EstimatorHelperTest
    *         genesis
    */
 
-  "Blocks" should "conflict if they use the same deploys in different histories" in effectTest {
+  "Conflicts" should "be also detected when they occur in uncommon ancestor blocks" in effectTest {
 
     HashSetCasperTestNode.networkEff(genesisContext, networkSize = 4).use {
       case n1 +: n2 +: n3 +: n4 +: _ =>
         implicit val blockStore = n4.blockStore
 
         for {
-          deploys <- (0 until 6).toList.traverse(i => basicDeployData[Task](i))
+          produceDeploys <- (0 until 6).toList.traverse(i => basicSendDeployData[Task](i))
+          consumeDeploys <- (0 until 6).toList
+                             .traverse(i => basicReceiveDeployData[Task](i))
 
-          b2 <- n1.addBlock(deploys(0))
-          b3 <- n2.addBlock(deploys(1))
+          b2 <- n1.addBlock(produceDeploys(0))
+          b3 <- n2.addBlock(produceDeploys(1))
           _  <- n3.receive()
 
-          b4 <- n1.addBlock(deploys(2))
-          b5 <- n2.addBlock(deploys(2))
-          b6 <- n3.addBlock(deploys(2))
+          b4 <- n1.addBlock(produceDeploys(2))
+          b5 <- n2.addBlock(consumeDeploys(2))
+          b6 <- n3.addBlock(produceDeploys(2))
           _  <- n4.receive()
 
-          b7  <- n3.addBlock(deploys(3))
-          b8  <- n4.addBlock(deploys(5))
-          b9  <- n3.addBlock(deploys(5))
-          b10 <- n4.addBlock(deploys(4))
+          b7  <- n3.addBlock(produceDeploys(3))
+          b8  <- n4.addBlock(produceDeploys(5))
+          b9  <- n3.addBlock(consumeDeploys(5))
+          b10 <- n4.addBlock(produceDeploys(4))
 
           _   <- n4.receive()
           dag <- n4.blockDagStorage.getRepresentation
 
           _ <- conflicts[Task](b2, b3, dag) shouldBeF false
+          // b4 conflicts with b5 directly
           _ <- conflicts[Task](b4, b5, dag) shouldBeF true
           _ <- conflicts[Task](b6, b6, dag) shouldBeF false
           _ <- conflicts[Task](b6, b9, dag) shouldBeF false
           _ <- conflicts[Task](b7, b8, dag) shouldBeF false
           _ <- conflicts[Task](b7, b10, dag) shouldBeF false
+          // b8 conflicts with b9 directly
+          _ <- conflicts[Task](b8, b9, dag) shouldBeF true
+          // b9 conflicts indirectly with b10 due to the fact that b8 is the latter's ancestor
           _ <- conflicts[Task](b9, b10, dag) shouldBeF true
         } yield ()
     }
   }
 
-  it should "conflict if their deploys conflict in the deployLog" in withStorage {
+  "Blocks" should "conflict if their deploys conflict in the deployLog" in withStorage {
     implicit blockStore => implicit blockDagStorage =>
       testConflict[Task] { deploy =>
         deploy.copy(
