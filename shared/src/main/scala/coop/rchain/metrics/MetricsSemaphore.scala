ab77e20150a862a2097b6f5c347f0f77d27e54ec
Sebastian Bach <sebastian.bach@rchain.coop>
2019-08-20 10:06:37 +0200

    Add MetricsSemaphore

---
+++ shared/src/main/scala/coop/rchain/metrics/MetricsSemaphore.scala
@@ -0,0 +1,43 @@
+package coop.rchain.metrics
+
+import cats.effect._
+import cats.effect.concurrent.Semaphore
+import cats.implicits._
+
+import coop.rchain.catscontrib.ski.kp
+
+class MetricsSemaphore[F[_]: Sync: Metrics](
+    underlying: Semaphore[F]
+)(implicit ms: Metrics.Source)
+    extends Semaphore[F] {
+  import implicits._
+
+  def available: F[Long] = underlying.available
+  def count: F[Long]     = underlying.count
+  def acquireN(n: Long): F[Unit] =
+    for {
+      _ <- Metrics[F].incrementGauge("lock.queue")
+      _ <- Sync[F].defer(underlying.acquireN(n)).timer("lock.acquire")
+      _ <- Metrics[F].decrementGauge("lock.queue")
+    } yield ()
+
+  def tryAcquireN(n: Long): F[Boolean] = underlying.tryAcquireN(n)
+  def releaseN(n: Long): F[Unit]       = underlying.releaseN(n)
+  def withPermit[A](t: F[A]): F[A]     = Sync[F].bracket(acquire)(kp(t))(kp(release))
+}
+
+object MetricsSemaphore {
+
+  def apply[F[_]: Sync: Metrics](
+      underlying: Semaphore[F]
+  )(implicit ms: Metrics.Source): MetricsSemaphore[F] =
+    new MetricsSemaphore(underlying)
+
+  def apply[F[_]: Concurrent: Metrics](
+      n: Long
+  )(implicit ms: Metrics.Source): F[MetricsSemaphore[F]] =
+    Semaphore[F](n).map(apply(_))
+
+  def single[F[_]: Concurrent: Metrics](implicit ms: Metrics.Source): F[MetricsSemaphore[F]] =
+    apply(1L)
+}
