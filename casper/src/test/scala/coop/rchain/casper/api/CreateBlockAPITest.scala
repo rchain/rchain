86c4f1e1ee95378d3932dc6ce2f2093cb4bd7ddb
Daniyar Itegulov <daniyar.itegulov@rchain.coop>
2019-09-16 11:41:09 +1000

    Add tests for block creation synchrony constraint

--- casper/src/test/scala/coop/rchain/casper/api/CreateBlockAPITest.scala
+++ casper/src/test/scala/coop/rchain/casper/api/CreateBlockAPITest.scala
@@ -4,10 +4,11 @@ import cats.Monad
 import cats.effect.concurrent.Semaphore
 import cats.implicits._
 
-import coop.rchain.blockstorage.dag.BlockDagRepresentation
 import coop.rchain.casper.MultiParentCasper.ignoreDoppelgangerCheck
 import coop.rchain.casper.engine._
 import EngineCell._
+import cats.effect.{Concurrent, Sync}
+import coop.rchain.blockstorage.BlockStore
 import coop.rchain.blockstorage.dag.BlockDagRepresentation
 import coop.rchain.blockstorage.dag.BlockDagStorage.DeployId
 import coop.rchain.casper._
@@ -15,13 +16,14 @@ import coop.rchain.casper.api.BlockAPI.ApiErr
 import coop.rchain.casper.helper.HashSetCasperTestNode
 import coop.rchain.casper.protocol._
 import coop.rchain.casper.util._
+import coop.rchain.casper.util.ConstructDeploy.basicDeployData
 import coop.rchain.casper.util.rholang._
 import coop.rchain.catscontrib.TaskContrib._
-import coop.rchain.metrics.{Metrics, NoopSpan}
+import coop.rchain.metrics.{Metrics, NoopSpan, Span}
 import coop.rchain.models.BlockHash.BlockHash
 import coop.rchain.models.Validator.Validator
 import coop.rchain.p2p.EffectsTestInstances._
-import coop.rchain.shared.{Cell, Time}
+import coop.rchain.shared.{Cell, Log, Time}
 import coop.rchain.shared.scalatestcontrib._
 
 import monix.eval.Task
@@ -35,15 +37,31 @@ class CreateBlockAPITest extends FlatSpec with Matchers with EitherValues {
   import GenesisBuilder._
   import HashSetCasperTestNode.Effect
 
-  val genesis                              = buildGenesis()
-  implicit val spanEff: NoopSpan[Effect]   = NoopSpan[Effect]
-  implicit val metricsEff: Metrics[Effect] = new metrics.Metrics.MetricsNOP[Effect]
+  def createBlock(blockApiLock: Semaphore[Task])(engineCell: Cell[Task, Engine[Task]])(
+      implicit log: Log[Task],
+      metrics: Metrics[Task],
+      span: Span[Task]
+  ): Task[Either[String, DeployServiceResponse]] =
+    BlockAPI.createBlock[Task](blockApiLock)(
+      Sync[Task],
+      Concurrent[Task],
+      engineCell,
+      log,
+      metrics,
+      span
+    )
+
+  val genesisParameters = buildGenesisParameters(
+    bondsFunction = _.zip(List(10L, 10L, 10L, 10L)).toMap
+  )
+  val genesis = buildGenesis(genesisParameters)
 
   implicit val scheduler = Scheduler.fixedPool("three-threads", 3)
 
   "createBlock" should "not allow simultaneous calls" in effectTest {
-    implicit val logEff  = new LogStub[Effect]
-    implicit val spanEff = NoopSpan[Effect]
+    implicit val metricsEff: Metrics[Effect] = new metrics.Metrics.MetricsNOP[Effect]
+    implicit val logEff                      = new LogStub[Effect]
+    implicit val spanEff                     = NoopSpan[Effect]
     implicit val time = new Time[Task] {
       private val timer                               = Task.timer
       def currentMillis: Task[Long]                   = timer.clock.realTime(MILLISECONDS)
@@ -99,6 +117,55 @@ class CreateBlockAPITest extends FlatSpec with Matchers with EitherValues {
 
       ().pure[Effect]
     }
+  }
+
+  it should "not allow proposals without enough new blocks from other validators" in effectTest {
+    HashSetCasperTestNode
+      .networkEff(genesis, networkSize = 4, synchronyConstraintThreshold = 1d / 3d)
+      .use {
+        case nodes @ n1 +: n2 +: _ +: _ +: Seq() =>
+          import n1.{logEff, metricEff, span, timeEff}
+          val engine = new EngineWithCasper[Task](n1.casperEff)
+          for {
+            deploys      <- (0 until 3).toList.traverse(i => basicDeployData[Task](i))
+            engineCell   <- Cell.mvarCell[Task, Engine[Task]](engine)
+            blockApiLock <- Semaphore[Effect](1)
+
+            b1 <- n1.publishBlock(deploys(0))(nodes: _*)
+            b2 <- n2.publishBlock(deploys(1))(nodes: _*)
+
+            _        <- n1.casperEff.addDeploy(deploys(2))
+            b3Status <- createBlock(blockApiLock)(engineCell)
+
+            _ = b3Status.left.value should include(
+              "Must wait for more blocks from other validators"
+            )
+          } yield ()
+      }
+  }
+
+  it should "allow proposals with enough new blocks from other validators" in effectTest {
+    HashSetCasperTestNode
+      .networkEff(genesis, networkSize = 4, synchronyConstraintThreshold = 1d / 3d)
+      .use {
+        case nodes @ n1 +: n2 +: n3 +: _ +: Seq() =>
+          import n1.{logEff, metricEff, span, timeEff}
+          val engine = new EngineWithCasper[Task](n1.casperEff)
+          for {
+            deploys      <- (0 until 4).toList.traverse(i => basicDeployData[Task](i))
+            engineCell   <- Cell.mvarCell[Task, Engine[Task]](engine)
+            blockApiLock <- Semaphore[Effect](1)
+
+            b1 <- n1.publishBlock(deploys(0))(nodes: _*)
+            b2 <- n2.publishBlock(deploys(1))(nodes: _*)
+            b3 <- n3.publishBlock(deploys(2))(nodes: _*)
+
+            _        <- n1.casperEff.addDeploy(deploys(3))
+            b4Status <- createBlock(blockApiLock)(engineCell)
+
+            _ = b4Status shouldBe a[Right[_, DeployServiceResponse]]
+          } yield ()
+      }
   }
 }
 
