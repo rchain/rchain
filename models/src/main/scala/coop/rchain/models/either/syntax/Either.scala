649d90e5ee411721e7efd585eb5a15e1b1136315
Sebastian Bach <sebastian.bach@rchain.coop>
2019-09-16 16:16:33 +0200

    Use Deploy API V2 in the GrpcDeployService

--- models/src/main/scala/coop/rchain/models/either/syntax/Either.scala
+++ models/src/main/scala/coop/rchain/models/either/syntax/Either.scala
@@ -1,8 +1,11 @@
 package coop.rchain.models.either.syntax
 
+import cats.effect.Sync
+import cats.implicits._
+
+import coop.rchain.casper.protocol.deployV2.ServiceError
 import coop.rchain.models.StacksafeMessage
 import coop.rchain.models.either.EitherHelper
-import cats.syntax.either._
 
 import scalapb.GeneratedMessageCompanion
 
@@ -10,10 +13,15 @@ trait EitherSyntax {
   implicit final def modelEitherSyntaxGrpcEither(msg: coop.rchain.either.Either): GrpcEitherOps =
     new GrpcEitherOps(msg)
 
-  implicit final def modelEitherSyntaxScalaEitheString[A <: StacksafeMessage[A]](
+  implicit final def modelEitherSyntaxScalaEitherString[A <: StacksafeMessage[A]](
       response: Either[String, A]
   ): ScalaEitherStringOps[A] =
     new ScalaEitherStringOps(response)
+
+  implicit final def modelEitherSyntaxGrpModel[F[_]: Sync, R <: StacksafeMessage[R], A](
+      response: F[R]
+  ): ScalaEitherServiceErrorOps[F, R, A] =
+    new ScalaEitherServiceErrorOps(response)
 
   implicit final def modelEitherSyntaxScalaEitherThrowable[A <: StacksafeMessage[A]](
       response: Either[Throwable, A]
@@ -29,6 +37,19 @@ final class GrpcEitherOps(msg: coop.rchain.either.Either) {
 
 final class ScalaEitherStringOps[A <: StacksafeMessage[A]](response: Either[String, A]) {
   def toGrpcEither: coop.rchain.either.Either = EitherHelper.fromEitherString[A](response)
+}
+
+final class ScalaEitherServiceErrorOps[F[_]: Sync, R <: StacksafeMessage[R], A](response: F[R]) {
+  def toEitherF(
+      error: R => Option[ServiceError],
+      result: R => Option[A]
+  ): F[Either[Seq[String], A]] =
+    response.flatMap { r =>
+      error(r)
+        .map(_.messages.asLeft[A].pure[F])
+        .orElse(result(r).map(_.asRight[Seq[String]].pure[F]))
+        .getOrElse(Sync[F].raiseError(new RuntimeException("Response is empty")))
+    }
 }
 
 final class ScalaEitherThrowableOps[A <: StacksafeMessage[A]](response: Either[Throwable, A]) {
