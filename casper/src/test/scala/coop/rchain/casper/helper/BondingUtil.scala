3840de40f0d88b5a35ba73d411c647335e3d5139
Daniyar Itegulov <daniyar.itegulov@rchain.coop>
2019-09-19 22:21:32 +1000

    Add tests for bonded status API

---
+++ casper/src/test/scala/coop/rchain/casper/helper/BondingUtil.scala
@@ -0,0 +1,25 @@
+package coop.rchain.casper.helper
+
+import cats.Functor
+import cats.syntax.functor._
+import coop.rchain.casper.SignDeployment
+import coop.rchain.casper.protocol.DeployData
+import coop.rchain.casper.util.ConstructDeploy
+import coop.rchain.crypto.PrivateKey
+import coop.rchain.shared.Time
+
+object BondingUtil {
+  def bondingDeploy[F[_]: Functor: Time](amount: Long, privateKey: PrivateKey): F[DeployData] =
+    ConstructDeploy
+      .sourceDeployNowF(
+        s"""
+         |new retCh, PoSCh, rl(`rho:registry:lookup`), stdout(`rho:io:stdout`), deployerId(`rho:rchain:deployerId`) in {
+         |  rl!(`rho:rchain:pos`, *PoSCh) |
+         |  for(@(_, PoS) <- PoSCh) {
+         |    @PoS!("bond", *deployerId, 1000, *retCh)
+         |  }
+         |}
+         |""".stripMargin
+      )
+      .map(deploy => SignDeployment.sign(privateKey, deploy))
+}
