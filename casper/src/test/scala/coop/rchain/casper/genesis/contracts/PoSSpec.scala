7bb025ba87829a71f3087710d19008c236fee9ad
Joseph Denman <josephrileydenman@gmail.com>
2019-09-24 17:24:41 -0400

    Remove last vestiges of `PoSTest`.

--- casper/src/test/scala/coop/rchain/casper/genesis/contracts/PoSSpec.scala
+++ casper/src/test/scala/coop/rchain/casper/genesis/contracts/PoSSpec.scala
@@ -1,12 +1,9 @@
 package coop.rchain.casper.genesis.contracts
 
-import cats.implicits._
 import coop.rchain.casper.helper.RhoSpec
-import coop.rchain.casper.util.GenesisBuilder
-import coop.rchain.crypto.PublicKey
-import coop.rchain.crypto.codec.Base16
-import coop.rchain.rholang.interpreter.util.RevAddress
 import coop.rchain.casper.util.ConstructDeploy.{defaultPub, defaultSec, defaultSec2}
+import coop.rchain.casper.util.GenesisBuilder
+import coop.rchain.crypto.codec.Base16
 import coop.rchain.crypto.signatures.Secp256k1
 
 import scala.concurrent.duration._
@@ -151,81 +148,9 @@ class TestDontPayInactiveValidators
       120.seconds
     )
 
-class PoSSpec
-    extends RhoSpec(
-      Seq((Source.fromResource("PoSTest.rho").mkString, defaultSec)),
-      120.seconds,
-      genesisParameters = GenesisBuilder
-        .buildGenesisParameters()
-        .map(genesis => genesis.copy(vaults = genesis.vaults ++ PoSSpec.testVaults))
-    )
-
 object PoSSpec {
-
   def templateSource(source: String, substitutions: Map[String, String]): String =
     substitutions.foldLeft(source) {
       case (acc, (name, value)) => acc.replace(s"""$$$$$name$$$$""", value.toString)
     }
-
-  def prepareVault(vaultData: (String, Long)): Vault =
-    Vault(RevAddress.fromPublicKey(PublicKey(Base16.decode(vaultData._1).get)).get, vaultData._2)
-
-  val testVaults: Seq[Vault] = Seq(
-    (
-      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-      10000L
-    ),
-    (
-      "1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111",
-      10000L
-    ),
-    (
-      "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
-      10000L
-    ),
-    (
-      "cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc",
-      10000L
-    ),
-    (
-      "2222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222",
-      10000L
-    ),
-    (
-      "3333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333",
-      10000L
-    ),
-    (
-      "4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444",
-      10000L
-    ),
-    (
-      "5555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555",
-      10000L
-    ),
-    (
-      "6666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666",
-      10000L
-    ),
-    (
-      "7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777",
-      10000L
-    ),
-    (
-      "8888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888",
-      10000L
-    ),
-    (
-      "9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999",
-      10000L
-    ),
-    (
-      "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
-      10000L
-    ),
-    (
-      "047b43d6548b72813b89ac1b9f9ca67624a8b372feedd71d4e2da036384a3e1236812227e524e6f237cde5f80dbb921cac12e6500791e9a9ed1254a745a816fe1f",
-      10000L
-    )
-  ).map(prepareVault)
 }
