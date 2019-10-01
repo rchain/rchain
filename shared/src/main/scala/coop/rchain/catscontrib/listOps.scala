533f790176d8773968e285a078e316f148a67a43
Artur Gajowy <artur.gajowy@gmail.com>
2019-10-01 22:21:46 +0200

    Replace `ListContrib.findM` with cats usage

--- shared/src/main/scala/coop/rchain/catscontrib/listOps.scala
+++ shared/src/main/scala/coop/rchain/catscontrib/listOps.scala
@@ -8,15 +8,4 @@ object ListContrib {
       implicit ord: Ordering[(K, A)]
   ): List[A] =
     list.sortBy(e => (map.getOrElse(e, Monoid[K].empty), e))(ord)
-
-  // From https://hygt.github.io/2018/08/05/Cats-findM-collectFirstM.html
-  def findM[G[_], A](list: List[A], p: A => G[Boolean])(implicit G: Monad[G]): G[Option[A]] =
-    list.tailRecM[G, Option[A]] {
-      case head :: tail =>
-        p(head).map {
-          case true  => Some(head).asRight[List[A]]
-          case false => tail.asLeft[Option[A]]
-        }
-      case Nil => G.pure(None.asRight[List[A]])
-    }
 }
