f42a3d24f608bd89acfda2551d4ab1e941cb39b0
Joseph Denman <josephrileydenman@gmail.com>
2019-10-14 17:06:15 -0400

    Revert replay state in case of ReplayException.

--- casper/src/main/scala/coop/rchain/casper/util/rholang/RuntimeManager.scala
+++ casper/src/main/scala/coop/rchain/casper/util/rholang/RuntimeManager.scala
@@ -323,14 +323,12 @@ class RuntimeManagerImpl[F[_]: Concurrent: Metrics: Span: Log](
                      .attempt
                      .flatMap {
                        case Right(_) => none[ReplayFailure].pure[F]
-                       case Left(ex: ReplayException) => {
-                         Log[F].error(s"Failed during processing of deploy: ${processedDeploy}") >>
-                           (none[DeployData], UnusedCommEvent(ex): Failed).some
-                             .pure[F]
-                       }
+                       case Left(ex: ReplayException) =>
+                         runtime.replaySpace.revertToSoftCheckpoint(softCheckpoint) >> Log[F].error(
+                           s"Failed during processing of deploy: $processedDeploy"
+                         ) >> (none[DeployData], UnusedCommEvent(ex): Failed).some.pure[F]
                        case Left(ex) =>
-                         (none[DeployData], UserErrors(Vector(ex)): Failed).some
-                           .pure[F]
+                         (none[DeployData], UserErrors(Vector(ex)): Failed).some.pure[F]
                      }
                  }
              }
