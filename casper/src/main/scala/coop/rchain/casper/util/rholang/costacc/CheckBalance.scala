99314de4caef5801a78b6ca5d21155b3c36a06e7
Joseph Denman <josephrileydenman@gmail.com>
2019-10-26 14:59:57 -0400

    Introduce `CheckBalance` system deploy.

---
+++ casper/src/main/scala/coop/rchain/casper/util/rholang/costacc/CheckBalance.scala
@@ -0,0 +1,60 @@
+package coop.rchain.casper.util.rholang.costacc
+
+import cats.syntax.all._
+import com.google.protobuf.ByteString
+import coop.rchain.casper.util.rholang.{SystemDeploy, SystemDeployFailure}
+import coop.rchain.crypto.PublicKey
+import coop.rchain.crypto.hash.Blake2b512Random
+import coop.rchain.models.NormalizerEnv.{Contains, ToEnvMap}
+import coop.rchain.rholang.interpreter.RhoType.{Extractor, RhoNumber}
+
+class CheckBalance(pk: PublicKey, rand: Blake2b512Random) extends SystemDeploy(rand) {
+
+  import coop.rchain.models._
+  import rholang.{implicits => toPar}
+  import shapeless._
+  import syntax.singleton._
+
+  val `sys:casper:deployerId` = Witness("sys:casper:deployerId")
+  type `sys:casper:deployerId` = `sys:casper:deployerId`.T
+
+  type Output = RhoNumber
+  type Result = Long
+  type Env =
+    (`sys:casper:deployerId` ->> GDeployerId) ::
+      (`sys:casper:return` ->> GUnforgeable) :: HNil
+
+  import toPar._
+  protected def toEnvMap                   = ToEnvMap[Env]
+  implicit protected val envsReturnChannel = Contains[Env, `sys:casper:return`]
+  protected val normalizerEnv =
+    new NormalizerEnv(
+      ("sys:casper:deployerId" ->> GDeployerId(ByteString.copyFrom(pk.bytes))) ::
+        mkReturnChannel ::
+        HNil
+    )
+  protected val extractor = Extractor.derive
+
+  val source: String =
+    """
+      # new deployerId(`sys:casper:deployerId`),
+      #     return(`sys:casper:return`),
+      #     rl(`rho:registry:lookup`),
+      #     revAddressOps(`rho:rev:address`),
+      #     revAddressCh,
+      #     revVaultCh in {
+      #   rl!(`rho:rchain:revVault`, *revVaultCh) |
+      #   revAddressOps!("fromDeployerId", *deployerId, *revAddressCh) |
+      #   for(@userRevAddress <- revAddressCh; @(_, revVault) <- revVaultCh){
+      #     new userVaultCh in {
+      #       @revVault!("findOrCreate", userRevAddress, *userVaultCh) |
+      #       for(@(true, userVault) <- userVaultCh){
+      #         @userVault!("balance", *return)
+      #       }
+      #     }
+      #   }
+      # }
+      #""".stripMargin('#')
+
+  protected def processResult(value: Long): Either[SystemDeployFailure, Result] = value.asRight
+}
