504c8ce00a172e25f7fd0790f52efb4d884b387d
Tomáš Virtus <tomas.virtus@rchain.coop>
2019-12-16 12:21:51 +0100

    itests: Make test launch script more portable
    - Do not install Pyenv and custom Python in Drone launcher. This change
      depends on latest buildenv[1] that has Pyenv already installed.
      Also put Python version selection into .python-version file which is
      picked up by Pyenv by default. This file is intentionally missing
      because tests run fine with Python 3.6 in Ubuntu bionic. In case you
      require specific Python version, just put it into .python-version.
    
    - Do not launch Python from hardcoded path. Use "pyenv exec" instead.
      This makes it possible to use Pyenv in custom location (using
      PYENV_ROOT and PATH variables), as it is done in buildenv[1].
    
    [1] https://github.com/rchain/buildenv

--- scripts/ci/run-integration-tests.sh
+++ scripts/ci/run-integration-tests.sh
@@ -13,21 +13,12 @@ main () {
     apt-get update
     env DEBIAN_FRONTEND=noninteractive apt-get -yq install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
 
-    curl https://pyenv.run | bash
-    ~/.pyenv/bin/pyenv install 3.7.3
-    env PYENV_VERSION=3.7.3 ~/.pyenv/shims/python -m pip install pipenv
-
-    cd integration-tests
-    env PYENV_VERSION=3.7.3 ~/.pyenv/shims/python -m pipenv sync
-
     trap delete_resources_dir EXIT
     TEMP_RESOURCES_DIR=$(mktemp --directory --tmpdir=/tmp integration-tests.${DRONE_BUILD_NUMBER:-0}.XXXXXXXXXX)
     cp -r resources/* "$TEMP_RESOURCES_DIR/"
 
-    ./mypy.sh
-    ./pylint.sh
-	export TMPDIR=$TEMP_RESOURCES_DIR
-    ./run_tests.sh --log-cli-level=ERROR
+	export TEMP_RESOURCES_DIR
+    ./run_tests --log-cli-level=ERROR
 }
 
 
