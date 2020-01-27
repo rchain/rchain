b957e854e06c0cd9dd0942c61382a4ffcdafbe03
Isaac-DeFrain <isaacdefrain@gmail.com>
2020-01-27 18:51:08 -0500

    Resolve MakeMint overflow bug
    Resolve cross-currency deposits

--- integration-tests/test/test_wallets.py
+++ integration-tests/test/test_wallets.py
@@ -126,7 +126,7 @@ def test_transfer_failed_with_insufficient_funds(command_line_options: CommandLi
         assert alice_balance < 2000000
 
         with pytest.raises(TransderFundsError) as e:
-            transfer_funds(context, bootstrap, alice_rev_address, bob_rev_address, 2000000, ALICE_KEY, 100000, 1)
+            transfer_funds(context, bootstrap, alice_rev_address, bob_rev_address, 2000000, ALICE_KEY, 200000, 1)
         assert e.value.reason == "Insufficient funds"
         bob_balance = get_vault_balance(context, bootstrap, bob_rev_address, CHARLIE_KEY, 100000, 1)
         assert bob_balance == 0
