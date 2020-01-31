763adb53689ca449332e36ed1eb7c5e612eed874
Tomislav Grospic <grospic@gmail.com>
2020-01-31 14:52:09 +0100

    Consume all phlos on any interpreter error

--- casper/src/test/scala/coop/rchain/casper/util/rholang/RuntimeManagerTest.scala
+++ casper/src/test/scala/coop/rchain/casper/util/rholang/RuntimeManagerTest.scala
@@ -648,7 +648,7 @@ class RuntimeManagerTest extends FlatSpec with Matchers {
   "replayComputeState" should "not catch discrepancies in initial and replay cost when user errors are thrown" in effectTest {
     invalidReplay("@0!(0) | for(@x <- @0){ x.undefined() }").map {
       case Left(ReplayCostMismatch(initialCost, replayCost)) =>
-        assert(initialCost == 395L && replayCost == 396L)
+        assert(initialCost == 9999L && replayCost == 10000L)
       case _ => fail()
     }
   }
