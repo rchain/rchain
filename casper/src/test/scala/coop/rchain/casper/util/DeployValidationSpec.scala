8279e0e669ac494ed3c88fadf8051bf8c1afb02c
Tomislav Grospic <grospic@gmail.com>
2020-02-12 17:22:43 +0100

    Temp fix deploy signature test

--- casper/src/test/scala/coop/rchain/casper/util/DeployValidationSpec.scala
+++ casper/src/test/scala/coop/rchain/casper/util/DeployValidationSpec.scala
@@ -2,6 +2,8 @@ package coop.rchain.casper.util
 
 import com.google.protobuf.ByteString
 import coop.rchain.casper.protocol.{DeployData, DeployDataProto}
+import coop.rchain.crypto.PrivateKey
+import coop.rchain.crypto.codec.Base16
 import coop.rchain.crypto.signatures.{Ed25519, Secp256k1, Secp256k1Eth, SignaturesAlg, Signed}
 import org.scalatest.{FlatSpec, Matchers}
 
@@ -15,8 +17,14 @@ class DeployValidationSpec extends FlatSpec with Matchers {
       phloPrice = 1,
       validAfterBlockNumber = 0L
     )
-    val (privKey, _) = alg.newKeyPair
-    val signed       = Signed(deploy, alg, privKey)
+
+    // TODO: fixed key is used because Secp256k1Eth sign/verify is unstable with some generated keys ???
+    // - check CertificateHelper.{encodeSignatureRStoDER, decodeSignatureDERtoRS}
+    val privKeyHex = "6a42f5941bdaec35fdfa93d735c04f58d5d51ca3529d9a2fe753b818f1fa32e1"
+    val privKey    = PrivateKey(Base16.unsafeDecode(privKeyHex))
+//    val (privKey, _) = alg.newKeyPair
+
+    val signed = Signed(deploy, alg, privKey)
     val deployProto = DeployDataProto()
       .withSigAlgorithm(alg.name)
       .withSig(signed.sig)
