48da1c49246320ffaffa57b6db2d34e9b4d7f43a
Tomislav Grospic <grospic@gmail.com>
2020-02-14 19:19:16 +0100

    Fix tests with prepare-deploy block number

--- integration-tests/test/http_client.py
+++ integration-tests/test/http_client.py
@@ -20,7 +20,6 @@ class ApiStatus:
 @dataclass
 class PrepareResponse:
     names: List[str]
-    block_number: int
     seq_number: int
 
 
@@ -61,7 +60,7 @@ class HttpClient():
             rep = requests.get(prepare_deploy_url)
         _check_reponse(rep)
         message = rep.json()
-        return PrepareResponse(names=message['names'], block_number=message['blockNumber'], seq_number=message['seqNumber'])
+        return PrepareResponse(names=message['names'], seq_number=message['seqNumber'])
 
     def deploy(self, term: str, phlo_limit: int, phlo_price: int, valid_after_block_number: int, deployer: PrivateKey) -> str:
         timestamp = int(time.time()* 1000)
