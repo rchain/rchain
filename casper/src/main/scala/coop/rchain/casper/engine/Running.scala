492cef6f787137bdff19989dbf8213fd1dead868
nutzipper <1746367+nzpr@users.noreply.github.com>
2020-02-22 19:55:02 +0300

    check if block is inside blockstore instead of DAG when handling block message

--- casper/src/main/scala/coop/rchain/casper/engine/Running.scala
+++ casper/src/main/scala/coop/rchain/casper/engine/Running.scala
@@ -197,10 +197,10 @@ object Running {
     )
 
   def handleBlockMessage[F[_]: Monad: Log: RequestedBlocks](peer: PeerNode, b: BlockMessage)(
-      casperContains: BlockHash => F[Boolean],
+      blockStoreContains: BlockHash => F[Boolean],
       casperAdd: BlockMessage => F[ValidBlockProcessing]
   ): F[Unit] =
-    casperContains(b.blockHash)
+    blockStoreContains(b.blockHash)
       .ifM(
         Log[F].info(s"Received block ${PrettyPrinter.buildString(b.blockHash)} again."),
         Log[F].info(s"Received ${PrettyPrinter.buildString(b)}.") >> casperAdd(b) >>= (
@@ -299,7 +299,7 @@ class Running[F[_]: Sync: BlockStore: CommUtil: TransportLayer: ConnectionsCell:
 
   override def handle(peer: PeerNode, msg: CasperMessage): F[Unit] = msg match {
     case blockHash: BlockHashMessage => handleBlockHashMessage(peer, blockHash)(casper.contains)
-    case b: BlockMessage             => handleBlockMessage(peer, b)(casper.contains, casperAdd(peer))
+    case b: BlockMessage             => handleBlockMessage(peer, b)(BlockStore[F].contains, casperAdd(peer))
     case br: BlockRequest            => handleBlockRequest(peer, br)
     case hbr: HasBlockRequest        => handleHasBlockRequest(peer, hbr)(casper.contains)
     case hb: HasBlock                => handleHasBlock(peer, hb)(casper.contains)
