5670b00bcd45d4f082d0f138c1a92ba7969be1bb
Tomislav Grospic <grospic@gmail.com>
2020-03-10 14:19:59 +0100

    Make higher AND and OR combinators lazy in second argument

--- shared/src/main/scala/coop/rchain/catscontrib/BooleanF.scala
+++ shared/src/main/scala/coop/rchain/catscontrib/BooleanF.scala
@@ -1,19 +1,22 @@
 package coop.rchain.catscontrib
 
-import cats._, cats.data._, cats.implicits._
+import cats._, cats.syntax.all._
 
 final class BooleanF[F[_]: FlatMap](val self: F[Boolean]) {
-  def &&^(other: F[Boolean]): F[Boolean] =
+  def &&^(other: => F[Boolean]): F[Boolean] =
     BooleanF.&&^[F](self, other)
-  def ||^(other: F[Boolean]): F[Boolean] =
+
+  def ||^(other: => F[Boolean]): F[Boolean] =
     BooleanF.||^[F](self, other)
+
+  def not: F[Boolean] = BooleanF.~^(self)
 }
 
 object BooleanF {
-  def &&^[F[_]: FlatMap](m1: F[Boolean], m2: F[Boolean]): F[Boolean] =
+  def &&^[F[_]: FlatMap](m1: F[Boolean], m2: => F[Boolean]): F[Boolean] =
     FlatMap[F].ifM(m1)(m2, m1)
 
-  def ||^[F[_]: FlatMap](m1: F[Boolean], m2: F[Boolean]): F[Boolean] =
+  def ||^[F[_]: FlatMap](m1: F[Boolean], m2: => F[Boolean]): F[Boolean] =
     FlatMap[F].ifM(m1)(m1, m2)
 
   def ~^[F[_]: FlatMap](m: F[Boolean]): F[Boolean] =
