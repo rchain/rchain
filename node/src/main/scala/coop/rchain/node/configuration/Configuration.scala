c58d752279c53ad6a0a40fcd165824b4621f74d4
nutzipper <1746367+nzpr@users.noreply.github.com>
2020-04-19 11:38:51 +0300

    Update configuration object

--- node/src/main/scala/coop/rchain/node/configuration/Configuration.scala
+++ node/src/main/scala/coop/rchain/node/configuration/Configuration.scala
@@ -2,264 +2,124 @@ package coop.rchain.node.configuration
 
 import java.nio.file.{Path, Paths}
 
+import com.typesafe.config.{Config, ConfigFactory}
+
 import collection.JavaConverters._
 import scala.util.Try
 import coop.rchain.blockstorage.FileLMDBIndexBlockStore
 import coop.rchain.blockstorage.dag.BlockDagFileStorage
-import coop.rchain.casper.CasperConf
+import coop.rchain.comm.PeerNode
 import coop.rchain.node.configuration.commandline.ConfigMapper
-
-import com.typesafe.config._
+import coop.rchain.node.diagnostics.{BatchInfluxDBReporter, UdpInfluxDBReporter}
+import kamon.Kamon
+import kamon.system.SystemMetrics
+import kamon.zipkin.ZipkinReporter
 import monix.eval.Task
+import pureconfig._
+import pureconfig.generic.ProductHint
+import pureconfig.generic.auto._
 
+/**
+  * Configuration for RChain node instance
+  */
 object Configuration {
 
+  /**
+    * Builds Configuration instance from CLI options.
+    * If config file is provided as part of CLI options, it shall be parsed and merged
+    * with CLI options having higher priority.
+    * @param options CLI options
+    * @return Configuration instance
+    */
+  def build(options: commandline.Options): (NodeConf, Config) = {
+    val profile = options.profile.toOption.flatMap(profiles.get).getOrElse(defaultProfile)
+    val dataDir = options.run.dataDir.getOrElse(profile.dataDir._1())
+    val configFile = options.run.configFile
+      .getOrElse(
+        dataDir.resolve("rnode.conf")
+      )
+      .toFile
+
+    /**
+      * Target Configuration is a merge of 3 sources according to the following priority, highest to lowers
+      * 1. optionsConfig - CLI options
+      * 2. fileConfig - `.conf` file provided via CLI option `-c`
+      * 3. defaultConfig - defaults in `node/src/main/resources/defaults.conf`,
+      *    defaultConfig is modified with parameters set in profile
+      */
+    val optionsConfig = ConfigSource.fromConfig(
+      ConfigMapper.fromOptions(options)
+    )
+    val fileConfig =
+      if (configFile.exists())
+        ConfigSource.file(configFile)
+      else
+        ConfigSource.empty
+    val defaultConfig = ConfigSource
+      .resources("defaults.conf")
+      .withFallback(
+        ConfigSource.string(
+          s"default-data-dir = $dataDir"
+        )
+      )
+
+    // Throw an error is unknown keys found
+    //implicit val hint = ProductHint[NodeConf](allowUnknownKeys = false)
+    // Custom reader for PeerNode type
+    implicit val PeerNodeReader = ConfigReader.fromString[PeerNode](
+      ConvertHelpers.catchReadError(s => PeerNode.fromAddress(s).getOrElse(null))
+    )
+    // Make Long values support size-in-bytes format, e.g. 16M
+    implicit val myIntReader = ConfigReader.fromString[Long](
+      ConvertHelpers.catchReadError(s => ConfigFactory.parseString(s"v = $s").getBytes("v"))
+    )
+    // Merge configs and create NodeConf instance
+    val mergedConf = optionsConfig
+      .withFallback(fileConfig)
+      .withFallback(defaultConfig)
+    val nodeConfE = mergedConf.load[NodeConf]
+
+    nodeConfE match {
+      case Left(t) =>
+        System.err.println(s"Can't build the configuration: ${t}")
+        System.exit(1)
+      case _ =>
+    }
+    val nodeConf = nodeConfE.right.get
+
+    val kamonConf = ConfigFactory.parseString(mergedConf.at("kamon").value().right.get.render())
+
+    (nodeConf, kamonConf)
+  }
+
+  final case class Profile(name: String, dataDir: (() => Path, String))
+
   private val dockerProfile =
-    Profile("docker", dataDir = (() => Paths.get("/var/lib/rnode"), "Defaults to /var/lib/rnode"))
+    Profile(
+      "docker",
+      dataDir = (
+        () => Paths.get("/var/lib/rnode"),
+        "Defaults to /var/lib/rnode"
+      )
+    )
 
   private val defaultProfile =
     Profile(
       "default",
-      dataDir = (() => Paths.get(sys.props("user.home"), ".rnode"), "Defaults to $HOME/.rnode")
+      dataDir = (
+        () => Paths.get(sys.props("user.home"), ".rnode"),
+        "Defaults to $HOME/.rnode"
+      )
     )
 
-  private val profiles: Map[String, Profile] =
-    Map(defaultProfile.name -> defaultProfile, dockerProfile.name -> dockerProfile)
+  def profiles: Map[String, Profile] =
+    Map(
+      defaultProfile.name -> defaultProfile,
+      dockerProfile.name  -> dockerProfile
+    )
 
   // within range HTTP2 RFC 7540
-  private val MaxMessageSizeMinimumValue: Int = 256 * 1024 // 0.25 MB
-  private val MaxMessageSizeMaximumValue: Int = 10 * 1024 * 1024
-
-  private val DataDirConfigKey     = s"${hocon.Server.Key}.${hocon.Server.keys.DataDir}"
-  private val CertificateConfigKey = s"${hocon.Tls.Key}.${hocon.Tls.keys.Certificate}"
-  private val KeyConfigKey         = s"${hocon.Tls.Key}.${hocon.Tls.keys.Key}"
-  private val GenesisPathConfigKey = s"${hocon.Casper.Key}.${hocon.Casper.keys.GenesisPath}"
-
-  def build(arguments: Seq[String]): Configuration = {
-    val configurationE = Try(buildConfiguration(arguments)).toEither
-
-    configurationE match {
-      case Left(t) =>
-        System.err.println(s"Can't build the configuration: ${t.getMessage}")
-        System.exit(1)
-      case _ =>
-    }
-
-    configurationE.right.get
-  }
-
-  private def loadConfigFile(configFile: Path): Config = {
-    val configE = Try(ConfigFactory.parseFile(configFile.toFile)).toEither
-
-    configE match {
-      case Left(t) =>
-        System.err.println(s"Can't parse the configuration file: ${t.getMessage}")
-        System.exit(1)
-      case _ =>
-    }
-
-    configE.right.get
-  }
-
-  private def buildConfiguration(arguments: Seq[String]): Configuration = {
-    val options = commandline.Options(arguments)
-    val profile = options.profile.toOption.flatMap(profiles.get).getOrElse(defaultProfile)
-    val command = subcommand(options)
-
-    val dataDir =
-      if (command == Run) options.run.dataDir.getOrElse(profile.dataDir._1())
-      else profile.dataDir._1()
-
-    val baseConfig =
-      ConfigFactory.parseMap(
-        Map(DataDirConfigKey -> dataDir.toString).asJava,
-        "base configuration"
-      )
-
-    val configFile    = options.configFile.getOrElse(dataDir.resolve("rnode.conf"))
-    val optionsConfig = ConfigMapper.fromOptions(options)
-    val fileConfig    = if (command == Run) loadConfigFile(configFile) else ConfigFactory.empty()
-
-    val config = optionsConfig
-      .withFallback(fileConfig)
-      .withFallback(baseConfig)
-      .withFallback(ConfigFactory.load())
-
-    val configuration =
-      createConfiguration(
-        addDynamicParts(config),
-        config.hasPath(CertificateConfigKey),
-        config.hasPath(KeyConfigKey),
-        command,
-        profile.name,
-        configFile
-      )
-
-    configuration.copy(printHelpToConsole = () => options.printHelp())
-  }
-
-  private def addDynamicParts(config: Config): Config = {
-    val dataDir = Paths.get(config.getString(DataDirConfigKey))
-    val map =
-      Map(
-        CertificateConfigKey -> dataDir.resolve("node.certificate.pem").toString,
-        KeyConfigKey         -> dataDir.resolve("node.key.pem").toString,
-        GenesisPathConfigKey -> dataDir.resolve("genesis").toString
-      )
-    config.withFallback(ConfigFactory.parseMap(map.asJava))
-  }
-
-  private def createConfiguration(
-      config: Config,
-      customCertificateLocation: Boolean,
-      customKeyLocation: Boolean,
-      command: Command,
-      profile: String,
-      configFile: Path
-  ): Configuration = {
-    val grpcServer           = hocon.GrpcServer.fromConfig(config)
-    val server               = hocon.Server.fromConfig(config)
-    val roundRobinDispatcher = hocon.RoundRobinDispatcher.fromConfig(config)
-    val tls = hocon.Tls
-      .fromConfig(config)
-      .copy(
-        customCertificateLocation = customCertificateLocation,
-        customKeyLocation = customKeyLocation
-      )
-    val kamon   = hocon.Kamon.fromConfig(config)
-    val casper  = hocon.Casper.fromConfig(config)
-    val dataDir = server.dataDir
-    val blockDagStorage = BlockDagFileStorage.Config(
-      dataDir.resolve("casper-block-dag-file-storage-latest-messages-log"),
-      dataDir.resolve("casper-block-dag-file-storage-latest-messages-crc"),
-      dataDir.resolve("casper-block-dag-file-storage-block-metadata-log"),
-      dataDir.resolve("casper-block-dag-file-storage-block-metadata-crc"),
-      dataDir.resolve("casper-block-dag-file-storage-equivocation-tracker-log"),
-      dataDir.resolve("casper-block-dag-file-storage-equivocation-tracker-crc"),
-      dataDir.resolve("casper-block-dag-file-storage-invalid-blocks-log"),
-      dataDir.resolve("casper-block-dag-file-storage-invalid-blocks-crc"),
-      dataDir.resolve("casper-block-dag-file-storage-block-hashes-by-deploy-data"),
-      dataDir.resolve("casper-block-dag-file-storage-block-hashes-by-deploy-crc"),
-      dataDir.resolve("casper-block-dag-file-storage-checkpoints"),
-      dataDir.resolve("casper-block-dag-file-storage-block-number-index"),
-      server.dagStorageSize
-    )
-    val blockStorage =
-      FileLMDBIndexBlockStore.Config(
-        dataDir.resolve("casper-block-store").resolve("storage"),
-        dataDir.resolve("casper-block-store").resolve("index"),
-        dataDir.resolve("casper-block-store").resolve("approved-block"),
-        dataDir.resolve("casper-block-store").resolve("checkpoints"),
-        server.storeSize
-      )
-    val maxMessageSize: Int =
-      Math.max(
-        MaxMessageSizeMinimumValue,
-        Math.min(
-          MaxMessageSizeMaximumValue,
-          server.maxMessageSize
-        )
-      )
-    val grpcMaxMessageSize: Int =
-      Math.max(
-        MaxMessageSizeMinimumValue,
-        Math.min(
-          MaxMessageSizeMaximumValue,
-          grpcServer.maxMessageSize
-        )
-      )
-    val allKeys = config
-      .getConfig(hocon.Configuration.Key)
-      .entrySet()
-      .asScala
-      .filterNot(_.getKey.startsWith("influxdb"))
-      .map(k => s"${hocon.Configuration.Key}.${k.getKey}")
-      .toSet
-    val knownKeys = List(
-      hocon.Casper.Keys,
-      hocon.RoundRobinDispatcher.Keys,
-      hocon.GrpcServer.Keys,
-      hocon.Kamon.Keys,
-      hocon.Server.Keys,
-      hocon.Tls.Keys
-    ).flatten
-
-    new Configuration(
-      config,
-      allKeys -- knownKeys,
-      command,
-      profile,
-      configFile,
-      server.copy(maxMessageSize = maxMessageSize),
-      roundRobinDispatcher,
-      grpcServer.copy(maxMessageSize = grpcMaxMessageSize),
-      tls,
-      casper.copy(createGenesis = server.standalone),
-      blockStorage,
-      blockDagStorage,
-      kamon
-    )
-  }
-
-  private def subcommand(options: commandline.Options): Command =
-    options.subcommand match {
-      case Some(options.eval) =>
-        Eval(options.eval.fileNames(), options.eval.printUnmatchedSendsOnly())
-      case Some(options.repl)   => Repl
-      case Some(options.deploy) =>
-        //TODO: change the defaults before main net
-        import options.deploy._
-        Deploy(
-          phloLimit(),
-          phloPrice(),
-          validAfterBlockNumber.getOrElse(-1L),
-          privateKey.toOption,
-          privateKeyPath.toOption,
-          location()
-        )
-      case Some(options.findDeploy) => FindDeploy(options.findDeploy.deployId())
-      case Some(options.propose)    => Propose(options.propose.printUnmatchedSends())
-      case Some(options.showBlock)  => ShowBlock(options.showBlock.hash())
-      case Some(options.showBlocks) =>
-        import options.showBlocks._
-        ShowBlocks(depth.getOrElse(1))
-      case Some(options.visualizeBlocks) =>
-        import options.visualizeBlocks._
-        VisualizeDag(depth.getOrElse(-1), showJustificationLines.getOrElse(false))
-      case Some(options.machineVerifiableDag) => MachineVerifiableDag
-      case Some(options.run)                  => Run
-      case Some(options.keygen) =>
-        Keygen(
-          options.keygen.algorithm(),
-          options.keygen.privateKeyPath(),
-          options.keygen.publicKeyPath()
-        )
-      case Some(options.lastFinalizedBlock) => LastFinalizedBlock
-      case Some(options.isFinalized)        => IsFinalized(options.isFinalized.hash())
-      case Some(options.bondStatus)         => BondStatus(options.bondStatus.validatorPublicKey())
-      case Some(options.dataAtName)         => DataAtName(options.dataAtName.name())
-      case Some(options.contAtName)         => ContAtName(options.contAtName.name())
-      case _                                => Help
-    }
+  // TODO do we need this constraint? Let user set any but warn?
+  // private val MaxMessageSizeMinimumValue: Int = 256 * 1024 // 0.25 MB
+  // private val MaxMessageSizeMaximumValue: Int = 10 * 1024 * 1024
 }
-
-final case class Configuration(
-    underlying: Config,
-    unknownConfigKeys: Set[String],
-    command: Command,
-    profile: String,
-    configurationFile: Path,
-    server: Server,
-    roundRobinDispatcher: RoundRobinDispatcher,
-    grpcServer: GrpcServer,
-    tls: Tls,
-    casper: CasperConf,
-    blockstorage: FileLMDBIndexBlockStore.Config,
-    blockDagStorage: BlockDagFileStorage.Config,
-    kamon: Kamon,
-    printHelpToConsole: () => Unit = () => ()
-) {
-  def printHelp(): Task[Unit] = Task.delay(printHelpToConsole())
-}
-
-final case class Profile(name: String, dataDir: (() => Path, String))
