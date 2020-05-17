8ab1bb5f39811a63c0eb19d4a2a08e041d9588c3
Tomislav Grospic <grospic@gmail.com>
2020-05-17 17:50:28 +0200

    Block metadata index implementation with KeyValueTypedStore

---
+++ block-storage/src/main/scala/coop/rchain/blockstorage/dag/BlockMetadataStore.scala
@@ -0,0 +1,105 @@
+package coop.rchain.blockstorage.dag
+
+import cats.Monad
+import cats.effect.Sync
+import cats.mtl.MonadState
+import cats.syntax.all._
+import coop.rchain.models.BlockHash.BlockHash
+import coop.rchain.models.BlockMetadata
+import coop.rchain.shared.syntax._
+import coop.rchain.shared.{AtomicMonadState, Log}
+import coop.rchain.store.KeyValueTypedStore
+import monix.execution.atomic.AtomicAny
+
+import scala.collection.immutable.SortedMap
+
+object BlockMetadataStore {
+  def apply[F[_]: Sync: Log](
+      blockMetadataStore: KeyValueTypedStore[F, BlockHash, BlockMetadata]
+  ): F[BlockMetadataStore[F]] =
+//    implicit val raiseIOError: RaiseIOError[F] = IOError.raiseIOErrorThroughSync[F]
+    for {
+      // Migrating data from previous metadata index
+//      result <- BlockMetadataPersistentIndex.read(Paths.get("index_path"))
+//      (oldMetadaatMap, _, _) = result
+//      _ <- blockMetadataStore.put(oldMetadaatMap.toSeq)
+      blockMetadataMap <- blockMetadataStore.toMap
+      dagState         = recreateInMemoryState(blockMetadataMap)
+    } yield new BlockMetadataStore[F](
+      blockMetadataStore,
+      new AtomicMonadState(AtomicAny(dagState))
+    )
+
+  private final case class DagState(
+      dagSet: Set[BlockHash],
+      childMap: Map[BlockHash, Set[BlockHash]],
+      heightMap: SortedMap[Long, Set[BlockHash]]
+  )
+
+  class BlockMetadataStore[F[_]: Monad](
+      private val store: KeyValueTypedStore[F, BlockHash, BlockMetadata],
+      private val dagState: MonadState[F, DagState]
+  ) {
+    def add(block: BlockMetadata): F[Unit] =
+      for {
+        // Update DAG state with new block
+        _ <- dagState.modify(st => validateDagState(addBlockToDagState(block)(st)))
+
+        // Update persistent block metadata store
+        _ <- store.put(block.blockHash, block)
+      } yield ()
+
+    def get(hash: BlockHash): F[Option[BlockMetadata]] = store.get(hash)
+
+    // DAG state operations
+
+    def dagSet: F[Set[BlockHash]] = dagState.get.map(_.dagSet)
+
+    def contains(hash: BlockHash): F[Boolean] = dagState.get.map(_.dagSet.contains(hash))
+
+    def childMapData: F[Map[BlockHash, Set[BlockHash]]] =
+      dagState.get.map(_.childMap)
+
+    def heightMap: F[SortedMap[Long, Set[BlockHash]]] =
+      dagState.get.map(_.heightMap)
+  }
+
+  private def addBlockToDagState(block: BlockMetadata)(state: DagState): DagState = {
+    // Update dag set / all blocks in the DAG
+    val newDagSet = state.dagSet + block.blockHash
+
+    // Update children relation map
+    val blockChilds = block.parents.map((_, Set(block.blockHash))) :+ (block.blockHash, Set())
+    val newChildMap = blockChilds.foldLeft(state.childMap) {
+      case (acc, (key, newChildren)) =>
+        val currChildren = acc.getOrElse(key, Set.empty[BlockHash])
+        acc.updated(key, currChildren ++ newChildren)
+    }
+
+    // Update block height map
+    val newHeightMap = if (!block.invalid) {
+      val currSet = state.heightMap.getOrElse(block.blockNum, Set())
+      state.heightMap.updated(block.blockNum, currSet + block.blockHash)
+    } else state.heightMap
+
+    state.copy(dagSet = newDagSet, childMap = newChildMap, heightMap = newHeightMap)
+  }
+
+  private def validateDagState(state: DagState): DagState = {
+    // Validate height map index (block numbers) are in sequence without holes
+    val m          = state.heightMap
+    val (min, max) = if (m.nonEmpty) (m.firstKey, m.lastKey + 1) else (0L, 0L)
+    assert(max - min == m.size.toLong, "DAG store height map has numbers not in sequence.")
+    state
+  }
+
+  private def recreateInMemoryState(
+      blockMetadataMap: Map[BlockHash, BlockMetadata]
+  ): DagState = {
+    val emptyState = DagState(dagSet = Set(), childMap = Map(), heightMap = SortedMap())
+    val dagState = blockMetadataMap.foldLeft(emptyState) {
+      case (state, (_, block)) => addBlockToDagState(block)(state)
+    }
+    validateDagState(dagState)
+  }
+}
