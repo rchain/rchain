f24a38267b64c812946dd5038737d3385b470759
Tomislav Grospic <grospic@gmail.com>
2020-06-05 20:10:22 +0200

    Update reporting tests to use in-memory LMDB cache

--- casper/src/test/scala/coop/rchain/casper/batch1/MultiParentCasperReportingSpec.scala
+++ casper/src/test/scala/coop/rchain/casper/batch1/MultiParentCasperReportingSpec.scala
@@ -1,11 +1,13 @@
 package coop.rchain.casper.batch1
 
-import coop.rchain.casper.ReportingCasper
 import coop.rchain.casper.helper.TestNode
 import coop.rchain.casper.helper.TestNode.Effect
 import coop.rchain.casper.util.ConstructDeploy
+import coop.rchain.casper.{ReportMemStore, ReportingCasper}
+import coop.rchain.models.{BindPattern, ListParWithRandom, Par, TaggedContinuation}
 import coop.rchain.p2p.EffectsTestInstances.LogicalTime
 import coop.rchain.shared.scalatestcontrib.effectTest
+import coop.rchain.store.InMemoryStoreManager
 import monix.execution.Scheduler.Implicits.global
 import org.scalatest.{FlatSpec, Inspectors, Matchers}
 
@@ -22,19 +24,21 @@ class MultiParentCasperReportingSpec extends FlatSpec with Matchers with Inspect
       """ for(@a <- @"1"){ Nil } | @"1"!("x") """
     TestNode.standaloneEff(genesis).use { node =>
       import node._
+      import coop.rchain.rholang.interpreter.storage._
       implicit val timeEff = new LogicalTime[Effect]
-
-      val reportingCasper: ReportingCasper[Effect] =
-        ReportingCasper.rhoReporter(node.rhoHistoryRepository)
+      implicit val kvm     = InMemoryStoreManager[Effect]
 
       for {
-        deploy      <- ConstructDeploy.sourceDeployNowF(correctRholang)
-        signedBlock <- node.addBlock(deploy)
-        _           = logEff.warns.isEmpty should be(true)
-        dag         <- node.casperEff.blockDag
-        estimate    <- node.casperEff.estimator(dag)
-        _           = estimate shouldBe IndexedSeq(signedBlock.blockHash)
-        trace       <- reportingCasper.trace(signedBlock.blockHash)
+        reportingStore <- ReportMemStore
+                           .store[Effect, Par, BindPattern, ListParWithRandom, TaggedContinuation]
+        reportingCasper = ReportingCasper.rhoReporter(node.rhoHistoryRepository, reportingStore)
+        deploy          <- ConstructDeploy.sourceDeployNowF(correctRholang)
+        signedBlock     <- node.addBlock(deploy)
+        _               = logEff.warns.isEmpty should be(true)
+        dag             <- node.casperEff.blockDag
+        estimate        <- node.casperEff.estimator(dag)
+        _               = estimate shouldBe IndexedSeq(signedBlock.blockHash)
+        trace           <- reportingCasper.trace(signedBlock.blockHash)
         result = trace match {
           case Right(value) => value.head._2.foldLeft(0)(_ + _.length)
           case Left(_)      => 0
