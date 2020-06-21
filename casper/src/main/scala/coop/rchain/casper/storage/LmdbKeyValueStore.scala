073253fb541f9a915a88cd8786b4a8cfd09499bd
Tomislav Grospic <grospic@gmail.com>
2020-06-21 20:47:31 +0200

    Fix LMDB commit transaction (freeze)

--- casper/src/main/scala/coop/rchain/casper/storage/LmdbKeyValueStore.scala
+++ casper/src/main/scala/coop/rchain/casper/storage/LmdbKeyValueStore.scala
@@ -10,22 +10,22 @@ import coop.rchain.shared.Resources.withResource
 import coop.rchain.store.KeyValueStore
 import org.lmdbjava._
 
+import scala.util.control.NonFatal
+
 final case class DbEnv[F[_]](env: Env[ByteBuffer], dbi: Dbi[ByteBuffer], done: F[Unit])
 
 final case class LmdbKeyValueStore[F[_]: Sync](
     getEnvDbi: F[DbEnv[F]]
 ) extends KeyValueStore[F] {
 
-  private def withTxn[T](
-      isWrite: Boolean
-  )(op: (Txn[ByteBuffer], Dbi[ByteBuffer]) => F[T]): F[T] =
+  private def withReadTxn[T](op: (Txn[ByteBuffer], Dbi[ByteBuffer]) => F[T]): F[T] =
     for {
       // Acquire current LMDB environment and DBI interface
       dbEnv <- getEnvDbi
       // "Done" must be called to mark finished operation with the environment/dbi
       DbEnv(env, dbi, done) = dbEnv
-      // Create read or write transaction
-      txnF = Sync[F].delay(if (isWrite) env.txnWrite else env.txnRead)
+      // Create read transaction
+      txnF = Sync[F].delay(env.txnRead)
       // Execute database operation, commit at the end
       result <- txnF.bracketCase(
                  txn =>
@@ -49,39 +49,68 @@ final case class LmdbKeyValueStore[F[_]: Sync](
                }
     } yield result
 
-  def withReadTxnF[T](f: (Txn[ByteBuffer], Dbi[ByteBuffer]) => F[T]): F[T] =
-    withTxn(isWrite = false)(f)
+  // Ensures transaction is used only on one thread.
+  // > A write Transaction may only be used from the thread it was created on.
+  // https://lmdb.readthedocs.io/en/release/#threads
+  private def withTxnSingleThread[T](
+      isWrite: Boolean
+  )(op: (Txn[ByteBuffer], Dbi[ByteBuffer]) => T): F[T] =
+    for {
+      // Acquire current LMDB environment and DBI interface
+      dbEnv <- getEnvDbi
+      // "Done" must be called to mark finished operation with the environment/dbi
+      DbEnv(env, dbi, done) = dbEnv
+      // Create read or write transaction
+      txn = if (isWrite) env.txnWrite else env.txnRead
+      // Execute database operation, commit at the end
+      result <- try {
+                 // Execute DB operation within transaction
+                 val res = op(txn, dbi)
+                 // Commit transaction (read and write)
+                 txn.commit()
+                 // DB result
+                 res.pure[F]
+               } catch {
+                 case NonFatal(ex) =>
+                   // Ack done and rethrow error.
+                   done *> ex.raiseError[F, T]
+               } finally {
+                 // Close transaction at the end.
+                 txn.close()
+               }
+      // Ack done
+      _ <- done
+    } yield result
 
-  def withWriteTxnF[T](f: (Txn[ByteBuffer], Dbi[ByteBuffer]) => F[T]): F[T] =
-    withTxn(isWrite = true)(f)
+  def withWriteTxn[T](f: (Txn[ByteBuffer], Dbi[ByteBuffer]) => T): F[T] =
+    withTxnSingleThread(isWrite = true)(f)
 
   // GET
   override def get[T](keys: Seq[ByteBuffer], fromBuffer: ByteBuffer => T): F[Seq[Option[T]]] =
-    withReadTxnF { (txn, dbi) =>
-      import cats.instances.vector._
-      keys.toVector
-        .traverse(x => Sync[F].delay(Option(dbi.get(txn, x)).map(fromBuffer)))
-        .map(_.toSeq)
+    withReadTxn { (txn, dbi) =>
+      Sync[F].delay {
+        keys.map(x => Option(dbi.get(txn, x)).map(fromBuffer))
+      }
     }
 
   // PUT
   override def put[T](kvPairs: Seq[(ByteBuffer, T)], toBuffer: T => ByteBuffer): F[Unit] =
-    withWriteTxnF { (txn, dbi) =>
-      Sync[F].delay(kvPairs.foreach {
+    withWriteTxn { (txn, dbi) =>
+      kvPairs.foreach {
         case (key, value) =>
           if (dbi.put(txn, key, toBuffer(value))) () else ()
-      })
+      }
     }
 
   // DELETE
   override def delete(keys: Seq[ByteBuffer]): F[Int] =
-    withWriteTxnF { (txn, dbi) =>
-      Sync[F].delay(keys.foldLeft(0)((acc, key) => if (dbi.delete(txn, key)) acc + 1 else acc))
+    withWriteTxn { (txn, dbi) =>
+      keys.foldLeft(0)((acc, key) => if (dbi.delete(txn, key)) acc + 1 else acc)
     }
 
   // ITERATE
   override def iterate[T](f: Iterator[(ByteBuffer, ByteBuffer)] => F[T]): F[T] =
-    withReadTxnF { (txn, dbi) =>
+    withReadTxn { (txn, dbi) =>
       withResource(dbi.iterate(txn)) { iterator =>
         import scala.collection.JavaConverters._
         f(iterator.asScala.map(c => (c.key, c.`val`)))
