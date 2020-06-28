f66c6a05a977d514190eadb254a38fd4d222ce1b
Tomislav Grospic <grospic@gmail.com>
2020-06-28 17:49:05 +0200

    KeyValueStoreSyntax implicit class refactored to extends AnyVal

--- shared/src/main/scala/coop/rchain/store/KeyValueTypedStoreSyntax.scala
+++ shared/src/main/scala/coop/rchain/store/KeyValueTypedStoreSyntax.scala
@@ -4,20 +4,20 @@ import cats.Functor
 import cats.syntax.all._
 
 trait KeyValueTypedStoreSyntax {
-  implicit final def sharedSyntaxKeyValueTypedStore[F[_]: Functor, K, V](
+  implicit final def sharedSyntaxKeyValueTypedStore[F[_], K, V](
       store: KeyValueTypedStore[F, K, V]
   ): KeyValueTypedStoreOps[F, K, V] = new KeyValueTypedStoreOps[F, K, V](store)
 }
 
-final class KeyValueTypedStoreOps[F[_]: Functor, K, V](
+final class KeyValueTypedStoreOps[F[_], K, V](
     // KeyValueTypedStore extensions / syntax
     private val store: KeyValueTypedStore[F, K, V]
-) {
-  def get(key: K): F[Option[V]] = store.get(Seq(key)).map(_.head)
+) extends AnyVal {
+  def get(key: K)(implicit f: Functor[F]): F[Option[V]] = store.get(Seq(key)).map(_.head)
 
   def put(key: K, value: V): F[Unit] = store.put(Seq((key, value)))
 
-  def delete(key: K): F[Boolean] = store.delete(Seq(key)).map(_ == 1)
+  def delete(key: K)(implicit f: Functor[F]): F[Boolean] = store.delete(Seq(key)).map(_ == 1)
 
-  def contains(key: K): F[Boolean] = store.contains(Seq(key)).map(_.head)
+  def contains(key: K)(implicit f: Functor[F]): F[Boolean] = store.contains(Seq(key)).map(_.head)
 }
