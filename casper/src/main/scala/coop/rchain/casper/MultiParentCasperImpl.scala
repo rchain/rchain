5bf7b8671d5d196f073acf885ce528e2c65ba7ff
nutzipper <1746367+nzpr@users.noreply.github.com>
2020-07-21 10:34:50 +0300

    Prioritise pendant block when picking the next bock to add

--- casper/src/main/scala/coop/rchain/casper/MultiParentCasperImpl.scala
+++ casper/src/main/scala/coop/rchain/casper/MultiParentCasperImpl.scala
@@ -274,9 +274,12 @@ class MultiParentCasperImpl[F[_]: Sync: Concurrent: Log: Time: SafetyOracle: Las
                           } yield !missingDep
                         }
       enqueued <- BlockRetriever[F].getEnqueuedToCasper
-      all      = enqueued ++ depFreePendants
-      _ <- Log[F].info(s"Blocks ready to be added: enqueued to Casper ${PrettyPrinter
-            .buildString(enqueued)}, buffer pendants ${PrettyPrinter.buildString(depFreePendants)}.")
+      all      = depFreePendants ++ enqueued
+      _ <- Log[F].info(
+            s"Blocks ready to be added: " +
+              s"dependency free buffer pendants ${PrettyPrinter.buildString(depFreePendants)}, " +
+              s"enqueued to Casper ${PrettyPrinter.buildString(enqueued)}"
+          )
     } yield all.headOption
   }
 
