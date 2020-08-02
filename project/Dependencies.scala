381f3b06c16ba7bc842fed140884f37912e9ee0d
Scala Steward <me@scala-steward.org>
2020-08-02 20:36:34 +0200

    Update kamon-zipkin to 1.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -46,7 +46,7 @@ object Dependencies {
   val kamonSystemMetrics  = "io.kamon"                   %% "kamon-system-metrics"      % "1.0.1"
   val kamonPrometheus     = "io.kamon"                   %% "kamon-prometheus"          % "1.1.1"
   val kamonInfluxDb       = "io.kamon"                   %% "kamon-influxdb"            % "1.0.2"
-  val kamonZipkin         = "io.kamon"                   %% "kamon-zipkin"              % "1.0.0"
+  val kamonZipkin         = "io.kamon"                   %% "kamon-zipkin"              % "1.0.1"
   val lightningj          = ("org.lightningj"             % "lightningj"                % "0.5.0-Beta-rc2")
     .intransitive() //we only use the lib for one util class (org.lightningj.util.ZBase32) that has no dependencies
   val lmdbjava            = "org.lmdbjava"                % "lmdbjava"                  % "0.6.1"
