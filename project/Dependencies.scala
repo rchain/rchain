f4d58f50550fdb60634fbee6951014353c3ae0b7
Scala Steward <me@scala-steward.org>
2020-08-02 20:33:52 +0200

    Update enumeratum to 1.5.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val osClassifier: String = Detector.detect(Seq("fedora")).osClassifier
 
   val circeVersion      = "0.13.0"
-  val enumeratumVersion = "1.5.13"
+  val enumeratumVersion = "1.5.15"
   val http4sVersion     = "0.21.4"
   val kamonVersion      = "1.1.5"
   val catsVersion       = "2.1.0"
