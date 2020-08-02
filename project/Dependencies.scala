f68bda0f625fc0f405bb6ecb5f5fc56cea0b664a
Scala Steward <me@scala-steward.org>
2020-08-02 20:37:58 +0200

    Update lightningj to 0.5.2-Beta

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -47,7 +47,7 @@ object Dependencies {
   val kamonPrometheus     = "io.kamon"                   %% "kamon-prometheus"          % "1.1.1"
   val kamonInfluxDb       = "io.kamon"                   %% "kamon-influxdb"            % "1.0.2"
   val kamonZipkin         = "io.kamon"                   %% "kamon-zipkin"              % "1.0.0"
-  val lightningj          = ("org.lightningj"             % "lightningj"                % "0.5.0-Beta-rc2")
+  val lightningj          = ("org.lightningj"             % "lightningj"                % "0.5.2-Beta")
     .intransitive() //we only use the lib for one util class (org.lightningj.util.ZBase32) that has no dependencies
   val lmdbjava            = "org.lmdbjava"                % "lmdbjava"                  % "0.6.1"
   val logbackClassic      = "ch.qos.logback"              % "logback-classic"           % "1.2.3"
