25a996585590134aa3f8e6ec3a8ccc7b34c07f23
Tomislav Grospic <grospic@gmail.com>
2020-08-03 10:52:59 +0200

    Test to print public keys used for signing blessed contracts

---
+++ casper/src/test/scala/coop/rchain/casper/genesis/contracts/StandardDeploysSpec.scala
@@ -0,0 +1,16 @@
+package coop.rchain.casper.genesis.contracts
+
+import coop.rchain.crypto.codec.Base16
+import org.scalatest.FlatSpec
+
+class StandardDeploysSpec extends FlatSpec {
+  it should "print public keys used for signing standard (blessed) contracts" in {
+    println(s"Public keys used to sign standard (blessed) contracts")
+    println(s"=====================================================")
+
+    StandardDeploys.systemPublicKeys.zipWithIndex.foreach {
+      case (pubKey, idx) =>
+        println(s"${idx + 1}. ${Base16.encode(pubKey.bytes)}")
+    }
+  }
+}
