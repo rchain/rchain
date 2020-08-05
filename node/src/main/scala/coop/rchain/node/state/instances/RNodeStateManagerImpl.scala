7b7bee67f22c397f9f8f1bd8b21c5b932b589a6d
Tomislav Grospic <grospic@gmail.com>
2020-08-05 19:08:29 +0200

    Initial implementation of Block, RSpace and RNode state managers

---
+++ node/src/main/scala/coop/rchain/node/state/instances/RNodeStateManagerImpl.scala
@@ -0,0 +1,21 @@
+package coop.rchain.node.state.instances
+
+import cats.effect.Sync
+import coop.rchain.casper.state.{BlockStateManager, RNodeStateManager}
+import coop.rchain.catscontrib.Catscontrib.ToBooleanF
+import coop.rchain.rspace.state.RSpaceStateManager
+
+object RNodeStateManagerImpl {
+  def apply[F[_]: Sync](
+      rSpaceStateManager: RSpaceStateManager[F],
+      blockStateManager: BlockStateManager[F]
+  ): RNodeStateManager[F] =
+    RNodeStateManagerImpl[F](rSpaceStateManager, blockStateManager)
+
+  private final case class RNodeStateManagerImpl[F[_]: Sync](
+      rSpaceStateManager: RSpaceStateManager[F],
+      blockStateManager: BlockStateManager[F]
+  ) extends RNodeStateManager[F] {
+    override def isEmpty: F[Boolean] = rSpaceStateManager.isEmpty &&^ blockStateManager.isEmpty
+  }
+}
