e4f917227e34902e38eadbd5a108f0ea146749b4
Tomislav Grospic <grospic@gmail.com>
2020-08-05 19:09:27 +0200

    Add RSpace exporter/importer to HistoryRepository, update NodeRuntime

--- node/src/main/scala/coop/rchain/node/NodeRuntime.scala
+++ node/src/main/scala/coop/rchain/node/NodeRuntime.scala
@@ -23,6 +23,7 @@ import coop.rchain.casper.engine.{BlockRetriever, _}
 import coop.rchain.casper.engine.EngineCell._
 import coop.rchain.casper.protocol.deploy.v1.DeployServiceV1GrpcMonix
 import coop.rchain.casper.protocol.propose.v1.ProposeServiceV1GrpcMonix
+import coop.rchain.casper.state.instances.BlockStateManagerImpl
 import coop.rchain.casper.storage.RNodeKeyValueStoreManager
 import coop.rchain.casper.util.comm._
 import coop.rchain.casper.util.rholang.RuntimeManager
@@ -48,10 +49,12 @@ import coop.rchain.node.diagnostics.{NewPrometheusReporter, _}
 import coop.rchain.node.diagnostics.Trace.TraceId
 import coop.rchain.node.effects.{EventConsumer, RchainEvents}
 import coop.rchain.node.model.repl.ReplGrpcMonix
+import coop.rchain.node.state.instances.RNodeStateManagerImpl
 import coop.rchain.node.web._
 import coop.rchain.p2p.effects._
 import coop.rchain.rholang.interpreter.Runtime
 import coop.rchain.rspace.Context
+import coop.rchain.rspace.state.instances.RSpaceStateManagerImpl
 import coop.rchain.shared._
 import coop.rchain.shared.PathOps._
 import coop.rchain.store.KeyValueStoreManager
@@ -811,7 +814,7 @@ object NodeRuntime {
         }
       }
       _ <- Runtime.bootstrapRegistry[F](evalRuntime)
-      casperRuntimeAndReporter <- {
+      casperInitialized <- {
         implicit val s  = rspaceScheduler
         implicit val sp = span
         implicit val bs = blockStore
@@ -835,14 +838,25 @@ object NodeRuntime {
                        } yield ReportingCasper.rhoReporter(hr, reportingCache)
                      } else
                        ReportingCasper.noop.pure[F]
-        } yield (runtime, reporter)
+        } yield (runtime, reporter, hr)
       }
-      (casperRuntime, reportingCasper) = casperRuntimeAndReporter
+      (casperRuntime, reportingCasper, historyRepo) = casperInitialized
       runtimeManager <- {
         implicit val sp = span
         RuntimeManager.fromRuntime[F](casperRuntime)
       }
-
+      // RNodeStateManager
+      stateManagers <- {
+        for {
+          exporter           <- historyRepo.exporter
+          importer           <- historyRepo.importer
+          rspaceStateManager = RSpaceStateManagerImpl(exporter, importer)
+          blockStateManager  = BlockStateManagerImpl(blockStore, blockDagStorage)
+          rnodeStateManager  = RNodeStateManagerImpl(rspaceStateManager, blockStateManager)
+        } yield (rnodeStateManager, rspaceStateManager)
+      }
+      (rnodeStateManager, rspaceStateManager) = stateManagers
+      // Engine dynamic reference
       engineCell   <- EngineCell.init[F]
       envVars      = EnvVars.envVars[F]
       raiseIOError = IOError.raiseIOErrorThroughSync[F]
