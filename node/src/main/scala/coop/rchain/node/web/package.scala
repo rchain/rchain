19f421ab90bcdf5f6e09c9a93ce85c84565130e5
Tomislav Grospic <grospic@gmail.com>
2020-11-17 11:23:21 +0100

    Web API refactor to use abstract effect type

--- node/src/main/scala/coop/rchain/node/web/package.scala
+++ node/src/main/scala/coop/rchain/node/web/package.scala
@@ -1,16 +1,15 @@
 package coop.rchain.node
 
-import cats.effect.Concurrent
+import cats.effect.{Async, ConcurrentEffect, Timer}
+import cats.syntax.all._
+import cats.~>
 import coop.rchain.casper.ReportingCasper
 import coop.rchain.comm.discovery.NodeDiscovery
 import coop.rchain.comm.rp.Connect.{ConnectionsCell, RPConfAsk}
-import coop.rchain.node.NodeRuntime.TaskEnv
-import coop.rchain.node.api.WebApi
-import coop.rchain.node.api.AdminWebApi
+import coop.rchain.node.api.{AdminWebApi, WebApi}
 import coop.rchain.node.diagnostics.NewPrometheusReporter
 import coop.rchain.node.effects.EventConsumer
 import coop.rchain.shared.Log
-import monix.eval.Task
 import monix.execution.Scheduler
 import org.http4s.implicits._
 import org.http4s.server.Router
@@ -20,79 +19,68 @@ import org.http4s.server.middleware.CORS
 import scala.concurrent.duration.FiniteDuration
 
 package object web {
-  def aquireHttpServer(
+  // TODO: Temp until web API is refactored with one effect type.
+  def natId[F[_]]: F ~> F = λ[F ~> F](x => x)
+
+  def aquireHttpServer[F[_]: ConcurrentEffect: Timer: RPConfAsk: NodeDiscovery: ConnectionsCell: EventConsumer: Log](
       reporting: Boolean,
       host: String = "0.0.0.0",
       httpPort: Int,
       prometheusReporter: NewPrometheusReporter,
-      reportingCasper: ReportingCasper[TaskEnv],
-      webApiRoutes: WebApi[TaskEnv],
+      reportingCasper: ReportingCasper[F],
+      webApiRoutes: WebApi[F],
       connectionIdleTimeout: FiniteDuration
-  )(
-      implicit
-      nodeDiscovery: NodeDiscovery[Task],
-      connectionsCell: ConnectionsCell[Task],
-      concurrent: Concurrent[Task],
-      rPConfAsk: RPConfAsk[Task],
-      consumer: EventConsumer[Task],
-      scheduler: Scheduler,
-      log: Log[Task]
-  ): Task[Unit] =
+  )(implicit scheduler: Scheduler): F[Unit] =
     for {
-      event <- EventsInfo.service[Task]
+      event <- EventsInfo.service[F]
       baseRoutes = Map(
-        "/metrics"   -> CORS(NewPrometheusReporter.service[Task](prometheusReporter)),
-        "/version"   -> CORS(VersionInfo.service[Task]),
-        "/status"    -> CORS(StatusInfo.service[Task]),
+        "/metrics"   -> CORS(NewPrometheusReporter.service[F](prometheusReporter)),
+        "/version"   -> CORS(VersionInfo.service[F]),
+        "/status"    -> CORS(StatusInfo.service[F]),
         "/ws/events" -> CORS(event),
         "/api" -> CORS({
-          implicit val et = NodeRuntime.envToTask
-          WebApiRoutes.service[Task, TaskEnv](webApiRoutes)
+          implicit val n = natId[F]
+          WebApiRoutes.service[F, F](webApiRoutes)
         })
       )
       extraRoutes = if (reporting)
         Map(
           "/reporting" -> CORS({
-            implicit val et = NodeRuntime.envToTask
-            ReportingRoutes.service[Task, TaskEnv](reportingCasper)
+            implicit val n = natId[F]
+            ReportingRoutes.service[F, F](reportingCasper)
           })
         )
       else
         Map.empty
       allRoutes = baseRoutes ++ extraRoutes
-      httpServerFiber <- BlazeServerBuilder[Task](scheduler)
+      httpServerFiber <- BlazeServerBuilder[F](scheduler)
                           .bindHttp(httpPort, host)
                           .withHttpApp(Router(allRoutes.toList: _*).orNotFound)
                           .withIdleTimeout(connectionIdleTimeout)
                           .resource
-                          .use(_ => Task.never[Unit])
+                          .use(_ => Async[F].never[Unit])
     } yield httpServerFiber
 
-  def aquireAdminHttpServer(
+  def aquireAdminHttpServer[F[_]: ConcurrentEffect: Timer: EventConsumer](
       host: String = "0.0.0.0",
       httpPort: Int,
-      adminWebApiRoutes: AdminWebApi[TaskEnv],
+      adminWebApiRoutes: AdminWebApi[F],
       connectionIdleTimeout: FiniteDuration
-  )(
-      implicit
-      concurrent: Concurrent[Task],
-      consumer: EventConsumer[Task],
-      scheduler: Scheduler
-  ): Task[Unit] =
+  )(implicit scheduler: Scheduler): F[Unit] =
     for {
-      event <- EventsInfo.service[Task]
+      event <- EventsInfo.service[F]
       baseRoutes = Map(
         "/api" -> CORS({
-          implicit val et = NodeRuntime.envToTask
-          AdminWebApiRoutes.service[Task, TaskEnv](adminWebApiRoutes)
+          implicit val n = natId[F]
+          AdminWebApiRoutes.service[F, F](adminWebApiRoutes)
         })
       )
-      adminHttpServerFiber <- BlazeServerBuilder[Task](scheduler)
+      adminHttpServerFiber <- BlazeServerBuilder[F](scheduler)
                                .bindHttp(httpPort, host)
                                .withHttpApp(Router(baseRoutes.toList: _*).orNotFound)
                                .withResponseHeaderTimeout(connectionIdleTimeout)
                                .withIdleTimeout(connectionIdleTimeout)
                                .resource
-                               .use(_ => Task.never[Unit])
+                               .use(_ => Async[F].never[Unit])
     } yield adminHttpServerFiber
 }
