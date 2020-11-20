ab3a52f45c1ee61cf458297d2a891be80446fccd
Will Qiu <zsluedem06@gmail.com>
2020-11-20 15:42:01 +0800

    add rspace Hasher

---
+++ rspace/src/main/scala/coop/rchain/rspace/Hasher.scala
@@ -0,0 +1,33 @@
+package coop.rchain.rspace
+
+import java.nio.charset.StandardCharsets
+
+import coop.rchain.rspace.internal.{codecByteVector, codecSeq, RichAttempt}
+import coop.rchain.shared.Serialize
+import scodec.Codec
+import scodec.bits.BitVector
+
+object Hasher {
+  val joinSuffixBits         = BitVector("-joins".getBytes(StandardCharsets.UTF_8))
+  val dataSuffixBits         = BitVector("-data".getBytes(StandardCharsets.UTF_8))
+  val continuationSuffixBits = BitVector("-continuation".getBytes(StandardCharsets.UTF_8))
+
+  def hashWithSuffix(bits: BitVector, suffix: BitVector): Blake2b256Hash = {
+    val suffixed = bits ++ suffix
+    Blake2b256Hash.create(suffixed.toByteVector)
+  }
+
+  def hashJoinsChannel[C](channel: C, codeC: Codec[C]): Blake2b256Hash =
+    hashWithSuffix(codeC.encode(channel).get, joinSuffixBits)
+
+  def hashContinuationsChannels[C](channels: Seq[C], serializeC: Serialize[C]): Blake2b256Hash = {
+    val chs = channels
+      .map(c => serializeC.encode(c))
+      .sorted(util.ordByteVector)
+    val channelsBits = codecSeq(codecByteVector).encode(chs).get
+    hashWithSuffix(channelsBits, continuationSuffixBits)
+  }
+
+  def hashDataChannel[C](channel: C, codeC: Codec[C]): Blake2b256Hash =
+    hashWithSuffix(codeC.encode(channel).get, dataSuffixBits)
+}
