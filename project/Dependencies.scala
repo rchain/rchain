241e7f52add2c649f92c3fbe7f38ef31e5388af2
9rb <rao.bhamidipati@gmail.com>
2020-11-20 04:20:35 -0500

    guava to 30.0-jre in Dependencies.scala
    Update guava to 30.0-jre in Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -33,7 +33,7 @@ object Dependencies {
   val disciplineCore      = "org.typelevel"              %% "discipline-core"           % "1.0.3"
   val enumeratum          = "com.beachape"               %% "enumeratum"                % enumeratumVersion
   val fs2Core             = "co.fs2"                     %% "fs2-core"                  % "2.4.4"
-  val guava               = "com.google.guava"            % "guava"                     % "24.1.1-jre"
+  val guava               = "com.google.guava"            % "guava"                     % "30.0-jre"
   val hasher              = "com.roundeights"            %% "hasher"                    % "1.2.0"
   val http4sBlazeClient   = "org.http4s"                 %% "http4s-blaze-client"       % http4sVersion
   val http4sBlazeServer   = "org.http4s"                 %% "http4s-blaze-server"       % http4sVersion
