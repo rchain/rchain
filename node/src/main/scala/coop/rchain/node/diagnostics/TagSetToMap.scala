5d96be93be6c72c805d33f9077a0d92316ab64a6
vb-cs <vbeecs@tutanota.com>
2021-01-07 16:44:42 -0500

    Fix formatting

--- node/src/main/scala/coop/rchain/node/diagnostics/TagSetToMap.scala
+++ node/src/main/scala/coop/rchain/node/diagnostics/TagSetToMap.scala
@@ -1,12 +1,13 @@
 package coop.rchain.node.diagnostics
 
 object TagSetToMap {
-    def tagSetToMap(tagSet: kamon.tag.TagSet): Map[String, String] = {
-        if(tagSet.nonEmpty()) {
-            tagSet.all().map((tag) => (tag.key, kamon.tag.Tag.unwrapValue(tag).asInstanceOf[String])).toMap
-        }
-        else{
-            Map.empty
-        }
+  def tagSetToMap(tagSet: kamon.tag.TagSet): Map[String, String] =
+    if (tagSet.nonEmpty()) {
+      tagSet
+        .all()
+        .map((tag) => (tag.key, kamon.tag.Tag.unwrapValue(tag).asInstanceOf[String]))
+        .toMap
+    } else {
+      Map.empty
     }
 }
