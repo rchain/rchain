b685a29e3a2e03c35e373b1496d7480cd95c31d8
Will Qiu <zsluedem06@gmail.com>
2021-01-12 15:22:20 +0800

    move LmdbKeyValueStore and LmdbStoreManager to shared, introduce introduce LmdbDirStoreManager and make it reusable

--- casper/src/main/scala/coop/rchain/casper/storage/RNodeKeyValueStoreManager.scala
+++ casper/src/main/scala/coop/rchain/casper/storage/RNodeKeyValueStoreManager.scala
@@ -6,29 +6,11 @@ import cats.effect.Concurrent
 import cats.effect.concurrent.{Deferred, Ref}
 import cats.syntax.all._
 import coop.rchain.shared.Log
-import coop.rchain.store.{KeyValueStore, KeyValueStoreManager}
+import coop.rchain.shared.store.LmdbDirStoreManager._
+import coop.rchain.shared.store.LmdbDirStoreManager
+import coop.rchain.store.KeyValueStoreManager
 
 object RNodeKeyValueStoreManager {
-  def apply[F[_]: Concurrent: Log](dirPath: Path): F[KeyValueStoreManager[F]] =
-    Concurrent[F].delay(new RNodeKeyValueStoreManager(dirPath))
-}
-
-// The idea for this class is to manage all instances of key-value databases for RNode.
-// For LMDB this allows control which databases are part of the same environment (file).
-private final case class RNodeKeyValueStoreManager[F[_]: Concurrent: Log](
-    dirPath: Path
-) extends KeyValueStoreManager[F] {
-
-  // Giga and tera bytes
-  val gb = 1024L * 1024L * 1024L
-  val tb = 1024 * gb
-
-  private case class LmdbEnvConfig(
-      name: String,
-      // Max LMDB environment (file) size
-      maxEnvSize: Long = 1 * gb
-  )
-
   // Config name is used as a sub-folder for LMDB files
   private val blockStorageEnvConfig = LmdbEnvConfig(name = "blockstorage", maxEnvSize = 1 * tb)
   private val dagStorageEnvConfig   = LmdbEnvConfig(name = "dagstorage", maxEnvSize = 100 * gb)
@@ -38,7 +20,7 @@ private final case class RNodeKeyValueStoreManager[F[_]: Concurrent: Log](
 
   // Database name to store instance name mapping (sub-folder for LMDB store)
   // - keys with the same instance will be in one LMDB file (environment)
-  private val dbInstanceMapping: Map[String, LmdbEnvConfig] = Map[String, LmdbEnvConfig](
+  private val rnodeDbMapping: Map[String, LmdbEnvConfig] = Map[String, LmdbEnvConfig](
     // Block storage
     ("blocks", blockStorageEnvConfig),
     // Block metadata storage
@@ -54,42 +36,23 @@ private final case class RNodeKeyValueStoreManager[F[_]: Concurrent: Log](
     // CasperBuffer
     ("parents-map", casperBufferEnvConfig)
   )
+  def rspaceDbMapping(mapSize: Long): Map[String, LmdbEnvConfig] =
+    Map[String, LmdbEnvConfig](
+      ("db-cold", LmdbEnvConfig(name = "rspace/casper/v2/cold", maxEnvSize = mapSize)),
+      ("db-history", LmdbEnvConfig(name = "rspace/casper/v2/history", maxEnvSize = mapSize)),
+      ("db-roots", LmdbEnvConfig(name = "rspace/casper/v2/roots", maxEnvSize = mapSize)),
+      ("db-eval-cold", LmdbEnvConfig(name = "rspace/v2/cold", maxEnvSize = mapSize)),
+      ("db-eval-history", LmdbEnvConfig(name = "rspace/v2/history", maxEnvSize = mapSize)),
+      ("db-eval-roots", LmdbEnvConfig(name = "rspace/v2/roots", maxEnvSize = mapSize)),
+      ("channels", LmdbEnvConfig(name = "channels", maxEnvSize = mapSize)),
+      ("eval-channels", LmdbEnvConfig(name = "eval-channels", maxEnvSize = mapSize))
+    )
 
-  private case class StoreState(
-      envs: Map[String, Deferred[F, KeyValueStoreManager[F]]]
-  )
+  // TODO remove legacy handle and migrate old rspace databases
+  // the legacy rspace databases use "db" by default
+  def legacyDBHandle(dbName: String, config: LmdbEnvConfig) =
+    if (config.name.contains("v2")) "db" else dbName
 
-  private val managersState = Ref.unsafe(StoreState(Map.empty))
-
-  // Creates database uniquely defined by the name
-  override def store(dbName: String): F[KeyValueStore[F]] =
-    for {
-      // Find DB ref / first time create deferred object
-      newDefer <- Deferred[F, KeyValueStoreManager[F]]
-      action <- managersState.modify { st =>
-                 val cfg @ LmdbEnvConfig(manName, _) = dbInstanceMapping(dbName)
-                 val (isNew, defer)                  = st.envs.get(manName).fold((true, newDefer))((false, _))
-                 val newSt                           = st.copy(envs = st.envs.updated(manName, defer))
-                 (newSt, (isNew, defer, cfg))
-               }
-      (isNew, defer, manCfg) = action
-      _                      <- createLmdbManger(manCfg, defer).whenA(isNew)
-      manager                <- defer.get
-      database               <- manager.store(dbName)
-    } yield database
-
-  private def createLmdbManger(config: LmdbEnvConfig, defer: Deferred[F, KeyValueStoreManager[F]]) =
-    for {
-      manager <- LmdbStoreManager(dirPath.resolve(config.name), config.maxEnvSize)
-      _       <- defer.complete(manager)
-    } yield ()
-
-  override def shutdown: F[Unit] = {
-    import cats.instances.vector._
-    for {
-      st <- managersState.get
-      // Shutdown all store managers
-      _ <- st.envs.values.toVector.traverse_(_.get.flatMap(_.shutdown))
-    } yield ()
-  }
+  def apply[F[_]: Concurrent: Log](dirPath: Path, rspaceMapSize: Long): F[KeyValueStoreManager[F]] =
+    LmdbDirStoreManager(dirPath, rnodeDbMapping ++ rspaceDbMapping(rspaceMapSize), legacyDBHandle)
 }
