79958a0dd7373eec5fdc2b1b0e4baa631d9c1541
Will Qiu <zsluedem06@gmail.com>
2021-02-18 13:36:35 +0800

    implement VaultBalanceGetter and tests

---
+++ casper/src/main/scala/coop/rchain/casper/util/rholang/VaultBalanceGetter.scala
@@ -0,0 +1,82 @@
+package coop.rchain.casper.util.rholang
+
+import cats.effect.Sync
+import cats.implicits._
+import com.google.protobuf.ByteString
+import coop.rchain.crypto.codec.Base16
+import coop.rchain.crypto.hash.Blake2b512Random
+import coop.rchain.models.Expr.ExprInstance.{GInt, GString}
+import coop.rchain.models.{Expr, GPrivate, Par, Send}
+import coop.rchain.models.rholang.implicits._
+import coop.rchain.rholang.interpreter.RhoRuntime
+import coop.rchain.rholang.interpreter.accounting.Cost
+
+import scala.util.Random
+
+object VaultBalanceGetter {
+
+  // hard-coded value in RevVault.rho
+  val genesisVaultMapDepth = 2
+  // The way to get this Unforgeable name needs reporting casper to get all the concrete comm events.
+  // Anyway, as long as RevVault.rho and genesis doesn't change, this value would be fixed.
+  val genesisVaultMapPar = GPrivate(
+    ByteString.copyFrom(
+      Base16.unsafeDecode("2dea7213bab34c96d4d3271f9b9dcb0b50a242de7de2177da615d9de23106afd")
+    )
+  )
+
+  def newReturnName: Par =
+    GPrivate(ByteString.copyFromUtf8(Random.alphanumeric.take(10).foldLeft("")(_ + _)))
+  def getBalancePar(vaultPar: Par, returnChannel: Par) =
+    Par(
+      sends = Seq(
+        Send(
+          chan = vaultPar,
+          data = Seq(
+            Par(exprs = Seq(Expr(GString("balance")))),
+            returnChannel
+          )
+        )
+      )
+    )
+
+  def getBalanceFromVaultPar[F[_]: Sync](vaultPar: Par, runtime: RhoRuntime[F]) =
+    for {
+      _             <- runtime.cost.set(Cost.UNSAFE_MAX)
+      ret           = VaultBalanceGetter.newReturnName
+      getBalancePar = VaultBalanceGetter.getBalancePar(vaultPar, ret)
+      _             <- runtime.inj(getBalancePar)(Blake2b512Random(10))
+      data          <- runtime.getData(ret)
+      result = data.headOption.flatMap(
+        d =>
+          d.a.pars match {
+            case headPar +: Nil =>
+              headPar.exprs match {
+                case headExpr +: Nil =>
+                  headExpr.exprInstance match {
+                    case GInt(i) => Some(i)
+                    case _       => None
+                  }
+                case _ => None
+              }
+            case _ => None
+          }
+      )
+    } yield result
+
+  def getAllVaultBalance[F[_]: Sync](runtime: RhoRuntime[F]): F[List[(ByteString, Long)]] =
+    for {
+      vaultMap <- RhoTrieTraverser.traverseTrie(genesisVaultMapDepth, genesisVaultMapPar, runtime)
+      extracted = RhoTrieTraverser.vecParMapToMap(
+        vaultMap,
+        p => p.exprs(0).getGByteArray,
+        p => p
+      )
+      result <- extracted.toList.traverse {
+                 case (key, vaultPar) =>
+                   for {
+                     balance <- getBalanceFromVaultPar(vaultPar, runtime)
+                   } yield (key, balance.get)
+               }
+    } yield result
+}
---
+++ casper/src/test/scala/coop/rchain/casper/util/rholang/VaultBalanceGetterTest.scala
@@ -0,0 +1,79 @@
+package coop.rchain.casper.util.rholang
+
+import cats.implicits._
+import com.google.protobuf.ByteString
+import coop.rchain.casper.helper.TestNode
+import coop.rchain.casper.helper.TestNode.Effect
+import coop.rchain.casper.util.GenesisBuilder.buildGenesis
+import coop.rchain.crypto.hash.Blake2b512Random
+import coop.rchain.p2p.EffectsTestInstances.LogicalTime
+import coop.rchain.rholang.interpreter.accounting.Cost
+import coop.rchain.rspace.Blake2b256Hash
+import org.scalatest.FlatSpec
+import monix.execution.Scheduler.Implicits.global
+import coop.rchain.rholang.interpreter.util.RevAddress
+
+class VaultBalanceGetterTest extends FlatSpec {
+  val genesis               = buildGenesis()
+  val genesisInitialBalance = 9000000
+  "Get balance from VaultPar" should "return balance" in {
+    val t = TestNode.standaloneEff(genesis).use { node =>
+      val genesisPostStateHash =
+        Blake2b256Hash.fromByteString(genesis.genesisBlock.body.state.postStateHash)
+      val genesisVaultAddr = RevAddress.fromPublicKey(genesis.genesisVaults.toList(0)._2).get
+      val getVault =
+        s"""new return, rl(`rho:registry:lookup`), RevVaultCh, vaultCh, balanceCh in {
+          |  rl!(`rho:rchain:revVault`, *RevVaultCh) |
+          |  for (@(_, RevVault) <- RevVaultCh) {
+          |    @RevVault!("findOrCreate", "${genesisVaultAddr.address.toBase58}", *vaultCh) |
+          |    for (@(true, vault) <- vaultCh) {
+          |      return!(vault)
+          |    }
+          |  }
+          |}
+          |""".stripMargin
+
+      for {
+        vaultPar <- node.runtimeManager
+                     .playExploratoryDeploy(getVault, genesis.genesisBlock.body.state.postStateHash)
+        _ <- node.runtimeManager.withRuntime(
+              runtime =>
+                for {
+                  _       <- runtime.reset(genesisPostStateHash)
+                  balance <- VaultBalanceGetter.getBalanceFromVaultPar(vaultPar(0), runtime)
+                  // 9000000 is hard coded in genesis block generation
+                  _ = assert(balance.get == genesisInitialBalance)
+                } yield ()
+            )
+      } yield ()
+    }
+    t.runSyncUnsafe()
+  }
+
+  "Get all vault" should "return all vault balance" in {
+    val t = TestNode.standaloneEff(genesis).use { node =>
+      val genesisPostStateHash =
+        Blake2b256Hash.fromByteString(genesis.genesisBlock.body.state.postStateHash)
+      for {
+        balances <- node.runtimeManager.withRuntime(
+                     runtime =>
+                       for {
+                         _      <- runtime.reset(genesisPostStateHash)
+                         result <- VaultBalanceGetter.getAllVaultBalance(runtime)
+                         // 9000000 is hard coded in genesis block generation
+                       } yield result
+                   )
+        balancesMap = balances.toMap
+        _ = genesis.genesisVaults
+          .map { case (_, pub) => RevAddress.fromPublicKey(pub).get }
+          .map(addr => RhoTrieTraverser.keccakParString(addr.address.toBase58).drop(2))
+          .map(
+            byteAddr =>
+              assert(balancesMap.get(ByteString.copyFrom(byteAddr)).get == genesisInitialBalance)
+          )
+      } yield ()
+    }
+    t.runSyncUnsafe()
+  }
+
+}
