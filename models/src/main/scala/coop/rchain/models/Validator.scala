b2e9b0a39b4e29c260bdd24383c5faedb6570808
nutzipper <1746367+nzpr@users.noreply.github.com>
2021-04-13 18:19:15 +0300

    add implicit ordering for ByteString based models

--- models/src/main/scala/coop/rchain/models/Validator.scala
+++ models/src/main/scala/coop/rchain/models/Validator.scala
@@ -1,9 +1,19 @@
 package coop.rchain.models
 
+import cats.Show
 import com.google.protobuf.ByteString
+import coop.rchain.crypto.codec.Base16
 
 object Validator {
   type Validator = ByteString
 
   val Length = 65
+
+  implicit def ordering: Ordering[Validator] =
+    Ordering.by((b: Validator) => b.toByteArray.toIterable)
+
+  implicit val show = new Show[Validator] {
+    def show(validator: Validator): String =
+      Base16.encode(validator.toByteArray).take(10)
+  }
 }
