606c87d944ec7e4080387d0b8bd7b961e122992e
nutzipper <1746367+nzpr@users.noreply.github.com>
2021-06-01 21:05:42 +0300

    syntax for bytestring based models

--- models/src/main/scala/coop/rchain/models/block/StateHash.scala
+++ models/src/main/scala/coop/rchain/models/block/StateHash.scala
@@ -1,13 +1,9 @@
 package coop.rchain.models.block
 
 import com.google.protobuf.ByteString
-import coop.rchain.crypto.codec.Base16
 
 object StateHash {
   type StateHash = ByteString
 
   val Length = 32
-  implicit class StateHashOps(bs: StateHash) {
-    def base16String: String = Base16.encode(bs.toByteArray)
-  }
 }
