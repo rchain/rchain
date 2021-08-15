2b82c0f3d0e26fb3f47f359ddb4918531efddf88
nutzipper <1746367+nzpr@users.noreply.github.com>
2021-08-15 21:26:36 +0300

    Tests

--- casper/src/test/scala/coop/rchain/casper/util/rholang/Resources.scala
+++ casper/src/test/scala/coop/rchain/casper/util/rholang/Resources.scala
@@ -97,9 +97,10 @@ object Resources {
 
       override def latestMessageHash(validator: Validator): F[Option[BlockHash]] = ???
 
-      override def latestMessageHashes: F[Map[Validator, BlockHash]] = ???
+      override def latestMessageHashes: F[Map[Validator, BlockHash]] =
+        Map.empty[Validator, BlockHash].pure[F]
 
-      override def invalidBlocks: F[Set[BlockMetadata]] = ???
+      override def invalidBlocks: F[Set[BlockMetadata]] = Set.empty[BlockMetadata].pure[F]
 
       override def latestBlockNumber: F[Long] = ???
 
