a8ac60da8cf15fd350ab9a5bda4f35fda1193d32
nutzipper <1746367+nzpr@users.noreply.github.com>
2021-09-27 14:16:16 +0300

    Message processing traits

---
+++ casper-v2/src/main/scala/coop/rchain/casper/v2/processing/MessageValidator.scala
@@ -0,0 +1,27 @@
+package coop.rchain.casper.v2.processing
+
+import coop.rchain.casper.v2.processing.MessageValidator.ValidationResult
+import fs2.Stream
+
+/** Validation of messages that have all dependencies validated. */
+trait MessageValidator[F[_], M, S] {
+
+  /**
+    * Input messages to be validated.
+    */
+  def input: Stream[F, M]
+
+  /**
+    * Validate the message.
+    */
+  def validate(message: M): F[ValidationResult[M, S]]
+
+  /**
+    * Validation of a message can trigger validation of a child, so being able to append to input is required.
+    */
+  def appendToInput(message: M): F[Unit]
+}
+
+object MessageValidator {
+  final case class ValidationResult[M, S](newState: S, dependentUnlocked: Set[M])
+}
