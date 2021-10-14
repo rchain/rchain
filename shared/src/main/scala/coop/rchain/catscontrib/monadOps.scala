95e73d65d63873098c6138bdc4c42b5edead21fc
Tomislav Grospic <grospic@gmail.com>
2021-10-14 09:34:24 +0200

    Additional comments for liftEitherT

--- shared/src/main/scala/coop/rchain/catscontrib/monadOps.scala
+++ shared/src/main/scala/coop/rchain/catscontrib/monadOps.scala
@@ -13,6 +13,21 @@ final class MonadOps[F[_], A](val self: F[A]) extends AnyVal {
 
   /* EitherT syntax */
 
+  /**
+    * Helper extension function to lift F[A] to EitherT.right(A).
+    *
+    * Because function requires only left type to be specified it can be used as a shorter expression
+    * which requires all three types specified.
+    *
+    * Example:
+    *     val fa = 42.pure[F]
+    *
+    *     // MyError cannot be inferred so all three types must be specified.
+    *     val e1: EitherT[F, MyError, Int] = EitherT.liftF[F, MyError, Int](fa)
+    *
+    *     // Instead fa can be lifted by specifying only left type.
+    *     val e2: EitherT[F, MyError, Int] = fa.liftEitherT[MyError]
+    */
   def liftEitherT[L](implicit f: Functor[F]): EitherT[F, L, A] = EitherT.liftF[F, L, A](self)
 }
 
