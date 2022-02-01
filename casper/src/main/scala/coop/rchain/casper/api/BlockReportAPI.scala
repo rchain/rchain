246a42779575c99b7aca24bbf9e131552a07f7b0
Tomislav Grospic <grospic@gmail.com>
2022-02-01 10:54:18 +0100

    Rename _get_ on KeyValueTypedStore to _get1_

--- casper/src/main/scala/coop/rchain/casper/api/BlockReportAPI.scala
+++ casper/src/main/scala/coop/rchain/casper/api/BlockReportAPI.scala
@@ -60,7 +60,7 @@ class BlockReportAPI[F[_]: Concurrent: Metrics: EngineCell: Log: SafetyOracle: B
       lock      = blockLockMap.getOrElseUpdate(b.blockHash, semaphore)
       result <- lock.withPermit(
                  for {
-                   cached <- reportStore.get(b.blockHash)
+                   cached <- reportStore.get1(b.blockHash)
                    res <- if (cached.isEmpty || forceReplay)
                            replayBlock(b).flatTap(reportStore.put(b.blockHash, _))
                          else
