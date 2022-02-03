3952afda1337df96b0f8bb2ebebaf4c2b90c0088
nutzipper <1746367+nzpr@users.noreply.github.com>
2022-02-03 15:47:13 +0300

    remove broken references

--- shared/src/main/scala/coop/rchain/sdk/finalization/syntax/package.scala
+++
@@ -1,3 +0,0 @@
-package coop.rchain.sdk.finalization
-
-package object syntax extends FinalizationDagViewSyntax
--- shared/src/main/scala/coop/rchain/sdk/simulation/syntax/package.scala
+++
@@ -1,3 +0,0 @@
-package coop.rchain.sdk.simulation
-
-package object syntax extends SimulationSyntax
