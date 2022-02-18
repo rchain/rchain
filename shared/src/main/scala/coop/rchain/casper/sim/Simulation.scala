045e79e600f440da41fbfe2c29521f53fd7c1cb9
nutzipper <1746367+nzpr@users.noreply.github.com>
2022-02-18 23:06:34 +0300

    Remove old code

--- shared/src/main/scala/coop/rchain/casper/sim/Simulation.scala
+++ shared/src/main/scala/coop/rchain/casper/sim/Simulation.scala
@@ -3,8 +3,6 @@ package coop.rchain.casper.sim
 import cats.effect.concurrent.Ref
 import cats.syntax.all._
 import cats.{Id, Monad, Show}
-import coop.rchain.casper.pcasper.Fringe.{Fringe, LazyReconciler}
-import coop.rchain.casper.pcasper.{Fringe, PCasper}
 import coop.rchain.casper.lcasper.{FinalityView, LevelView}
 import coop.rchain.shared.Stopwatch
 
