42df64011b7714e1060c0a3db84a71df47461b95
Will Qiu <zsluedem06@gmail.com>
2022-03-15 14:40:42 +0800

    update emptyStateHashFixed

--- casper/src/main/scala/coop/rchain/casper/util/rholang/RuntimeManager.scala
+++ casper/src/main/scala/coop/rchain/casper/util/rholang/RuntimeManager.scala
@@ -238,7 +238,7 @@ object RuntimeManager {
     * the time. For some situations, we can just use the value directly for better performance.
     */
   val emptyStateHashFixed: StateHash =
-    "9619d9a34bdaf56d5de8cfb7c2304d63cd9e469a0bfc5600fd2f5b9808e290f1".unsafeHexToByteString
+    "c77114aafbfbd7278a01fa39aff50cce2eda680183a2ee933fb7b07d449d6806".unsafeHexToByteString
 
   def apply[F[_]](implicit F: RuntimeManager[F]): F.type = F
 
