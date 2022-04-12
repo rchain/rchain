ed10be1e029e455e2a83486984442db276201d83
stanislavlyalin <lyalinstas@gmail.com>
2022-04-12 12:29:22 +0300

    Simplified getApprovedBlock implementation

--- block-storage/src/main/scala/coop/rchain/blockstorage/ApprovedStoreSyntax.scala
+++ block-storage/src/main/scala/coop/rchain/blockstorage/ApprovedStoreSyntax.scala
@@ -1,13 +1,7 @@
 package coop.rchain.blockstorage
 
 import cats.effect.Sync
-import cats.implicits.{
-  catsSyntaxApplicativeErrorId,
-  catsSyntaxApplicativeId,
-  toFlatMapOps,
-  toFunctorOps
-}
-import cats.syntax.all._
+import cats.implicits.{catsSyntaxApplicativeErrorId, catsSyntaxApplicativeId, toFlatMapOps}
 import coop.rchain.blockstorage.ApprovedStore.ApprovedStore
 import coop.rchain.blockstorage.dag.codecs.codecApprovedBlockHash
 import coop.rchain.casper.protocol.ApprovedBlock
@@ -27,13 +21,7 @@ final class ApprovedStoreOps[F[_]: Sync](
     private val approvedStore: ApprovedStore[F]
 ) {
   def getApprovedBlock: F[Option[ApprovedBlock]] =
-    for {
-      // Optional block message from the store
-      approvedBlockHash <- approvedBlockKeyAsHash
-      proto             <- approvedStore.get1(approvedBlockHash).map(_.map(_.toProto))
-      // Decode protobuf message / throw if fail
-      block <- proto.traverse(ApprovedBlock.from(_).leftMap(errorApprovedBlock).liftTo[F])
-    } yield block
+    approvedBlockKeyAsHash.flatMap(approvedStore.get1(_))
 
   def putApprovedBlock(block: ApprovedBlock): F[Unit] =
     approvedBlockKeyAsHash.flatMap(approvedStore.put(_, block))
@@ -47,10 +35,6 @@ final class ApprovedStoreOps[F[_]: Sync](
         err => new Exception(err.message).raiseError[F, BlockHash],
         _.pure[F]
       )
-
-  private def errorApprovedBlock(cause: String): BlockStoreFatalError = BlockStoreFatalError(
-    s"Approved block decoding error. Cause: $cause"
-  )
 
   case class BlockStoreFatalError(message: String) extends Exception(message)
 }
