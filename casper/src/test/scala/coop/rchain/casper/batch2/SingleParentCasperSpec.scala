28059101898866d181c9467fedb3f8a9a1127563
Tomislav Grospic <grospic@gmail.com>
2022-04-26 08:00:40 +0200

    Test SingleParentCasper cleanup

--- casper/src/test/scala/coop/rchain/casper/batch2/SingleParentCasperSpec.scala
+++ casper/src/test/scala/coop/rchain/casper/batch2/SingleParentCasperSpec.scala
@@ -1,18 +1,14 @@
 package coop.rchain.casper.batch2
 
-import cats.effect.Sync
 import cats.syntax.all._
 import coop.rchain.casper.helper.TestNode
 import coop.rchain.casper.helper.TestNode.Effect
 import coop.rchain.casper.protocol.DeployData
 import coop.rchain.casper.util.ConstructDeploy
 import coop.rchain.casper.util.GenesisBuilder.buildGenesis
-import coop.rchain.casper.{BlockStatus, ValidBlock, Validate}
 import coop.rchain.crypto.signatures.Signed
-import coop.rchain.metrics.NoopSpan
 import coop.rchain.p2p.EffectsTestInstances.LogicalTime
 import coop.rchain.shared.scalatestcontrib.effectTest
-import monix.eval.Task
 import monix.execution.Scheduler.Implicits.global
 import org.scalatest.{FlatSpec, Inspectors, Matchers}
 
@@ -42,6 +38,4 @@ class SingleParentCasperSpec extends FlatSpec with Matchers with Inspectors {
         } yield b3.header.parentsHashList.size shouldBe 1
     }
   }
-            implicit val s  = Sync[Task]
-            implicit val sp = NoopSpan[Task]
 }
