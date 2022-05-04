0857833d14f218700c1d2675640e3ca35a06b1c8
Will Qiu <zsluedem06@gmail.com>
2022-05-04 17:34:01 +0800

    add example for updating registry to multisig controlled

---
+++ casper/src/test/scala/coop/rchain/casper/genesis/RegistryUpdate.scala
@@ -0,0 +1,166 @@
+package coop.rchain.casper.genesis
+
+import cats.syntax.all._
+import com.google.protobuf.ByteString
+import coop.rchain.casper.helper.TestNode
+import coop.rchain.casper.helper.TestNode._
+import coop.rchain.casper.util.ConstructDeploy
+import coop.rchain.casper.rholang.Tools
+import coop.rchain.crypto.PrivateKey
+import coop.rchain.crypto.hash.Blake2b256
+import coop.rchain.crypto.signatures.Secp256k1
+import coop.rchain.models.Expr.ExprInstance.{ETupleBody, GUri}
+import coop.rchain.models.GUnforgeable.UnfInstance.GPrivateBody
+import coop.rchain.models.{ETuple, Expr, GPrivate, GUnforgeable, Par}
+import coop.rchain.p2p.EffectsTestInstances.LogicalTime
+import coop.rchain.shared.Base16
+import coop.rchain.shared.scalatestcontrib._
+import coop.rchain.models.syntax._
+import coop.rchain.rholang.interpreter.util.RevAddress
+import monix.execution.Scheduler.Implicits.global
+import org.scalatest.{FlatSpec, Inspectors, Matchers}
+
+import scala.io.Source
+
+class RegistryUpdate extends FlatSpec with Matchers with Inspectors {
+
+  import coop.rchain.casper.util.GenesisBuilder._
+
+  implicit val timeEff = new LogicalTime[Effect]
+
+  it should "update the testLib right" in effectTest {
+    val shardId = "root"
+    val p1 =
+      PrivateKey("fc743bd08a822d544bfbe05a5663fc325039a44c8f0c7fbea95a85517da5c36b".unsafeDecodeHex)
+    val pub1 = Secp256k1.toPublic(p1)
+    val p2 =
+      PrivateKey("6e88cf274735f3f7f73ec3d7f0362c439ab508427682b5bd788007aca665d810".unsafeDecodeHex)
+    val pub2 = Secp256k1.toPublic(p2)
+    val p3 =
+      PrivateKey("87369d132ed6a7626dc4c5dfbaf41e954dd0ec55830e613a3f868c74d64a7a22".unsafeDecodeHex)
+    val pub3          = Secp256k1.toPublic(p3)
+    val validatorKeys = Seq((p1, pub1), (p2, pub2), (p3, pub3))
+    val genesisVaults = Seq((p1, 100000000000L), (p2, 100000000000L), (p3, 100000000000L))
+    val bonds         = Map(pub1 -> 1000000L, pub2 -> 1000000L, pub3 -> 1000000L)
+    val genesis       = buildGenesis(buildGenesisParameters(validatorKeys, genesisVaults, bonds))
+
+    val proposeTimestamp = 1648038427475L
+    val agreeTimestamp   = 1648038427476L
+    val updateTimestamp  = 1648038427477L
+    val proposeRnd       = Tools.unforgeableNameRng(pub1, proposeTimestamp)
+    val propose          = Source.fromResource("updateRegistry/ProposeAuthKey.rho").mkString
+    val agree            = Source.fromResource("updateRegistry/AgreeAuthKey.rho").mkString
+    val update           = Source.fromResource("updateRegistry/UpdateAuthKey.rho").mkString
+
+    val uri = Par(exprs = Seq(Expr(GUri("rho:rchain:authKey"))))
+    val updateCon = Par(
+      unforgeables =
+        Seq(GUnforgeable(GPrivateBody(GPrivate(ByteString.copyFrom(proposeRnd.next())))))
+    )
+    val newContract =
+      Par(
+        unforgeables =
+          Seq(GUnforgeable(GPrivateBody(GPrivate(ByteString.copyFrom(proposeRnd.next())))))
+      )
+    val signedTarget = Par(exprs = Seq(Expr(ETupleBody(ETuple(Seq(uri, updateCon, newContract))))))
+
+    val proposeDeploy =
+      ConstructDeploy.sourceDeploy(propose, proposeTimestamp, 100000000L, 1L, p1, shardId = shardId)
+    val proposeSig = Secp256k1.sign(Blake2b256.hash(signedTarget.toByteArray), p1)
+    //    println(proposeDeploy)
+    println(Base16.encode(proposeSig))
+
+    val agreeDeploy =
+      ConstructDeploy.sourceDeploy(agree, agreeTimestamp, 100000000L, 1L, p2, shardId = shardId)
+    val agreeSig = Secp256k1.sign(Blake2b256.hash(signedTarget.toByteArray), p2)
+
+    val updateDeploy =
+      ConstructDeploy.sourceDeploy(update, updateTimestamp, 100000000L, 1L, p1, shardId = shardId)
+    println(Base16.encode(agreeSig))
+
+    val updateRegistry = Source.fromResource("updateRegistry/updateRegistry.rho").mkString
+    val updateRegistryDeploy =
+      ConstructDeploy.sourceDeployNow(updateRegistry, p1, 100000000L, 0, shardId = shardId)
+    val transferAmount = 100000
+
+    val pub1RevAddr = RevAddress.fromPublicKey(pub1).get.toBase58
+    val pub2RevAddr = RevAddress.fromPublicKey(pub2).get.toBase58
+
+    val transferTerm =
+      s"""
+         #new rl(`rho:registry:lookup`), RevVaultCh, vaultCh, toVaultCh, deployerId(`rho:rchain:deployerId`), stdout(`rho:io:stdout`),revVaultKeyCh, resultCh in {
+         #  rl!(`rho:rchain:revVault`, *RevVaultCh) |
+         #  for (@(_, RevVault) <- RevVaultCh) {
+         #    @RevVault!("findOrCreate", "${pub1RevAddr}", *vaultCh) |
+         #    @RevVault!("findOrCreate", "${pub2RevAddr}", *toVaultCh) |
+         #    @RevVault!("deployerAuthKey", *deployerId, *revVaultKeyCh) |
+         #    for (@(true, vault) <- vaultCh; key <- revVaultKeyCh; @(true, toVault) <- toVaultCh) {
+         #      @vault!("transfer", "${pub2RevAddr}", $transferAmount, *key, *resultCh) |
+         #      for (@res <- resultCh) { stdout!(("outcome", res)) }
+         #    }
+         #  }
+         #}""".stripMargin('#')
+
+    val getBalanceTerm =
+      s"""new return, rl(`rho:registry:lookup`), RevVaultCh, vaultCh, balanceCh in {
+         #  rl!(`rho:rchain:revVault`, *RevVaultCh) |
+         #  for (@(_, RevVault) <- RevVaultCh) {
+         #    @RevVault!("findOrCreate", "${pub2RevAddr}", *vaultCh) |
+         #    for (@(true, vault) <- vaultCh) {
+         #      @vault!("balance", *balanceCh) |
+         #      for (@balance <- balanceCh) {
+         #        return!(balance)
+         #      }
+         #    }
+         #  }
+         #}
+         #""".stripMargin('#')
+    val exploreUpdateResultTerm =
+      """new return, registryLookup(`rho:registry:lookup`), resCh,stdout(`rho:io:stdout`), ret in {
+        #  registryLookup!(`rho:rchain:authKey`, *resCh) |
+        #  for (@(nonce, authKey) <- resCh){
+        #    @authKey!("add", 100, *ret)|
+        #    stdout!((authKey, nonce))|
+        #    for (@number <- ret){
+        #      return!(number)
+        #    }
+        #  }
+        #}""".stripMargin('#')
+    TestNode.standaloneEff(genesis).use { node =>
+      val rm = node.runtimeManager
+      for {
+        b1      <- node.addBlock(updateRegistryDeploy)
+        _       = assert(b1.body.deploys.head.cost.cost > 0L, s"$b1 deploy cost is 0L")
+        _       = assert(b1.body.deploys.head.systemDeployError.isEmpty, s"$b1 system deploy failed")
+        _       = assert(!b1.body.deploys.head.isFailed, s"$b1 deploy failed")
+        b2      <- node.addBlock(proposeDeploy)
+        _       = assert(b2.body.deploys.head.cost.cost > 0L, s"$b2 deploy cost is 0L")
+        _       = assert(b2.body.deploys.head.systemDeployError.isEmpty, s"$b2 system deploy failed")
+        _       = assert(!b2.body.deploys.head.isFailed, s"$b2 deploy failed")
+        b3      <- node.addBlock(agreeDeploy)
+        _       = assert(b3.body.deploys.head.cost.cost > 0L, s"$b3 deploy cost is 0L")
+        _       = assert(b3.body.deploys.head.systemDeployError.isEmpty, s"$b3 system deploy failed")
+        _       = assert(!b3.body.deploys.head.isFailed, s"$b3 deploy failed")
+        b4      <- node.addBlock(updateDeploy)
+        _       = assert(b4.body.deploys.head.cost.cost > 0L, s"$b4 deploy cost is 0L")
+        _       = assert(b4.body.deploys.head.systemDeployError.isEmpty, s"$b4 system deploy failed")
+        _       = assert(!b4.body.deploys.head.isFailed, s"$b4 deploy failed")
+        ret     <- rm.playExploratoryDeploy(getBalanceTerm, b4.body.state.postStateHash)
+        balance = ret.head.exprs.head.getGInt
+        b5 <- node
+               .addBlock(
+                 ConstructDeploy
+                   .sourceDeployNow(transferTerm, sec = p1, shardId = shardId, phloLimit = 900000L)
+               )
+        _    = assert(b5.body.deploys.head.cost.cost > 0L, s"$b5 deploy cost is 0L")
+        _    = assert(b5.body.deploys.head.systemDeployError.isEmpty, s"$b5 system deploy failed")
+        _    = assert(!b5.body.deploys.head.isFailed, s"$b5 deploy failed")
+        ret2 <- rm.playExploratoryDeploy(getBalanceTerm, b5.body.state.postStateHash)
+        _    = assert(ret2.head.exprs.head.getGInt == balance + transferAmount.toLong)
+        ret3 <- rm.playExploratoryDeploy(exploreUpdateResultTerm, b5.body.state.postStateHash)
+        _    = assert(ret3.head.exprs.head.getGInt == 100)
+      } yield ()
+    }
+  }
+
+}
