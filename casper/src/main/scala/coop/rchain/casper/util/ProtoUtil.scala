311b905b7055ce084caff4df0d0dacb3d0eb47b4
Tomislav Grospic <grospic@gmail.com>
2022-05-09 20:07:20 +0200

    Remove unused justification helper functions

--- casper/src/main/scala/coop/rchain/casper/util/ProtoUtil.scala
+++ casper/src/main/scala/coop/rchain/casper/util/ProtoUtil.scala
@@ -10,19 +10,13 @@ import coop.rchain.blockstorage.syntax._
 import coop.rchain.casper.PrettyPrinter
 import coop.rchain.casper.protocol._
 import coop.rchain.crypto.hash.Blake2b256
-import coop.rchain.dag.DagOps
 import coop.rchain.models.BlockHash.BlockHash
 import coop.rchain.models.Validator.Validator
 import coop.rchain.models._
 
-import scala.collection.immutable
-
 object ProtoUtil {
 
   def creatorJustification(block: BlockMessage): Option[Justification] =
-    block.justifications.find(_.validator == block.sender)
-
-  def creatorJustification(block: BlockMetadata): Option[Justification] =
     block.justifications.find(_.validator == block.sender)
 
   def parentHashes(b: BlockMessage): List[ByteString] =
@@ -85,34 +79,6 @@ object ProtoUtil {
       case (validator, blockMetadata) => Justification(validator, blockMetadata.blockHash)
     }
 
-  def toLatestMessageHashes(
-      justifications: Seq[Justification]
-  ): immutable.Map[Validator, BlockHash] =
-    justifications.foldLeft(Map.empty[Validator, BlockHash]) {
-      case (acc, Justification(validator, block)) =>
-        acc.updated(validator, block)
-    }
-
-  def toLatestMessage[F[_]: Sync: BlockDagStorage](
-      justifications: Seq[Justification],
-      dag: DagRepresentation
-  ): F[immutable.Map[Validator, BlockMetadata]] =
-    justifications.toList.foldM(Map.empty[Validator, BlockMetadata]) {
-      case (acc, Justification(validator, hash)) =>
-        for {
-          blockMetadataOpt <- dag.lookup(hash)
-          blockMetadata <- blockMetadataOpt
-                            .map(_.pure[F])
-                            .getOrElse(
-                              Sync[F].raiseError(
-                                new RuntimeException(
-                                  s"Could not find a block for ${PrettyPrinter.buildString(hash)} in the DAG storage"
-                                )
-                              )
-                            )
-        } yield acc.updated(validator, blockMetadata)
-    }
-
   def hashByteArrays(items: Array[Byte]*): ByteString =
     ByteString.copyFrom(Blake2b256.hash(Array.concat(items: _*)))
 
@@ -170,85 +136,4 @@ object ProtoUtil {
       .toSet
     (missingParents union missingJustifications).toList
   }
-
-  // Return hashes of all blocks that are yet to be seen by the passed in block
-  def unseenBlockHashes[F[_]: Sync: BlockDagStorage](
-      dag: DagRepresentation,
-      block: BlockMessage
-  ): F[Set[BlockHash]] =
-    for {
-      dagsLatestMessages   <- dag.latestMessages
-      blocksLatestMessages <- toLatestMessage(block.justifications, dag)
-
-      // From input block perspective we want to find what latest messages are not seen
-      //  that are in the DAG latest messages.
-      // - if validator is not in the justification of the block
-      // - if justification contains validator's newer latest message
-      unseenLatestMessages = dagsLatestMessages.filter {
-        case (validator, dagLatestMessage) =>
-          val validatorInJustification = blocksLatestMessages.contains(validator)
-          def blockHasNewerLatestMessage =
-            blocksLatestMessages.get(validator).map(dagLatestMessage.seqNum > _.seqNum)
-
-          !validatorInJustification || (validatorInJustification && blockHasNewerLatestMessage.get)
-      }
-
-      unseenBlockHashes <- unseenLatestMessages.toStream
-                            .traverse {
-                              case (validator, unseenLatestMessage) =>
-                                getCreatorBlocksBetween(
-                                  dag,
-                                  unseenLatestMessage,
-                                  blocksLatestMessages.get(validator)
-                                )
-                            }
-                            .map(_.flatten.toSet)
-    } yield unseenBlockHashes -- blocksLatestMessages.values.map(_.blockHash) - block.blockHash
-
-  private def getCreatorBlocksBetween[F[_]: Sync: BlockDagStorage](
-      dag: DagRepresentation,
-      topBlock: BlockMetadata,
-      bottomBlock: Option[BlockMetadata]
-  ): F[Set[BlockHash]] =
-    bottomBlock match {
-      case None => Set(topBlock.blockHash).pure[F]
-      case Some(bottomBlock) =>
-        DagOps
-          .bfTraverseF(List(topBlock))(
-            nextCreatorBlock =>
-              getCreatorJustificationUnlessGoal(
-                dag,
-                nextCreatorBlock,
-                bottomBlock
-              )
-          )
-          .map(_.blockHash)
-          .toSet
-    }
-
-  private def getCreatorJustificationUnlessGoal[F[_]: Sync: BlockDagStorage](
-      dag: DagRepresentation,
-      block: BlockMetadata,
-      goal: BlockMetadata
-  ): F[List[BlockMetadata]] =
-    creatorJustification(block) match {
-      case Some(Justification(_, hash)) =>
-        dag.lookup(hash).flatMap {
-          case Some(creatorJustification) =>
-            if (creatorJustification == goal) {
-              List.empty[BlockMetadata].pure
-            } else {
-              List(creatorJustification).pure
-            }
-          case None =>
-            Sync[F].raiseError[List[BlockMetadata]](
-              new RuntimeException(
-                s"BlockDAG is missing justification ${PrettyPrinter
-                  .buildString(hash)} for ${PrettyPrinter.buildString(block.blockHash)}."
-              )
-            )
-        }
-      case None =>
-        List.empty[BlockMetadata].pure
-    }
 }
--- casper/src/test/scala/coop/rchain/casper/util/ProtoUtilTest.scala
+++ casper/src/test/scala/coop/rchain/casper/util/ProtoUtilTest.scala
@@ -1,13 +1,6 @@
 package coop.rchain.casper.util
 
-import cats.syntax.all._
-import coop.rchain.casper.helper.TestNode
-import coop.rchain.casper.helper.TestNode.Effect
-import coop.rchain.models.BlockHash.BlockHash
 import coop.rchain.models.blockImplicits._
-import coop.rchain.p2p.EffectsTestInstances.LogicalTime
-import coop.rchain.shared.scalatestcontrib._
-import monix.execution.Scheduler.Implicits.global
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
@@ -24,45 +17,6 @@ class ProtoUtilTest extends AnyFlatSpec with Matchers with ScalaCheckDrivenPrope
       result should contain allElementsOf (justificationsHashes)
       result should contain allElementsOf (parentsHashes)
       result should contain theSameElementsAs ((justificationsHashes ++ parentsHashes).toSet)
-    }
-  }
-
-  import GenesisBuilder._
-
-  implicit val timeEff = new LogicalTime[Effect]
-
-  val genesis = buildGenesis()
-
-  "unseenBlockHashes" should "return empty for a single block dag" in effectTest {
-    TestNode.standaloneEff(genesis).use { node =>
-      import node.{blockDagStorage, blockStore}
-      for {
-        signedBlock <- ConstructDeploy.basicDeployData[Effect](
-                        0,
-                        shardId = genesis.genesisBlock.shardId
-                      ) >>= (node.addBlock(_))
-        dag               <- node.blockDagStorage.getRepresentation
-        unseenBlockHashes <- ProtoUtil.unseenBlockHashes[Effect](dag, signedBlock)
-        _                 = unseenBlockHashes should be(Set.empty[BlockHash])
-      } yield ()
-    }
-  }
-
-  it should "return all but the first block when passed the first block in a chain" in effectTest {
-    TestNode.standaloneEff(genesis).use { node =>
-      import node.{blockDagStorage, blockStore}
-      val shardId = this.genesis.genesisBlock.shardId
-
-      for {
-        block0 <- ConstructDeploy
-                   .basicDeployData[Effect](0, shardId = shardId) >>= (node
-                   .addBlock(_))
-        block1 <- ConstructDeploy
-                   .basicDeployData[Effect](1, shardId = shardId) >>= (node.addBlock(_))
-        dag               <- node.blockDagStorage.getRepresentation
-        unseenBlockHashes <- ProtoUtil.unseenBlockHashes[Effect](dag, block0)
-        _                 = unseenBlockHashes should be(Set(block1.blockHash))
-      } yield ()
     }
   }
 }
