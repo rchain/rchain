48cc99f014911f3f05cd6a89edf9ccc6a352cc68
maximus19910112 <max_permakov1991@mail.ru>
2022-05-25 16:39:05 +0300

    1) Use Blake2b256Hash.codecPureBlake2b256Hash instead of my codec 2) Optimized imports 3) Refactoring of the code

--- rholang/src/main/scala/BlockRandomSeed.scala
+++ rholang/src/main/scala/BlockRandomSeed.scala
@@ -1,10 +1,9 @@
 import coop.rchain.crypto.PublicKey
-import coop.rchain.crypto.hash.Blake2b256.hash
 import coop.rchain.crypto.hash.Blake2b512Random
 import coop.rchain.rspace.hashing.Blake2b256Hash
 import scodec.bits.ByteVector
+import scodec.codecs.{bytes, uint8, ulong, utf8, variableSizeBytes}
 import scodec.{Codec, TransformSyntax}
-import scodec.codecs.{bytes, uint16, uint8, ulong, utf8, variableSizeBytes}
 
 final case class BlockRandomSeed(
     shardId: String,
@@ -17,18 +16,13 @@ final case class BlockRandomSeed(
 object BlockRandomSeed {
   private val codecPublicKey: Codec[PublicKey] = variableSizeBytes(uint8, bytes)
     .xmap[PublicKey](bv => PublicKey(bv.toArray), pk => ByteVector(pk.bytes))
-  private val codecBlakeHash: Codec[Blake2b256Hash] =
-    variableSizeBytes(uint16, bytes)
-      .xmap[Blake2b256Hash](bv => Blake2b256Hash.fromByteVector(bv), _.bytes)
 
   private val codecBlockRandomSeed: Codec[BlockRandomSeed] = (utf8 :: ulong(bits = 64) ::
-    codecPublicKey :: codecPublicKey :: codecBlakeHash).as[BlockRandomSeed]
+    codecPublicKey :: codecPublicKey :: Blake2b256Hash.codecPureBlake2b256Hash).as[BlockRandomSeed]
 
   private def encode(blockRandomSeed: BlockRandomSeed): Array[Byte] =
     codecBlockRandomSeed.encode(blockRandomSeed).require.toByteArray
 
-  def randomSeed(blockRandomSeed: BlockRandomSeed): Blake2b512Random = {
-    val serialized = encode(blockRandomSeed)
-    Blake2b512Random(serialized)
-  }
+  def generateRandomNumber(blockRandomSeed: BlockRandomSeed): Blake2b512Random =
+    Blake2b512Random(encode(blockRandomSeed))
 }
