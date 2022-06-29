0234e8b74c97ebed1d1a5aeec4020894c3b83e51
stanislavlyalin <lyalinstas@gmail.com>
2022-06-29 12:56:17 +0300

    Test for conversion RhoExpr <-> Par implemented as property-based with ScalaCheck

--- node/src/test/scala/coop/rchain/node/RhoExprToParSpec.scala
+++ node/src/test/scala/coop/rchain/node/RhoExprToParSpec.scala
@@ -3,38 +3,122 @@ package coop.rchain.node
 import cats.syntax.all._
 import com.google.protobuf.ByteString
 import coop.rchain.models.syntax._
+import coop.rchain.models.testImplicits._
 import coop.rchain.node.api.WebApi._
-import org.scalatest.wordspec.AnyWordSpec
+import org.scalacheck.Arbitrary
+import org.scalacheck.Arbitrary._
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.propspec.AnyPropSpec
+import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 
-class RhoExprToParSpec extends AnyWordSpec with Matchers {
+class RhoExprToParSpec extends AnyPropSpec with ScalaCheckDrivenPropertyChecks with Matchers {
+
   private def roundTripTest(expr: RhoExpr): Option[RhoExpr] =
     exprFromParProto(rhoExprToParProto(expr))
 
-  "WebAPI RhoExpr <-> Par conversion" should {
-    "work correct with simple types" in {
-      roundTripTest(ExprBool(true)).collect { case ExprBool(data)            => data } shouldBe true.some
-      roundTripTest(ExprInt(42)).collect { case ExprInt(data)                => data } shouldBe 42.some
-      roundTripTest(ExprString("abc")).collect { case ExprString(data)       => data } shouldBe "abc".some
-      roundTripTest(ExprUri("http://test.com")).collect { case ExprUri(data) => data } shouldBe "http://test.com".some
+  // Simple types
 
-      val bytesAsString = ByteString.copyFrom(Array[Byte](1, 2, 3)).toHexString
-      roundTripTest(ExprBytes(bytesAsString)).collect { case ExprBytes(data) => data } shouldBe bytesAsString.some
-      roundTripTest(ExprUnforg(UnforgDeploy(bytesAsString))).collect {
-        case ExprUnforg(data) => data
-      } shouldBe UnforgDeploy(
-        bytesAsString
-      ).some
+  property("WebAPI ExprBool <-> Par conversion work correct") {
+    forAll { b: Boolean =>
+      roundTripTest(ExprBool(b)).collect { case ExprBool(data) => data } shouldBe b.some
     }
-    "work correct with nested types" in {
-      val listExpr = List[RhoExpr](ExprBool(true), ExprInt(42))
-      roundTripTest(ExprPar(listExpr)).collect { case ExprPar(data)     => data } shouldBe listExpr.some
-      roundTripTest(ExprTuple(listExpr)).collect { case ExprTuple(data) => data } shouldBe listExpr.some
-      roundTripTest(ExprList(listExpr)).collect { case ExprList(data)   => data } shouldBe listExpr.some
-      roundTripTest(ExprSet(listExpr)).collect { case ExprSet(data)     => data } shouldBe listExpr.some
+  }
 
-      val mapExpr =
-        Map[String, RhoExpr]("a" -> ExprBool(true), "b" -> ExprInt(42), "c" -> ExprList(listExpr))
+  property("WebAPI ExprInt <-> Par conversion work correct") {
+    forAll { i: Long =>
+      roundTripTest(ExprInt(i)).collect { case ExprInt(data) => data } shouldBe i.some
+    }
+  }
+
+  property("WebAPI ExprString <-> Par conversion work correct") {
+    forAll { s: String =>
+      roundTripTest(ExprString(s)).collect { case ExprString(data) => data } shouldBe s.some
+    }
+  }
+
+  property("WebAPI ExprUri <-> Par conversion work correct") {
+    forAll { uri: String =>
+      roundTripTest(ExprUri(uri)).collect { case ExprUri(data) => data } shouldBe uri.some
+    }
+  }
+
+  property("WebAPI ExprUnforg <-> Par conversion work correct") {
+    forAll { bs: ByteString =>
+      roundTripTest(ExprBytes(bs.toHexString)).collect { case ExprBytes(data) => data } shouldBe bs.toHexString.some
+    }
+  }
+
+  property("WebAPI ExprBytes <-> Par conversion work correct") {
+    forAll { bs: ByteString =>
+      roundTripTest(ExprUnforg(UnforgDeploy(bs.toHexString))).collect {
+        case ExprUnforg(data) => data
+      } shouldBe UnforgDeploy(bs.toHexString).some
+    }
+  }
+
+  // Nested types
+
+  implicit val listRhoExprArb: Arbitrary[List[RhoExpr]] = Arbitrary(
+    for {
+      bool <- arbBool.arbitrary
+      long <- arbLong.arbitrary
+      str  <- arbString.arbitrary
+      uri  <- arbString.arbitrary
+      bs   <- arbByteArray.arbitrary
+    } yield List[RhoExpr](
+      ExprBool(bool),
+      ExprInt(long),
+      ExprString(str),
+      ExprUri(uri),
+      ExprBytes(bs.toHexString)
+    )
+  )
+
+  property("WebAPI ExprPar <-> Par conversion work correct") {
+    forAll { listExpr: List[RhoExpr] =>
+      roundTripTest(ExprPar(listExpr)).collect { case ExprPar(data) => data } shouldBe listExpr.some
+    }
+  }
+
+  property("WebAPI ExprTuple <-> Par conversion work correct") {
+    forAll { listExpr: List[RhoExpr] =>
+      roundTripTest(ExprTuple(listExpr)).collect { case ExprTuple(data) => data } shouldBe listExpr.some
+    }
+  }
+
+  property("WebAPI ExprList <-> Par conversion work correct") {
+    forAll { listExpr: List[RhoExpr] =>
+      roundTripTest(ExprList(listExpr)).collect { case ExprList(data) => data } shouldBe listExpr.some
+    }
+  }
+
+  property("WebAPI ExprSet <-> Par conversion work correct") {
+    forAll { listExpr: List[RhoExpr] =>
+      roundTripTest(ExprSet(listExpr)).collect { case ExprSet(data) => data } shouldBe listExpr.some
+    }
+  }
+
+  implicit val mapRhoExprArb: Arbitrary[Map[String, RhoExpr]] = Arbitrary(
+    for {
+      bool <- arbBool.arbitrary
+      long <- arbLong.arbitrary
+      str  <- arbString.arbitrary
+      uri  <- arbString.arbitrary
+      bs   <- arbByteArray.arbitrary
+      list <- listRhoExprArb.arbitrary
+      keys = (1 to 6).map(_ => arbString.arbitrary.sample.get)
+    } yield Map[String, RhoExpr](
+      keys(0) -> ExprBool(bool),
+      keys(1) -> ExprInt(long),
+      keys(2) -> ExprString(str),
+      keys(3) -> ExprUri(uri),
+      keys(4) -> ExprBytes(bs.toHexString),
+      keys(5) -> ExprList(list)
+    )
+  )
+
+  property("WebAPI ExprMap <-> Par conversion work correct") {
+    forAll { mapExpr: Map[String, RhoExpr] =>
       roundTripTest(ExprMap(mapExpr)).collect { case ExprMap(data) => data } shouldBe mapExpr.some
     }
   }
