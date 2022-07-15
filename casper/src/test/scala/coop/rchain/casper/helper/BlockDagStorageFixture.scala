46591e5d088c38dc040bdac51b4ee3e0bb146f51
Tomislav Grospic <grospic@gmail.com>
2022-07-15 11:40:20 +0200

    Remove unused and simplify code from BlockDagStorageFixture

--- casper/src/test/scala/coop/rchain/casper/helper/BlockDagStorageFixture.scala
+++ casper/src/test/scala/coop/rchain/casper/helper/BlockDagStorageFixture.scala
@@ -1,36 +1,30 @@
 package coop.rchain.casper.helper
 
-import cats.effect.Concurrent
+import cats.effect.{Concurrent, Resource}
 import cats.syntax.all._
 import coop.rchain.blockstorage.BlockStore
 import coop.rchain.blockstorage.BlockStore.BlockStore
 import coop.rchain.blockstorage.dag.BlockDagStorage
 import coop.rchain.casper.BlockRandomSeed
 import coop.rchain.casper.dag.BlockDagKeyValueStorage
-import coop.rchain.casper.genesis.Genesis
 import coop.rchain.casper.rholang.{Resources, RuntimeManager}
-import coop.rchain.casper.storage.RNodeKeyValueStoreManager
 import coop.rchain.casper.util.GenesisBuilder.GenesisContext
-import coop.rchain.catscontrib.TaskContrib.TaskOps
 import coop.rchain.metrics.Metrics
 import coop.rchain.metrics.Metrics.MetricsNOP
 import coop.rchain.rholang
 import coop.rchain.shared.Log
 import monix.eval.Task
-import monix.execution.Scheduler
+import monix.execution.Scheduler.Implicits.global
 import org.scalatest.{BeforeAndAfter, Suite}
 
-import java.nio.file.{Files, Path}
+import java.nio.file.Path
 
 trait BlockDagStorageFixture extends BeforeAndAfter { self: Suite =>
-  val scheduler = Scheduler.fixedPool("block-dag-storage-fixture-scheduler", 4)
-
   val dummyParentsPreState = BlockGenerator.dummyParentsPreState
 
   def withGenesis[R](
       context: GenesisContext
   )(f: BlockStore[Task] => BlockDagStorage[Task] => RuntimeManager[Task] => Task[R]): R = {
-    implicit val s       = scheduler
     implicit val metrics = new MetricsNOP[Task]()
     implicit val log     = Log.log[Task]
 
@@ -50,43 +44,21 @@ trait BlockDagStorageFixture extends BeforeAndAfter { self: Suite =>
       .copyStorage[Task](context.storageDirectory)
       .evalMap(create)
       .use(Function.uncurried(f).tupled)
-      .unsafeRunSync
+      .runSyncUnsafe()
   }
 
   def withStorage[R](f: BlockStore[Task] => BlockDagStorage[Task] => Task[R]): R = {
-    implicit val s       = scheduler
     implicit val metrics = new MetricsNOP[Task]()
     implicit val log     = Log.log[Task]
 
-    BlockDagStorageTestFixture.withStorageF[Task, R](f).unsafeRunSync
+    BlockDagStorageTestFixture.withStorageF[Task].use(Function.uncurried(f).tupled).runSyncUnsafe()
   }
 }
 
 object BlockDagStorageTestFixture {
-  def blockDagStorageDir: Path = Files.createTempDirectory("casper-block-dag-storage-test-")
-  def blockStorageDir: Path    = Files.createTempDirectory("casper-block-storage-test-")
 
-  val mapSize: Long = 1024L * 1024L * 1024L
-  def createBlockStorage[F[_]: Concurrent: Log](
-      blockStorageDir: Path
-  ): F[BlockStore[F]] =
-    for {
-      storeManager <- RNodeKeyValueStoreManager[F](blockStorageDir)
-      blockStore   <- BlockStore[F](storeManager)
-    } yield blockStore
-
-  def createBlockDagStorage[F[_]: Concurrent](blockDagStorageDir: Path)(
-      implicit log: Log[F],
-      metrics: Metrics[F]
-  ): F[BlockDagStorage[F]] =
-    for {
-      storeManager    <- RNodeKeyValueStoreManager[F](blockDagStorageDir)
-      blockDagStorage <- BlockDagKeyValueStorage.create[F](storeManager)
-    } yield blockDagStorage
-
-  def withStorageF[F[_]: Concurrent: Metrics: Log, R](
-      f: BlockStore[F] => BlockDagStorage[F] => F[R]
-  ): F[R] = {
+  def withStorageF[F[_]: Concurrent: Metrics: Log]
+      : Resource[F, (BlockStore[F], BlockDagStorage[F])] = {
     def create(dir: Path) =
       for {
         kvm        <- Resources.mkTestRNodeStoreManager[F](dir)
@@ -98,6 +70,5 @@ object BlockDagStorageTestFixture {
     rholang.Resources
       .mkTempDir[F]("casper-block-dag-storage-test-")
       .evalMap(create)
-      .use(Function.uncurried(f).tupled)
   }
 }
