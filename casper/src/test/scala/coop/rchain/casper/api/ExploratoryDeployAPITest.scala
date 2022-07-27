54937e678b1ddc6eca3f3d124923552473cbd3c7
stanislavlyalin <lyalinstas@gmail.com>
2022-07-27 12:42:49 +0300

    Fixed tests for exploratory deploy API

--- casper/src/test/scala/coop/rchain/casper/api/ExploratoryDeployAPITest.scala
+++ casper/src/test/scala/coop/rchain/casper/api/ExploratoryDeployAPITest.scala
@@ -1,35 +1,39 @@
 package coop.rchain.casper.api
 
-import cats.effect.{Concurrent, Sync}
+import cats.effect.Concurrent
 import cats.syntax.all._
 import com.google.protobuf.ByteString
 import coop.rchain.blockstorage.BlockStore.BlockStore
-import coop.rchain.blockstorage.dag.BlockDagStorage
-import coop.rchain.casper.helper.BlockGenerator._
+import coop.rchain.blockstorage.dag.{BlockDagStorage, DagMessageState, DagRepresentation, Message}
+import coop.rchain.casper.api.BlockApi.ApiErr
 import coop.rchain.casper.helper.{BlockApiFixture, BlockDagStorageFixture, BlockGenerator}
-import coop.rchain.casper.protocol.{DeployData, ProcessedDeploy}
+import coop.rchain.casper.protocol.{BlockMessage, LightBlockInfo}
 import coop.rchain.casper.rholang.RuntimeManager
-import coop.rchain.casper.util.ConstructDeploy.{basicDeployData, sourceDeployNowF}
-import coop.rchain.casper.util.GenesisBuilder._
+import coop.rchain.casper.util.GenesisBuilder.randomValidatorKeyPairs
 import coop.rchain.casper.{PrettyPrinter, ValidatorIdentity}
-import coop.rchain.crypto.signatures.Signed
-import coop.rchain.crypto.{PrivateKey, PublicKey}
+import coop.rchain.crypto.hash.Blake2b256
 import coop.rchain.metrics.Span
 import coop.rchain.models.BlockHash.BlockHash
 import coop.rchain.models.Expr.ExprInstance.GString
+import coop.rchain.models.Validator.Validator
 import coop.rchain.models._
 import coop.rchain.models.blockImplicits.getRandomBlock
 import coop.rchain.models.syntax._
+import coop.rchain.rspace.hashing.Blake2b256Hash
 import coop.rchain.shared.Log
 import monix.eval.Task
+import monix.testing.scalatest.MonixTaskTest
 import org.mockito.cats.IdiomaticMockitoCats
 import org.mockito.{ArgumentMatchersSugar, IdiomaticMockito}
 import org.scalatest.EitherValues
-import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.flatspec.AsyncFlatSpec
 import org.scalatest.matchers.should.Matchers
 
+import scala.collection.immutable.SortedMap
+
 class ExploratoryDeployAPITest
-    extends AnyFlatSpec
+    extends AsyncFlatSpec
+    with MonixTaskTest
     with Matchers
     with EitherValues
     with BlockGenerator
@@ -38,10 +42,19 @@ class ExploratoryDeployAPITest
     with IdiomaticMockito
     with IdiomaticMockitoCats
     with ArgumentMatchersSugar {
-  private val bonds                     = List(10L, 10L, 10L, 10L)
-  private val validators                = randomValidatorKeyPairs.take(bonds.size).toList
-  private val initialComputeBondsResult = validators.map(_._2.bytes.toByteString).zip(bonds).toMap
-  private val gB                        = getRandomBlock(setBonds = initialComputeBondsResult.some)
+
+  private val genesis = getRandomBlock()
+  private val b1      = getRandomBlock()
+  private val b2      = getRandomBlock()
+  private val b3      = getRandomBlock()
+
+  private def validator(index: Int) = keys(index)._2.bytes.toByteString
+
+  private val keys     = randomValidatorKeyPairs.take(4).toList
+  private val vGenesis = validator(0)
+  private val v1       = validator(1)
+  private val v2       = validator(2)
+  private val v3       = validator(3)
 
   /*
    * DAG Looks like this:
@@ -54,76 +67,117 @@ class ExploratoryDeployAPITest
    *         genesis
    */
   it should "exploratoryDeploy get data from the read only node" in {
-    implicit val (c, log, bds, bs, rm, sp) = createMocks[Task]
+    implicit val log = mock[Log[Task]]
+    implicit val sp  = mock[Span[Task]]
 
-    val storedData = "data"
+    implicit val bs = mock[BlockStore[Task]]
+    bs.get(Seq(b2.blockHash)) returnsF Seq(b2.some)
+
+    val blocks     = List(genesis, b1, b2, b3)
+    val validators = List(vGenesis, v1, v2, v3)
+    val bondsMap   = validators.map(_ -> 10L).toMap
+
+    def toMessage(block: BlockMessage, sender: ByteString, senderSeq: Long) =
+      Message[BlockHash, Validator](
+        block.blockHash,
+        blocks.indexOf(block).toLong,
+        sender,
+        senderSeq,
+        bondsMap,
+        blocks.get(blocks.indexOf(block) - 1L).map(b => Set(b.blockHash)).getOrElse(Set.empty),
+        Set.empty,
+        blocks.take(blocks.indexOf(block) + 1).map(_.blockHash).toSet
+      )
+
+    implicit val bds = mock[BlockDagStorage[Task]]
+    bds.getRepresentation returnsF DagRepresentation(
+      blocks
+        .map(_.blockHash)
+        .toSet,
+      blocks.zipWithIndex.map {
+        case (b, i) =>
+          b.blockHash -> blocks
+            .get(i + 1L)
+            .map(nextBlock => Set(nextBlock.blockHash))
+            .getOrElse(Set.empty)
+      }.toMap,
+      blocks.zipWithIndex.foldLeft(SortedMap.empty[Long, Set[BlockHash]]) {
+        case (acc, (b, i)) => acc + (i.toLong -> Set(b.blockHash))
+      },
+      DagMessageState[BlockHash, Validator](
+        Set(toMessage(b2, vGenesis, 2), toMessage(b3, v1, 0)),
+        Map(
+          genesis.blockHash -> toMessage(genesis, vGenesis, 0),
+          b1.blockHash      -> toMessage(b1, vGenesis, 1),
+          b2.blockHash      -> toMessage(b2, vGenesis, 2),
+          b3.blockHash      -> toMessage(b3, v1, 0)
+        )
+      ),
+      Map(
+        Set.empty -> FringeData(
+          Blake2b256Hash.create(Blake2b256.hash("".getBytes)),
+          Set.empty,
+          Set.empty,
+          RuntimeManager.emptyStateHashFixed.toBlake2b256Hash,
+          Set.empty,
+          Set.empty,
+          Set.empty
+        )
+      )
+    )
+
+    val term        = "new return in { for (@data <- @\"store\") {return!(data)}}"
+    val storedData  = "data"
+    implicit val rm = mock[RuntimeManager[Task]]
+    rm.playExploratoryDeploy(term, *) returnsF List(Par(exprs = List(Expr(GString(storedData)))))
+
     for {
-      produceDeploys0 <- basicDeployData[Task](0, shardId = gB.shardId)
-      produceDeploys1 <- basicDeployData[Task](1, shardId = gB.shardId)
-      putDataDeploy   <- sourceDeployNowF[Task](s"""@"store"!("$storedData")""", shardId = gB.shardId)
+      result <- exploratoryDeploy[Task](term, b2.blockHash)
+    } yield {
+      result shouldBe 'right
 
-      _  = propagateBlock(validators.head, putDataDeploy)
-      b2 = propagateBlock(validators.head, produceDeploys0)
-      _  = propagateBlock(validators(1), produceDeploys1)
-
-      readOnlyValidator = ValidatorIdentity(validators.last._1).some
-      result <- exploratoryDeploy[Task](readOnlyValidator)(
-                 "new return in { for (@data <- @\"store\") {return!(data)}}",
-                 b2.blockHash
-               )
-      (par, b) = result.value
-      _        = b.blockHash shouldBe PrettyPrinter.buildStringNoLimit(b2.blockHash)
-      _ = par match {
+      val (par, b) = result.value
+      par match {
         case Seq(Par(_, _, _, Seq(expr), _, _, _, _, _, _)) =>
           expr match {
             case Expr(GString(data)) => data shouldBe storedData
-            case _                   => fail("Could not get data from exploretory api")
+            case _                   => fail("Could not get data from exploratory api")
           }
       }
-    } yield ()
+      b.blockHash shouldBe PrettyPrinter.buildStringNoLimit(b2.blockHash)
+
+      bs.get(Seq(b2.blockHash)) wasCalled once
+      bds.getRepresentation wasCalled once
+      rm.playExploratoryDeploy(term, *) wasCalled once
+    }
   }
 
   it should "exploratoryDeploy return error on bonded validator" in {
-    implicit val (c, log, bds, bs, rm, sp) = createMocks[Task]
+    implicit val blockDagStorage = mock[BlockDagStorage[Task]]
+    implicit val blockStore      = mock[BlockStore[Task]]
+    implicit val log             = mock[Log[Task]]
+    implicit val runtimeManager  = mock[RuntimeManager[Task]]
+    implicit val sp              = mock[Span[Task]]
 
     for {
-      produceDeploys <- basicDeployData[Task](0, shardId = gB.shardId)
-      _              = propagateBlock(validators.head, produceDeploys)
-
-      n1Validator = ValidatorIdentity(validators.head._1).some
-      result <- exploratoryDeploy[Task](n1Validator)(
+      result <- exploratoryDeploy[Task](
                  "new return in { return!(1) }",
-                 ByteString.EMPTY
+                 ByteString.EMPTY,
+                 ValidatorIdentity(keys.head._1).some
                )
-      _ = result.left.value shouldBe "Exploratory deploy can only be executed on read-only RNode."
-    } yield ()
+    } yield {
+      result shouldBe 'left
+      result.left.value shouldBe "Exploratory deploy can only be executed on read-only RNode."
+    }
   }
 
-  private def createMocks[F[_]: Concurrent: Sync] = {
-    val c   = Concurrent[F]
-    val sp  = Mocks.createSpan[F]
-    val log = Mocks.createLog[F]
-    val bds = Mocks.createBlockDagStorage[F]
-    val bs  = Mocks.createBlockStore[F]
-
-    val rm = mock[RuntimeManager[F]]
-    rm.computeBonds(*) returnsF initialComputeBondsResult
-
-    (c, log, bds, bs, rm, sp)
-  }
-
-  private def propagateBlock(validator: (PrivateKey, PublicKey), deploy: Signed[DeployData]) =
-    getRandomBlock(
-      setValidator = validator._1.bytes.toByteString.some,
-      setDeploys = Seq(ProcessedDeploy.empty(deploy)).some,
-      setBonds = initialComputeBondsResult.some
-    )
-
-  private def exploratoryDeploy[F[_]: Concurrent: RuntimeManager: BlockDagStorage: BlockStore: Log: Span](
-      validatorIdOpt: Option[ValidatorIdentity]
-  )(term: String, block: BlockHash) =
+  private def exploratoryDeploy[F[_]: Concurrent: BlockStore: BlockDagStorage: RuntimeManager: Log: Span](
+      term: String,
+      block: BlockHash,
+      validatorIdOpt: Option[ValidatorIdentity] = none
+  ): F[ApiErr[(Seq[Par], LightBlockInfo)]] =
     for {
-      blockApi <- createBlockApi[F](gB.shardId, 50, validatorIdOpt)
+      blockApi <- createBlockApi[F](genesis.shardId, 50, validatorIdOpt)
       res      <- blockApi.exploratoryDeploy(term, blockHash = block.toHexString.some)
     } yield res
 }
