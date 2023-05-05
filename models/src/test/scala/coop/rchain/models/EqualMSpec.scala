9c8d459ec13874ab427680737da479762cb4b9c7
nutzipper <1746367+nzpr@users.noreply.github.com>
2023-05-05 11:51:33 +0400

    Tests

--- models/src/test/scala/coop/rchain/models/EqualMSpec.scala
+++ models/src/test/scala/coop/rchain/models/EqualMSpec.scala
@@ -5,6 +5,8 @@ import coop.rchain.models.Expr.ExprInstance.GInt
 import coop.rchain.models.testImplicits._
 import coop.rchain.models.testUtils.TestUtils.forAllSimilarA
 import cats.Eval
+import cats.effect.IO
+import cats.effect.unsafe.implicits.global
 import org.scalacheck.{Arbitrary, Shrink}
 import org.scalatest.Assertion
 import org.scalatest.flatspec.AnyFlatSpec
@@ -13,7 +15,9 @@ import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 import coop.rchain.catscontrib.effect.implicits.sEval
 
 import scala.Function.tupled
+import scala.annotation.nowarn
 import scala.collection.immutable.BitSet
+import scala.concurrent.duration.DurationInt
 import scala.reflect.ClassTag
 
 class EqualMSpec extends AnyFlatSpec with ScalaCheckPropertyChecks with Matchers {
@@ -78,6 +82,10 @@ class EqualMSpec extends AnyFlatSpec with ScalaCheckPropertyChecks with Matchers
       forAllSimilarA[A]((x, y) => sameResultAsReference(x, y))
     }
 
+  // this @nowarn is to disable warning which are fatal according to the project configuration
+  // 'non-variable type argument Any in type pattern coop.rchain.models.AlwaysEqual[Any] is unchecked since it
+  // is eliminated by erasure [error] case (_: AlwaysEqual[Any], _: AlwaysEqual[Any]) => self.equals(other)'
+  @nowarn
   private def sameResultAsReference[A <: Any: EqualM: Pretty](x: A, y: A): Assertion = {
     // We are going to override the generated hashCode for our generated AST classes,
     // so in this test we rely on the underlying implementation from ScalaRuntime,
@@ -89,6 +97,8 @@ class EqualMSpec extends AnyFlatSpec with ScalaCheckPropertyChecks with Matchers
       case (left: ParMap, right: ParMap) =>
         val eqF = (x: ParMap) => (x.ps, x.remainder, x.connectiveUsed)
         Equiv.universal.equiv(eqF(left), eqF(right))
+      // this case is introduced after migration to scala 2.13 since AlwaysEqual falls under Product case for some reason
+      case (_: AlwaysEqual[Any], _: AlwaysEqual[Any]) => self.equals(other)
       case (left: Product, right: Product) =>
         left.getClass.isInstance(other) &&
           left.productIterator
@@ -99,7 +109,7 @@ class EqualMSpec extends AnyFlatSpec with ScalaCheckPropertyChecks with Matchers
 
     val referenceResult = reference(x, y)
     val equalsResult    = x == y
-    val equalMResult    = EqualM[A].equal[Eval](x, y).value
+    val equalMResult    = EqualM[A].equal[IO](x, y).timeout(20.seconds).unsafeRunSync()
 
     withClue(
       s"""
