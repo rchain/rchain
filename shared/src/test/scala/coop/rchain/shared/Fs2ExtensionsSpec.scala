b9cc7e34d4ad1cbafbdf8e6cafc4243fe15e1a04
Tomislav Grospic <grospic@gmail.com>
2023-05-31 12:51:46 +0200

    Updated tests with IO TestControl for fs2 extensions

--- shared/src/test/scala/coop/rchain/shared/Fs2ExtensionsSpec.scala
+++ shared/src/test/scala/coop/rchain/shared/Fs2ExtensionsSpec.scala
@@ -1,19 +1,19 @@
 package coop.rchain.shared
 
-import cats.effect.{Async, IO}
+import cats.Id
+import cats.effect.kernel.Outcome.Succeeded
+import cats.effect.testing.scalatest.AsyncIOSpec
+import cats.effect.testkit.TestControl
+import cats.effect.{Async, IO, Ref}
 import cats.syntax.all._
 import coop.rchain.shared.syntax.sharedSyntaxFs2Stream
 import fs2.Stream
-import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.flatspec.AsyncFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 import scala.concurrent.duration.{DurationInt, FiniteDuration}
-import scala.util.Success
-import cats.effect.unsafe.implicits.global
-import cats.effect.Ref
-import cats.effect.testkit.TestControl
 
-class Fs2ExtensionsSpec extends AnyFlatSpec with Matchers {
+class Fs2ExtensionsSpec extends AsyncFlatSpec with AsyncIOSpec with Matchers {
 
   /**
     * Creates a Stream of 2 elements creating String "11", if timeout occurs it will insert zeroes e.g. "101"
@@ -27,44 +27,54 @@ class Fs2ExtensionsSpec extends AnyFlatSpec with Matchers {
   }
 
   // Helper to construct success result
-  def success[A](a: A): Option[Success[A]] = Success(a).some
+  def success[A](a: A): Option[Succeeded[Id, Throwable, A]] = Succeeded[Id, Throwable, A](a).some
 
-  // TODO fix using TestControl
-//  "evalOnIdle" should "NOT trigger timeout if element IS produced within timeout period" in {
-//    val t = TestControl.execute(test[IO](1001.millis))
-//
-//    // Sanity check, value should be empty before start
-//    t.unsafeRunSync() shouldBe none
-//
-//    // Just before the next element produced, still no value
-//    t.flatMap(_.advance(999.millis)).unsafeRunSync() shouldBe none
-//
-//    //
-//    t.flatMap(_.advance(1000.millis)).unsafeToFuture() shouldBe success("11")
-//  }
-//
-//  it should "trigger timeout if element is NOT produced within timeout" in {
-//    val t = TestControl.execute(test[IO](750.millis))
-//
-//    // Sanity check, value should be empty before start
-//    t.unsafeRunSync() shouldBe none
-//
-//    // Just before the next element produced, still no value
-//    t.flatMap(_.advance(999.millis)).unsafeRunSync() shouldBe none
-//
-//    t.flatMap(_.advance(1000.millis)).unsafeRunSync() shouldBe success("101")
-//  }
-//
-//  it should "trigger two timeouts if element is NOT produced and timeout is double time shorter" in {
-//    val t = TestControl.execute(test[IO](499.millis))
-//
-//    // Sanity check, value should be empty before start
-//    t.unsafeRunSync() shouldBe none
-//
-//    // Just before the next element produced, still no value
-//    t.flatMap(_.advance(999.millis)).unsafeRunSync() shouldBe none
-//
-//    t.flatMap(_.advance(1000.millis)).unsafeRunSync() shouldBe success("1001")
-//  }
+  "evalOnIdle" should "NOT trigger timeout if element IS produced within timeout period" in {
+    for {
+      t <- TestControl.execute(test[IO](1001.millis))
+
+      // Sanity check, value should be empty before start
+      _ <- t.results.map(_ shouldBe none)
+
+      // Just before the next element produced, still no value
+      _ <- t.tickFor(999.millis)
+      _ <- t.results.map(_ shouldBe none)
+
+      _ <- t.tickFor(1.millis)
+      _ <- t.results.map(_ shouldBe success("11"))
+    } yield ()
+  }
+
+  it should "trigger timeout if element is NOT produced within timeout" in {
+    for {
+      t <- TestControl.execute(test[IO](750.millis))
+
+      // Sanity check, value should be empty before start
+      _ <- t.results.map(_ shouldBe none)
+
+      // Just before the next element produced, still no value
+      _ <- t.tickFor(999.millis)
+      _ <- t.results.map(_ shouldBe none)
+
+      _ <- t.tickFor(1.millis)
+      _ <- t.results.map(_ shouldBe success("101"))
+    } yield ()
+  }
+
+  it should "trigger two timeouts if element is NOT produced and timeout is double time shorter" in {
+    for {
+      t <- TestControl.execute(test[IO](499.millis))
+
+      // Sanity check, value should be empty before start
+      _ <- t.results.map(_ shouldBe none)
+
+      // Just before the next element produced, still no value
+      _ <- t.tickFor(999.millis)
+      _ <- t.results.map(_ shouldBe none)
+
+      _ <- t.tickFor(1.millis)
+      _ <- t.results.map(_ shouldBe success("1001"))
+    } yield ()
+  }
 
 }
