8ed7e49952e8f784996a1cd83a45746b064c66ff
Denis Garsh <hilltracer@yandex.ru>
2023-07-22 11:19:38 +0300

    Update types for disjunction

--- rholang/src/main/scala/coop/rchain/rholang/interpreter/compiler/normalizer/processes/PDisjunctionNormalizer.scala
+++ rholang/src/main/scala/coop/rchain/rholang/interpreter/compiler/normalizer/processes/PDisjunctionNormalizer.scala
@@ -1,10 +1,12 @@
 package coop.rchain.rholang.interpreter.compiler.normalizer.processes
 
-import cats.syntax.all._
 import cats.effect.Sync
-import coop.rchain.models.Connective.ConnectiveInstance.ConnOrBody
-import coop.rchain.models.{Connective, ConnectiveBody, Par}
+import cats.syntax.all._
+import coop.rchain.models.Par
 import coop.rchain.models.rholang.implicits._
+import coop.rchain.models.rholangN.Bindings._
+import coop.rchain.models.rholangN._
+import coop.rchain.rholang.ast.rholang_mercury.Absyn.PDisjunction
 import coop.rchain.rholang.interpreter.compiler.ProcNormalizeMatcher.normalizeMatch
 import coop.rchain.rholang.interpreter.compiler.{
   FreeMap,
@@ -12,9 +14,6 @@ import coop.rchain.rholang.interpreter.compiler.{
   ProcVisitOutputs,
   SourcePosition
 }
-import coop.rchain.rholang.ast.rholang_mercury.Absyn.PDisjunction
-
-import scala.collection.immutable.Vector
 
 object PDisjunctionNormalizer {
   def normalize[F[_]: Sync](p: PDisjunction, input: ProcVisitInputs)(
@@ -29,18 +28,15 @@ object PDisjunctionNormalizer {
                       p.proc_2,
                       ProcVisitInputs(VectorPar(), input.boundMapChain, FreeMap.empty)
                     )
-      lp = leftResult.par
-      resultConnective = lp.singleConnective() match {
-        case Some(Connective(ConnOrBody(ConnectiveBody(ps)))) =>
-          Connective(ConnOrBody(ConnectiveBody(ps :+ rightResult.par)))
-        case _ =>
-          Connective(ConnOrBody(ConnectiveBody(Vector(lp, rightResult.par))))
-      }
+      lp               = fromProto(leftResult.par)
+      rp               = fromProto(rightResult.par)
+      resultConnective = ConnOrN(Seq(lp, rp))
+
     } yield ProcVisitOutputs(
-      input.par.prepend(resultConnective, input.boundMapChain.depth),
+      toProto(fromProto(input.par).add(resultConnective)),
       input.freeMap
         .addConnective(
-          resultConnective.connectiveInstance,
+          toProtoConnective(resultConnective).connectiveInstance,
           SourcePosition(p.line_num, p.col_num)
         )
     )
