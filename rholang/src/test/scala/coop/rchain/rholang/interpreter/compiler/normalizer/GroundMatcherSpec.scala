da6ce4323999bf6673e19715405b8b1b982dcd96
Denis Garsh <hilltracer@yandex.ru>
2023-07-31 18:56:25 +0300

    Fix tests

--- rholang/src/test/scala/coop/rchain/rholang/interpreter/compiler/normalizer/GroundMatcherSpec.scala
+++ rholang/src/test/scala/coop/rchain/rholang/interpreter/compiler/normalizer/GroundMatcherSpec.scala
@@ -1,35 +1,31 @@
 package coop.rchain.rholang.interpreter.compiler.normalizer
 
+import cats.Eval
+import coop.rchain.catscontrib.effect.implicits.sEval
+import coop.rchain.models.rholangN._
 import coop.rchain.rholang.ast.rholang_mercury.Absyn._
-
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-import coop.rchain.models.Expr.ExprInstance._
-import coop.rchain.models._
-import coop.rchain.models.rholang.implicits._
-import cats.Eval
-import coop.rchain.catscontrib.effect.implicits.sEval
-
 class GroundMatcherSpec extends AnyFlatSpec with Matchers {
   "GroundInt" should "Compile as GInt" in {
-    val gi                   = new GroundInt("7")
-    val expectedResult: Expr = GInt(7)
+    val gi             = new GroundInt("7")
+    val expectedResult = GIntN(7)
     GroundNormalizeMatcher.normalizeMatch[Eval](gi).value should be(expectedResult)
   }
   "Positive groundBigInt" should "Compile GBigInt" in {
-    val gbi                  = new GroundBigInt("9999999999999999999999999999999999999999")
-    val expectedResult: Expr = GBigInt(BigInt("9999999999999999999999999999999999999999"))
+    val gbi            = new GroundBigInt("9999999999999999999999999999999999999999")
+    val expectedResult = GBigIntN(BigInt("9999999999999999999999999999999999999999"))
     GroundNormalizeMatcher.normalizeMatch[Eval](gbi).value should be(expectedResult)
   }
   "GroundString" should "Compile as GString" in {
-    val gs                   = new GroundString("\"String\"")
-    val expectedResult: Expr = GString("String")
+    val gs             = new GroundString("\"String\"")
+    val expectedResult = GStringN("String")
     GroundNormalizeMatcher.normalizeMatch[Eval](gs).value should be(expectedResult)
   }
   "GroundUri" should "Compile as GUri" in {
-    val gu                   = new GroundUri("`rho:uri`")
-    val expectedResult: Expr = GUri("rho:uri")
+    val gu             = new GroundUri("`rho:uri`")
+    val expectedResult = GUriN("rho:uri")
     GroundNormalizeMatcher.normalizeMatch[Eval](gu).value should be(expectedResult)
   }
 }
